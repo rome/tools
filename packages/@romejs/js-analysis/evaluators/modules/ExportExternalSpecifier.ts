edb66250cd2be22124c9229c42286ae39fc281cd
Sebastian McKenzie <sebmck@fb.com>
2020-02-27 18:02:08 -0800

    Run prettier

--- packages/@romejs/js-analysis/evaluators/modules/ExportExternalSpecifier.ts
+++ packages/@romejs/js-analysis/evaluators/modules/ExportExternalSpecifier.ts
@@ -1,14 +1,17 @@
 /**
-* Copyright (c) Facebook, Inc. and its affiliates.
-*
-* This source code is licensed under the MIT license found in the
-* LICENSE file in the root directory of this source tree.
-*/
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-import {AnyNode, ExportExternalSpecifier, exportExternalSpecifier} from '@romejs/js-ast';
+import {
+  AnyNode,
+  ExportExternalSpecifier,
+  exportExternalSpecifier,
+} from '@romejs/js-ast';
 
 export default function ExportExternalSpecifier(node: AnyNode) {
   node = exportExternalSpecifier.assert(node);
   throw new Error('unimplemented');
 }
- 
--- packages/@romejs/js-generator/generators/modules/ExportExternalSpecifier.ts
+++ packages/@romejs/js-generator/generators/modules/ExportExternalSpecifier.ts
@@ -1,14 +1,21 @@
 /**
-* Copyright (c) Facebook, Inc. and its affiliates.
-*
-* This source code is licensed under the MIT license found in the
-* LICENSE file in the root directory of this source tree.
-*/
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 import Generator from '../../Generator';
-import {AnyNode, ExportExternalSpecifier, exportExternalSpecifier} from '@romejs/js-ast';
+import {
+  AnyNode,
+  ExportExternalSpecifier,
+  exportExternalSpecifier,
+} from '@romejs/js-ast';
 
-export default function ExportExternalSpecifier(generator: Generator, node: AnyNode) {
+export default function ExportExternalSpecifier(
+  generator: Generator,
+  node: AnyNode,
+) {
   node = exportExternalSpecifier.assert(node);
   throw new Error('unimplemented');
 }
