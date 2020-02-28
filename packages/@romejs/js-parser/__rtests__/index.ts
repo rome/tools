d8919c828094bdebb6af11ad4d9072a8ae1c813b
Christopher Biscardi <chris@christopherbiscardi.com>
2020-02-28 11:59:20 -0800

    try to appease prettier

--- packages/@romejs/js-parser/__rtests__/index.ts
+++ packages/@romejs/js-parser/__rtests__/index.ts
@@ -17,7 +17,10 @@ const promise = createFixtureTests((fixture, t) => {
 
   // Get the input JS
   const inputFile =
-    files.get('input.js') || files.get('input.mjs') || files.get('input.ts') || files.get('input.tsx');
+    files.get('input.js') ||
+    files.get('input.mjs') ||
+    files.get('input.ts') ||
+    files.get('input.tsx');
   if (inputFile === undefined) {
     throw new Error(
       `The fixture ${fixture.dir} did not have an input.(mjs|js|ts|tsx)`,
