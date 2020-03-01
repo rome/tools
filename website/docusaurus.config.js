bc3f64414ee3b289c11dcdd974d8d31be95a231e
Michael Lancaster <michaell.llancaster@gmail.com>
2020-03-01 03:04:21 -0300

    Introduce initial Rome website setup (#62)
    Introduces the very minimum to get a website up and running.
    Includes: `Getting Started`, `Code of Conduct` and `Contributing`
    
    - No custom design.
    - Uses the temporary logo #1 .
    - The feature list is randomly taken from README.
    
    Assumes the website is part of the repository but can (should?) be extracted
    to a separate repository.
    
    Generated using Facebook template documented in [Docusaurus](https://v2.docusaurus.io/docs/installation#scaffold-project-website)
    
    Addresses #14
    
    Co-authored-by: Sebastian <853712+sebmck@users.noreply.github.com>

---
+++ website/docusaurus.config.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+module.exports = {
+  title: 'Rome',
+  tagline: 'An experimental JavaScript toolchain.',
+  url: '',
+  baseUrl: '/',
+  favicon: 'img/rome-logo.ico',
+  organizationName: 'facebookexperimental',
+  projectName: 'rome',
+  themeConfig: {
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Rome ',
+        src: 'img/rome-logo.png',
+      },
+      links: [
+        {
+          to: 'docs/introduction/getting-started',
+          label: 'Docs',
+          position: 'left',
+        },
+        // Please keep GitHub link to the right for consistency.
+        {
+          href: 'https://github.com/facebookexperimental/rome',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'docs/introduction/getting-started',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Code of Conduct',
+              href:
+                'https://github.com/facebookexperimental/rome/blob/master/.github/CODE_OF_CONDUCT.md',
+            },
+            {
+              label: 'Contributing',
+              to: 'docs/community/Contributing',
+            },
+          ],
+        },
+        {
+          title: 'More Resources',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebookexperimental/rome',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Facebook Open Source Logo',
+        src: 'img/oss_logo.png',
+        href: 'https://opensource.facebook.com/',
+      },
+      // Please do not remove the credits, help to publicize Docusaurus :)
+      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
