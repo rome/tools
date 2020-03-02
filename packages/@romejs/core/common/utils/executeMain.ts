2affea41e74b3c12e6e1e62418ed8aed8f85e961
Martin Veith <ndresx@gmail.com>
2020-03-02 01:31:26 +0100

    Use createRequire with createRequireFromPath as fallback

--- packages/@romejs/core/common/utils/executeMain.ts
+++ packages/@romejs/core/common/utils/executeMain.ts
@@ -51,7 +51,9 @@ export default async function executeMain(
     setImmediate,
     setInterval,
     setTimeout,
-    require: internalModule.createRequireFromPath(filename),
+    require:
+      internalModule.createRequire(filename) ||
+      internalModule.createRequireFromPath(filename),
     console,
     __dirname: path.getParent().join(),
     __filename: filename,
