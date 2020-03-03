6b720f3502481a74cc5bb6537b481e77c0e36275
zinyando <brizdigital@gmail.com>
2020-03-03 10:41:34 +0200

    Disallow no unsafe finally

--- packages/@romejs/js-compiler/__rtests__/lint.ts
+++ packages/@romejs/js-compiler/__rtests__/lint.ts
@@ -251,3 +251,98 @@ test('disallow var', async t => {
     },
   ]);
 });
+
+test('disallow unsafe usage of break, continue, throw and return', async t => {
+  const returnTest = await testLint(
+    `
+    function greet1() {
+      try {
+        throw new Error("Try")
+      } catch(err) {
+          throw err;
+      } finally {
+          return 1;
+      }
+    }
+    
+    greet1();
+    `,
+    LINT_ENABLED_FORMAT_DISABLED_CONFIG,
+  );
+
+  t.truthy(
+    returnTest.diagnostics.find(
+      d => d.message === `Unsafe usage of ReturnStatement.`,
+    ),
+  );
+
+  const breakTest = await testLint(
+    `
+    
+    function greet2() {
+      try {
+        throw new Error("Try")
+      } catch(err) {
+          throw err;
+      } finally {
+          break;
+      }
+    }
+
+    greet2();
+    `,
+    LINT_ENABLED_FORMAT_DISABLED_CONFIG,
+  );
+
+  t.truthy(
+    breakTest.diagnostics.find(
+      d => d.message === `Unsafe usage of BreakStatement.`,
+    ),
+  );
+
+  const continueTest = await testLint(
+    `
+    function greet3() {
+      try {
+        throw new Error("Try")
+      } catch(err) {
+          throw err;
+      } finally {
+          continue;
+      }
+    }
+    
+    greet3();
+    `,
+    LINT_ENABLED_FORMAT_DISABLED_CONFIG,
+  );
+
+  t.truthy(
+    continueTest.diagnostics.find(
+      d => d.message === `Unsafe usage of ContinueStatement.`,
+    ),
+  );
+
+  const throwTest = await testLint(
+    `
+    function greet4() {
+      try {
+        throw new Error("Try")
+      } catch(err) {
+          throw err;
+      } finally {
+          throw new Error("Finally");
+      }
+    }
+
+    greet4();
+    `,
+    LINT_ENABLED_FORMAT_DISABLED_CONFIG,
+  );
+
+  t.truthy(
+    throwTest.diagnostics.find(
+      d => d.message === `Unsafe usage of ThrowStatement.`,
+    ),
+  );
+});
