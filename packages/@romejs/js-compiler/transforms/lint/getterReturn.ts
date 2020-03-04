0a5a016c52fb9f600b43b4ee2f0e4ad81adef87b
macovedj <djmacovei@gmail.com>
2020-03-04 13:39:46 -0500

    use completion record util

--- packages/@romejs/js-compiler/transforms/lint/getterReturn.ts
+++ packages/@romejs/js-compiler/transforms/lint/getterReturn.ts
@@ -6,69 +6,25 @@
  */
 
 import {Path} from '@romejs/js-compiler';
-import {AnyNode, ObjectMethod} from '@romejs/js-ast';
+import {AnyNode} from '@romejs/js-ast';
+import {getCompletionRecords} from '@romejs/js-ast-utils';
 
 export default {
   name: 'getterReturn',
   enter(path: Path): AnyNode {
     const {node} = path;
-    switch (node.type) {
-      case 'ObjectExpression':
-        if (
-          node.properties.length !== 0 &&
-          (node.properties.find(
-            node => node.type === 'ObjectMethod' && node.kind === 'get',
-          ) as ObjectMethod) &&
-          (node.properties.find(
-            node => node.type === 'ObjectMethod' && node.kind === 'get',
-          ) as ObjectMethod).body &&
-          (node.properties.find(
-            node => node.type === 'ObjectMethod' && node.kind === 'get',
-          ) as ObjectMethod).body.body &&
-          (node.properties.find(
-            node => node.type === 'ObjectMethod' && node.kind === 'get',
-          ) as ObjectMethod).body.body.find(
-            node => node.type === 'ReturnStatement',
-          ) === undefined
-        ) {
-          path.context.addNodeDiagnostic(node, {
+    if (
+      (node.type === 'ClassMethod' || node.type === 'ObjectMethod') &&
+      node.kind === 'get'
+    ) {
+      for (const record of getCompletionRecords(node.body)) {
+        if (record.type === 'INVALID') {
+          path.context.addNodeDiagnostic(record.node, {
             category: 'lint/getterReturn',
-            message: 'Class get method does not have a return statement',
+            message: `Expected a 'return' at end of a getter method but got ${record.description}`,
           });
         }
-        break;
-      case 'ObjectProperty':
-        if (
-          node.value &&
-          node.value.type === 'FunctionExpression' &&
-          node.value.body &&
-          node.value.body.body &&
-          node.value.body.body.find(node => node.type === 'ReturnStatement') ===
-            undefined
-        ) {
-          path.context.addNodeDiagnostic(node, {
-            category: 'lint/getterReturn',
-            message: 'Class get method does not have a return statement',
-          });
-        }
-        break;
-      case 'ClassDeclaration':
-        if (
-          node.meta.body[0] &&
-          node.meta.body[0].type === 'ClassMethod' &&
-          node.meta.body[0].kind === 'get' &&
-          node.meta.body[0].body.body.filter(
-            node => node.type === 'ReturnStatement',
-          ).length === 0
-        ) {
-          path.context.addNodeDiagnostic(node, {
-            category: 'lint/getterReturn',
-            message: 'Class get method does not have a return statement',
-          });
-        }
-        break;
-      default:
-        break;
+      }
     }
     return node;
   },
