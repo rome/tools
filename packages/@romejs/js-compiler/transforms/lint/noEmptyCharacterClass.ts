29f5ff17aa421a35298e3167c5ffb6ddc83b7d76
Kevin Kelbie <notkelbie@outlook.com>
2020-03-04 15:39:54 +0000

    fix indentation width

--- packages/@romejs/js-compiler/transforms/lint/noEmptyCharacterClass.ts
+++ packages/@romejs/js-compiler/transforms/lint/noEmptyCharacterClass.ts
@@ -22,18 +22,17 @@ import {Path} from '@romejs/js-compiler';
 const regex = /^([^\\[]|\\.|\[([^\\\]]|\\.)+\])*$/u;
 
 export default {
-  name: 'noEmptyCharacterClass',
-  enter(path: Path): AnyNode {
-    const {context, node} = path;
+	name: 'noEmptyCharacterClass',
+	enter(path: Path): AnyNode {
+		const {context, node} = path;
 
 		if (node.type === "RegExpLiteral" && !regex.test(node.pattern)) {
 			context.addNodeDiagnostic(node, {
-        category: 'lint/noEmptyCharacterClass',
-        message:
-          'Empty character classes in regular expressions are not allowed',
-      });
+				category: 'lint/noEmptyCharacterClass',
+				message: 'Empty character classes in regular expressions are not allowed',
+			});
 		}
-    
-    return node;
-  },
+		
+		return node;
+	},
 };
