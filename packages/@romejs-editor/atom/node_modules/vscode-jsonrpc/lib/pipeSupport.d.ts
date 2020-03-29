e8cf7cedd7f4b49721bf412eb932dcff3fc14699
Sebastian McKenzie <sebmck@fb.com>
2020-03-29 09:22:19 -0700

    Remove Atom extension

--- packages/@romejs-editor/atom/node_modules/vscode-jsonrpc/lib/pipeSupport.d.ts
+++
@@ -1,8 +0,0 @@
-import { MessageReader } from './messageReader';
-import { MessageWriter } from './messageWriter';
-export declare function generateRandomPipeName(): string;
-export interface PipeTransport {
-    onConnected(): Thenable<[MessageReader, MessageWriter]>;
-}
-export declare function createClientPipeTransport(pipeName: string, encoding?: string): Thenable<PipeTransport>;
-export declare function createServerPipeTransport(pipeName: string, encoding?: string): [MessageReader, MessageWriter];
--- packages/@romejs-editor/atom/node_modules/vscode-jsonrpc/lib/thenable.d.ts
+++
@@ -1,2 +0,0 @@
-interface Thenable<T> extends PromiseLike<T> {
-}
