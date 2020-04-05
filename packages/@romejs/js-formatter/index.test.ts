265de0581386d27ed6d0d914b598d5a7a9874de8
Olivier Dusabimana <dusabima@adobe.com>
2020-04-05 18:40:06 -0400

    run formatter

--- packages/@romejs/js-formatter/index.test.ts
+++ packages/@romejs/js-formatter/index.test.ts
@@ -1,88 +1,90 @@
-import { createFixtureTests } from '@romejs/test';
-import { formatJS, BuilderOptions } from '.';
-import { parseJS } from '@romejs/js-parser';
-import { ConstProgramSyntax } from '@romejs/js-ast';
-import { writeFileSync } from '@romejs/fs';
+import {createFixtureTests} from '@romejs/test';
+import {formatJS, BuilderOptions} from '.';
+import {parseJS} from '@romejs/js-parser';
+import {ConstProgramSyntax} from '@romejs/js-ast';
+import {writeFileSync} from '@romejs/fs';
 
-const promise = createFixtureTests((fixture, t) => {
-  const {options, files} = fixture; 
-  const inputFile = files.get('input.js') || files.get('input.mjs') ||
-    files.get('input.ts') || files.get('input.tsx');
-  if (inputFile === undefined) {
-    throw new Error(
-      `The fixture ${fixture.dir} did not have an input.(mjs|js|ts|tsx)`,
+const promise = createFixtureTests(
+  (fixture, t) => {
+    const {options, files} = fixture;
+    const inputFile = files.get('input.js') || files.get('input.mjs') ||
+      files.get('input.ts') || files.get('input.tsx');
+    if (inputFile === undefined) {
+      throw new Error(
+        `The fixture ${fixture.dir} did not have an input.(mjs|js|ts|tsx)`,
+      );
+    }
+    const filename = inputFile.relative;
+    const extension = filename.join().slice(filename.join().lastIndexOf('.'));
+    const outputFileName = `output${extension}`;
+
+    const outputFile = files.get(outputFileName);
+
+    let outputContent = undefined;
+    if (outputFile !== undefined) {
+      outputContent = outputFile.content.toString().replace(/\r/g, '');
+    }
+
+    const sourceTypeProp = options.get('sourceType');
+    const sourceType = sourceTypeProp.asString('script');
+    if (sourceType !== 'module' && sourceType !== 'script') {
+      throw sourceTypeProp.unexpected('Expected either script or module');
+    }
+    const syntax: Array<ConstProgramSyntax> = options.get('syntax').asArray(true).map(
+      (
+        item,
+      ) => {
+        return item.asStringSet(['jsx', 'ts', 'flow']);
+      },
     );
-  }
-  const filename = inputFile.relative;
-  const extension = filename.join().slice(filename.join().lastIndexOf('.'));
-  const outputFileName = `output${extension}`;
+    const allowReturnOutsideFunction = options.get('allowReturnOutsideFunction').asBoolean(
+      false,
+    );
 
-  const outputFile = files.get(outputFileName);
+    const format = options.get('format').asStringOrVoid();
 
-  let outputContent = undefined;
-  if (outputFile !== undefined) {
-    outputContent = outputFile.content.toString().replace(/\r/g, '');
-  }
+    const updateSnapshots = t.options.updateSnapshots;
 
-  const sourceTypeProp = options.get('sourceType');
-  const sourceType = sourceTypeProp.asString('script');
-  if (sourceType !== 'module' && sourceType !== 'script') {
-    throw sourceTypeProp.unexpected('Expected either script or module');
-  }
-  const syntax: Array<ConstProgramSyntax> = options.get('syntax').asArray(true).map(
-    (
-      item,
-    ) => {
-      return item.asStringSet(['jsx', 'ts', 'flow']);
-    },
-  );
-  const allowReturnOutsideFunction = options.get('allowReturnOutsideFunction').asBoolean(
-    false,
-  );
-  
-  const format = options.get('format').asStringOrVoid();
-  
-  const updateSnapshots = t.options.updateSnapshots;
+    const inputContent = inputFile.content.toString().replace(/\r/g, '');
 
-  const inputContent = inputFile.content.toString().replace(/\r/g, '');
+    const ast = parseJS({
+      input: inputContent,
+      sourceType,
+      path: filename,
+      syntax,
+      allowReturnOutsideFunction,
+    });
 
-  const ast = parseJS({
-    input: inputContent,
-    sourceType,
-    path: filename,
-    syntax,
-    allowReturnOutsideFunction,
-  });
-  
-  const formatOptions: BuilderOptions = {
-    typeAnnotations: true,
-    sourceText: inputContent,
-    sourceFileName: filename.join(),
-    format: format === 'pretty' || format === 'compact' ? format : undefined,
-    sourceMaps: false,
-  }
-  
-  const printer = formatJS(ast, formatOptions);
-  
-  t.addToAdvice({
-    type: 'log',
-    category: 'info',
-    message: 'Fomat options',
-  });
+    const formatOptions: BuilderOptions = {
+      typeAnnotations: true,
+      sourceText: inputContent,
+      sourceFileName: filename.join(),
+      format: format === 'pretty' || format === 'compact' ? format : undefined,
+      sourceMaps: false,
+    };
 
-  t.addToAdvice({
-    type: 'inspect',
-    data: {
-      ...formatOptions
-    },
-  });
+    const printer = formatJS(ast, formatOptions);
 
-  if (outputContent !== undefined && !updateSnapshots) {
-    t.is(printer.getCode(), outputContent);
-  } else {
-    writeFileSync(fixture.dir.append(outputFileName), printer.getCode());
-  }
-});
+    t.addToAdvice({
+      type: 'log',
+      category: 'info',
+      message: 'Fomat options',
+    });
+
+    t.addToAdvice({
+      type: 'inspect',
+      data: {
+        ...formatOptions,
+      },
+    });
+
+    if (outputContent !== undefined && !updateSnapshots) {
+      t.is(printer.getCode(), outputContent);
+    } else {
+      writeFileSync(fixture.dir.append(outputFileName), printer.getCode());
+    }
+  },
+);
 
 // @ts-ignore allow top level await
 await promise;
