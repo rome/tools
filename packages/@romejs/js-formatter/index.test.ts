aa59794961b446cd40410a278a583c29837c6515
Olivier Dusabimana <dusabima@adobe.com>
2020-04-23 22:37:58 -0400

    run linter

--- packages/@romejs/js-formatter/index.test.ts
+++ packages/@romejs/js-formatter/index.test.ts
@@ -1,11 +1,11 @@
 import {createFixtureTests} from '@romejs/test-helpers';
 import {parseJS} from '@romejs/js-parser';
 import {ConstProgramSyntax} from '@romejs/js-ast';
-import { removeCarriageReturn } from '@romejs/string-utils';
-import { BuilderOptions, formatJS } from '.';
+import {removeCarriageReturn} from '@romejs/string-utils';
+import {BuilderOptions, formatJS} from '.';
 
 const promise = createFixtureTests(
-  async(fixture, t) => {
+  async (fixture, t) => {
     const {options, files} = fixture;
     // Get the input JS
     const inputFile = files.get('input.js') || files.get('input.mjs') ||
@@ -15,7 +15,7 @@ const promise = createFixtureTests(
         `The fixture ${fixture.dir} did not have an input.(mjs|js|ts|tsx)`,
       );
     }
-    
+
     const sourceTypeProp = options.get('sourceType');
     const sourceType = sourceTypeProp.asString('script');
     if (sourceType !== 'module' && sourceType !== 'script') {
@@ -54,7 +54,7 @@ const promise = createFixtureTests(
       format: format === 'pretty' || format === 'compact' ? format : undefined,
       sourceMaps: false,
     };
-    
+
     t.addToAdvice({
       type: 'log',
       category: 'info',
@@ -73,8 +73,18 @@ const promise = createFixtureTests(
     const outputFile = inputFile.absolute.getParent().append(
       `${inputFile.absolute.getExtensionlessBasename()}.test.md`,
     ).join();
-    await t.snapshotNamed('Javascript Input', inputContent, undefined, outputFile);
-    await t.snapshotNamed('Javascript Output', printer.getCode(), undefined, outputFile);
+    await t.snapshotNamed(
+      'Javascript Input',
+      inputContent,
+      undefined,
+      outputFile,
+    );
+    await t.snapshotNamed(
+      'Javascript Output',
+      printer.getCode(),
+      undefined,
+      outputFile,
+    );
   },
 );
 
