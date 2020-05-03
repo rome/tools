3b60cd8513bc64d7aff60aa56b569413caef69f2
Kevin Kelbie <notkelbie@outlook.com>
2020-05-03 19:00:12 +0100

    add stylePropObject tests

---
+++ packages/@romejs/js-compiler/lint/rules/react/stylePropObject.test.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {test} from 'rome';
+import {testLintMultiple} from '../testHelpers';
+
+test(
+  'ensure style property is an object',
+  async (t) => {
+    await testLintMultiple(
+      t,
+      [
+        // INVALID
+        '<div style={true} />',
+        `<div style="color: 'red'" />`,
+        `<div style={"color: 'red'"} />`,
+        // VALID
+        "<div style={{color: 'red'}} />",
+      ],
+      {category: 'lint/stylePropObject'},
+    );
+  },
+);
