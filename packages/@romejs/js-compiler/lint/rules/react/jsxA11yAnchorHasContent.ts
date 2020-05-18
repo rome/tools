ee612d54d60e408b7d90fb11cecdead3df272619
Victor Hom <victor@bluecore.com>
2020-05-18 10:38:12 -0400

    apply lint save to jsxA11yAnchorHasContent

--- packages/@romejs/js-compiler/lint/rules/react/jsxA11yAnchorHasContent.ts
+++ packages/@romejs/js-compiler/lint/rules/react/jsxA11yAnchorHasContent.ts
@@ -1,19 +1,19 @@
 import {Path, TransformExitResult} from "@romejs/js-compiler";
 import {JSXElement} from "@romejs/js-ast";
 import {descriptions} from "@romejs/diagnostics";
-import {
-	hasJSXAttribute,
-	isJSXElement
-} from "@romejs/js-ast-utils";
+import {hasJSXAttribute, isJSXElement} from "@romejs/js-ast-utils";
 
 function hasAnchorContent(node: JSXElement): boolean {
-	const dangerouslySetInnerHTML = hasJSXAttribute(node, "dangerouslySetInnerHTML")
+	const dangerouslySetInnerHTML = hasJSXAttribute(
+		node,
+		"dangerouslySetInnerHTML",
+	);
 
 	return (
-		(
-			node.children.length > 0 && 
-			!node.children.some(child => child.type === "JSXElement" && hasJSXAttribute(child, "aria-hidden"))
-		)||
+		(node.children.length > 0 &&
+		!node.children.some((child) =>
+			child.type === "JSXElement" && hasJSXAttribute(child, "aria-hidden")
+		)) ||
 		dangerouslySetInnerHTML
 	);
 }
@@ -21,14 +21,14 @@ function hasAnchorContent(node: JSXElement): boolean {
 export default {
 	name: "jsxA11YAnchorHasContent",
 	enter(path: Path): TransformExitResult {
-	const {node} = path;
+		const {node} = path;
 		if (isJSXElement(node, "a") && !hasAnchorContent(node)) {
 			path.context.addNodeDiagnostic(
 				node,
 				descriptions.LINT.JSX_A11Y_ANCHOR_HAS_CONTENT,
 			);
-	  }
+		}
 
 		return node;
- 	},
+	},
 };
