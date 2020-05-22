70679608f9299ef3391ba9b7be86f1eb6a202187
EduardoLopes <EduardoLopesDS@gmail.com>
2020-05-22 03:46:42 -0300

    Setup javascript minification

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -2,11 +2,23 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const CleanCSS = require("clean-css");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require('markdown-it-anchor');
+const Terser = require("terser");
 
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+
+  eleventyConfig.addFilter("jsmin", function(code) {
+    let minified = Terser.minify(code);
+    if( minified.error ) {
+        console.log("Terser error: ", minified.error);
+        return code;
+    }
+
+    return minified.code;
   });
 
   eleventyConfig.addPlugin(syntaxHighlight);
---
+++ website/src/includes/main.js
@@ -0,0 +1,7 @@
+tocbot.init({
+  tocSelector: '.table-of-contents',
+  contentSelector: '.content',
+  headingSelector: 'h1, h2, h3',
+  hasInnerContainers: true,
+  collapseDepth: 3,
+});
