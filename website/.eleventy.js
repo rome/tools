82785d8f1ee28811502b0d184c3193fa780d10ba
EduardoLopes <EduardoLopesDS@gmail.com>
2020-05-22 03:09:38 -0300

    Setup css minification

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,4 +1,10 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   return {
     dir: {
