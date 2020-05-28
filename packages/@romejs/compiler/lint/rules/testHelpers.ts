c5d6b63f31ebf966c465cb0264ef44f3e240c12d
Yasser Elassal <yassere@gmail.com>
2020-05-28 13:12:48 -0500

    Allow asserting lint test outcome using testLint (#531)
    * Add getNextSnapshotId to TestAPI
    
    * Change testLint to allow expecting valid or invalid lints
    
    * Run assert-generated script
    
    * Enable modified snapshot format for updated lint tests
    
    * Remove getNextSnapshotId from TestAPI

--- packages/@romejs/compiler/lint/rules/testHelpers.ts
+++ packages/@romejs/compiler/lint/rules/testHelpers.ts
@@ -20,6 +20,11 @@ type TestLintOptions = {
 	syntax?: Array<ConstProgramSyntax>;
 };
 
+type TestLintInput = {
+	valid?: Array<string>;
+	invalid?: Array<string>;
+};
+
 export async function testLintMultiple(
 	t: TestHelper,
 	inputs: Array<string>,
@@ -32,8 +37,32 @@ export async function testLintMultiple(
 
 export async function testLint(
 	t: TestHelper,
+	input: string | TestLintInput,
+	opts: TestLintOptions,
+) {
+	if (typeof input === "string") {
+		return testLintExpect(t, input, opts);
+	}
+
+	const {invalid, valid} = input;
+
+	if (invalid !== undefined) {
+		for (input of invalid) {
+			await testLintExpect(t, input, opts, "INVALID");
+		}
+	}
+	if (valid !== undefined) {
+		for (input of valid) {
+			await testLintExpect(t, input, opts, "VALID");
+		}
+	}
+}
+
+async function testLintExpect(
+	t: TestHelper,
 	input: string,
 	{syntax = ["jsx", "ts"], category, sourceType = "module"}: TestLintOptions,
+	expect?: "INVALID" | "VALID",
 ) {
 	t.addToAdvice({
 		type: "log",
@@ -91,6 +120,13 @@ export async function testLint(
 		};
 	});
 
+	if (expect === "INVALID") {
+		t.true(diagnostics.length > 0, "Expected test to have diagnostics.");
+	}
+	if (expect === "VALID") {
+		t.is(diagnostics.length, 0, "Expected test not to have diagnostics.");
+	}
+
 	const snapshotName = t.snapshot(
 		printDiagnosticsToString({
 			diagnostics,
@@ -98,7 +134,14 @@ export async function testLint(
 		}),
 	);
 
-	t.namedSnapshot(`${snapshotName}: formatted`, res.src);
+	const fixable = diagnostics.every((d) => d.fixable);
+
+	let formattedEntryName = `${snapshotName}: formatted`;
+	if (expect !== undefined && diagnostics.length !== 0) {
+		formattedEntryName += fixable ? " and fixed" : " but unfixable";
+	}
+
+	t.namedSnapshot(formattedEntryName, res.src);
 
 	t.clearAdvice();
 }
