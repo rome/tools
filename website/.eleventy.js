9fdc66b2934091e02b54ad4ec2accc29d93866c7
Sebastian <git@sebastian.barretomckenzie.com>
2020-07-02 21:33:58 -0700

    Enable lint for website folder

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -2,58 +2,67 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const fs = require("fs");
-const pluginTOC = require('eleventy-plugin-nesting-toc');
+const pluginTOC = require("eleventy-plugin-nesting-toc");
 const path = require("path");
 
 const opts = {
-  dirInput: "src",
-  staticPath: "static",
-  docsPath: "_includes/docs",
-  dirOutput: "build",
-  toc: {
-    headingText: 'test',      // Optional text to show in heading above the wrapper element
-  }
+	dirInput: "src",
+	staticPath: "static",
+	docsPath: "_includes/docs",
+	dirOutput: "build",
+	toc: {
+		headingText: "test", // Optional text to show in heading above the wrapper element
+	},
 };
 
-module.exports = function (eleventyConfig) {
-  // Aything listed in .gitignore will be ignored by the watch process,
-  // workaround to let eleventry rebuild when the css stylesheet gets rebuild.
-  eleventyConfig.setUseGitIgnore(false);
+module.exports = function(eleventyConfig) {
+	// Aything listed in .gitignore will be ignored by the watch process,
+	// workaround to let eleventry rebuild when the css stylesheet gets rebuild.
+	eleventyConfig.setUseGitIgnore(false);
 
-  eleventyConfig.addPassthroughCopy(opts.staticPath);
+	eleventyConfig.addPassthroughCopy(opts.staticPath);
 
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(pluginTOC);
+	eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addPlugin(pluginTOC);
 
-  const md = markdownIt({
-    html: true,
-    linkify: true,
-    typographer: true,
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkSymbol: '#'
-  });
+	const md = markdownIt({
+		html: true,
+		linkify: true,
+		typographer: true,
+	}).use(
+		markdownItAnchor,
+		{
+			permalink: true,
+			permalinkSymbol: "#",
+		},
+	);
 
-  eleventyConfig.setLibrary("md", md);
+	eleventyConfig.setLibrary("md", md);
 
-  eleventyConfig.addShortcode("rootmd", function (file) {
-    const relativeFilePath = path.join(__dirname, '..', file);
-    const data = fs.readFileSync(relativeFilePath, function (err, contents) {
-      if (err) {
-        throw new Error(err);
-      }
-      return contents;
-    });
-    return md.render(data.toString());
-  });
+	eleventyConfig.addShortcode(
+		"rootmd",
+		function(file) {
+			const relativeFilePath = path.join(__dirname, "..", file);
+			const data = fs.readFileSync(
+				relativeFilePath,
+				function(err, contents) {
+					if (err) {
+						throw new Error(err);
+					}
+					return contents;
+				},
+			);
+			return md.render(data.toString());
+		},
+	);
 
-  return {
-    dir: {
-      input: opts.dirInput,
-      output: opts.dirOutput,
-    },
-    passthroughFileCopy: true,
-    templateFormats: ["njk", "md", "css", "html", "yml"],
-    htmlTemplateEngine: "njk",
-  };
+	return {
+		dir: {
+			input: opts.dirInput,
+			output: opts.dirOutput,
+		},
+		passthroughFileCopy: true,
+		templateFormats: ["njk", "md", "css", "html", "yml"],
+		htmlTemplateEngine: "njk",
+	};
 };
--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,192 +1,202 @@
 const elements = {
-  mobileHandle: document.getElementsByClassName('mobile-handle')[0],
-  toc: document.getElementsByClassName('toc-container')[0],
-  tocLinks: [...document.querySelectorAll('.toc-container a')],
-  sidebar: document.getElementsByClassName('sidebar')[0],
-  sidebarRight: document.querySelectorAll('.sidebar.right')[0],
-  overlay: document.getElementsByClassName('overlay')[0],
-  headings: [...document.querySelectorAll('.content h1, .content h2')],
-  headerMobile: document.getElementsByClassName('header-mobile')[0],
-  colorSchemeSwitch: document.getElementById('color-scheme-switch'),
-  colorSchemeSwitchText: document.getElementById('color-scheme-switch-text'),
+	mobileHandle: document.getElementsByClassName("mobile-handle")[0],
+	toc: document.getElementsByClassName("toc-container")[0],
+	tocLinks: [...document.querySelectorAll(".toc-container a")],
+	sidebar: document.getElementsByClassName("sidebar")[0],
+	sidebarRight: document.querySelectorAll(".sidebar.right")[0],
+	overlay: document.getElementsByClassName("overlay")[0],
+	headings: [...document.querySelectorAll(".content h1, .content h2")],
+	headerMobile: document.getElementsByClassName("header-mobile")[0],
+	colorSchemeSwitch: document.getElementById("color-scheme-switch"),
+	colorSchemeSwitchText: document.getElementById("color-scheme-switch-text"),
 };
 
-function isMobile(){
-  return  elements.sidebar.classList.contains('visible') || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+function isMobile() {
+	return (
+		elements.sidebar.classList.contains("visible") ||
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent,
+		)
+	);
 }
 
 const toc = {
-  currentH1Link: null,
-  getMobileNavbarHeight(){
+	currentH1Link: null,
+	getMobileNavbarHeight() {
+		if (isMobile()) {
+			return parseFloat(
+				window.getComputedStyle(elements.headerMobile).height,
+				10,
+			);
+		}
 
-    if(isMobile()){
-      return parseFloat(window.getComputedStyle(elements.headerMobile).height, 10);
-    }
+		return 0;
+	},
+	highlight() {
+		const scrollY = window.scrollY;
 
-    return 0;
-  },
-  highlight(){
+		for (let i = 0; i < elements.headings.length; i++) {
+			const element = elements.headings[i];
 
-    var scrollY = window.scrollY;
+			const id = `#${element.getAttribute("id")}`;
+			const y = element.offsetTop;
+			const marginTop = parseFloat(
+				window.getComputedStyle(element).marginTop,
+				10,
+			);
+			const height = parseFloat(window.getComputedStyle(element).marginTop, 10);
+			const link = document.querySelectorAll(`.toc-container a[href='${id}']`)[0];
 
-    for (let i = 0; i < elements.headings.length; i++) {
-      const element = elements.headings[i];
+			const nextElement = elements.headings[i + 1];
 
-      const id = `#${element.getAttribute('id')}`;
-      const y = element.offsetTop;
-      const marginTop = parseFloat(window.getComputedStyle(element).marginTop, 10);
-      const height = parseFloat(window.getComputedStyle(element).marginTop, 10);
-      const link = document.querySelectorAll(`.toc-container a[href='${id}']`)[0];
+			let start = y - marginTop;
+			let end = y + height + marginTop;
 
-      const nextElement = elements.headings[i + 1];
+			if (nextElement) {
+				const nextMarginTop = parseFloat(
+					window.getComputedStyle(nextElement).marginTop,
+					10,
+				);
+				end = nextElement.offsetTop - nextMarginTop;
+			}
 
-      let start = y - marginTop;
-      let end = y + height + marginTop;
+			start -= toc.getMobileNavbarHeight();
+			end -= toc.getMobileNavbarHeight();
 
-      if(nextElement){
-        const nextMarginTop = parseFloat(window.getComputedStyle(nextElement).marginTop, 10);
-        end = (nextElement.offsetTop - nextMarginTop);
-      }
+			if (scrollY > start && scrollY < end) {
+				link.classList.add("active");
+			} else {
+				link.classList.remove("active");
+			}
+		}
+	},
+	handleClick(event) {
+		const target = event.target;
+		event.preventDefault();
 
-      start -= toc.getMobileNavbarHeight();
-      end -= toc.getMobileNavbarHeight();
+		const hash = target.getAttribute("href");
 
-      if (scrollY > start && scrollY < end) {
+		if (target.hasAttribute("href")) {
+			window.location.hash = hash;
 
-        link.classList.add('active');
+			scrollToHeading(hash);
 
-      } else {
+			if (isMobile()) {
+				mobileToggleEvent(event);
+			}
+		}
+	},
+};
 
-        link.classList.remove('active');
+function scrollToHeading(hash) {
+	const heading = document.getElementById(hash.replace(/^(#)/, ""));
 
-      }
+	if (!heading) {
+		return null;
+	}
 
-    }
+	heading.setAttribute("tabindex", "-1");
+	heading.focus();
 
-  },
-  handleClick(event){
-    const target = event.target;
-    event.preventDefault();
-
-    const hash = target.getAttribute("href");
-
-    if(target.hasAttribute("href")){
-
-      window.location.hash = hash;
-
-      scrollToHeading(hash);
-
-      if(isMobile()){
-        mobileToggleEvent(event);
-      }
-
-    }
-  },
+	const marginTop = parseFloat(window.getComputedStyle(heading).marginTop, 10);
+	window.scrollTo(
+		0,
+		heading.offsetTop - toc.getMobileNavbarHeight() - (marginTop - 2),
+	);
 }
 
-function scrollToHeading(hash){
-  const heading = document.getElementById(hash.replace(/^(#)/,''));
+function handleScroll() {
+	if (isMobile()) {
+		return false;
+	}
 
-  if(!heading) return null;
+	toc.highlight();
 
-  heading.setAttribute('tabindex','-1');
-  heading.focus();
-
-  const marginTop = parseFloat(window.getComputedStyle(heading).marginTop, 10);
-  window.scrollTo(0, (heading.offsetTop) - toc.getMobileNavbarHeight() - (marginTop - 2));
+	if (window.scrollY > 6) {
+		elements.sidebar.style["border-top-width"] = "5.3333333333px";
+	} else {
+		elements.sidebar.style["border-top-width"] = "0px";
+	}
 }
 
-function handleScroll(){
-
-  if(isMobile()){
-    return false;
-  }
-
-  toc.highlight();
-
-  if(window.scrollY > 6){
-    elements.sidebar.style['border-top-width'] = '5.3333333333px';
-  } else {
-    elements.sidebar.style['border-top-width'] = '0px';
-  }
-
+function mobileToggleEvent(event) {
+	const bodyClassList = document.body.classList;
+	event.preventDefault();
+	elements.sidebar.classList.toggle("visible");
+	elements.sidebarRight.classList.toggle("visible");
+	elements.overlay.classList.toggle("visible");
+	bodyClassList.toggle("no-scroll");
+	toc.highlight();
 }
 
-function mobileToggleEvent(event){
-  const bodyClassList = document.body.classList;
-  event.preventDefault();
-  elements.sidebar.classList.toggle('visible');
-  elements.sidebarRight.classList.toggle('visible');
-  elements.overlay.classList.toggle('visible');
-  bodyClassList.toggle('no-scroll');
-  toc.highlight();
+function modeSwitch() {
+	const $doc = document.documentElement;
+	let theme = $doc.getAttribute("data-theme");
+
+	if (theme === "light") {
+		elements.colorSchemeSwitchText.innerText = "Light Mode";
+		theme = "dark";
+	} else {
+		elements.colorSchemeSwitchText.innerText = "Dark Mode";
+		theme = "light";
+	}
+
+	$doc.setAttribute("data-theme", theme);
+	window.localStorage.setItem("data-theme", theme);
 }
 
-function modeSwitch(event){
+const themeInStorage = window.localStorage.getItem("data-theme");
 
-  const $doc = document.documentElement;
-  let theme = $doc.getAttribute('data-theme');
-
-  if(theme === 'light'){
-    elements.colorSchemeSwitchText.innerText = "Light Mode";
-    theme = 'dark';
-  } else {
-    elements.colorSchemeSwitchText.innerText = "Dark Mode";
-    theme = 'light';
-  }
-
-  $doc.setAttribute('data-theme', theme);
-  window.localStorage.setItem('data-theme', theme);
-
-}
-
-const themeInStorage = window.localStorage.getItem('data-theme');
-
-if(themeInStorage){
-
-  if (themeInStorage === "dark") {
-    elements.colorSchemeSwitchText.innerText = "Light Mode";
-  }
-
+if (themeInStorage) {
+	if (themeInStorage === "dark") {
+		elements.colorSchemeSwitchText.innerText = "Light Mode";
+	}
 }
 
 //remove permalinkSymbol "#" from table of contents
-elements.tocLinks.forEach(function(link){
-  link.innerText = link.innerText.replace(/(\s#)$/,'');
+elements.tocLinks.forEach(function(link) {
+	link.innerText = link.innerText.replace(/(\s#)$/, "");
 });
 
-window.onload = function(){
-  if(window.location.hash !== ''){
-    scrollToHeading(window.location.hash);
-  }
-  toc.highlight();
-}
+window.onload = function() {
+	if (window.location.hash !== "") {
+		scrollToHeading(window.location.hash);
+	}
+	toc.highlight();
+};
 
-document.addEventListener('click', function (event) {
+document.addEventListener(
+	"click",
+	function(event) {
+		if (!event.target.matches(".header-anchor")) {
+			return;
+		}
 
-	if (!event.target.matches('.header-anchor')) return;
+		event.preventDefault();
 
-  event.preventDefault();
+		const hash = event.target.getAttribute("href");
 
-  const hash = event.target.getAttribute('href');
+		window.location.hash = hash;
 
-  window.location.hash = hash;
+		scrollToHeading(hash);
+	},
+	false,
+);
 
-  scrollToHeading(hash);
-
-}, false);
-
-elements.toc.addEventListener('click', toc.handleClick, false);
-elements.mobileHandle.addEventListener('click', mobileToggleEvent, false);
-elements.overlay.addEventListener('click', mobileToggleEvent, false);
+elements.toc.addEventListener("click", toc.handleClick, false);
+elements.mobileHandle.addEventListener("click", mobileToggleEvent, false);
+elements.overlay.addEventListener("click", mobileToggleEvent, false);
 elements.overlay.addEventListener("touchstart", mobileToggleEvent, false);
-window.addEventListener('scroll', handleScroll, false);
+window.addEventListener("scroll", handleScroll, false);
 
-elements.colorSchemeSwitch.addEventListener('click', modeSwitch, false);
+elements.colorSchemeSwitch.addEventListener("click", modeSwitch, false);
 
 const homepageExample = document.querySelector(".homepage-example");
 if (homepageExample !== undefined) {
-  homepageExample.addEventListener("click", () => {
-    homepageExample.classList.remove("collapsed");
-  });
+	homepageExample.addEventListener(
+		"click",
+		() => {
+			homepageExample.classList.remove("collapsed");
+		},
+	);
 }
