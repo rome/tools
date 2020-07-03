026024a5e7568bd45353039bed03816c58438bb9
Sebastian <853712+sebmck@users.noreply.github.com>
2020-07-02 21:45:49 -0700

    Enable lint for website folder (#690)

--- website/src/_data/config.js
+++ website/src/_data/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  description: 'An experimental JavaScript toolchain',
-  og: {
-    title: 'An experimental JavaScript toolchain | Rome',
-    description: 'An experimental JavaScript toolchain',
-    image: '/static/img/rome-logo-black.svg',
-  },
-  twitter: {
-    image: '/static/img/rome-logo-black.svg',
-    imageAlt: 'Image for An experimental JavaScript toolchain | Rome',
-    card: 'summary_large_image',
-  },
+	description: "An experimental JavaScript toolchain",
+	og: {
+		title: "An experimental JavaScript toolchain | Rome",
+		description: "An experimental JavaScript toolchain",
+		image: "/static/img/rome-logo-black.svg",
+	},
+	twitter: {
+		image: "/static/img/rome-logo-black.svg",
+		imageAlt: "Image for An experimental JavaScript toolchain | Rome",
+		card: "summary_large_image",
+	},
 };
