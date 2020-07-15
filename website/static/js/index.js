ea665407f89216552ed04b20095e63210fce0610
aravind reddy <aparapatakam@aconex.com>
2020-07-15 11:09:33 +0530

    apply null/undefined checks in website static js

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -63,11 +63,12 @@ const toc = {
 
 			start -= toc.getMobileNavbarHeight();
 			end -= toc.getMobileNavbarHeight();
-
-			if (scrollY > start && scrollY < end) {
-				link.classList.add("active");
-			} else {
-				link.classList.remove("active");
+			if (link) {
+				if (scrollY > start && scrollY < end) {
+					link.classList.add("active");
+				} else {
+					link.classList.remove("active");
+				}
 			}
 		}
 	},
@@ -204,9 +205,11 @@ elements.overlay.addEventListener("touchstart", mobileToggleEvent, false);
 window.addEventListener("scroll", handleScroll, false);
 
 elements.colorSchemeSwitch.addEventListener("click", modeSwitch, false);
-const teamArr = Array.from(elements.teamList.getElementsByTagName("li"));
-for (const li of randomShuffle(teamArr)) {
-	elements.teamList.appendChild(li);
+if (elements.teamList) {
+	const teamArr = Array.from(elements.teamList.getElementsByTagName("li"));
+	for (const li of randomShuffle(teamArr)) {
+		elements.teamList.appendChild(li);
+	}
 }
 
 const homepageExample = document.querySelector(".homepage-example");
