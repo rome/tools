2961e4b77bdc800e7e2df1a00cda2833f2023006
emanuele <my.burning@gmail.com>
2020-07-20 11:17:11 +0100

    Fix

--- packages/@romefrontend/formatter/builders/markdown/blocks/MarkdownHeadingBlock.ts
+++ packages/@romefrontend/formatter/builders/markdown/blocks/MarkdownHeadingBlock.ts
@@ -1,21 +1,9 @@
 import {MarkdownHeadingBlock} from "@romefrontend/ast";
-import {
-	Builder,
-	Token,
-	Tokens,
-	concat,
-	hardline,
-	space,
-} from "@romefrontend/formatter";
+import {Builder, Token, concat, hardline, space} from "@romefrontend/formatter";
 
 export default function MarkdownHeadingBlock(
 	builder: Builder,
 	node: MarkdownHeadingBlock,
 ): Token {
-  return concat([
-    "#".repeat(node.level),
-    space,
-    node.value,
-    hardline,
-  ]);
+	return concat(["#".repeat(node.level), space, node.value, hardline]);
 }
