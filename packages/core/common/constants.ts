dd072cf80309c4596fba830a251572efacd0b10a
Sebastian <853712+sebmck@users.noreply.github.com>
2020-07-24 22:44:55 -0700

    Move `packages/@romefrontend` to top level `packages` folder (#887)

---
+++ packages/core/common/constants.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import packageJson from "../../../package.json";
+import {HOME_PATH, TEMP_PATH, createAbsoluteFilePath} from "@romefrontend/path";
+import {getEnvVar} from "@romefrontend/cli-environment";
+import os = require("os");
+
+// Node flags to pass to all forked processes
+export const CHILD_ARGS = ["--max-old-space-size=8192"];
+
+// @ts-ignore: this will be wrong if we weren't the entry node script
+export const BIN = createAbsoluteFilePath(process.mainModule.filename);
+export const MAP = BIN.addExtension(".map");
+
+const MEGABYTE = 10_000;
+
+// Where we store user configuration, recoverable files etc
+export const USER_CONFIG_DIRECTORY = HOME_PATH.appendList(".rome");
+export const USER_CONFIG_FILENAMES: Array<string> = [
+	"config.json",
+	"config.rjson",
+];
+
+// Constants used to handle scaling
+export const MAX_MASTER_BYTES_BEFORE_WORKERS = 0.5 * MEGABYTE;
+export const MAX_WORKER_BYTES_BEFORE_ADD = 1 * MEGABYTE;
+const CPU_COUNT: number = os.cpus().length;
+export const MAX_WORKER_COUNT = Math.min(CPU_COUNT, 4);
+
+// Verson constants
+export let VERSION = String(packageJson.version);
+// Vendor Rome and Trunk Rome could have the same version number if there was no release in between
+// Ensure they are properly namespaced to avoid having daemon socket conflicts
+if (getEnvVar("ROME_DEV").type === "ENABLED") {
+	VERSION += "-dev";
+}
+export const SOCKET_PATH = TEMP_PATH.append(`rome-${VERSION}.sock`);
+export const CLI_SOCKET_PATH = TEMP_PATH.append(`rome-wait-${VERSION}.sock`);
+
+// Misc
+export const MOCKS_DIRECTORY_NAME = "__rmocks__";
---
+++ packages/test-helpers/index.ts
@@ -0,0 +1,208 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {Consumer, consumeUnknown} from "@romefrontend/consume";
+import {consumeJSON} from "@romefrontend/codec-json";
+import {TestHelper, test, testOptions} from "rome";
+
+import {
+	AbsoluteFilePath,
+	AbsoluteFilePathSet,
+	UnknownFilePath,
+	createAbsoluteFilePath,
+} from "@romefrontend/path";
+import {
+	exists,
+	lstat,
+	readDirectory,
+	readFile,
+	readFileText,
+} from "@romefrontend/fs";
+
+const dirname = testOptions.dirname === undefined ? "" : testOptions.dirname;
+
+async function isFile(path: AbsoluteFilePath): Promise<boolean> {
+	return (await lstat(path)).isFile();
+}
+
+async function getOptions(dir: AbsoluteFilePath): Promise<Consumer> {
+	const optionsLoc = dir.append("options.json");
+	const input = await readFileText(optionsLoc);
+	return consumeJSON({
+		input,
+		path: optionsLoc,
+	});
+}
+
+export type Fixture = {
+	name: Array<string>;
+	dir: AbsoluteFilePath;
+	options: Consumer;
+	files: Map<string, FixtureFile>;
+};
+
+export type FixtureFile = {
+	relative: UnknownFilePath;
+	absolute: AbsoluteFilePath;
+	content: Buffer;
+};
+
+async function _getFixtures(
+	opts: {
+		root: AbsoluteFilePath;
+		name: undefined | string;
+		dir: AbsoluteFilePath;
+		parts: Array<string>;
+		options: Consumer;
+	},
+): Promise<Array<Fixture>> {
+	const {name, dir, parts, options: inheritOptions} = opts;
+
+	// Check if directory even exists
+	if (!(await exists(dir))) {
+		throw new Error(`The directory ${dir} doesn't exist`);
+	}
+
+	// If the name starts with a dot then we're hidden
+	if (name !== undefined && name[0] === ".") {
+		return [];
+	}
+
+	// Get all the filenames in the directory
+	const filenames: AbsoluteFilePathSet = await readDirectory(dir);
+
+	// Get options for this directory
+	let ownOptions;
+	if (filenames.has(dir.append("options.json"))) {
+		ownOptions = await getOptions(dir);
+	}
+
+	// Merge options
+	const options: Consumer =
+		ownOptions === undefined
+			? inheritOptions
+			: consumeUnknown(
+					{
+						...inheritOptions.asUnknownObject(),
+						...ownOptions.asUnknownObject(),
+					},
+					"tests/fixtureOptions",
+				);
+
+	// An array of directories names that lead to this fixture
+	const ownParts = name === undefined ? parts : [...parts, name];
+
+	// Split up all files and directories
+	const directories: Set<AbsoluteFilePath> = new Set();
+	const files: Set<AbsoluteFilePath> = new Set();
+	for (const path of filenames) {
+		if (await isFile(path)) {
+			files.add(path);
+		} else {
+			directories.add(path);
+		}
+	}
+
+	// If there's any directories then get the fixtures from 'all of them
+	if (directories.size > 0) {
+		let fixtures: Array<Fixture> = [];
+
+		for (const path of directories) {
+			fixtures = fixtures.concat(
+				await _getFixtures({
+					root: opts.root,
+					name: path.getBasename(),
+					dir: path,
+					parts: ownParts,
+					options,
+				}),
+			);
+		}
+
+		return fixtures;
+	}
+
+	// Get the contents of all the files
+	const fileContents: Map<string, FixtureFile> = new Map();
+	for (const path of filenames) {
+		fileContents.set(
+			path.getBasename(),
+			{
+				relative: opts.root.relative(path),
+				absolute: path,
+				content: await readFile(path),
+			},
+		);
+	}
+
+	// Create the fixture
+	return [
+		{
+			name: ownParts,
+			dir,
+			options,
+			files: fileContents,
+		},
+	];
+}
+
+export async function getFixtures(dir: string): Promise<Array<Fixture>> {
+	const root = createAbsoluteFilePath(dir).append("test-fixtures");
+	return _getFixtures({
+		root,
+		name: undefined,
+		dir: root,
+		parts: [],
+		options: consumeUnknown({}, "tests/fixtureOptions"),
+	});
+}
+
+export async function createFixtureTests(
+	callback: (fixture: Fixture, t: TestHelper) => void | Promise<void>,
+	dir: string = dirname,
+): Promise<void> {
+	for (const fixture of await getFixtures(dir)) {
+		test(
+			fixture.name,
+			{},
+			async (t) => {
+				t.addToAdvice({
+					type: "log",
+					category: "info",
+					text: "Fixture options",
+				});
+
+				t.addToAdvice({
+					type: "inspect",
+					data: fixture.options.asJSONPropertyValue(),
+				});
+
+				t.addToAdvice({
+					type: "log",
+					category: "info",
+					text: "Fixture files",
+				});
+
+				t.addToAdvice({
+					type: "list",
+					list: Array.from(
+						fixture.files,
+						([basename, info]) =>
+							`<filelink target="${info.absolute}">${basename}</filelink>`
+						,
+					),
+				});
+
+				await callback(fixture, t);
+
+				fixture.options.enforceUsedProperties();
+			},
+		);
+	}
+}
+
+export * from "./integration";
