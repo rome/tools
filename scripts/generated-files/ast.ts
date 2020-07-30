b9726ddef35dede863f3f999dd3c22dd9beeefcf
emanuele <my.burning@gmail.com>
2020-07-30 14:51:08 +0100

    Moved `export` before hash generation

--- scripts/generated-files/ast.ts
+++ scripts/generated-files/ast.ts
@@ -1,7 +1,7 @@
 import {INTERNAL, modifyGeneratedFile} from "../_utils";
 import {lstat, readDirectory} from "@internal/fs";
 import {AbsoluteFilePath} from "@internal/path";
-import {dedent} from "@romefrontend/string-utils";
+import {dedent} from "@internal/string-utils";
 
 const astFolder = INTERNAL.append("ast");
 
@@ -93,11 +93,7 @@ export async function main() {
 					return `\n	| n.${def.nodeType}`;
 				},
 				wrapCallback(buff) {
-					return dedent`
-						export type AnyNodes = AnyNode | Array<AnyNode>;
-						
-						export type AnyNode = ${buff};						
-					`;
+					return `export type AnyNode = ${buff};`;
 				},
 			},
 		],
