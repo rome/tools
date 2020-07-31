2a4c00554902397a91db65694df3f3f57c539c25
arneesh <arneeshaima@gmail.com>
2020-08-01 02:18:56 +0530

    Removed liniting issues of credits.js

--- website/src/credits.js
+++ website/src/credits.js
@@ -1,42 +1,49 @@
-
 /**
  * Helper file for -> credits.md
  * Use Case: Fetches information about all the users who have forked this repository
  */
 
-const https = require('https');
+const https = require("https");
 
 const options = {
-    host: 'api.github.com',
-    path: '/repos/romefrontend/rome/forks',
-    headers: { 'User-Agent': 'romefrontend/rome'}
+	host: "api.github.com",
+	path: "/repos/romefrontend/rome/forks",
+	headers: {"User-Agent": "romefrontend/rome"},
 };
 
 let forkList;
 
-https.get(options , resp => {
+https.get(
+	options,
+	(resp) => {
+		let data = "";
+		resp.on(
+			"data",
+			(chunk) => {
+				data += chunk;
+			},
+		);
 
-    let data='';
-    resp.on('data', chunk => {
-        data += chunk;
-    });
+		resp.on(
+			"end",
+			() => {
+				// forst_list -> contains list of all the people who have forked this repository
+				forkList = JSON.parse(data);
 
-    resp.on('end', () => {
-
-        // forst_list -> contains list of all the people who have forked this repository
-        forkList = JSON.parse(data);
-
-        if(forkList && forkList.length > 0) {
-            forkList.forEach(function(fork)  {
-                // fork.owner -> contains user information of a single person
-                console.log(fork.owner.login)
-            })
-        } else {
-            forkList = [];
-        }
-    })
-
-}).on("error", err => {
-    console.log(`Error ${err.message}`)
-});
-
+				if (forkList && forkList.length > 0) {
+					forkList.forEach(function(fork) {
+						// fork.owner -> contains user information of a single person
+						console.log(fork.owner.login);
+					});
+				} else {
+					forkList = [];
+				}
+			},
+		);
+	},
+).on(
+	"error",
+	(err) => {
+		console.log(`Error ${err.message}`);
+	},
+);
