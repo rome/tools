c879003ff113aa8eab9c8a9e4f015bb10038b718
Sebastian <git@sebastian.barretomckenzie.com>
2020-08-07 01:17:47 -0700

    Format

--- internal/core/client/Client.test.ts
+++ internal/core/client/Client.test.ts
@@ -1,8 +1,14 @@
 import {test} from "rome";
 import {createIntegrationTest} from "@internal/test-helpers";
 
-test("Client#generateRageSummary", createIntegrationTest({},async (t, helper) => {
-	await t.notThrowsAsync(async () => {
-		await helper.client.generateRageSummary();
-	});
-}));
+test(
+	"Client#generateRageSummary",
+	createIntegrationTest(
+		{},
+		async (t, helper) => {
+			await t.notThrowsAsync(async () => {
+				await helper.client.generateRageSummary();
+			});
+		},
+	),
+);
