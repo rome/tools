cbd8b68ad66e8fa72718786b688a669d686ecd41
Kyle Cutler <kyle1320@users.noreply.github.com>
2020-08-10 16:33:22 -0700

    Fix filelink in rage output (#1004)

--- internal/core/client/Client.ts
+++ internal/core/client/Client.ts
@@ -502,7 +502,7 @@ export default class Client {
 
 			await writer.finalize();
 			this.reporter.success(
-				markup`Rage archive written to <emphasis><filelink target="${ragePath}" /></emphasis>`,
+				markup`Rage archive written to <emphasis>${ragePath}</emphasis>`,
 			);
 		});
 	}
