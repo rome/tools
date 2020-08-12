51e488db2b2d88ccc0d7aebf28154698610d37d5
Nikhil Thomas <nikhil@arceoanalytics.com>
2020-08-12 17:10:54 -0400

    isBinary tests - PR feedback
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- internal/js-ast-utils/isBinary.test.ts
+++ internal/js-ast-utils/isBinary.test.ts
@@ -1,20 +1,15 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import {test} from "rome";
 import {isBinary} from "./isBinary";
 import {parseJS} from "@internal/js-parser";
-import {JSExpressionStatement} from "@internal/ast";
+import {jsExpressionStatement} from "@internal/ast";
 
 function binaryExpressionHelper(input: string) {
-	const node = (parseJS({
-		path: "unknown",
-		input,
-	}).body[0] as JSExpressionStatement);
+	const node = jsExpressionStatement.assert(
+		parseJS({
+			path: "unknown",
+			input,
+		}).body[0],
+	);
 
 	return isBinary(node.expression);
 }
@@ -22,22 +17,18 @@ function binaryExpressionHelper(input: string) {
 test(
 	"isBinary",
 	(t) => {
-		t.inlineSnapshot(binaryExpressionHelper("2+2;"), true);
-		t.inlineSnapshot(binaryExpressionHelper("variableToReference * 10;"), true);
-		t.inlineSnapshot(binaryExpressionHelper("100 / constantDeclared;"), true);
-		t.inlineSnapshot(binaryExpressionHelper("100 << 2;"), true);
-		t.inlineSnapshot(binaryExpressionHelper("listlike instanceof array;"), true);
+		t.true(binaryExpressionHelper("2+2;"));
+		t.true(binaryExpressionHelper("variableToReference * 10;"));
+		t.true(binaryExpressionHelper("100 / constantDeclared;"));
+		t.true(binaryExpressionHelper("100 << 2;"));
+		t.true(binaryExpressionHelper("listlike instanceof array;"));
 
-		t.inlineSnapshot(binaryExpressionHelper("true && false;"), true);
-		t.inlineSnapshot(binaryExpressionHelper("identifierLookup || true;"), true);
-		t.inlineSnapshot(binaryExpressionHelper("identifierLookup || true;"), true);
-		t.inlineSnapshot(
-			binaryExpressionHelper("nullishValue ?? defaultArgument;"),
-			true,
-		);
+		t.true(binaryExpressionHelper("true && false;"));
+		t.true(binaryExpressionHelper("identifierLookup || true;"));
+		t.true(binaryExpressionHelper("nullishValue ?? defaultArgument;"));
 
-		t.inlineSnapshot(binaryExpressionHelper("[list, of, values];"), false);
-		t.inlineSnapshot(binaryExpressionHelper("-1;"), false);
-		t.inlineSnapshot(binaryExpressionHelper("!true;"), false);
+		t.false(binaryExpressionHelper("[list, of, values];"));
+		t.false(binaryExpressionHelper("-1;"));
+		t.false(binaryExpressionHelper("!true;"));
 	},
 );
