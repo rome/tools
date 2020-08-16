2e4c75cbe7ddd1fa75e654caafbd78c047a0b39f
jer3m01 <jer3m01@jer3m01.com>
2020-08-16 03:46:36 +0200

    Moved the variable declarations into the test

--- internal/ast-utils/removeLoc.test.ts
+++ internal/ast-utils/removeLoc.test.ts
@@ -5,30 +5,29 @@ import {dedent} from "@internal/string-utils";
 import {removeLoc} from "@internal/ast-utils/removeLoc";
 import {AnyNode} from "@internal/ast";
 
-// Needed to access non existing property body
-// rome-ignore lint/ts/noExplicitAny
-const jsNode: any = removeLoc(
-	parseJS({
-		path: "unknown",
-		input: "function foo() { return bar; }",
-	}),
-);
+test(
+	"nodes and their children should no longer have locations",
+	async (t) => {
+		// Needed to access non existing property body
+		// rome-ignore lint/ts/noExplicitAny
+		const jsNode: any = removeLoc(
+			parseJS({
+				path: "unknown",
+				input: "function foo() { return bar; }",
+			}),
+		);
 
-// rome-ignore lint/ts/noExplicitAny
-const cssNode: any = removeLoc(
-	parseCSS({
-		path: "unknown",
-		input: dedent`
+		// rome-ignore lint/ts/noExplicitAny
+		const cssNode: any = removeLoc(
+			parseCSS({
+				path: "unknown",
+				input: dedent`
 		div {
 			background: red;
 		}
 		`,
-	}),
-);
-
-test(
-	"nodes and their children should no longer have locations",
-	async (t) => {
+			}),
+		);
 		t.is(jsNode.loc, undefined);
 
 		jsNode.body.map((child: AnyNode) => {
