8a7a620f54839b9dc2a51b445846ece945f9cff8
jer3m01 <jer3m01@jer3m01.com>
2020-08-19 16:34:39 +0200

    Changed from `as` to `.assert` in test getJSXAttribute

--- internal/js-ast-utils/getJSXAttribute.test.ts
+++ internal/js-ast-utils/getJSXAttribute.test.ts
@@ -2,13 +2,13 @@ import {test} from "rome";
 import {parseJS} from "@internal/js-parser";
 import {dedent} from "@internal/string-utils";
 import {
-	JSXElement,
 	jsArrowFunctionExpression,
 	jsBooleanLiteral,
 	jsExpressionStatement,
 	jsObjectExpression,
 	jsStringLiteral,
 	jsxAttribute,
+	jsxElement,
 	jsxExpressionContainer,
 } from "@internal/ast";
 import {getJSXAttribute} from "@internal/js-ast-utils/getJSXAttribute";
@@ -16,32 +16,33 @@ import {getJSXAttribute} from "@internal/js-ast-utils/getJSXAttribute";
 test(
 	"verify attribute resolution",
 	async (t) => {
-		const jsxElement = (jsExpressionStatement.assert(
-			parseJS({
-				path: "unknown",
-				input: dedent`
-		<div className="foo" onClick={() => {alert("hello")}} custom={true} other={{one: "first", two: "second"}}/>
-		`,
-			}).body[0],
-		).expression as JSXElement);
-
+		const jsx = jsxElement.assert(
+			jsExpressionStatement.assert(
+				parseJS({
+					path: "unknown",
+					input: dedent`
+						<div className="foo" onClick={() => {alert("hello")}} custom={true} other={{one: "first", two: "second"}}/>
+					`,
+				}).body[0],
+			).expression,
+		);
 		t.is(
 			jsStringLiteral.assert(
-				jsxAttribute.assert(getJSXAttribute(jsxElement, "className")).value,
+				jsxAttribute.assert(getJSXAttribute(jsx, "className")).value,
 			).value,
 			"foo",
 		);
 		t.notThrows(() => {
 			jsArrowFunctionExpression.assert(
 				jsxExpressionContainer.assert(
-					jsxAttribute.assert(getJSXAttribute(jsxElement, "onClick")).value,
+					jsxAttribute.assert(getJSXAttribute(jsx, "onClick")).value,
 				).expression,
 			);
 		});
 		t.is(
 			jsBooleanLiteral.assert(
 				jsxExpressionContainer.assert(
-					jsxAttribute.assert(getJSXAttribute(jsxElement, "custom")).value,
+					jsxAttribute.assert(getJSXAttribute(jsx, "custom")).value,
 				).expression,
 			).value,
 			true,
@@ -49,7 +50,7 @@ test(
 		t.notThrows(() => {
 			jsObjectExpression.assert(
 				jsxExpressionContainer.assert(
-					jsxAttribute.assert(getJSXAttribute(jsxElement, "other")).value,
+					jsxAttribute.assert(getJSXAttribute(jsx, "other")).value,
 				).expression,
 			);
 		});
