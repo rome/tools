cbff0046386a133d2df81d2c42dafc7a4010342e
Sebastian <git@sebastian.barretomckenzie.com>
2020-08-30 16:04:30 -0700

    Add lint tests

---
+++ internal/compiler/lint/index.test.ts
@@ -0,0 +1,67 @@
+import {test} from "rome";
+import {lint, LintRequest, LintResult, lintRuleNames} from "@internal/compiler";
+import {template} from "@internal/js-ast-utils";
+import {createDefaultProjectConfig, ProjectConfig} from "@internal/project";
+import {JSRoot} from "@internal/ast";
+
+function createLintTransformOptions(ast: JSRoot, mutateConfig: (config: ProjectConfig) => ProjectConfig): LintRequest {
+	return {
+		applySafeFixes: false,
+		suppressionExplanation: "",
+		sourceText: "",
+		ast,
+		options: {},
+		project: {
+			config: mutateConfig(createDefaultProjectConfig()),
+			directory: undefined,
+		},
+	};
+}
+
+test("disabledLintRules single", async (t) => {
+	function hasUndeclaredDiag(res: LintResult): boolean {
+		for (const diag of res.diagnostics) {
+			if (diag.description.category === "lint/js/noUndeclaredVariables") {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Make sure when it's not disabled the diagnostic is present
+	const res = await lint(createLintTransformOptions(template.root`foo;`, (config) => config));
+	t.true(hasUndeclaredDiag(res));
+
+	// Make sure when it's not disabled the diagnostic it is not present
+	const res2 = await lint(createLintTransformOptions(template.root`foo;`, (config) => ({
+		...config,
+		lint: {
+			...config.lint,
+			disabledRules: ["js/noUndeclaredVariables"],
+		},
+	})));
+	t.false(hasUndeclaredDiag(res2));
+});
+
+test("disabledLintRules all rules", async (t) => {
+	const res = await lint(createLintTransformOptions(template.root`foo;`, (config) => ({
+		...config,
+		lint: {
+			...config.lint,
+			disabledRules: lintRuleNames,
+		}
+	})));
+	t.is(res.diagnostics.length, 0);
+});
+
+
+test("format disabled", async (t) => {
+	const res = await lint(createLintTransformOptions(template.root`wacky\n\tformatting( yes,\nok );`, (config) => ({
+		...config,
+		format: {
+			...config.format,
+			enabled: false,
+		}
+	})));
+	t.snapshot(res);
+});
