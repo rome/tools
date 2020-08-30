94f4c49c6ea9e6a78b0b85f4a2a7cf88f39108b6
Aravind Reddy <reddy.aravind178@gmail.com>
2020-08-31 02:09:34 +0530

    tests: added tests for nodeAssertion files (#1093)

---
+++ internal/js-ast-utils/assertMultipleNodes.test.ts
@@ -0,0 +1,30 @@
+import {test} from "rome";
+import {assertMultipleNodes} from "./assertMultipleNodes";
+import {AnyNode, AnyNodes} from "@internal/ast";
+import {template} from "@internal/js-ast-utils";
+
+const node: AnyNode = template.statement`let a;`;
+const nodeArray: AnyNodes = [node, node];
+
+test(
+	"returns back the same list of nodes",
+	(t) => {
+		let returnedArray = assertMultipleNodes(nodeArray);
+		t.is(returnedArray.length, nodeArray.length);
+		t.looksLike(returnedArray, nodeArray);
+	},
+);
+
+test(
+	"returns an empty list if asserted with undefined",
+	(t) => {
+		t.looksLike(assertMultipleNodes(((undefined as unknown) as AnyNode)), []);
+	},
+);
+
+test(
+	"returns a list with node when asserted with a single node",
+	(t) => {
+		t.looksLike(assertMultipleNodes(node), [node]);
+	},
+);
