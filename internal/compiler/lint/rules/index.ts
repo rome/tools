ed5e81de7fa38576417be959aa18cd14427b401b
antonk52 <halloy52@gmail.com>
2020-09-05 18:43:27 +0300

    regenerate all files

--- internal/compiler/lint/rules/index.ts
+++ internal/compiler/lint/rules/index.ts
@@ -1,4 +1,4 @@
-/* GENERATED:START(hash:036ee9bf2f9cb7e3d500cfd8a09b87829de06088,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/lint-rules` to update. */
+/* GENERATED:START(hash:e56437371df83618255c4186dd04ab56602325cd,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/lint-rules` to update. */
 import useClosingNonVoid from "./html/useClosingNonVoid";
 import noArguments from "./js/noArguments";
 import noAsyncPromiseExecutor from "./js/noAsyncPromiseExecutor";
@@ -36,6 +36,7 @@ import noUnsafeNegation from "./js/noUnsafeNegation";
 import noUnusedTemplateLiteral from "./js/noUnusedTemplateLiteral";
 import noUnusedVariables from "./js/noUnusedVariables";
 import noVar from "./js/noVar";
+import preferOptionalChaining from "./js/preferOptionalChaining";
 import useBlockStatements from "./js/useBlockStatements";
 import useCamelCase from "./js/useCamelCase";
 import useDefaultExportBasename from "./js/useDefaultExportBasename";
@@ -149,6 +150,7 @@ lintTransforms.set("js/noUnsafeNegation", noUnsafeNegation);
 lintTransforms.set("js/noUnusedTemplateLiteral", noUnusedTemplateLiteral);
 lintTransforms.set("js/noUnusedVariables", noUnusedVariables);
 lintTransforms.set("js/noVar", noVar);
+lintTransforms.set("js/preferOptionalChaining", preferOptionalChaining);
 lintTransforms.set("js/useBlockStatements", useBlockStatements);
 lintTransforms.set("js/useCamelCase", useCamelCase);
 lintTransforms.set("js/useDefaultExportBasename", useDefaultExportBasename);
@@ -288,6 +290,7 @@ export const lintRuleNames: Array<LintRuleName> = [
 	"js/noUnusedTemplateLiteral",
 	"js/noUnusedVariables",
 	"js/noVar",
+	"js/preferOptionalChaining",
 	"js/useBlockStatements",
 	"js/useCamelCase",
 	"js/useDefaultExportBasename",
@@ -401,6 +404,7 @@ export type LintRuleName =
 	| "js/noUnusedTemplateLiteral"
 	| "js/noUnusedVariables"
 	| "js/noVar"
+	| "js/preferOptionalChaining"
 	| "js/useBlockStatements"
 	| "js/useCamelCase"
 	| "js/useDefaultExportBasename"
