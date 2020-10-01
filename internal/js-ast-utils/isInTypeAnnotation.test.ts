bcce451bbb2f35dcf48207d90bfffa0ea4b47f7d
jer3m01 <jer3m01@jer3m01.com>
2020-10-01 23:36:52 +0200

    test: js-ast-utils/isInTypeAnnotation

---
+++ internal/js-ast-utils/isInTypeAnnotation.test.ts
@@ -0,0 +1,77 @@
+import {test} from "rome";
+import {isInTypeAnnotation} from "@internal/js-ast-utils/isInTypeAnnotation";
+import {
+	AnyNode,
+	MOCK_PARENT,
+	jsArrayExpression,
+	jsCommentLine,
+	jsRoot,
+	tsArrayType,
+	tsAsExpression,
+	tsNonNullExpression,
+	tsNullKeywordTypeAnnotation,
+	tsThisType,
+	tsTypeAssertion,
+} from "@internal/ast";
+import {CompilerContext, Path} from "@internal/compiler";
+
+function helper(node: AnyNode) {
+	let path = new Path(
+		MOCK_PARENT,
+		new CompilerContext({
+			ast: jsRoot.create({
+				body: [],
+				comments: [],
+				corrupt: false,
+				diagnostics: [],
+				directives: [],
+				filename: "",
+				hasHoistedVars: false,
+				interpreter: undefined,
+				mtime: undefined,
+				sourceType: "script",
+				syntax: [],
+			}),
+		}),
+		{},
+	);
+
+	path.parent = node;
+
+	return isInTypeAnnotation(path);
+}
+
+test(
+	"returns true if the node is in type annotation",
+	async (t) => {
+		t.false(helper(jsCommentLine.create({id: "", value: "hello"})));
+
+		t.false(
+			helper(
+				tsAsExpression.create({
+					expression: jsArrayExpression.quick([]),
+					typeAnnotation: tsThisType.create({}),
+				}),
+			),
+		);
+
+		t.false(
+			helper(
+				tsTypeAssertion.create({
+					expression: jsArrayExpression.quick([]),
+					typeAnnotation: tsThisType.create({}),
+				}),
+			),
+		);
+
+		t.false(
+			helper(
+				tsNonNullExpression.create({expression: jsArrayExpression.quick([])}),
+			),
+		);
+
+		t.true(helper(tsArrayType.create({elementType: tsThisType.create({})})));
+
+		t.true(helper(tsNullKeywordTypeAnnotation.create({})));
+	},
+);
