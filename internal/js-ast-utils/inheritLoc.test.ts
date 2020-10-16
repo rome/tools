11f2d69e0da086dc8078be2bd5e16734e989ecf6
jer3m01 <jer3m01@jer3m01.com>
2020-10-16 06:27:15 +0200

    test: js-ast-utils/inheritLoc (#1194)

---
+++ internal/js-ast-utils/inheritLoc.test.ts
@@ -0,0 +1,52 @@
+import {test} from "rome";
+import {SourceLocation} from "@internal/parser-core";
+import {ob1Coerce0, ob1Coerce1} from "@internal/ob1";
+import {inheritLoc} from "@internal/js-ast-utils/inheritLoc";
+import {jsCommentLine, jsIdentifier} from "@internal/ast";
+
+test(
+	"returns the node's source location with it's name",
+	async (t) => {
+		const commentLoc: SourceLocation = {
+			start: {
+				line: ob1Coerce1(1),
+				column: ob1Coerce0(0),
+			},
+			end: {
+				line: ob1Coerce1(1),
+				column: ob1Coerce0(13),
+			},
+		};
+
+		t.is(
+			inheritLoc(
+				jsCommentLine.create({id: "1", value: "hello world", loc: commentLoc}),
+			),
+			commentLoc,
+		);
+
+		t.looksLike(
+			inheritLoc(
+				jsCommentLine.create({id: "1", value: "hello world", loc: commentLoc}),
+				"foo",
+			),
+			{identifierName: "foo", ...commentLoc},
+		);
+
+		const identifierLoc: SourceLocation = {
+			start: {
+				line: ob1Coerce1(3),
+				column: ob1Coerce0(0),
+			},
+			end: {
+				line: ob1Coerce1(3),
+				column: ob1Coerce0(15),
+			},
+		};
+
+		t.looksLike(
+			inheritLoc(jsIdentifier.create({name: "bar", loc: identifierLoc})),
+			{identifierName: "bar", ...identifierLoc},
+		);
+	},
+);
