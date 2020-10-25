3a26d44310d9e1fbbf57391e1f7ba2e97cfed1bd
Yuku Kotani <yukukotani@gmail.com>
2020-10-26 05:34:14 +0900

    add token builder for cell and row

---
+++ internal/formatter/builders/markdown/tables/MarkdownTableCell.ts
@@ -0,0 +1,9 @@
+import {MarkdownTableCell} from "@internal/ast";
+import {Builder, Token} from "@internal/formatter";
+
+export default function MarkdownTableCell(
+	builder: Builder,
+	node: MarkdownTableCell,
+): Token {
+  throw Error("Must be rendered through MarkdownTable.")
+}
---
+++ internal/formatter/builders/markdown/tables/MarkdownTableRow.ts
@@ -0,0 +1,9 @@
+import {MarkdownTableRow} from "@internal/ast";
+import {Builder, Token} from "@internal/formatter";
+
+export default function MarkdownTableRow(
+	builder: Builder,
+	node: MarkdownTableRow,
+): Token {
+  throw Error("Must be rendered through MarkdownTable.")
+}
