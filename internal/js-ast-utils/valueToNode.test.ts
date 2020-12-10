b3657adfb3caa89742cb3752373f501cc2ed1f61
gie <gie68i@gmail.com>
2020-12-11 00:09:41 +0700

    add test for js-ast-utils/valueToNode.ts

---
+++ internal/js-ast-utils/valueToNode.test.ts
@@ -0,0 +1,84 @@
+import {test} from "rome";
+import {valueToNode} from "./valueToNode";
+import {
+	jsArrayExpression,
+	jsBooleanLiteral,
+	jsNullLiteral,
+	jsNumericLiteral,
+	jsObjectExpression,
+	jsObjectProperty,
+	jsReferenceIdentifier,
+	jsStaticPropertyKey,
+	jsStringLiteral,
+} from "@internal/ast";
+import {createPropertyKey} from "./createPropertyKey";
+
+test(
+	"verify valueToNode returns the correct supported primitive type",
+	(t) => {
+		t.looksLike(valueToNode("string"), jsStringLiteral.quick("string"));
+		t.looksLike(valueToNode(true), jsBooleanLiteral.quick(true));
+		t.looksLike(valueToNode(123), jsNumericLiteral.quick(123));
+		t.looksLike(
+			valueToNode(undefined),
+			jsReferenceIdentifier.quick("undefined"),
+		);
+		t.looksLike(valueToNode(null), jsNullLiteral.create({}));
+	},
+);
+
+test(
+	"verify valueToNode returns the correct structural type",
+	(t) => {
+		t.looksLike(valueToNode([1]), jsArrayExpression.quick([valueToNode(1)]));
+		t.looksLike(
+			valueToNode({
+				key: "value",
+			}),
+			jsObjectExpression.quick([
+				jsObjectProperty.create({
+					key: jsStaticPropertyKey.create({
+						value: createPropertyKey("key"),
+					}),
+					value: valueToNode("value"),
+				}),
+			]),
+		);
+	},
+);
+
+test(
+	"fail with error when do not know how to turn this value into a literal",
+	(t) => {
+		t.throws(
+			() => {
+				valueToNode(Symbol("Sym"));
+			},
+			"Do not know how to turn this value into a literal",
+		);
+		t.throws(
+			() => {
+				valueToNode(() => {});
+			},
+			"Do not know how to turn this value into a literal",
+		);
+		t.throws(
+			() => {
+				valueToNode(BigInt(10));
+			},
+			"Do not know how to turn this value into a literal",
+		);
+	},
+);
+
+test(
+	"fail with error when recursion detected",
+	(t) => {
+		t.throws(
+			() => {
+				valueToNode(1, [1, 2]);
+			},
+			"Recursion detected",
+		);
+	},
+);
