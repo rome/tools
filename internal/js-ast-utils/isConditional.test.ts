bfdcbc025932d0c75dac9be4a3a1aebfeb21ac4f
Cody Cahoon <codyjcahoon@gmail.com>
2020-12-13 19:41:30 -0500

    test: Updates test to use template.expression|statement when testing JS

--- internal/js-ast-utils/isConditional.test.ts
+++ internal/js-ast-utils/isConditional.test.ts
@@ -1,77 +1,35 @@
 import {test} from "rome";
-import {parseJS} from "@internal/js-parser";
-import {
-	jsExpressionStatement,
-	jsFunctionDeclaration,
-	jsIfStatement,
-	jsReturnStatement,
-	jsVariableDeclarationStatement,
-} from "@internal/ast";
+import {template} from "./template";
 import {isConditional} from "./isConditional";
-
-function createNode(input: string) {
-	return parseJS({
-		path: "unknown",
-		input,
-	}).body[0];
-}
 
 test(
 	"returns true for if-statements",
 	(t) => {
-		function isConditionalHelper(input: string) {
-			const node = jsIfStatement.assert(createNode(input));
-			return isConditional(node);
-		}
-
-		t.true(isConditionalHelper("if (x) { return 1; }"));
-		t.true(isConditionalHelper("if (x) { return 1; } else { return 2; }"));
-		t.true(
-			isConditionalHelper(
-				"if (x) { return 1; } else if (y) { return 2; } else { return 3; }",
-			),
-		);
+		t.true(isConditional(template.statement`if(x){}`));
 	},
 );
 
 test(
 	"returns true for conditional expressions",
 	(t) => {
-		const conditionalExpression = jsExpressionStatement.assert(
-			createNode("foo ? foo.bar : undefined"),
-		).expression;
-		t.true(isConditional(conditionalExpression));
+		t.true(isConditional(template.expression`x ? 1 : 2`));
 	},
 );
 
 test(
 	"returns true for logical expressions",
 	(t) => {
-		function isConditionalHelper(input: string) {
-			const node = jsExpressionStatement.assert(createNode(input));
-			return isConditional(node.expression);
-		}
-
-		t.true(isConditionalHelper("x && y"));
-		t.true(isConditionalHelper("x || y"));
-		t.true(isConditionalHelper("x || y && z"));
+		t.true(isConditional(template.expression`x && y`));
+		t.true(isConditional(template.expression`x || y`));
+		t.true(isConditional(template.expression`x || y && z`));
 	},
 );
 
 test(
 	"returns false for non-conditionals",
 	(t) => {
-		const functionDeclaration = jsFunctionDeclaration.assert(
-			createNode("function x() {}"),
-		);
-		t.false(isConditional(functionDeclaration));
-
-		const variableStatement = jsVariableDeclarationStatement.assert(
-			createNode("const y = 1;"),
-		);
-		t.false(isConditional(variableStatement));
-
-		const returnStatement = jsReturnStatement.assert(createNode("return z;"));
-		t.false(isConditional(returnStatement));
+		t.false(isConditional(template.statement`function x(){}`));
+		t.false(isConditional(template.statement`const y = 1`));
+		t.false(isConditional(template.statement`return z`));
 	},
 );
