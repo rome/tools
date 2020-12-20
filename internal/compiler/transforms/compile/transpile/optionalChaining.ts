c18c67e6c71f48b8c09a15eb2f56af2ea33c7588
Anton <halloy52@gmail.com>
2020-12-20 23:52:54 +0300

    Optional call expressions fix (#1231)

--- internal/compiler/transforms/compile/transpile/optionalChaining.ts
+++ internal/compiler/transforms/compile/transpile/optionalChaining.ts
@@ -7,7 +7,87 @@
 
 import {createVisitor, signals} from "@internal/compiler";
 import {template} from "@internal/js-ast-utils";
-import {jsCallExpression} from "@internal/ast";
+import {
+	AnyJSExpression,
+	JSComputedMemberProperty,
+	JSOptionalCallExpression,
+	JSStaticMemberProperty,
+	jsCallExpression,
+	jsMemberExpression,
+} from "@internal/ast";
+
+function unoptionifyMemberExpression(node: AnyJSExpression): AnyJSExpression {
+	let root!: AnyJSExpression;
+	const properties: (JSStaticMemberProperty | JSComputedMemberProperty)[] = [];
+	let current: AnyJSExpression = node;
+
+	while (true) {
+		if (current.type === "JSMemberExpression") {
+			properties.unshift({
+				...current.property,
+				optional: false,
+			});
+			current = current.object;
+		} else {
+			root = current;
+			break;
+		}
+	}
+
+	return properties.reduce(
+		(object, property) =>
+			jsMemberExpression.create({
+				object,
+				property,
+			})
+		,
+		(root as AnyJSExpression),
+	);
+}
+
+function findParts(
+	node: JSOptionalCallExpression,
+): {
+	needsChecking: AnyJSExpression;
+	callee: AnyJSExpression;
+} {
+	let needsChecking: AnyJSExpression | null = null;
+	const properties: (JSStaticMemberProperty | JSComputedMemberProperty)[] = [];
+
+	let current = node.callee;
+	while (true) {
+		if (current.type === "JSMemberExpression") {
+			if (current.property.optional === true) {
+				needsChecking = current.object;
+				properties.unshift({
+					...current.property,
+					optional: false,
+				});
+				break;
+			} else {
+				properties.unshift(current.property);
+				current = current.object;
+			}
+		}
+
+		throw new Error("Not implemented non js memeber expression case");
+	}
+
+	const callee = properties.reduce(
+		(accum, property) => {
+			return jsMemberExpression.create({
+				object: accum,
+				property,
+			});
+		},
+		unoptionifyMemberExpression(needsChecking),
+	);
+
+	return {
+		needsChecking,
+		callee,
+	};
+}
 
 export default createVisitor({
 	name: "optionalChaining",
@@ -29,12 +109,22 @@ export default createVisitor({
 
 		if (node.type === "JSOptionalCallExpression") {
 			// TODO assign `node.callee` to a variable and use it as a reference
-			return signals.replace(
-				template.expression`${node.callee} == null ? undefined : ${jsCallExpression.create({
-					callee: node.callee,
-					arguments: node.arguments,
-				})}`,
-			);
+			if (node.optional) {
+				return signals.replace(
+					template.expression`${node.callee} == null ? undefined : ${jsCallExpression.create({
+						callee: unoptionifyMemberExpression(node.callee),
+						arguments: node.arguments,
+					})}`,
+				);
+			} else {
+				const {needsChecking, callee} = findParts(node);
+				return signals.replace(
+					template.expression`${needsChecking} == null ? undefined : ${jsCallExpression.create({
+						callee,
+						arguments: node.arguments,
+					})}`,
+				);
+			}
 		}
 
 		return signals.retain;
