066b2cebaae63e610404bbdc36b9427982d456d4
YeonJuan <yeonjuan93@naver.com>
2020-12-24 00:56:49 +0900

    fix(compiler): handle optionalChaining properly (#1248)

--- internal/compiler/transforms/compile/transpile/optionalChaining.ts
+++ internal/compiler/transforms/compile/transpile/optionalChaining.ts
@@ -10,6 +10,7 @@ import {template} from "@internal/js-ast-utils";
 import {
 	AnyJSExpression,
 	JSComputedMemberProperty,
+	JSMemberExpression,
 	JSOptionalCallExpression,
 	JSStaticMemberProperty,
 	jsCallExpression,
@@ -46,46 +47,45 @@ function unoptionifyMemberExpression(node: AnyJSExpression): AnyJSExpression {
 }
 
 function findParts(
-	node: JSOptionalCallExpression,
+	node: JSOptionalCallExpression | JSMemberExpression,
 ): {
-	needsChecking: AnyJSExpression;
-	callee: AnyJSExpression;
+	needsChecking: AnyJSExpression | null;
+	unoptionifedMembers: AnyJSExpression | null;
 } {
 	let needsChecking: AnyJSExpression | null = null;
 	const properties: (JSStaticMemberProperty | JSComputedMemberProperty)[] = [];
 
-	let current = node.callee;
-	while (true) {
-		if (current.type === "JSMemberExpression") {
-			if (current.property.optional === true) {
-				needsChecking = current.object;
-				properties.unshift({
-					...current.property,
-					optional: false,
-				});
-				break;
-			} else {
-				properties.unshift(current.property);
-				current = current.object;
-			}
-		}
+	let current = node.type === "JSOptionalCallExpression" ? node.callee : node;
 
-		throw new Error("Not implemented non js memeber expression case");
+	while (current.type === "JSMemberExpression") {
+		if (current.property.optional === true) {
+			needsChecking = current.object;
+			properties.unshift({
+				...current.property,
+				optional: false,
+			});
+			break;
+		} else {
+			properties.unshift(current.property);
+			current = current.object;
+		}
 	}
 
-	const callee = properties.reduce(
-		(accum, property) => {
-			return jsMemberExpression.create({
-				object: accum,
-				property,
-			});
-		},
-		unoptionifyMemberExpression(needsChecking),
-	);
+	const unoptionifedMembers =
+		needsChecking &&
+		properties.reduce(
+			(accum, property) => {
+				return jsMemberExpression.create({
+					object: accum,
+					property,
+				});
+			},
+			unoptionifyMemberExpression(needsChecking),
+		);
 
 	return {
 		needsChecking,
-		callee,
+		unoptionifedMembers,
 	};
 }
 
@@ -94,39 +94,34 @@ export default createVisitor({
 	enter(path) {
 		const {node} = path;
 
-		if (node.type === "JSMemberExpression" && node.property.optional) {
-			// TODO assign `node.object` to a variable and use it as a reference
-			if (node.property.type === "JSComputedMemberProperty") {
+		if (node.type === "JSMemberExpression") {
+			const {needsChecking, unoptionifedMembers} = findParts(node);
+			if (needsChecking && unoptionifedMembers) {
 				return signals.replace(
-					template.expression`${node.object} == null ? undefined : ${node.object}[${node.property.value}]`,
-				);
-			} else {
-				return signals.replace(
-					template.expression`${node.object} == null ? undefined : ${node.object}.${node.property.value}`,
+					template.expression`${needsChecking} == void 0 ? void 0 : ${unoptionifedMembers}`,
 				);
 			}
 		}
-
 		if (node.type === "JSOptionalCallExpression") {
-			// TODO assign `node.callee` to a variable and use it as a reference
 			if (node.optional) {
 				return signals.replace(
-					template.expression`${node.callee} == null ? undefined : ${jsCallExpression.create({
+					template.expression`${node.callee} == void 0 ? void 0 : ${jsCallExpression.create({
 						callee: unoptionifyMemberExpression(node.callee),
 						arguments: node.arguments,
 					})}`,
 				);
 			} else {
-				const {needsChecking, callee} = findParts(node);
-				return signals.replace(
-					template.expression`${needsChecking} == null ? undefined : ${jsCallExpression.create({
-						callee,
-						arguments: node.arguments,
-					})}`,
-				);
+				const {needsChecking, unoptionifedMembers} = findParts(node);
+				if (needsChecking && unoptionifedMembers) {
+					return signals.replace(
+						template.expression`${needsChecking} == void 0 ? void 0 : ${jsCallExpression.create({
+							callee: unoptionifedMembers,
+							arguments: node.arguments,
+						})}`,
+					);
+				}
 			}
 		}
-
 		return signals.retain;
 	},
 });
