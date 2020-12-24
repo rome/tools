e5ed01507aa899ade84345e6b18e873b26580181
jer3m01 <jer3m01@jer3m01.com>
2020-12-24 10:58:27 +0100

    test: add test for js-ast-utils/removeShallowLoc.ts (#1249)
    * test: add test for js-ast-utils/removeShallowLoc.ts
    
    * test: test children location

---
+++ internal/js-ast-utils/removeShallowLoc.test.ts
@@ -0,0 +1,19 @@
+import {test} from "rome";
+import {removeShallowLoc} from "@internal/js-ast-utils/removeShallowLoc";
+import {parseJS} from "@internal/js-parser";
+
+test(
+	"should remove the location of the node",
+	async (t) => {
+		const node = parseJS({
+			path: "unknown",
+			input: "function foo() { return bar; }",
+		});
+
+		t.not(node.loc, undefined);
+		t.not(node.body[0].loc, undefined);
+
+		t.is(removeShallowLoc(node).loc, undefined);
+		t.not(removeShallowLoc(node).body[0].loc, undefined);
+	},
+);
