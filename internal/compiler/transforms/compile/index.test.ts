6a723f1ad8086e4c33b343ef3007872efe6050a7
Emanuele <my.burning@gmail.com>
2021-01-08 12:09:15 +0000

    test(compiler): test fixtures for transformations (#1289)

---
+++ internal/compiler/transforms/compile/index.test.ts
@@ -0,0 +1,67 @@
+import {
+	createFixtureTests,
+	createMockWorker,
+	findFixtureInput,
+} from "@internal/test-helpers";
+import {removeCarriageReturn} from "@internal/string-utils";
+import {printDiagnosticsToString} from "@internal/cli-diagnostics";
+
+const promise = createFixtureTests(async (fixture, t) => {
+	const {worker, performFileOperation} = createMockWorker();
+	const {input, handler} = findFixtureInput(fixture, undefined);
+
+	const filename = input.relative;
+	const content = removeCarriageReturn(input.content.toString());
+
+	const res = await performFileOperation(
+		{
+			uid: filename.join(),
+			sourceText: content,
+		},
+		async (ref) => {
+			return await worker.api.compile(ref, "compile", {}, {});
+		},
+	);
+	if (res === undefined) {
+		throw new Error("No format value returned");
+	}
+
+	const snapshotFile = input.absolute.getParent().append(
+		input.absolute.getExtensionlessBasename(),
+	).join();
+
+	t.namedSnapshot(
+		"Input",
+		content,
+		undefined,
+		{
+			filename: snapshotFile,
+			language: handler.language,
+		},
+	);
+
+	t.namedSnapshot(
+		"Output",
+		res.value.compiledCode,
+		undefined,
+		{
+			filename: snapshotFile,
+			language: handler.language,
+		},
+	);
+
+	t.namedSnapshot(
+		"Diagnostics",
+		await printDiagnosticsToString({
+			diagnostics: res.value.diagnostics,
+			suppressions: res.value.suppressions,
+		}),
+		undefined,
+		{
+			filename: snapshotFile,
+		},
+	);
+});
+
+// @ts-ignore allow top level await
+await promise;
