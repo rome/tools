c13ee2f59d55aeaf66c8ace7c0782832029a2eb8
yeonjuan <yeonjuan93@naver.com>
2021-01-14 01:55:12 +0900

    feat(css-formatter): support number and percentage

--- internal/formatter/builders/css/core/CSSPercentage.ts
+++ internal/formatter/builders/css/core/CSSPercentage.ts
@@ -1,9 +1,9 @@
 import {CSSPercentage} from "@internal/ast";
-import {Builder, Token} from "@internal/formatter";
+import {Builder, Token, concat} from "@internal/formatter";
 
 export default function CSSPercentage(
 	builder: Builder,
 	node: CSSPercentage,
 ): Token {
-	throw new Error("unimplemented");
+	return concat([String(node.value), "px"]);
 }
