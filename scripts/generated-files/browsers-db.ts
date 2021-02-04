3ef06143b30edbca1752719ed8921066a5688a85
jer3m01 <jer3m01@jer3m01.com>
2021-02-04 01:36:51 +0100

    refactor(browsers-db): use reporter instead of console.log refactor(browsers-db): convert to Map refactor(browsers-db): better typescript types refactor(browsers-db): use consumeUnknown feat: add browsers-db to generate-all-files

--- scripts/generated-files/browsers-db.ts
+++ scripts/generated-files/browsers-db.ts
@@ -1,6 +1,7 @@
-import {INTERNAL, modifyGeneratedFile} from "../_utils";
+import {INTERNAL, modifyGeneratedFile, reporter} from "../_utils";
 import https = require("https");
 import {version as currentVersion} from "@internal/browsers-db";
+import {Consumer, consumeUnknown} from "@internal/consume";
 
 const browsersDbFolder = INTERNAL.append("browsers-db");
 
@@ -252,7 +253,7 @@ const REGIONS = new Set([
 	"alt-ww",
 ]);
 
-function get<T>(url: string): Promise<T> {
+function get(url: string): Promise<unknown> {
 	return new Promise((resolve, reject) => {
 		const req = https.get(
 			url,
@@ -290,23 +291,16 @@ function get<T>(url: string): Promise<T> {
 }
 
 export async function main() {
-	const version = (await get<{
-		version: string;
-	}>(packageJsonUrl)).version;
-	console.log("Checking for update...");
+	const version = consumeUnknown(await get(packageJsonUrl), "parse").get(
+		"version",
+	).asString();
 	if (currentVersion !== version) {
-		console.log(`Update found! ${currentVersion} -> ${version}`);
+		reporter.success(`Update found! ${currentVersion} -> ${version}`);
 		await updateVersion(version);
-
-		console.log("Updating data...");
 		await updateData();
-		console.log("Updating data... done!");
-
-		console.log("Updating regions...");
 		await updateRegions();
-		console.log("Updating regions... done!");
 	} else {
-		console.log(`Already using latest version! ${version}`);
+		reporter.success(`Already using latest version! ${version}`);
 	}
 }
 
@@ -323,7 +317,7 @@ async function updateVersion(version: string) {
 }
 
 // See browsers-db/README.md#Format
-interface agent {
+interface Agent {
 	b: string;
 	a: string;
 	p: string;
@@ -331,148 +325,44 @@ interface agent {
 	vs: {
 		v: string;
 		g: number;
-		r: number;
+		r?: number;
 		p?: string;
 	}[];
 	cv: string;
 }
 
-interface feature {
-	s: {
-		[key: string]: {
-			[key: string]: boolean;
-		};
-	};
+interface Feature {
+	s: Map<string, Map<string, boolean>>;
 	c: string[];
 }
 
-interface dataFormat {
-	agents: {
-		[key: string]: agent;
-	};
-	categories: {
-		[key: string]: string[];
-	};
-	data: {
-		[key: string]: feature;
-	};
+interface DataFormat {
+	agents: Map<string, Agent>;
+	categories: Map<string, string[]>;
+	data: Map<string, Feature>;
 }
 
-interface regionFormat {
+interface RegionFormat {
 	name: string;
-	data: usage;
+	data: Usage;
 }
 
-interface usage {
-	[key: string]: {
-		[key: string]: number;
-	};
-}
+type Usage = Map<string, Map<string, number>>;
 
-interface regionsFormat {
-	[key: string]: regionFormat;
-}
+type RegionsFormat = Map<string, RegionFormat>;
 
 async function updateData() {
-	const rawData = await get<{
-		agents: {
-			[key: string]: {
-				browser: string;
-				abbr: string;
-				prefix: string;
-				type: string;
-				version_list: {
-					version: string;
-					global_usage: number;
-					release_date: number;
-					prefix?: string;
-				}[];
-				current_version: string;
-			};
-		};
-		cats: {
-			[key: string]: string[];
-		};
-		data: {
-			[key: string]: {
-				stats: {
-					[key: string]: {
-						[key: string]: string;
-					};
-				};
-				categories: string[];
-			};
-		};
-	}>(fulldataUrl);
+	const progress = reporter.progress({title: "Updating data"});
 
-	const data = {
-		agents: (() => {
-			let temp = (<dataFormat["agents"]>{});
+	const rawData = consumeUnknown(await get(fulldataUrl), "parse");
 
-			for (const agent in rawData.agents) {
-				if (agent === "ie" || agent === "ie_mob") {
-					continue;
-				}
-
-				temp[agent] = {
-					b: rawData.agents[agent].browser,
-					a: rawData.agents[agent].abbr,
-					p: rawData.agents[agent].prefix,
-					t: rawData.agents[agent].type,
-					vs: rawData.agents[agent].version_list.filter((v) =>
-						v.prefix! !== "ms"
-					).map((v) => ({
-						v: v.version,
-						g: v.global_usage,
-						r: v.release_date,
-						p: v.prefix!.length === 0 ? undefined : v.prefix,
-					})),
-					cv: rawData.agents[agent].current_version,
-				};
-			}
-			return temp;
-		})(),
-		categories: rawData.cats,
-		data: (() => {
-			let temp = (<dataFormat["data"]>{});
-
-			for (const feature in rawData.data) {
-				if (
-					!rawData.cats.CSS.some((c) =>
-						rawData.data[feature].categories.includes(c)
-					)
-				) {
-					continue;
-				} // Skip non CSS features for the time being
-
-				let tempFeature = (<feature>{
-					s: {},
-				});
-
-				tempFeature.c = rawData.data[feature].categories;
-
-				for (const agent in rawData.data[feature].stats) {
-					if (agent === "ie" || agent === "ie_mob") {
-						continue;
-					}
-
-					const tempFeatureAgent = (<feature["s"][""]>{});
-
-					for (const v in rawData.data[feature].stats[agent]) {
-						if (rawData.data[feature].stats[agent][v].includes("x")) {
-							tempFeatureAgent[v] = true;
-						}
-					}
-
-					if (Object.keys(tempFeatureAgent).length !== 0) {
-						tempFeature.s[agent] = tempFeatureAgent;
-					}
-				}
-
-				temp[feature] = tempFeature;
-			}
-			return temp;
-		})(),
+	const data: DataFormat = {
+		agents: generateDataAgents(rawData),
+		categories: new Map<string, string[]>(
+			Object.entries(rawData.get("cats").asAny()),
+		),
+		// Would take to many lines to convert it without 'any'
+		data: generateDataData(rawData),
 	};
 
 	await modifyGeneratedFile(
@@ -482,53 +372,125 @@ async function updateData() {
 		},
 		async () => {
 			return {
-				lines: [`export default ${JSON.stringify(data)}`],
-				hash: JSON.stringify(data),
+				lines: [`export default ${JSON.stringify(mapToObject(data))}`],
+				hash: JSON.stringify(mapToObject(data)),
 			};
 		},
 	);
+
+	progress.end();
+}
+
+function generateDataAgents(rawData: Consumer) {
+	let agents: DataFormat["agents"] = new Map<string, Agent>();
+
+	for (const agent in rawData.get("agents").asUnknownObject()) {
+		if (agent === "ie" || agent === "ie_mob") {
+			continue;
+		}
+
+		agents.set(
+			agent,
+			{
+				b: rawData.get("agents").get(agent).get("browser").asString(),
+				a: rawData.get("agents").get(agent).get("abbr").asString(),
+				p: rawData.get("agents").get(agent).get("prefix").asString(),
+				t: rawData.get("agents").get(agent).get("type").asString(),
+				vs: rawData.get("agents").get(agent).get("version_list").asImplicitArray().filter((
+					v,
+				) => v.get("prefix").asString() !== "ms").map((v) => ({
+					v: v.get("version").asString(),
+					g: v.get("global_usage").asNumber(),
+					r: v.get("release_date").asNumberOrVoid(),
+					p: v.get("prefix").asString().length === 0
+						? undefined
+						: v.get("prefix").asString(),
+				})),
+				cv: rawData.get("agents").get(agent).get("current_version").asString(),
+			},
+		);
+	}
+	return agents;
+}
+
+function generateDataData(rawData: Consumer) {
+	let dataData: DataFormat["data"] = new Map<string, Feature>();
+
+	for (const feature in rawData.get("data").asUnknownObject()) {
+		// Skip non CSS features for the time being
+		if (
+			!rawData.get("cats").get("CSS").asMappedArray((c) => c.asString()).some((
+				c,
+			) =>
+				rawData.get("data").get(feature).get("categories").asMappedArray((c) =>
+					c.asString()
+				).includes(c)
+			)
+		) {
+			continue;
+		}
+
+		const stats = new Map<string, Map<string, boolean>>();
+
+		for (const agent in rawData.get("data").get(feature).get("stats").asUnknownObject()) {
+			if (agent === "ie" || agent === "ie_mob") {
+				continue;
+			}
+
+			const featureAgents = new Map<string, boolean>();
+
+			for (const v in rawData.get("data").get(feature).get("stats").get(agent).asUnknownObject()) {
+				if (
+					rawData.get("data").get(feature).get("stats").get(agent).get(v).asString().includes(
+						"x",
+					)
+				) {
+					featureAgents.set(v, true);
+				}
+			}
+
+			if (featureAgents.size !== 0) {
+				stats.set(agent, featureAgents);
+			}
+		}
+
+		dataData.set(
+			feature,
+			{
+				s: stats,
+				c: rawData.get("data").get(feature).get("categories").asMappedArray((c) =>
+					c.asString()
+				),
+			},
+		);
+	}
+	return dataData;
 }
 
 async function updateRegions() {
-	let regionsUsage = (<regionsFormat>{});
+	const progress = reporter.progress({title: "Updating regions"});
+	progress.setTotal(REGIONS.size);
+
+	let regionsUsage: RegionsFormat = new Map<string, RegionFormat>();
 
 	for (const region of REGIONS) {
 		// Can't use .forEach because of await
-		console.log(`Updating region "${region}"...`);
+		progress.setText(`Updating region "${region}"`);
 
-		const rawRegionUsage = await get<{
-			name: string;
-			data: {
-				[key: string]: {
-					[key: string]: number;
-				};
-			};
-		}>(regionUsageUrl.replace("<REGION>", region));
+		const rawRegionUsage = consumeUnknown(
+			await get(regionUsageUrl.replace("<REGION>", region)),
+			"parse",
+		);
 
-		regionsUsage[region] = {
-			name: rawRegionUsage.name,
-			data: (() => {
-				let temp = (<usage>{});
+		regionsUsage.set(
+			region,
+			{
+				name: rawRegionUsage.get("name").asString(),
+				data: generateRegionsData(rawRegionUsage),
+			},
+		);
 
-				for (const agent in rawRegionUsage.data) {
-					if (agent === "ie" || agent === "ie_mob") {
-						continue;
-					}
-
-					temp[agent] = {};
-
-					for (const v in rawRegionUsage.data[agent]) {
-						if (
-							rawRegionUsage.data[agent][v] &&
-							rawRegionUsage.data[agent][v] > 0
-						) {
-							temp[agent][v] = rawRegionUsage.data[agent][v];
-						}
-					}
-				}
-				return temp;
-			})(),
-		};
+		progress.tick();
 	}
 
 	await modifyGeneratedFile(
@@ -538,9 +500,61 @@ async function updateRegions() {
 		},
 		async () => {
 			return {
-				lines: [`export default ${JSON.stringify(regionsUsage)}`],
-				hash: JSON.stringify(regionsUsage),
+				lines: [`export default ${JSON.stringify(mapToObject(regionsUsage))}`],
+				hash: JSON.stringify(mapToObject(regionsUsage)),
 			};
 		},
 	);
+
+	progress.end();
+}
+
+function generateRegionsData(rawRegionUsage: Consumer) {
+	const usage: Usage = new Map<string, Map<string, number>>();
+
+	for (const agent in rawRegionUsage.get("data").asUnknownObject()) {
+		if (agent === "ie" || agent === "ie_mob") {
+			continue;
+		}
+
+		const usageAgent = new Map<string, number>();
+
+		for (const v in rawRegionUsage.get("data").get(agent).asUnknownObject()) {
+			if (
+				rawRegionUsage.get("data").get(agent).get(v).asNumberOrVoid() != null &&
+				rawRegionUsage.get("data").get(agent).get(v).asNumber() > 0
+			) {
+				usageAgent.set(
+					v,
+					rawRegionUsage.get("data").get(agent).get(v).asNumber(),
+				);
+			}
+		}
+
+		usage.set(agent, usageAgent);
+	}
+	return usage;
+}
+
+// Needed to convert to JSON
+// rome-ignore lint/ts/noExplicitAny: the object could be anything and is just used to be converted to a JSON string
+function mapToObject(object: any): object {
+	if (object instanceof Map) {
+		let result: any = {};
+		object.forEach((value, key) => {
+			result[key] = mapToObject(value);
+		});
+		return result;
+	}
+	if (Array.isArray(object)) {
+		return object;
+	}
+	if (typeof object === "object") {
+		let result: any = {};
+		for (const key in object) {
+			result[key] = mapToObject(object[key]);
+		}
+		return result;
+	}
+	return object;
 }
