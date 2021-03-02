2131b4158d3e8fd83b536499ac435c98ba2be0a1
Sebastian McKenzie <sebastian@rome.tools>
2021-03-02 12:06:50 -0600

    Remove website2

--- website2/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Router from "./Router";
-
-const root = document.querySelector("#root")!;
-ReactDOM.render(<Router url={location.pathname} />, root);
--- website2/src/pages/Homepage.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export default function Homepage() {
-  return <span>home!</span>;  
-}
