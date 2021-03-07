35429e6e3f812438d5acd54da753a7f3aa9ff749
Sebastian McKenzie <sebastian@rome.tools>
2021-03-07 11:45:08 -0600

    feat(website2): Add new WIP website

---
+++ website2/src/App.tsx
@@ -0,0 +1,6 @@
+import Router from "./Router";
+import * as React from "react";
+
+export default function App() {
+  return <Router url={location.pathname} />;
+}
---
+++ website2/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+const root = document.querySelector("#root")!;
+ReactDOM.render(<App />, root);
