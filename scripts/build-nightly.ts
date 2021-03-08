8dea3657efdef2987f1d6dc79c7c3fc803ed0063
Sebastian McKenzie <sebastian@rome.tools>
2021-03-08 00:37:20 -0600

    fix: Add runs-on field to nightly workflow

--- scripts/build-nightly.ts
+++ scripts/build-nightly.ts
@@ -4,21 +4,21 @@ import {main as buildRelease} from "./build-release";
 import {VERSION} from "@internal/core";
 
 export async function main() {
-  // Build a version number with a tag unique to the current day
-  const [version] = VERSION.split("-");
-  const date = new Date();
-  const dateParts = [date.getFullYear(), date.getMonth(), date.getDate()];
-  const dateTag = dateParts.map(num => {
-    const str = String(num);
-    if (str.length === 1) {
-      return `0${str}`;
-    } else {
-      return str;
-    }
-  }).join(".");
-  const newVersion = `${version}-nightly.${dateTag}`;
-  await updateVersion([newVersion]);
+	// Build a version number with a tag unique to the current day
+	const [version] = VERSION.split("-");
+	const date = new Date();
+	const dateParts = [date.getFullYear(), date.getMonth(), date.getDate()];
+	const dateTag = dateParts.map((num) => {
+		const str = String(num);
+		if (str.length === 1) {
+			return `0${str}`;
+		} else {
+			return str;
+		}
+	}).join(".");
+	const newVersion = `${version}-nightly.${dateTag}`;
+	await updateVersion([newVersion]);
 
-  // Build a release to the dist folder in the root
-  await buildRelease([ROOT.append("dist").join()]);
+	// Build a release to the dist folder in the root
+	await buildRelease([ROOT.append("dist").join()]);
 }
