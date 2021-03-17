38656d84b3017a93498036c0b39943a162d43bb0
jer3m01 <jer3m01@jer3m01.com>
2021-03-18 00:31:09 +0100

    chore(css-prefix): run generate script

--- internal/compiler/transforms/compile/css-handler/prefix/index.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/index.ts
@@ -5,12 +5,12 @@ import {
 import {ProjectConfig} from "@internal/project";
 import {UnknownObject} from "@internal/typescript-helpers";
 import {CompilerOptions} from "@internal/compiler";
+import {getBrowser} from "@internal/browser-features";
 
 /* GENERATED:START(hash:cd506990d1f961e2a79cd38e90f7adcdabbb4055,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/lint-rules` to update. */
 import display from "./prefixes/display";
 import transform from "./prefixes/transform";
 import transition from "./prefixes/transition";
-import {getBrowser} from "@internal/browser-features";
 
 const prefixVisitors: PrefixVisitor<UnknownObject>[] = [
 	...display,
