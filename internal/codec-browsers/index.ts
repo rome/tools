b3d46439908059eca2e9d8abe9d00f1ffafffe4b
jer3m01 <jer3m01@jer3m01.com>
2021-03-18 09:57:51 +0100

    test(codec-browsers): use fixed date inside tests

--- internal/codec-browsers/index.ts
+++ internal/codec-browsers/index.ts
@@ -1,10 +1,11 @@
 import {Browser} from "@internal/browser-features/Browser";
-import {resolveTargets} from "@internal/codec-browsers/resolve";
+import {ResolveOptions, resolveTargets} from "@internal/codec-browsers/resolve";
 import {parseBrowserQuery} from "@internal/codec-browsers/parse";
 import {ParserOptions} from "@internal/parser-core";
 
 export function resolveBrowsers(
 	queries: string | string[] | ParserOptions,
+	resolveOptions?: ResolveOptions,
 ): Browser[] {
 	let opt: ParserOptions;
 
@@ -16,5 +17,5 @@ export function resolveBrowsers(
 		opt = queries;
 	}
 
-	return Array.from(resolveTargets(parseBrowserQuery(opt)));
+	return Array.from(resolveTargets(parseBrowserQuery(opt), resolveOptions));
 }
