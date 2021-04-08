fcdcdea67c7dd88d2a8404566e3e9ed9502f3684
jer3m01 <jer3m01@jer3m01.com>
2021-04-08 02:30:39 +0200

    refactor(css-prefix): map values instead of writing multiple times (#1422)
    * refactor(css-prefix): map values instead of writing multiple times
    
    * chore(CODEOWNERS): add compiler/transforms/compile/css-handler/prefix/* to jer3m01

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/display.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/display.ts
@@ -4,36 +4,21 @@ import {
 } from "@internal/compiler/transforms/compile/css-handler/prefix/utils";
 
 function flexRenamer(value: string) {
-	return value === "-moz-flex" ? "-moz-box" : value;
-}
-function inlineflexRenamer(value: string) {
-	return value === "-moz-inline-flex" ? "-moz-inline-box" : value;
+	return value.startsWith("-moz-") ? value.replace("flex", "box") : value;
 }
 
 // https://github.com/Fyrd/caniuse/blob/main/features-json/flexbox.json
-export default [
+export default ["flex", "inline-flex"].map((value) =>
 	createPrefixVisitor({
 		name: "display/flex",
 		enter(path) {
 			return prefixCSSValue({
 				path,
 				propertyName: "display",
-				value: "flex",
+				value,
 				browserFeaturesKey: "flexbox",
 				rename: flexRenamer,
 			});
 		},
-	}),
-	createPrefixVisitor({
-		name: "display/inline-flex",
-		enter(path) {
-			return prefixCSSValue({
-				path,
-				propertyName: "display",
-				value: "inline-flex",
-				browserFeaturesKey: "flexbox",
-				rename: inlineflexRenamer,
-			});
-		},
-	}),
-];
+	})
+);
