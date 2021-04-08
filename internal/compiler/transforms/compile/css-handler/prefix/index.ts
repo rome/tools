5750f7b68fa2aa503bb966459143718f172ee816
Andrew Kachnic <ajkachnic@protonmail.com>
2021-04-08 19:05:21 -0400

    feat(css): box-shadow support

--- internal/compiler/transforms/compile/css-handler/prefix/index.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/index.ts
@@ -1,7 +1,8 @@
-/* GENERATED:START(hash:5fb179e874f73fc64e32026cfd46a65fd59630e1,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
+/* GENERATED:START(hash:ab4c465879394d13c1468b7726e27aa2da974b57,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
+import boxShadow from "./prefixes/box-shadow";
 import display from "./prefixes/display";
 import transform from "./prefixes/transform";
 import transition from "./prefixes/transition";
 
-export default [...display, ...transform, ...transition];
+export default [...boxShadow, ...display, ...transform, ...transition];
 /* GENERATED:END(id:main) */
