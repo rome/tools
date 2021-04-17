ce0f38ae6c1f538d12367df21ca698211a9b9d8e
Guillaume Démésy <gh@magsout.com>
2021-04-17 04:14:02 +0200

    feat(css-prefix): user-select support (#1436)

---
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/user-select.ts
@@ -0,0 +1,19 @@
+import {
+	createPrefixVisitor,
+	prefixCSSProperty,
+} from "@internal/compiler/transforms/compile/css-handler/prefix/utils";
+
+// https://github.com/Fyrd/caniuse/blob/main/features-json/user-select-none.json
+// https://github.com/postcss/autoprefixer/blob/main/data/prefixes.js#L299-L302
+export default [
+	createPrefixVisitor({
+		name: "user-select",
+		enter(path) {
+			return prefixCSSProperty({
+				path,
+				propertyName: "user-select",
+				browserFeaturesKey: "user-select-none",
+			});
+		},
+	}),
+];
