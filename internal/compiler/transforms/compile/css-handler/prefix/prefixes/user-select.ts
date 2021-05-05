1acf8c9b2cf5fa78d0481a0e28b48b42b3a294e3
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-05-05 10:25:06 -0300

    Made some checks on the code

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/user-select.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/user-select.ts
@@ -4,7 +4,7 @@ import {
 } from "@internal/compiler/transforms/compile/css-handler/prefix/utils";
 
 // https://github.com/Fyrd/caniuse/blob/main/features-json/user-select-none.json
-export default ['auto', 'none', 'text', 'all'].map((value) => [
+export default ["auto", "none", "text", "all"].map((value) => [
 	createPrefixVisitor({
 		name: `user-select/${value}`,
 		enter(path) {
