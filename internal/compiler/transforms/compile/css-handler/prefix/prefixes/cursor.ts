3c4275ba4b45797111d4021db761166583f35939
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-05-06 17:34:42 -0300

    Checking the state of value and delivering the correct suffix to browser features key

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/cursor.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/cursor.ts
@@ -6,15 +6,27 @@ import {
 // https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-grab.json
 // https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-newer.json
 // https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors.json
-export default [
-	createPrefixVisitor({
-		name: "cursor",
-		enter(path) {
-			return prefixCSSProperty({
-				path,
-				propertyName: "cursor",
-				browserFeaturesKey: "css3-cursors",
-			});
-		},
-	}),
-];
+export default ["zoom-in", "zoom-out", "grab", "grabbing"].map((value) => {
+	let browserFeaturesKeySufix = "";
+	if (value === "zoom-in" || value === "zoom-out") {
+		browserFeaturesKeySufix = "newer";
+	}
+	if (value === "grab" || value === "grabbing") {
+		browserFeaturesKeySufix = "grab";
+	}
+	return [
+		createPrefixVisitor({
+			name: `cursor/${value}`,
+			enter(path) {
+				return prefixCSSProperty({
+					path,
+					propertyName: "cursor",
+					value,
+					browserFeaturesKey: ["css3-cursors", browserFeaturesKeySufix].join(
+						"-",
+					),
+				});
+			},
+		}),
+	];
+});
