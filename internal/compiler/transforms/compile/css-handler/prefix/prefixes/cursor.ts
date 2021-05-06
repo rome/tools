9bfabdc2bf864aeded8ed61b06116e5f5c955bda
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-05-06 18:33:25 -0300

    Checked the implementation of cursor css prefix

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/cursor.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/cursor.ts
@@ -1,33 +1,47 @@
 import {
 	createPrefixVisitor,
 	prefixCSSProperty,
+	prefixCSSValue,
 } from "@internal/compiler/transforms/compile/css-handler/prefix/utils";
 
-// https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-grab.json
-// https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-newer.json
-// https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors.json
-export default ["zoom-in", "zoom-out", "grab", "grabbing"].map((value) => {
-	let browserFeaturesKey = "css3-cursors";
-	let browserFeaturesKeySufix = "";
-	if (value === "zoom-in" || value === "zoom-out") {
-		browserFeaturesKeySufix = "newer";
-	}
-	if (value === "grab" || value === "grabbing") {
-		browserFeaturesKeySufix = "grab";
-	}
-	browserFeaturesKey = [browserFeaturesKey, browserFeaturesKeySufix].join("-");
-	browserFeaturesKey = (browserFeaturesKey as unknown) as string;
-	return [
+export default [
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-newer.json
+	...["zoom-in", "zoom-out"].map((value) =>
 		createPrefixVisitor({
 			name: `cursor/${value}`,
 			enter(path) {
-				return prefixCSSProperty({
+				return prefixCSSValue({
 					path,
 					propertyName: "cursor",
 					value,
-					browserFeaturesKey,
+					browserFeaturesKey: "css3-cursors-newer",
 				});
 			},
-		}),
-	];
-});
+		})
+	),
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-grab.json
+	...["grab", "grabbing"].map((value) =>
+		createPrefixVisitor({
+			name: `cursor/${value}`,
+			enter(path) {
+				return prefixCSSValue({
+					path,
+					propertyName: "cursor",
+					value,
+					browserFeaturesKey: "css3-cursors-grab",
+				});
+			},
+		})
+	),
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors.json
+	createPrefixVisitor({
+		name: "cursor",
+		enter(path) {
+			return prefixCSSProperty({
+				path,
+				propertyName: "cursor",
+				browserFeaturesKey: "css3-cursors",
+			});
+		},
+	}),
+];
