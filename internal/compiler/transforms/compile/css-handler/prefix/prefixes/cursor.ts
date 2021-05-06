664b72d40e2c730d0a23bd06d5a4d7ce454af8a5
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-05-06 18:01:40 -0300

    Checking the integrity of code

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/cursor.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/cursor.ts
@@ -7,6 +7,7 @@ import {
 // https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors-newer.json
 // https://github.com/Fyrd/caniuse/blob/main/features-json/css3-cursors.json
 export default ["zoom-in", "zoom-out", "grab", "grabbing"].map((value) => {
+	let browserFeaturesKey = "css3-cursors";
 	let browserFeaturesKeySufix = "";
 	if (value === "zoom-in" || value === "zoom-out") {
 		browserFeaturesKeySufix = "newer";
@@ -14,6 +15,8 @@ export default ["zoom-in", "zoom-out", "grab", "grabbing"].map((value) => {
 	if (value === "grab" || value === "grabbing") {
 		browserFeaturesKeySufix = "grab";
 	}
+	browserFeaturesKey = [browserFeaturesKey, browserFeaturesKeySufix].join("-");
+	browserFeaturesKey = (browserFeaturesKey as unknown) as string;
 	return [
 		createPrefixVisitor({
 			name: `cursor/${value}`,
@@ -22,9 +25,7 @@ export default ["zoom-in", "zoom-out", "grab", "grabbing"].map((value) => {
 					path,
 					propertyName: "cursor",
 					value,
-					browserFeaturesKey: ["css3-cursors", browserFeaturesKeySufix].join(
-						"-",
-					),
+					browserFeaturesKey,
 				});
 			},
 		}),
