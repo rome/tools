c2b8ecf310c8ffbcdc7be6a5c977232b4d2de993
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-05-07 19:46:21 -0300

    Generated files css prefix

--- internal/compiler/transforms/compile/css-handler/prefix/index.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/index.ts
@@ -1,4 +1,4 @@
-/* GENERATED:START(hash:c13649588ac54a0bb920b391cd7849cf33a5df9d,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
+/* GENERATED:START(hash:11ae0c68b0e686e80871271e5d12e9bf5ad66b5b,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
 import animation from "./prefixes/animation";
 import appearance from "./prefixes/appearance";
 import backdropFilter from "./prefixes/backdrop-filter";
