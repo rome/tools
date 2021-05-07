0b72bb4a2ce1112b9c31539be7e99c13e3d2b577
ktfth <44123854+ktfth@users.noreply.github.com>
2021-05-07 15:54:51 -0300

    feat(css-prefix): cursor support (#1457)

--- internal/compiler/transforms/compile/css-handler/prefix/index.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/index.ts
@@ -1,10 +1,11 @@
-/* GENERATED:START(hash:11c4f4d54e6311af362fec292ee5bffe5ebf04df,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
+/* GENERATED:START(hash:c38d60ce89df872ad613cfd746a4aaeba1a69016,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
 import animation from "./prefixes/animation";
 import appearance from "./prefixes/appearance";
 import backdropFilter from "./prefixes/backdrop-filter";
 import borderRadius from "./prefixes/border-radius";
 import boxShadow from "./prefixes/box-shadow";
 import boxSizing from "./prefixes/box-sizing";
+import cursor from "./prefixes/cursor";
 import display from "./prefixes/display";
 import hyphens from "./prefixes/hyphens";
 import multicolumn from "./prefixes/multicolumn";
@@ -23,6 +24,7 @@ export default [
 	...borderRadius,
 	...boxShadow,
 	...boxSizing,
+	...cursor,
 	...display,
 	...hyphens,
 	...multicolumn,
---
+++ website/src/docs/css-handler/prefix/cursor.md
@@ -0,0 +1,14 @@
+---
+title: Prefix cursor
+layout: layouts/prefix.liquid
+showHero: false
+description: MISSING DOCUMENTATION
+eleventyNavigation:
+	key: css-handler/prefix/cursor
+	parent: css-handler
+	title: cursor
+---
+
+# cursor
+
+MISSING DOCUMENTATION
