21d7392e00d10ea1cdf18ea4bb0d196d08e2ef91
ktfth <44123854+ktfth@users.noreply.github.com>
2021-05-07 21:00:03 -0300

    feat(css-prefix): image-set support (#1468)

--- internal/compiler/transforms/compile/css-handler/prefix/index.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/index.ts
@@ -1,4 +1,4 @@
-/* GENERATED:START(hash:c38d60ce89df872ad613cfd746a4aaeba1a69016,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
+/* GENERATED:START(hash:11ae0c68b0e686e80871271e5d12e9bf5ad66b5b,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
 import animation from "./prefixes/animation";
 import appearance from "./prefixes/appearance";
 import backdropFilter from "./prefixes/backdrop-filter";
@@ -8,6 +8,7 @@ import boxSizing from "./prefixes/box-sizing";
 import cursor from "./prefixes/cursor";
 import display from "./prefixes/display";
 import hyphens from "./prefixes/hyphens";
+import imageSet from "./prefixes/image-set";
 import multicolumn from "./prefixes/multicolumn";
 import position from "./prefixes/position";
 import textOrientation from "./prefixes/text-orientation";
@@ -27,6 +28,7 @@ export default [
 	...cursor,
 	...display,
 	...hyphens,
+	...imageSet,
 	...multicolumn,
 	...position,
 	...textOrientation,
