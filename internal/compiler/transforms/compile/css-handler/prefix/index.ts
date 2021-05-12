de022fb18e9cc8fa8d63e65552558b9e48a43c68
usama <usamatech2@gmail.com>
2021-05-12 23:06:22 +0200

    feat(css-prefix): tab-size and scroll-snap-type support (#1476)

--- internal/compiler/transforms/compile/css-handler/prefix/index.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/index.ts
@@ -1,4 +1,4 @@
-/* GENERATED:START(hash:cef7f5f2736247ed31692e797c3e63f9ef6c20be,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
+/* GENERATED:START(hash:9d2f97abaad6ed5af257118f6a0f51bd0305122f,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/css-prefix` to update. */
 import animation from "./prefixes/animation";
 import appearance from "./prefixes/appearance";
 import backdropFilter from "./prefixes/backdrop-filter";
@@ -12,6 +12,8 @@ import hyphens from "./prefixes/hyphens";
 import imageSet from "./prefixes/image-set";
 import multicolumn from "./prefixes/multicolumn";
 import position from "./prefixes/position";
+import scrollSnapType from "./prefixes/scroll-snap-type";
+import tabSize from "./prefixes/tab-size";
 import textOrientation from "./prefixes/text-orientation";
 import textOverflow from "./prefixes/text-overflow";
 import transform from "./prefixes/transform";
@@ -33,6 +35,8 @@ export default [
 	...imageSet,
 	...multicolumn,
 	...position,
+	...scrollSnapType,
+	...tabSize,
 	...textOrientation,
 	...textOverflow,
 	...transform,
