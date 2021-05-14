c9aaa3596798bbf33e0197059d44db3813fa24d3
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-05-14 12:46:50 -0300

    Checks on the css prefix support file for block

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/block.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/block.ts
@@ -3,15 +3,23 @@ import {
 	prefixCSSProperty,
 } from "@internal/compiler/transforms/compile/css-handler/prefix/utils";
 
+// https://github.com/Fyrd/caniuse/blob/main/features-json/css-logical-props.json
 export default [
+	"border-block-start",
+	"border-block-end",
+	"margin-block-start",
+	"margin-block-end",
+	"padding-block-start",
+	"padding-block-end",
+].map((propertyName) =>
 	createPrefixVisitor({
 		name: "block",
 		enter(path) {
 			return prefixCSSProperty({
 				path,
-				propertyName: "block",
-				browserFeaturesKey: "",
+				propertyName,
+				browserFeaturesKey: "css-logical-props",
 			});
 		},
-	}),
-];
+	})
+);
