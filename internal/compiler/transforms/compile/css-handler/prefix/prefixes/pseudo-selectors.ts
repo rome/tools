ed3c1e064d2c498fc723241f4256da67628e3ca6
JustBeYou <mihailferaru2000@gmail.com>
2021-06-02 23:17:58 +0300

    add references

--- internal/compiler/transforms/compile/css-handler/prefix/prefixes/pseudo-selectors.ts
+++ internal/compiler/transforms/compile/css-handler/prefix/prefixes/pseudo-selectors.ts
@@ -7,14 +7,26 @@ import {
 
 const pseudoSelectorsFeatures = new Map([
 	// classes
+	
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css-any-link.json
 	["any-link", "css-any-link"],
+
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/fullscreen.json
 	["fullscreen", "fullscreen"],
+
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css-read-only-write.json
 	["read-only", "css-read-only-write"],
 	["write-only", "css-read-only-write"],
 
 	// elements
+
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css-selection.json
 	["selection", "css-selection"],
+
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/css-placeholder.json
 	["placeholder", "css-placeholder"],
+
+	// https://github.com/Fyrd/caniuse/blob/main/features-json/fullscreen.json
 	["backdrop", "fullscreen"],
 ])
 
