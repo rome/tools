cd467be59fde124eb5b1b424a505c5e2a0661851
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-06-07 19:39:41 -0300

    Included special properties

--- internal/ast/css/import/CSSImport.ts
+++ internal/ast/css/import/CSSImport.ts
@@ -3,12 +3,17 @@ import {createBuilder} from "../../utils";
 
 export interface CSSImport extends NodeBaseWithComments {
 	readonly type: "CSSImport";
+	readonly prelude: CSSSelector[];
+	readonly block?: CSSBlock;
 }
 
 export const cssImport = createBuilder<CSSImport>(
 	"CSSImport",
 	{
 		bindingKeys: {},
-		visitorKeys: {},
+		visitorKeys: {
+			prelude: true,
+			block: true,
+		},
 	},
 );
