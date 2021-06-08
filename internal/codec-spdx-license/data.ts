cca0a806012f1abf2f773711b24c4b2ba83b0775
ematipico <my.burning@gmail.com>
2021-06-08 12:59:14 +0100

    chore: keep the license in the file

--- internal/codec-spdx-license/data.ts
+++ internal/codec-spdx-license/data.ts
@@ -1,3 +1,9 @@
+/**
+ * Portions Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 export default {
 	licenseListVersion: "3.13",
 	licenses: [
--- scripts/update-licenses.ts
+++ scripts/update-licenses.ts
@@ -19,6 +19,12 @@ export async function main() {
 		await writeFile(
 			licensesFolder.append("data.ts"),
 			dedent`
+			  /**
+				 * Portions Copyright (c) Facebook, Inc. and its affiliates.
+				 *
+				 * This source code is licensed under the MIT license found in the
+				 * LICENSE file in the root directory of this source tree.
+				 */
 				export default ${JSON.stringify(licenses)}
 			`,
 		);
