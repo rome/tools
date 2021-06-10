69c7bed0938c1c9692cfece2bf43f855683755da
Emanuele Stoppa <my.burning@gmail.com>
2021-06-10 17:34:20 +0100

    fix: handle semi colon

--- internal/css-parser/parser/at-import.ts
+++ internal/css-parser/parser/at-import.ts
@@ -6,7 +6,7 @@ import {descriptions} from "@internal/diagnostics";
 
 export function parseAtImport(parser: CSSParser): CSSAtImport | undefined {
 	const start = parser.getPosition();
-	let value: CSSAtImportValue;
+	let value: CSSAtImportValue | undefined = undefined;
 	while (matchToken(parser, "Whitespace")) {
 		readToken(parser, "Whitespace");
 	}
@@ -15,13 +15,6 @@ export function parseAtImport(parser: CSSParser): CSSAtImport | undefined {
 	if (functionArgumentToken.type === "String") {
 		nextToken(parser);
 		value = functionArgumentToken.value;
-		return parser.finishNode(
-			start,
-			{
-				type: "CSSAtImport",
-				value
-			}
-		);
 	}
 
 	const token = parser.getToken();
@@ -29,6 +22,15 @@ export function parseAtImport(parser: CSSParser): CSSAtImport | undefined {
 	if (token.type === "Function") {
 		nextToken(parser);
 		value = token.value;
+	}
+
+	if (value) {
+		nextToken(parser)
+		// Semi colon is optional, but if present we have to move forward in order to not
+		// break the existing usage and make it as part of the current node
+		if (parser.getToken().type === "Semi") {
+			nextToken(parser);
+		}
 		return parser.finishNode(
 			start,
 			{
@@ -37,8 +39,6 @@ export function parseAtImport(parser: CSSParser): CSSAtImport | undefined {
 			}
 		);
 	}
-
-	if (token)
 	parser.unexpectedDiagnostic({
 		description: descriptions.CSS_PARSER.AT_IMPORT_INVALID_ARGUMENT, // we need to create this diagnostic,
 		token: functionArgumentToken
