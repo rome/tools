755a58776da663305c43f7eb64f9a9ce6e59948c
jer3m01 <jer3m01@jer3m01.com>
2021-07-15 16:42:46 +0200

    perf(browsers-db): store data as JSON.parse string instead of json chore(browsers-db): update data

--- internal/browsers-db/index.ts
+++ internal/browsers-db/index.ts
@@ -1,7 +1,7 @@
-/* GENERATED:START(hash:e936424b66aca4c5b46666f0cafa68543a7008c7,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/browsers-db` to update. */
-export const version = "1.0.30001242";
+/* GENERATED:START(hash:f380bad482ec5bd748dd18cfc60f1e341a605048,id:main) Everything below is automatically generated. DO NOT MODIFY. Run `./rome run scripts/generated-files/browsers-db` to update. */
+export const version = "1.0.30001245";
 /* GENERATED:END(id:main) */
 
-export {default as data} from "./data.json";
+export {default as data} from "./data";
 
-export {default as regions} from "./regions.json";
+export {default as regions} from "./regions";
