7e7ea5276f00384c7bd873f9b07a19e3d3e1ea96
Yasser Elassal <yassere@gmail.com>
2021-10-04 11:35:18 -0500

    feat: add syntax crate and experimental codegen (#1681)

--- crates/parser/src/main.rs
+++ crates/parser/src/main.rs
@@ -1,24 +1,39 @@
 use anyhow::{anyhow, Result};
-use parser::{languages, ParserLanguage};
+use parser::{Language, TreeSink};
+
+struct TestPrinter {
+	depth: usize,
+}
+
+impl TestPrinter {
+	fn new() -> Self {
+		Self { depth: 0 }
+	}
+}
+
+impl TreeSink for TestPrinter {
+	fn token(&mut self, kind: parser::SyntaxKind, text: &str) {
+		println!("{}{:?} {:?}", "  ".repeat(self.depth), kind, text)
+	}
+
+	fn start_node(&mut self, kind: parser::SyntaxKind) {
+		println!("{}{:?}", "  ".repeat(self.depth), kind);
+		self.depth += 1;
+	}
+
+	fn finish_node(&mut self) {
+		debug_assert!(self.depth > 0);
+		self.depth -= 1;
+	}
+}
 
 fn main() -> Result<()> {
 	let args: Vec<String> = std::env::args().collect();
 
-	let lang = args.get(1).ok_or_else(|| anyhow!("Missing language"))?;
-	let path = args.get(2).ok_or_else(|| anyhow!("Missing filename"))?;
-
+	let path = args.get(1).ok_or_else(|| anyhow!("Missing filename"))?;
 	let src = std::fs::read_to_string(path)?;
 
-	match lang.as_str() {
-		"tsx" => print_tree(&src, languages::TSX),
-		"ts" => print_tree(&src, languages::TS),
-		lang => return Err(anyhow!("Invalid language: {}", lang)),
-	}
-}
-
-fn print_tree(src: &str, syntax: impl ParserLanguage) -> Result<()> {
-	let mut parser = parser::Parser::new(syntax)?;
-	let tree = parser.parse_text(src)?;
-	println!("{:#?}", tree);
+	let mut printer = TestPrinter::new();
+	parser::parse(&src, &mut printer, Language::Tsx)?;
 	Ok(())
 }
