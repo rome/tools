b9d5ff2d508d95b93a252ec6fd957711e5dc0ff5
Emanuele Stoppa <my.burning@gmail.com>
2021-10-06 09:43:23 +0100

    chore: feat

--- crates/formatter/src/ts/params.rs
+++ crates/formatter/src/ts/params.rs
@@ -1,15 +1,15 @@
 use crate::{format_tokens, FormatToken, ListToken};
 use syntax::{
-	ast::{self, AstNode},
+	ast::{self, AstNode, FormalParameters},
 	SyntaxNode,
 };
 
-pub fn format(node: SyntaxNode) -> FormatToken {
-	if let None = node.first_child() {
-		return format_tokens!("()");
-	}
-	let formal_parameters = ast::FormalParameters::cast(node).unwrap();
-	let params = formal_parameters.parameters();
+pub fn format(node: FormalParameters) -> FormatToken {
+	// if let None = node.inner_children() {
+	// 	return format_tokens!("()");
+	// }
+	// let formal_parameters = ast::FormalParameters::cast(node).unwrap();
+	let params = node.inner_children();
 	let mut tokens = vec![];
 	for param in params {
 		tokens.push(format_tokens!(param.value().unwrap().to_string().as_str()))
