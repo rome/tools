8c958aff591741dd06945eab4c7c5eb6867d3a6e
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2021-10-07 14:05:54 +0100

    using the older spec_test method

--- crates/formatter/tests/spec_test.rs
+++ crates/formatter/tests/spec_test.rs
@@ -1,6 +1,6 @@
 use rome_formatter::{format_str, FormatOptions};
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 
 /// Tests that format a given input file, results in the expected formatted output.
 ///
@@ -13,45 +13,25 @@ use std::path::PathBuf;
 ///
 /// * `json/null` -> input: `tests/specs/json/null.json`, expected output: `tests/specs/json/null.expected.json`
 /// * `null` -> input: `tests/specs/null.json`, expected output: `tests/specs/null.expected.json`
-pub fn spec_test(spec_name: &str) {
-	let spec_input_file = spec_file_name(spec_name, "json");
+pub fn run(spec_input_file: &str, expected_file: &str) {
+	let spec_input_file = Path::new(spec_input_file);
+	let expected_file = Path::new(expected_file);
 
 	assert!(
 		spec_input_file.is_file(),
-		"The input '{}' for the spec '{}' must exist and be a file.",
-		spec_name,
+		"The input '{}' must exist and be a file.",
 		spec_input_file.display()
 	);
 
-	let expected_file = spec_file_name(spec_name, "expected.json");
 	assert!(
 		expected_file.is_file(),
-		"The expected output '{}' for the spec '{}' must exist and be a file.",
+		"The expected output '{}' must exist and be a file.",
 		expected_file.display(),
-		spec_name
 	);
 
 	let spec_content = fs::read_to_string(spec_input_file).unwrap();
-
 	let result = format_str(spec_content.as_str(), FormatOptions::default());
-
 	let expected_output = fs::read_to_string(expected_file).unwrap();
+
 	assert_eq!(&expected_output, result.code());
-}
-
-fn spec_file_name(spec_name: &str, extension: &str) -> PathBuf {
-	let specs_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-		.join("tests")
-		.join("specs");
-
-	assert!(
-		specs_dir.is_dir(),
-		"The directory '{}' containing the specification files doesn't exist or isn't a directory.",
-		specs_dir.display()
-	);
-
-	let base_spec_path =
-		PathBuf::from(spec_name.replace('/', std::path::MAIN_SEPARATOR.to_string().as_str()));
-
-	specs_dir.join(base_spec_path.with_extension(extension))
 }
--- crates/formatter/tests/spec_tests.rs
+++ crates/formatter/tests/spec_tests.rs
@@ -2,15 +2,7 @@ mod spec_test;
 
 mod formatter {
 	mod json {
-		tests_macros::gen_tests! {"tests/specs/json/*.json", super::assert_formatter}
-	}
-
-	fn assert_formatter(spec_input_file: &str, expected_file: &str) {
-		println!("{}", spec_input_file);
-		use rome_formatter::{format_str, FormatOptions};
-		let spec_content = std::fs::read_to_string(spec_input_file).unwrap();
-		let result = format_str(spec_content.as_str(), FormatOptions::default());
-		let expected_output = std::fs::read_to_string(expected_file).unwrap();
-		assert_eq!(&expected_output, result.code());
+		use crate::spec_test;
+		tests_macros::gen_tests! {"tests/specs/json/*.json", spec_test::run}
 	}
 }
