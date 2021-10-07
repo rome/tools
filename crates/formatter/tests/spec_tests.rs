9cbaae3e54f9709ef76bf42b3eaf3e3606eb06c3
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2021-10-07 17:13:23 +0100

    json formatter spec tests (#1694)

--- crates/formatter/tests/spec_tests.rs
+++ crates/formatter/tests/spec_tests.rs
@@ -1,46 +1,8 @@
 mod spec_test;
 
-#[test]
-pub fn json_boolean() {
-	spec_test::spec_test("json/boolean");
-}
-
-#[test]
-pub fn json_null() {
-	spec_test::spec_test("json/null");
-}
-
-#[test]
-pub fn json_string() {
-	spec_test::spec_test("json/string");
-}
-
-#[test]
-pub fn json_number() {
-	spec_test::spec_test("json/number");
-}
-
-#[test]
-pub fn json_single_line() {
-	spec_test::spec_test("json/single_line");
-}
-
-#[test]
-pub fn json_array() {
-	spec_test::spec_test("json/array");
-}
-
-#[test]
-pub fn json_key_value() {
-	spec_test::spec_test("json/key_value");
-}
-
-#[test]
-pub fn json_multi_line() {
-	spec_test::spec_test("json/multi_line");
-}
-
-#[test]
-pub fn json_int1() {
-	spec_test::spec_test("json/int1");
+mod formatter {
+	mod json {
+		use crate::spec_test;
+		tests_macros::gen_tests! {"tests/specs/json/*.json", spec_test::run}
+	}
 }
