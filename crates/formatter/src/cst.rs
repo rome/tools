aca33a1d349c614ae67d8104f782e52a966a1926
Micha Reiser <micha@rome.tools>
2021-10-11 08:20:32 +0200

    Rename the `context` parameter in the cst file to `formatter`

--- crates/formatter/src/cst.rs
+++ crates/formatter/src/cst.rs
@@ -31,57 +31,59 @@ pub fn syntax_token(syntax_token: &SyntaxToken) -> FormatElement {
 }
 
 impl ToFormatElement for SyntaxNode {
-	fn to_format_element(&self, context: &Formatter) -> FormatElement {
+	fn to_format_element(&self, formatter: &Formatter) -> FormatElement {
 		match self.kind() {
 			SyntaxKind::ARRAY_EXPR => ArrayExpr::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::ARROW_EXPR => ArrowExpr::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::ASSIGN_PATTERN => AssignPattern::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::LITERAL => Literal::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
-			SyntaxKind::NAME => Name::cast(self.clone()).unwrap().to_format_element(context),
+				.to_format_element(formatter),
+			SyntaxKind::NAME => Name::cast(self.clone())
+				.unwrap()
+				.to_format_element(formatter),
 			SyntaxKind::NAME_REF => NameRef::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::PARAMETER_LIST => ParameterList::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::SCRIPT => Script::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::SINGLE_PATTERN => SinglePattern::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::SPREAD_ELEMENT => SinglePattern::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::VAR_DECL => VarDecl::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::DECLARATOR => Declarator::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::FN_DECL => FnDecl::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::SEQUENCE_EXPR => SequenceExpr::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::BLOCK_STMT => BlockStmt::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::EXPR_STMT => ExprStmt::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			SyntaxKind::RETURN_STMT => ReturnStmt::cast(self.clone())
 				.unwrap()
-				.to_format_element(context),
+				.to_format_element(formatter),
 			_ => todo!(
 				"Implement formatting for the {:?} syntax kind.",
 				self.kind()
