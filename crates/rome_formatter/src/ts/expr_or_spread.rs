052744de0843ecf33ed1615b39e854c74be887fc
Emanuele Stoppa <my.burning@gmail.com>
2021-11-03 13:17:46 -0300

    feat: flat union of unions in codegen

--- crates/rome_formatter/src/ts/expr_or_spread.rs
+++ crates/rome_formatter/src/ts/expr_or_spread.rs
@@ -5,7 +5,6 @@ impl ToFormatElement for ExprOrSpread {
 	fn to_format_element(&self, formatter: &Formatter) -> Option<FormatElement> {
 		match self {
 			ExprOrSpread::SpreadElement(spread) => spread.to_format_element(formatter),
-			ExprOrSpread::Expr(expr) => expr.to_format_element(formatter),
 			ExprOrSpread::Literal(literal) => literal.to_format_element(formatter),
 			ExprOrSpread::ObjectExpr(object_expression) => {
 				object_expression.to_format_element(formatter)
