f45fdc49059efba3f79fe46c9a3f1bf97d16ac58
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2021-11-25 13:24:52 +0000

    fmt and clippy

--- xtask/src/libs/mod.rs
+++ xtask/src/libs/mod.rs
@@ -13,13 +13,27 @@ fn print_diff(before: dhat::Stats, current: dhat::Stats) -> dhat::Stats {
 	println!("\tMemory");
 	if let Some(heap) = &current.heap {
 		println!("\t\tCurrent Blocks: {}", heap.curr_blocks);
-		println!("\t\tCurrent Bytes: {}", heap.curr_bytes.file_size(options::CONVENTIONAL).unwrap());
+		println!(
+			"\t\tCurrent Bytes: {}",
+			heap.curr_bytes.file_size(options::CONVENTIONAL).unwrap()
+		);
 		println!("\t\tMax Blocks: {}", heap.max_blocks);
-		println!("\t\tMax Bytes: {}", heap.max_bytes.file_size(options::CONVENTIONAL).unwrap());
+		println!(
+			"\t\tMax Bytes: {}",
+			heap.max_bytes.file_size(options::CONVENTIONAL).unwrap()
+		);
 	}
 
-	println!("\t\tTotal Blocks: {}", current.total_blocks - before.total_blocks);
-	println!("\t\tTotal Bytes: {}", (current.total_bytes - before.total_bytes).file_size(options::CONVENTIONAL).unwrap());
+	println!(
+		"\t\tTotal Blocks: {}",
+		current.total_blocks - before.total_blocks
+	);
+	println!(
+		"\t\tTotal Bytes: {}",
+		(current.total_bytes - before.total_bytes)
+			.file_size(options::CONVENTIONAL)
+			.unwrap()
+	);
 
 	current
 }
