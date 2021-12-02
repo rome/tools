ab971ab1c1e9c4443f0606120381c8e182b53311
Micha Reiser <micha@rome.tools>
2021-12-02 10:44:39 +0100

    fix(rslint_parser): Fix parsing of multiple tokens (#1843)
    The parser allows grouping multiple lexer-tokens into a single "parsed" token using `bump_multiple`. However, this path wasn't yet updated to account for leading/trailing trivia.
    
    This PR ensures that trivia is also handled for the multiple token case by unifying the `do_token` implementation.
    
    Fixes #1842

--- crates/rslint_parser/src/lossy_tree_sink.rs
+++ crates/rslint_parser/src/lossy_tree_sink.rs
@@ -43,11 +43,7 @@ impl<'a> TreeSink for LossyTreeSink<'a> {
 				.sum::<usize>() as u32,
 		);
 
-		let range = TextRange::at(self.text_pos, len);
-		let text = &self.text[range];
-		self.text_pos += len;
-		self.token_pos += amount as usize;
-		self.inner.token(kind, text);
+		self.do_tokens(kind, len, amount)
 	}
 
 	fn token(&mut self, kind: SyntaxKind) {
@@ -165,11 +161,16 @@ impl<'a> LossyTreeSink<'a> {
 		}
 	}
 
+	#[inline]
 	fn do_token(&mut self, kind: SyntaxKind, len: TextSize) {
+		self.do_tokens(kind, len, 1)
+	}
+
+	fn do_tokens(&mut self, kind: SyntaxKind, len: TextSize, token_count: u8) {
 		let token_range = TextRange::at(self.text_pos, len);
 
 		self.text_pos += len;
-		self.token_pos += 1;
+		self.token_pos += token_count as usize;
 
 		// Everything until the next linebreak (but not including it)
 		// will be the trailing trivia...
