ac1358125d2d6cebd2a6775af865b6786e0e0bf2
Boshen <boshenc@gmail.com>
2021-12-08 23:39:02 +0800

    feat(formatter): impl ToFormatElement for JsStaticMemberExpression, JsName, JsAnyName

--- crates/rome_formatter/src/ts/auxiliary/name.rs
+++ crates/rome_formatter/src/ts/auxiliary/name.rs
@@ -1,8 +1,26 @@
-use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
-use rslint_parser::ast::JsName;
+use crate::{format_elements, FormatElement, FormatResult, Formatter, ToFormatElement};
+use rslint_parser::ast::{JsAnyName, JsName, JsPrivateName};
+
+impl ToFormatElement for JsAnyName {
+	fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
+		match self {
+			JsAnyName::JsName(name) => name.to_format_element(formatter),
+			JsAnyName::JsPrivateName(name) => name.to_format_element(formatter),
+		}
+	}
+}
 
 impl ToFormatElement for JsName {
 	fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
 		formatter.format_token(&self.value_token()?)
+	}
+}
+
+impl ToFormatElement for JsPrivateName {
+	fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
+		Ok(format_elements![
+			formatter.format_token(&self.hash_token()?)?,
+			formatter.format_token(&self.value_token()?)?
+		])
 	}
 }
