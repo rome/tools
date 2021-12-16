1af4b5b52623419f763cb7a58c8a5b94fe415e9d
Micha Reiser <micha@rome.tools>
2021-12-16 15:16:40 +0100

    Revert "Avoid re-iterating children"
    This reverts commit 9af858c814fbe427af989d095479d1cd4de93e8f.

--- crates/rome_rowan/src/ast_shape.rs
+++ crates/rome_rowan/src/ast_shape.rs
@@ -2,17 +2,71 @@ use crate::api::RawLanguage;
 use crate::Language;
 
 pub trait AstTreeShape: Language {
-	fn validate_slot(parent: Self::Kind, index: usize, value: Option<Self::Kind>) -> bool;
+	/// Verifies if the `children` fit the expected shape of `kind` node.
+	fn fits_shape_of(
+		kind: &Self::Kind,
+		children_len: usize,
+		children_kinds: impl Iterator<Item = Option<Self::Kind>>,
+	) -> bool;
 
-	fn validate_end(parent: Self::Kind, length: usize) -> bool;
-}
+	#[inline]
+	fn fits_list_shape<I, N>(can_cast: N, actual: I) -> bool
+	where
+		N: Fn(Self::Kind) -> bool,
+		I: Iterator<Item = Option<Self::Kind>>,
+	{
+		for actual_child in actual {
+			if actual_child.is_some() && !can_cast(actual_child.unwrap()) {
+				return false;
+			}
+		}
 
-impl AstTreeShape for RawLanguage {
-	fn validate_slot(_: Self::Kind, _: usize, _: Option<Self::Kind>) -> bool {
 		true
 	}
 
-	fn validate_end(_: Self::Kind, _: usize) -> bool {
+	#[inline]
+	fn fits_separated_list_shape<I, N>(
+		can_cast: N,
+		separator: Self::Kind,
+		allow_trailing: bool,
+		actual: I,
+	) -> bool
+	where
+		N: Fn(Self::Kind) -> bool,
+		I: Iterator<Item = Option<Self::Kind>>,
+	{
+		// Is the next expected element a separator?
+		let mut next_element = true;
+		let mut empty = true;
+
+		for actual_child in actual {
+			if next_element {
+				if actual_child.is_some() && !can_cast(actual_child.unwrap()) {
+					return false;
+				}
+
+				next_element = false;
+			} else {
+				if actual_child.is_some() && actual_child.unwrap() != separator {
+					return false;
+				}
+
+				next_element = true;
+			}
+			empty = false;
+		}
+
+		// OK if the list allows trailing separator or is empty or if the last element wasn't a trailing separator
+		allow_trailing || empty || !next_element
+	}
+}
+
+impl AstTreeShape for RawLanguage {
+	fn fits_shape_of(
+		_: &Self::Kind,
+		_: usize,
+		_: impl Iterator<Item = Option<Self::Kind>>,
+	) -> bool {
 		true
 	}
 }
--- xtask/src/codegen/generate_js_tree_shape.rs
+++ xtask/src/codegen/generate_js_tree_shape.rs
@@ -5,91 +5,56 @@ use crate::{
 	codegen::{kinds_src::Field, to_upper_snake_case},
 	Result,
 };
-use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 
 pub fn generate_js_tree_shape(ast: &AstSrc) -> Result<String> {
-	let validate_slot = generate_validate_slot(ast);
-	let validate_end = generate_validate_end(ast);
-
-	let output = quote! {
-		use crate::{
-			ast::*,
-			T,
-			JsLanguage,
-			SyntaxKind::{*}
-		};
-
-		use rome_rowan::AstTreeShape;
-
-		impl AstTreeShape for JsLanguage {
-			#validate_slot
-
-			#validate_end
-		}
-	};
-
-	let pretty = crate::reformat(output)?;
-	Ok(pretty)
-}
-
-fn generate_validate_slot(ast: &AstSrc) -> TokenStream {
 	let normal_node_arms = ast.nodes.iter().map(|node| {
 		let kind = format_ident!("{}", to_upper_snake_case(&node.name));
+		let expected_len = node.fields.len();
 
-		let fields: Vec<_> = node
-			.fields
-			.iter()
-			.enumerate()
-			.map(|(index, field)| {
-				let arm_body = match field {
-					Field::Node { ty, .. } => {
-						let ast_type_name = format_ident!("{}", ty);
+		let fields = node.fields.iter().map(|field| match field {
+			Field::Node { ty, .. } => {
+				let ast_type_name = format_ident!("{}", ty);
 
-						quote! {
-							value.is_none() || #ast_type_name::can_cast(value.unwrap())
-						}
+				// TODO, optional / non optional branches are identical. It's just the "eq" function that differs
+
+				quote! {
+					if actual_kinds.next().unwrap().map(#ast_type_name::can_cast) == Some(false) {
+						return false;
 					}
-
-					Field::Token { kind, .. } => {
-						let matches = match kind {
-							TokenKind::Single(expected) => {
-								let expected_kind = token_kind_to_code(expected);
-								quote! { value.unwrap() == #expected_kind}
-							}
-							TokenKind::Many(expected) => {
-								let expected_kinds =
-									expected.iter().map(|kind| token_kind_to_code(kind));
-								quote! {
-									matches!(value.unwrap(), #(#expected_kinds)|*)
-								}
-							}
-						};
-
+				}
+			}
+			Field::Token { kind, .. } => {
+				let eq_kind = match kind {
+					TokenKind::Single(expected) => {
+						let expected_kind = token_kind_to_code(expected);
+						quote! { actual == #expected_kind}
+					}
+					TokenKind::Many(expected) => {
+						let expected_kinds = expected.iter().map(|kind| token_kind_to_code(kind));
 						quote! {
-							value.is_none() || #matches
+							matches!(actual, #(#expected_kinds)|*)
 						}
 					}
 				};
 
 				quote! {
-					#index => #arm_body
-				}
-			})
-			.collect();
-
-		if fields.is_empty() {
-			quote! {
-				#kind => false
-			}
-		} else {
-			quote! {
-				#kind => {
-					match index {
-						#(#fields),*,
-						_ => false
+					if actual_kinds.next().unwrap().map(|actual| #eq_kind) == Some(false) {
+						return false;
 					}
 				}
+			}
+		});
+
+		quote! {
+			#kind => {
+				if actual_len != #expected_len {
+					return false;
+				}
+
+				#(#fields)*
+
+				true
 			}
 		}
 	});
@@ -97,24 +62,15 @@ fn generate_validate_slot(ast: &AstSrc) -> TokenStream {
 	let lists = ast.lists().map(|(name, data)| {
 		let element_type = format_ident!("{}", data.element_name);
 		let kind = format_ident!("{}", to_upper_snake_case(name));
-
 		if let Some(separator) = &data.separator {
 			let allow_trailing = separator.allow_trailing;
 			let separator_kind = token_kind_to_code(&separator.separator_token);
 			quote! {
-				#kind => {
-					let expects_element = index % 2 == 0;
-
-					if expects_element {
-						value.is_none() ||  #element_type::can_cast(value.unwrap())
-					} else {
-						value.is_none() || value.unwrap() == #separator_kind
-					}
-				}
+				#kind => Self::fits_separated_list_shape(#element_type::can_cast, #separator_kind, #allow_trailing, actual_kinds)
 			}
 		} else {
 			quote! {
-				#kind => value.is_none() || #element_type::can_cast(value.unwrap())
+				#kind => Self::fits_list_shape(#element_type::can_cast, actual_kinds)
 			}
 		}
 	});
@@ -124,62 +80,28 @@ fn generate_validate_slot(ast: &AstSrc) -> TokenStream {
 		.iter()
 		.map(|node| format_ident!("{}", to_upper_snake_case(node)));
 
-	quote! {
-		fn validate_slot(parent: Self::Kind, index: usize, value: Option<Self::Kind>) -> bool {
-			match parent {
-				#(#unknown_kinds)|* | ERROR => true,
-				#(#normal_node_arms),*,
-				#(#lists),*,
-				_ => unreachable!("Is {:?} a token?", parent),
-			}
-		}
-	}
-}
-
-fn generate_validate_end(ast: &AstSrc) -> TokenStream {
-	let normal_node_arms = ast.nodes.iter().map(|node| {
-		let kind = format_ident!("{}", to_upper_snake_case(&node.name));
-		let expected_len = node.fields.len();
-
-		quote! {
-			#kind => actual_len == #expected_len
-		}
-	});
-
-	let lists = ast.lists().map(|(name, data)| {
-		let kind = format_ident!("{}", to_upper_snake_case(name));
-		let body = if let Some(separator) = &data.separator {
-			let allow_trailing = separator.allow_trailing;
-
-			if allow_trailing {
-				quote! { true }
-			} else {
-				quote! {
-					actual_len == 0 || actual_len % 2 == 1
-				}
-			}
-		} else {
-			quote! {
-				true
-			}
+	let output = quote! {
+		use crate::{
+			ast::*,
+			T,
+			JsLanguage,
+			SyntaxKind::{self, *}
 		};
 
-		quote! { #kind => #body }
-	});
+		use rome_rowan::AstTreeShape;
 
-	let unknown_kinds = ast
-		.unknowns
-		.iter()
-		.map(|node| format_ident!("{}", to_upper_snake_case(node)));
-
-	quote! {
-		fn validate_end(parent: Self::Kind, actual_len: usize) -> bool {
-			match parent {
-				#(#unknown_kinds)|* | ERROR => true,
-				#(#normal_node_arms),*,
-				#(#lists),*,
-				_ => unreachable!("Is {:?} a token?", parent),
+		impl AstTreeShape for JsLanguage {
+			fn fits_shape_of(kind: &Self::Kind, actual_len: usize, mut actual_kinds: impl Iterator<Item = Option<Self::Kind>>, ) -> bool {
+				match kind {
+					#(#unknown_kinds)|* | ERROR => true,
+					#(#normal_node_arms),*
+					#(#lists),*,
+					_ => unreachable!("Is {:?} a token?", kind),
+				}
 			}
 		}
-	}
+	};
+
+	let pretty = crate::reformat(output)?;
+	Ok(pretty)
 }
