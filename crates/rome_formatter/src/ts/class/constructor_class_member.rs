8569179df2aa007da9cf8939f97c892758898f27
Boshen <boshenc@gmail.com>
2021-12-17 18:42:10 +0800

    fix(formatter): fix class constructor parameters' line wrap

--- crates/rome_formatter/src/ts/class/constructor_class_member.rs
+++ crates/rome_formatter/src/ts/class/constructor_class_member.rs
@@ -1,6 +1,7 @@
 use crate::{
-	format_elements, group_elements, join_elements, soft_line_break_or_space, space_token,
-	FormatElement, FormatResult, Formatter, ToFormatElement,
+	format_elements, group_elements, if_group_breaks, join_elements, soft_indent,
+	soft_line_break_or_space, space_token, token, FormatElement, FormatResult, Formatter,
+	ToFormatElement,
 };
 use rslint_parser::ast::{
 	JsAnyConstructorParameter, JsConstructorClassMember, JsConstructorParameters,
@@ -19,15 +20,16 @@ impl ToFormatElement for JsConstructorClassMember {
 
 impl ToFormatElement for JsConstructorParameters {
 	fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-		let l_bracket = formatter.format_token(&self.l_paren_token()?)?;
 		let params = formatter.format_separated(self.parameters())?;
-		let r_bracket = formatter.format_token(&self.r_paren_token()?)?;
 
-		Ok(format_elements![group_elements(format_elements![
-			l_bracket,
-			join_elements(soft_line_break_or_space(), params),
-			r_bracket
-		])])
+		Ok(group_elements(format_elements!(
+			formatter.format_token(&self.l_paren_token()?)?,
+			soft_indent(format_elements![
+				join_elements(soft_line_break_or_space(), params),
+				if_group_breaks(token(",")),
+			]),
+			formatter.format_token(&self.r_paren_token()?)?,
+		)))
 	}
 }
 
