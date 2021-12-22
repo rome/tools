31fceb931b318f4a8bf37388c08b326d5850b856
Boshen <boshenc@gmail.com>
2021-12-22 12:34:16 +0800

    fix(formatter): add space around JsObjectExpression if it fits on a single line

--- crates/rome_formatter/src/ts/expressions/object_expression.rs
+++ crates/rome_formatter/src/ts/expressions/object_expression.rs
@@ -1,19 +1,31 @@
 use crate::{
-	format_elements, group_elements, if_group_breaks, join_elements, soft_indent,
-	soft_line_break_or_space, token, FormatElement, FormatResult, Formatter, ToFormatElement,
+	empty_element, format_elements, group_elements, if_group_breaks, if_group_fits_on_single_line,
+	join_elements, soft_indent, soft_line_break_or_space, space_token, token, FormatElement,
+	FormatResult, Formatter, ToFormatElement,
 };
 use rslint_parser::ast::JsObjectExpression;
+use rslint_parser::AstSeparatedList;
 
 impl ToFormatElement for JsObjectExpression {
 	fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-		let props = formatter.format_separated(self.members())?;
+		let members = self.members();
+
+		let space = if members.len() == 0 {
+			empty_element()
+		} else {
+			if_group_fits_on_single_line(space_token())
+		};
+
+		let props = formatter.format_separated(members)?;
 
 		Ok(group_elements(format_elements!(
 			formatter.format_token(&self.l_curly_token()?)?,
+			space.clone(),
 			soft_indent(format_elements![
 				join_elements(soft_line_break_or_space(), props),
 				if_group_breaks(token(",")),
 			]),
+			space,
 			formatter.format_token(&self.r_curly_token()?)?,
 		)))
 	}
