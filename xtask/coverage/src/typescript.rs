2bd3c59020c0c57117b70e266501a62e291fe9db
Micha Reiser <micha@rome.tools>
2022-01-28 13:08:44 +0100

    chor(coverage): Extract shared test suite infrastructure (#2019)
    Refactors the coverage runner to allow more code-reuse between the TypeScript and T262 test suite by extracting the shared infrastructure around loading, running, and reporting.
    
    * Introduces a new `TestSuite` trait that represents a test set like T262 and Typescript.
    * Introduces a new `TestCase` that runs a single test of a test suite
    * Introduces a new `TestReporter` that can hook into the test execution (JSON, CLI Progress, RAST, ...)
    * Enabled concurrent loading and running of typescript tests
    
    This refactor will simplify adding more test suites in the future and provides a base for running multiple suites and show aggregate results.

--- xtask/coverage/src/typescript.rs
+++ xtask/coverage/src/typescript.rs
@@ -1,39 +1,61 @@
 use super::*;
-use colored::Colorize;
 use rslint_parser::{parse, Syntax};
-use walkdir::{DirEntry, WalkDir};
-use yastl::Pool;
+use std::path::Path;
 
-use crate::{FailReason, Outcome, TestResult};
+use crate::runner::{TestCase, TestRunOutcome, TestSuite};
 
 const BASE_PATH: &str = "xtask/coverage/Typescript/tests";
 
-pub fn load_ts_files(query: Option<&str>) -> Vec<DirEntry> {
-    WalkDir::new(BASE_PATH)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|file| {
-            let path = file.path();
-            if !path.is_file() {
-                return false;
-            }
-            let ext = path.extension();
-            if ext.is_none() {
-                return false;
-            }
-            if let Some(ext) = ext {
-                if ext.to_str() != Some("ts") {
-                    return false;
-                }
-            }
-            if let Some(query) = query {
-                let path = file.path().to_string_lossy().replace("\\", "/");
-                path.contains(query)
-            } else {
-                true
-            }
-        })
-        .collect::<Vec<_>>()
+#[derive(Debug)]
+struct TypeScriptTestCase {
+    code: String,
+    path: PathBuf,
+}
+
+impl TestCase for TypeScriptTestCase {
+    fn path(&self) -> &Path {
+        self.path.strip_prefix(BASE_PATH).unwrap()
+    }
+
+    fn code(&self) -> &str {
+        &self.code
+    }
+
+    fn run(&self) -> TestRunOutcome {
+        let syntax = Syntax::default().typescript();
+        let r = parse(self.code(), 0, syntax);
+
+        match r.ok() {
+            Err(errors) => TestRunOutcome::IncorrectlyErrored { errors, syntax },
+            _ => TestRunOutcome::Passed(syntax),
+        }
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct TypeScriptTestSuite;
+
+impl TestSuite for TypeScriptTestSuite {
+    fn name(&self) -> &str {
+        "TS"
+    }
+
+    fn base_path(&self) -> &str {
+        BASE_PATH
+    }
+
+    fn is_test(&self, path: &Path) -> bool {
+        match path.extension() {
+            None => false,
+            Some(ext) => ext == "ts",
+        }
+    }
+
+    fn load_test(&self, path: PathBuf) -> Option<Box<dyn TestCase>> {
+        let code = check_file_encoding(&path)?;
+
+        Some(Box::new(TypeScriptTestCase { path, code }))
+    }
 }
 
 pub fn check_file_encoding(path: &std::path::Path) -> Option<String> {
@@ -46,112 +68,5 @@ pub fn check_file_encoding(path: &std::path::Path) -> Option<String> {
         std::str::from_utf8(buffer.as_slice())
             .ok()
             .map(str::to_string)
-    }
-}
-
-pub fn run_ts(
-    query: Option<&str>,
-    pool: Pool,
-    _json: bool,
-    show_rast: bool,
-    show_diagnostics: bool,
-) {
-    let files = load_ts_files(query);
-
-    let pb = indicatif::ProgressBar::new(files.len() as u64);
-    let msg = format!("{} tests", "Running".bold().cyan());
-    pb.set_position(1);
-    pb.set_message(msg);
-    pb.set_style(super::default_bar_style());
-
-    std::panic::set_hook(Box::new(|_| {}));
-    let (tx, rx) = std::sync::mpsc::channel();
-
-    pool.scoped(|_| {
-        let pb = &pb;
-
-        let is_detailed = files.len() < 10;
-
-        for file in files {
-            let path = file.path();
-            let code = check_file_encoding(path);
-            if code.is_none() {
-                continue;
-            }
-            let code = code.unwrap();
-            let result = std::panic::catch_unwind(|| {
-                let r = parse(&code, 0, Syntax::default().typescript());
-
-                if is_detailed && show_rast {
-                    println!("{:#?}", r.syntax());
-                }
-
-                if is_detailed && show_diagnostics {
-                    let file = rslint_errors::file::SimpleFile::new(
-                        path.display().to_string(),
-                        code.clone(),
-                    );
-                    let mut emitter = rslint_errors::Emitter::new(&file);
-
-                    for diagnostic in r.errors() {
-                        emitter.emit_stdout(diagnostic, true).unwrap();
-                    }
-                }
-
-                r.ok().map(drop)
-            });
-
-            let result = result
-                .map(|res| {
-                    if let Err(errors) = res {
-                        TestResult {
-                            fail: Some(FailReason::IncorrectlyErrored(errors)),
-                            code: code.clone(),
-                            outcome: Outcome::Failed,
-                            path: path.to_path_buf(),
-                        }
-                    } else {
-                        TestResult {
-                            fail: None,
-                            code: code.clone(),
-                            outcome: Outcome::Passed,
-                            path: path.to_path_buf(),
-                        }
-                    }
-                })
-                .unwrap_or_else(|err| TestResult {
-                    fail: Some(FailReason::ParserPanic(err)),
-                    code: code.clone(),
-                    outcome: Outcome::Panicked,
-                    path: path.to_path_buf(),
-                });
-
-            if result.outcome != Outcome::Passed {
-                let filename = path.to_str().unwrap();
-                let reason = match result.outcome {
-                    Outcome::Failed => "incorrectly threw an error",
-                    Outcome::Panicked => "panicked while parsing",
-                    _ => unreachable!(),
-                };
-                let msg = format!("{} '{}' {}", "Test".bold().red(), filename, reason.bold());
-                pb.println(msg);
-            }
-
-            let _ = tx.send(result);
-            pb.inc(1);
-        }
-    });
-
-    drop(tx);
-
-    let mut test_results = TestResults::new();
-    test_results.store_results(rx.into_iter().collect::<Vec<_>>());
-
-    draw_table(&test_results);
-
-    if test_results.summary.passed > 0 {
-        std::process::exit(1);
-    } else {
-        std::process::exit(0);
     }
 }
