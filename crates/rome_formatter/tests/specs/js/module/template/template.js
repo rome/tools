be3ba03879eeaf50fa175da95e5b9b729e478da3
Emanuele Stoppa <my.burning@gmail.com>
2022-01-28 13:51:33 +0000

    case with line breaks and expressions

--- crates/rome_formatter/tests/specs/js/module/template/template.js
+++ crates/rome_formatter/tests/specs/js/module/template/template.js
@@ -4,4 +4,15 @@ tag`something`
 `something ${ " hello" }`;
 
 `something ${ () => { var hey; const looooooooooong_expression = "loooooooooong_expression" }} something else ${ ehy }`;
-    `something ${ () => { var hey; const looooooooooong_expression = "loooooooooong_expression"; return hey; }} something else ${ ehy }`
+    `something ${ () => { var hey; const looooooooooong_expression = "loooooooooong_expression"; return hey; }} something else ${ ehy }`;
+
+
+`test
+  abcd ${input}
+output
+`;
+
+`test
+  abcd ${ () => { var hey; const looooooooooong_expression = "loooooooooong_expression"; return hey; }}
+output
+`;
