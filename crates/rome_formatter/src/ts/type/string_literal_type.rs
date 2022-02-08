f33fcb0226587187f37e095474b3ffa518c0e60d
Emanuele Stoppa <my.burning@gmail.com>
2022-02-08 13:59:31 +0000

    chore: rebase

--- crates/rome_formatter/src/ts/type/string_literal_type.rs
+++
@@ -1,9 +0,0 @@
-use crate::formatter_traits::FormatTokenAndNode;
-use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
-use rslint_parser::ast::TsStringLiteralType;
-
-impl ToFormatElement for TsStringLiteralType {
-    fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        self.literal_token().format(formatter)
-    }
-}
