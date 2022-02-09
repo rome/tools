b465f4105ffd69e04f1d9ad11c37b1f00707005d
Emanuele Stoppa <my.burning@gmail.com>
2022-02-09 09:04:10 +0000

    chore: rebase

--- crates/rome_formatter/tests/spec_tests.rs
+++ crates/rome_formatter/tests/spec_tests.rs
@@ -16,9 +16,4 @@ mod formatter {
         use crate::spec_test;
         tests_macros::gen_tests! {"tests/specs/ts/**/*.ts", spec_test::run, "module"}
     }
-
-    mod ts_module {
-        use crate::spec_test;
-        tests_macros::gen_tests! {"tests/specs/ts/**/*.ts", spec_test::run, "module"}
-    }
 }
