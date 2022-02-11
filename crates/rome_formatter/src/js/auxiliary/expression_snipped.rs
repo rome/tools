70c6e9f6cd1d35bb3e5ad52d988c6c779fd9fcc6
l3ops <leops@users.noreply.github.com>
2022-02-11 14:21:32 +0100

    fix: implement ToFormatElement for JsExpressionSnipped (#2097)

--- crates/rome_formatter/src/js/auxiliary/expression_snipped.rs
+++ crates/rome_formatter/src/js/auxiliary/expression_snipped.rs
@@ -1,7 +1,14 @@
-use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
-use rslint_parser::{ast::JsExpressionSnipped, AstNode};
+use crate::{
+    format_elements, formatter_traits::FormatTokenAndNode, FormatElement, FormatResult, Formatter,
+    ToFormatElement,
+};
+use rslint_parser::ast::JsExpressionSnipped;
+
 impl ToFormatElement for JsExpressionSnipped {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        Ok(formatter.format_verbatim(self.syntax()))
+        Ok(format_elements![
+            self.expression().format(formatter)?,
+            self.eof_token().format(formatter)?,
+        ])
     }
 }
