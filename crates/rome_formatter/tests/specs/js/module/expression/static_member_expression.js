a137f007e3db16fb313765bf4a2c381fbc8fdc31
Emanuele Stoppa <my.burning@gmail.com>
2022-02-15 13:19:39 +0000

    test: add test cases

--- crates/rome_formatter/tests/specs/js/module/expression/static_member_expression.js
+++ crates/rome_formatter/tests/specs/js/module/expression/static_member_expression.js
@@ -4,3 +4,7 @@ a.#b
 a?.#b
 a?.b.#c
 a?.#b.c().d
+
+lorem.ipsum();
+lorem.ipsum().looooooooooooooooooooooooooong().looooooooooooooooooooooooooong().looooooooooooooooooooooooooong();
+lorem()[0]().ipsum().looooooooooooooooooooooooooong().looooooooooooooooooooooooooong().looooooooooooooooooooooooooong();
