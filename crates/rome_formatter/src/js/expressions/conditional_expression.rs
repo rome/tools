7615659c4f70012e686cb1c7cca991dd81c7fc3f
Emanuele Stoppa <my.burning@gmail.com>
2022-03-01 11:59:33 +0000

    chore: use recursion instead checking the parent

--- crates/rome_formatter/src/js/expressions/conditional_expression.rs
+++ crates/rome_formatter/src/js/expressions/conditional_expression.rs
@@ -5,57 +5,79 @@ use crate::{
 };
 use rslint_parser::ast::JsConditionalExpression;
 use rslint_parser::ast::JsConditionalExpressionFields;
-use rslint_parser::{AstNode, JsSyntaxKind};
+use rslint_parser::{AstNode, JsSyntaxKind, SyntaxNode};
 
 impl ToFormatElement for JsConditionalExpression {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        let JsConditionalExpressionFields {
-            test,
-            question_mark_token,
-            consequent,
-            colon_token,
-            alternate,
-        } = self.as_fields();
-
-        let alternate = alternate?;
-        let consequent = consequent?;
-
-        let is_alternate_conditional =
-            alternate.syntax().kind() == JsSyntaxKind::JS_CONDITIONAL_EXPRESSION;
-        let is_consequent_conditional =
-            consequent.syntax().kind() == JsSyntaxKind::JS_CONDITIONAL_EXPRESSION;
-        let parent_is_conditional = self.syntax().parent().map_or(false, |n| {
-            n.kind() == JsSyntaxKind::JS_CONDITIONAL_EXPRESSION
-        });
-
-        let test = test.format(formatter)?;
-        let consequent = format_elements![
-            question_mark_token.format(formatter)?,
-            space_token(),
-            consequent.format(formatter)?,
-        ];
-        let alternate = format_elements![
-            colon_token.format(formatter)?,
-            space_token(),
-            alternate.format(formatter)?
-        ];
-
-        let body = if is_alternate_conditional || is_consequent_conditional || parent_is_conditional
-        {
-            indent(format_elements![
-                hard_line_break(),
-                consequent,
-                hard_line_break(),
-                alternate
-            ])
-        } else {
-            group_elements(format_elements![
-                space_token(),
-                consequent,
-                space_token(),
-                alternate
-            ])
-        };
-        Ok(format_elements![test, space_token(), body])
+        format_conditional(self, formatter, false)
     }
+}
+
+fn format_conditional(
+    current_node: &JsConditionalExpression,
+    formatter: &Formatter,
+    parent_is_conditional: bool,
+) -> FormatResult<FormatElement> {
+    let JsConditionalExpressionFields {
+        test,
+        question_mark_token,
+        consequent,
+        colon_token,
+        alternate,
+    } = current_node.as_fields();
+
+    let alternate = alternate?;
+    let consequent = consequent?;
+
+    let is_alternate_conditional =
+        alternate.syntax().kind() == JsSyntaxKind::JS_CONDITIONAL_EXPRESSION;
+    let is_consequent_conditional =
+        consequent.syntax().kind() == JsSyntaxKind::JS_CONDITIONAL_EXPRESSION;
+
+    let test = test.format(formatter)?;
+    let consequent =
+        if is_consequent_conditional || (parent_is_conditional && is_consequent_conditional) {
+            let consequent = JsConditionalExpression::cast(consequent.syntax().to_owned()).unwrap();
+            let consequent = format_conditional(&consequent, formatter, true)?;
+            format_elements![
+                question_mark_token.format(formatter)?,
+                space_token(),
+                consequent
+            ]
+        } else {
+            format_elements![
+                question_mark_token.format(formatter)?,
+                space_token(),
+                consequent.format(formatter)?
+            ]
+        };
+    let alternate =
+        if is_alternate_conditional || (parent_is_conditional && is_alternate_conditional) {
+            let alternate = JsConditionalExpression::cast(alternate.syntax().to_owned()).unwrap();
+            let alternate = format_conditional(&alternate, formatter, true)?;
+            format_elements![colon_token.format(formatter)?, space_token(), alternate]
+        } else {
+            format_elements![
+                colon_token.format(formatter)?,
+                space_token(),
+                alternate.format(formatter)?
+            ]
+        };
+
+    let body = if is_alternate_conditional || is_consequent_conditional || parent_is_conditional {
+        indent(format_elements![
+            hard_line_break(),
+            consequent,
+            hard_line_break(),
+            alternate
+        ])
+    } else {
+        group_elements(format_elements![
+            space_token(),
+            consequent,
+            space_token(),
+            alternate
+        ])
+    };
+    Ok(format_elements![test, space_token(), body])
 }
--- crates/rome_formatter/src/ts/types/conditional_type.rs
+++ crates/rome_formatter/src/ts/types/conditional_type.rs
@@ -9,62 +9,82 @@ use rslint_parser::{AstNode, JsSyntaxKind};
 
 impl ToFormatElement for TsConditionalType {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        let TsConditionalTypeFields {
-            check_type,
-            extends_token,
-            extends_type,
-            question_mark_token,
-            true_type,
-            colon_token,
-            false_type,
-        } = self.as_fields();
+        format_conditional(self, formatter, false)
+    }
+}
 
-        let true_type = true_type?;
-        let false_type = false_type?;
+fn format_conditional(
+    current_node: &TsConditionalType,
+    formatter: &Formatter,
+    parent_is_conditional: bool,
+) -> FormatResult<FormatElement> {
+    let TsConditionalTypeFields {
+        check_type,
+        extends_token,
+        extends_type,
+        question_mark_token,
+        true_type,
+        colon_token,
+        false_type,
+    } = current_node.as_fields();
 
-        let is_true_type_conditional =
-            true_type.syntax().kind() == JsSyntaxKind::TS_CONDITIONAL_TYPE;
-        let is_false_type_conditional =
-            false_type.syntax().kind() == JsSyntaxKind::TS_CONDITIONAL_TYPE;
-        let parent_is_conditional = self
-            .syntax()
-            .parent()
-            .map_or(false, |n| n.kind() == JsSyntaxKind::TS_CONDITIONAL_TYPE);
+    let true_type = true_type?;
+    let false_type = false_type?;
 
-        let true_type = format_elements![
-            question_mark_token.format(formatter)?,
-            space_token(),
-            true_type.format(formatter)?,
-        ];
-        let false_type = format_elements![
-            colon_token.format(formatter)?,
-            space_token(),
-            false_type.format(formatter)?
-        ];
+    let is_true_type_conditional = true_type.syntax().kind() == JsSyntaxKind::TS_CONDITIONAL_TYPE;
+    let is_false_type_conditional = false_type.syntax().kind() == JsSyntaxKind::TS_CONDITIONAL_TYPE;
 
-        let body = if is_true_type_conditional || is_false_type_conditional || parent_is_conditional
-        {
-            indent(format_elements![
-                hard_line_break(),
-                format_elements![soft_line_break(), true_type, soft_line_break(), false_type]
-            ])
+    let true_type =
+        if is_true_type_conditional || (is_true_type_conditional && parent_is_conditional) {
+            let true_type = TsConditionalType::cast(true_type.syntax().to_owned()).unwrap();
+            let true_type = format_conditional(&true_type, formatter, true)?;
+            format_elements![
+                question_mark_token.format(formatter)?,
+                space_token(),
+                true_type
+            ]
         } else {
-            group_elements(format_elements![
+            format_elements![
+                question_mark_token.format(formatter)?,
                 space_token(),
-                true_type,
-                space_token(),
-                false_type
-            ])
+                true_type.format(formatter)?,
+            ]
         };
 
-        Ok(format_elements![
-            check_type.format(formatter)?,
-            space_token(),
-            extends_token.format(formatter)?,
-            space_token(),
-            extends_type.format(formatter)?,
-            space_token(),
-            body
+    let false_type =
+        if is_false_type_conditional || (is_false_type_conditional && parent_is_conditional) {
+            let false_type = TsConditionalType::cast(false_type.syntax().to_owned()).unwrap();
+            let false_type = format_conditional(&false_type, formatter, true)?;
+            format_elements![colon_token.format(formatter)?, space_token(), false_type]
+        } else {
+            format_elements![
+                colon_token.format(formatter)?,
+                space_token(),
+                false_type.format(formatter)?
+            ]
+        };
+
+    let body = if is_true_type_conditional || is_false_type_conditional || parent_is_conditional {
+        indent(format_elements![
+            hard_line_break(),
+            format_elements![soft_line_break(), true_type, soft_line_break(), false_type]
         ])
-    }
+    } else {
+        group_elements(format_elements![
+            space_token(),
+            true_type,
+            space_token(),
+            false_type
+        ])
+    };
+
+    Ok(format_elements![
+        check_type.format(formatter)?,
+        space_token(),
+        extends_token.format(formatter)?,
+        space_token(),
+        extends_type.format(formatter)?,
+        space_token(),
+        body
+    ])
 }
