2c40540b153a05cc20f733d6024b0a3033c22238
Emanuele Stoppa <my.burning@gmail.com>
2022-03-03 16:52:06 +0000

    refactor: move logic inside the enum

--- crates/rome_formatter/src/utils/format_conditional.rs
+++ crates/rome_formatter/src/utils/format_conditional.rs
@@ -1,21 +1,10 @@
 use crate::formatter_traits::FormatTokenAndNode;
 use crate::{
     format_elements, group_elements, hard_line_break, indent, space_token, FormatElement,
-    FormatResult, Formatter, ToFormatElement,
+    FormatResult, Formatter,
 };
-use rslint_parser::ast::{
-    JsAnyExpression, JsConditionalExpression, JsConditionalExpressionFields, TsConditionalType,
-    TsConditionalTypeFields, TsType,
-};
-use rslint_parser::{AstNode, SyntaxToken};
-
-pub struct FormatConditionalPayload<'f, Node: AstNode + ToFormatElement> {
-    pub question_mark: SyntaxToken,
-    pub colon: SyntaxToken,
-    pub consequent: Node,
-    pub alternate: Node,
-    pub formatter: &'f Formatter,
-}
+use rslint_parser::ast::{JsAnyExpression, JsConditionalExpression, TsConditionalType, TsType};
+use rslint_parser::AstNode;
 
 pub enum Conditional {
     Expression(JsConditionalExpression),
@@ -23,7 +12,7 @@ pub enum Conditional {
 }
 
 impl Conditional {
-    pub fn from_type(ts_type: TsType) -> Option<Self> {
+    fn from_type(ts_type: TsType) -> Option<Self> {
         if let Some(TsType::TsConditionalType(conditional)) = TsType::cast(ts_type.syntax().clone())
         {
             Some(Self::Type(conditional))
@@ -32,13 +21,151 @@ impl Conditional {
         }
     }
 
-    pub fn from_expression(any_expression: JsAnyExpression) -> Option<Self> {
+    fn from_expression(any_expression: JsAnyExpression) -> Option<Self> {
         if let Some(JsAnyExpression::JsConditionalExpression(conditional)) =
             JsAnyExpression::cast(any_expression.syntax().clone())
         {
             Some(Self::Expression(conditional))
         } else {
             None
+        }
+    }
+
+    fn into_format_element(
+        self,
+        formatter: &Formatter,
+        parent_is_conditional: bool,
+    ) -> FormatResult<FormatElement> {
+        let (head, body) = match self {
+            Conditional::Expression(_) => (
+                self.format_head(formatter)?,
+                self.format_body(formatter, parent_is_conditional)?,
+            ),
+            Conditional::Type(_) => (
+                self.format_head(formatter)?,
+                self.format_body(formatter, parent_is_conditional)?,
+            ),
+        };
+
+        Ok(format_elements![head, body])
+    }
+
+    fn format_head(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
+        match self {
+            Conditional::Expression(expr) => Ok(format_elements![
+                expr.test()?.format(formatter)?,
+                space_token(),
+            ]),
+            Conditional::Type(t) => Ok(format_elements![
+                t.check_type()?.format(formatter)?,
+                space_token(),
+                t.extends_token()?.format(formatter)?,
+                space_token(),
+                t.extends_type()?.format(formatter)?,
+                space_token(),
+            ]),
+        }
+    }
+    fn consequent_to_conditional(&self) -> FormatResult<Option<Self>> {
+        match self {
+            Conditional::Expression(conditional_expression) => Ok(Conditional::from_expression(
+                conditional_expression.consequent()?,
+            )),
+            Conditional::Type(conditional_type) => {
+                Ok(Conditional::from_type(conditional_type.true_type()?))
+            }
+        }
+    }
+
+    fn alternate_to_conditional(&self) -> FormatResult<Option<Self>> {
+        match self {
+            Conditional::Expression(conditional_expression) => Ok(Conditional::from_expression(
+                conditional_expression.alternate()?,
+            )),
+            Conditional::Type(conditional_type) => {
+                Ok(Conditional::from_type(conditional_type.false_type()?))
+            }
+        }
+    }
+
+    fn format_body(
+        &self,
+        formatter: &Formatter,
+        parent_is_conditional: bool,
+    ) -> FormatResult<FormatElement> {
+        let mut left_or_right_is_conditional = false;
+        let consequent = self.consequent_to_conditional()?;
+        let alternate = self.alternate_to_conditional()?;
+
+        let consequent = if let Some(consequent) = consequent {
+            left_or_right_is_conditional = true;
+            let consequent = format_conditional(consequent, formatter, true)?;
+            self.format_with_consequent(formatter, Some(consequent))?
+        } else {
+            self.format_with_consequent(formatter, None)?
+        };
+
+        let alternate = if let Some(alternate) = alternate {
+            left_or_right_is_conditional = true;
+            let alternate = format_conditional(alternate, formatter, true)?;
+            self.format_with_alternate(formatter, Some(alternate))?
+        } else {
+            self.format_with_alternate(formatter, None)?
+        };
+
+        let body = if left_or_right_is_conditional || parent_is_conditional {
+            indent(format_elements![
+                hard_line_break(),
+                consequent,
+                hard_line_break(),
+                alternate
+            ])
+        } else {
+            group_elements(format_elements![
+                space_token(),
+                consequent,
+                space_token(),
+                alternate
+            ])
+        };
+        Ok(body)
+    }
+
+    fn format_with_consequent(
+        &self,
+        formatter: &Formatter,
+        consequent: Option<FormatElement>,
+    ) -> FormatResult<FormatElement> {
+        match self {
+            Conditional::Expression(expr) => Ok(format_elements![
+                expr.colon_token().format(formatter)?,
+                space_token(),
+                consequent
+            ]),
+            Conditional::Type(ty) => Ok(format_elements![
+                ty.colon_token().format(formatter)?,
+                space_token(),
+                consequent
+            ]),
+        }
+    }
+
+    fn format_with_alternate(
+        &self,
+        formatter: &Formatter,
+        alternate: Option<FormatElement>,
+    ) -> FormatResult<FormatElement> {
+        match self {
+            Conditional::Expression(expr) => Ok(format_elements![
+                expr.colon_token().format(formatter)?,
+                space_token(),
+                alternate
+            ]),
+            Conditional::Type(ty) => Ok(format_elements![
+                ty.colon_token().format(formatter)?,
+                space_token(),
+                alternate
+            ]),
         }
     }
 }
@@ -55,126 +182,5 @@ pub fn format_conditional(
     formatter: &Formatter,
     parent_is_conditional: bool,
 ) -> FormatResult<FormatElement> {
-    let (head, body) = match conditional {
-        Conditional::Expression(conditional_expression) => {
-            let JsConditionalExpressionFields {
-                consequent,
-                colon_token,
-                test,
-                alternate,
-                question_mark_token,
-            } = conditional_expression.as_fields();
-
-            (
-                format_elements![test.format(formatter)?, space_token(),],
-                format_conditional_body(
-                    FormatConditionalPayload {
-                        colon: colon_token?,
-                        question_mark: question_mark_token?,
-                        consequent: consequent?,
-                        alternate: alternate?,
-                        formatter,
-                    },
-                    parent_is_conditional,
-                    |node| node.and_then(Conditional::from_expression),
-                )?,
-            )
-        }
-        Conditional::Type(conditional_type) => {
-            let TsConditionalTypeFields {
-                check_type,
-                extends_token,
-                extends_type,
-                question_mark_token,
-                true_type,
-                colon_token,
-                false_type,
-            } = conditional_type.as_fields();
-
-            (
-                format_elements![
-                    check_type.format(formatter)?,
-                    space_token(),
-                    extends_token.format(formatter)?,
-                    space_token(),
-                    extends_type.format(formatter)?,
-                    space_token(),
-                ],
-                format_conditional_body(
-                    FormatConditionalPayload {
-                        colon: colon_token?,
-                        question_mark: question_mark_token?,
-                        consequent: true_type?,
-                        alternate: false_type?,
-                        formatter,
-                    },
-                    parent_is_conditional,
-                    |node| node.and_then(Conditional::from_type),
-                )?,
-            )
-        }
-    };
-
-    Ok(format_elements![head, body])
-}
-
-fn format_conditional_body<Node: AstNode + ToFormatElement, ToConditional>(
-    payload: FormatConditionalPayload<Node>,
-    parent_is_conditional: bool,
-    to_conditional: ToConditional,
-) -> FormatResult<FormatElement>
-where
-    ToConditional: Fn(Option<Node>) -> Option<Conditional>,
-{
-    let FormatConditionalPayload {
-        colon,
-        question_mark,
-        formatter,
-        consequent,
-        alternate,
-    } = payload;
-
-    let mut left_or_right_is_conditional = false;
-    let conditional = Node::cast(consequent.syntax().clone());
-    let consequent = if let Some(consequent) = to_conditional(conditional) {
-        left_or_right_is_conditional = true;
-        let consequent = format_conditional(consequent, formatter, true)?;
-        format_elements![question_mark.format(formatter)?, space_token(), consequent]
-    } else {
-        format_elements![
-            question_mark.format(formatter)?,
-            space_token(),
-            consequent.format(formatter)?
-        ]
-    };
-
-    let conditional = Node::cast(alternate.syntax().clone());
-    let alternate = if let Some(alternate) = to_conditional(conditional) {
-        left_or_right_is_conditional = true;
-        let alternate = format_conditional(alternate, formatter, true)?;
-        format_elements![colon.format(formatter)?, space_token(), alternate]
-    } else {
-        format_elements![
-            colon.format(formatter)?,
-            space_token(),
-            alternate.format(formatter)?
-        ]
-    };
-
-    let body = if left_or_right_is_conditional || parent_is_conditional {
-        indent(format_elements![
-            hard_line_break(),
-            consequent,
-            hard_line_break(),
-            alternate
-        ])
-    } else {
-        group_elements(format_elements![
-            space_token(),
-            consequent,
-            space_token(),
-            alternate
-        ])
-    };
-    Ok(body)
+    conditional.into_format_element(formatter, parent_is_conditional)
 }
