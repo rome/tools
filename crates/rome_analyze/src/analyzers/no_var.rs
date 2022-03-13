a1e5f0e1b95f064c7d8b72ce615adbc1d73fccc2
Akshay <nerdy@peppe.rs>
2022-03-13 18:07:58 +0530

    refactor(rome_analyze): remove dependency on once_cell
    By using `const` over lazily built statics, we can remove the dependency
    on `once_cell`.

--- crates/rome_analyze/src/analyzers/no_var.rs
+++ crates/rome_analyze/src/analyzers/no_var.rs
@@ -1,13 +1,11 @@
 use crate::{signals::DiagnosticExt, Analysis, Analyzer, AnalyzerContext};
 use rome_js_syntax::{AstNode, JsVariableDeclaration};
 
-pub fn create() -> Analyzer {
-    Analyzer {
-        name: "noVar",
-        action_categories: vec![],
-        analyze,
-    }
-}
+pub const ANALYZER: Analyzer = Analyzer {
+    name: "noVar",
+    action_categories: &[],
+    analyze,
+};
 
 fn analyze(ctx: &AnalyzerContext) -> Option<Analysis> {
     ctx.query_nodes::<JsVariableDeclaration>()
