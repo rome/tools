b162af23fafed971daf8a3acfe52b17b2f98cb63
Emanuele Stoppa <my.burning@gmail.com>
2022-03-23 16:59:50 +0000

    refactor(rome_formatter): use `prelude` terminology (#2291)
    * refactor(rome_formatter): use `prelude` terminology
    
    * refactor(rome_formatter): use `prelude` terminology

---
+++ crates/rome_formatter/src/prelude.rs
@@ -0,0 +1,6 @@
+//! This module provides important and useful traits to help to format tokens and nodes
+//! when implementing the [crate::ToFormatElement] trait.
+
+pub use crate::formatter_traits::{
+    FormatOptionalTokenAndNode as _, FormatTokenAndNode as _, IntoFormatResult as _,
+};
