366dd2ac53639e82bc72a4dc611b2d6e095c326d
Emanuele Stoppa <my.burning@gmail.com>
2022-03-24 13:28:37 +0000

    chore: rebase

--- crates/rome_formatter/src/jsx/any/child.rs
+++ crates/rome_formatter/src/jsx/any/child.rs
@@ -1,15 +1,15 @@
 //! Generated file, do not edit by hand, see `xtask/codegen`
 
-use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
+use crate::{prelude::*, FormatElement, FormatResult, Formatter, ToFormatElement};
 use rome_js_syntax::JsxAnyChild;
 impl ToFormatElement for JsxAnyChild {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
         match self {
-            Self::JsxElement(node) => node.to_format_element(formatter),
-            Self::JsxSelfClosingElement(node) => node.to_format_element(formatter),
-            Self::JsxText(node) => node.to_format_element(formatter),
-            Self::JsxExpressionChild(node) => node.to_format_element(formatter),
-            Self::JsxSpreadChild(node) => node.to_format_element(formatter),
+            Self::JsxElement(node) => node.format(formatter),
+            Self::JsxSelfClosingElement(node) => node.format(formatter),
+            Self::JsxText(node) => node.format(formatter),
+            Self::JsxExpressionChild(node) => node.format(formatter),
+            Self::JsxSpreadChild(node) => node.format(formatter),
         }
     }
 }
