def62650a7573f91307370253c57bc48c866792c
Micha Reiser <micha@rome.tools>
2022-03-24 11:15:14 +0100

    fix(rome_formatter): Don't remove parenthesis from member assignments
    ## Summary
    
    I played around with our formatter and noticed that formatting the benchmark test files twice results in the following parse errors:
    
    ```
    error[SyntaxError]: Invalid assignment to `s || (s = Object.create(null))[i]`
         ┌─ vue.global.prod.js:4372:44
         │
    4372 │                             this._props[i] = Z(this._props[i]), s || (s = Object.create(null))[i] =
         │                                                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ This expression cannot be assigned to
    ```
    
    The issue is that the formatter removes the parenthesis around the `object` of a static or computed member assignment.
    
    This PR ensures that parenthesis around removed from the left-hand sie of a member assignment which is in line with what the formatter does for expressions.

--- crates/rome_formatter/tests/specs/js/module/assignment/assignment.js
+++ crates/rome_formatter/tests/specs/js/module/assignment/assignment.js
@@ -20,4 +20,7 @@ a[ b ]  =  c[ d ]
 ;[a, b = "b", ...c] = d
 ;[fooooooooooooooooooooooooooooooooooooooooooooooooo, barrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr, bazzzzzzzzzzzzzzzzzzzzzzzzzz] = d
 ;({a,b=c,d:e,f:g=h,...j} = x)
-;({aaaaaaaaaa,bbbbbbbbbb=cccccccccc,dddddddddd:eeeeeeeeee,ffffffffff:gggggggggg=hhhhhhhhhh,...jjjjjjjjjj} = x)
+;({aaaaaaaaaa,bbbbbbbbbb=cccccccccc,dddddddddd:eeeeeeeeee,ffffffffff:gggggggggg=hhhhhhhhhh,...jjjjjjjjjj} = x);
+
+(s||(s=Object.create(null)))[i]=!0;
+(s||(s=Object.create(null))).test=!0;
