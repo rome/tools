a96b3d9d600a436155259ff8dc341a7b15d89985
l3ops <leops@users.noreply.github.com>
2022-03-31 16:55:47 +0200

    feat(npm): add a postinstall script to the cli package (#2333)
    * feat(npm) add a postinstall script to the cli package
    
    * use the `node:` prefix

---
+++ npm/rome/scripts/postinstall.js
@@ -0,0 +1,44 @@
+const { platform, arch } = process;
+
+const PLATFORMS = {
+	win32: {
+		x64: "@rometools/cli-win32-x64/rome.exe",
+		arm64: "@rometools/cli-win32-arm64/rome.exe",
+	},
+	darwin: {
+		x64: "@rometools/cli-darwin-x64/rome",
+		arm64: "@rometools/cli-darwin-arm64/rome",
+	},
+	linux: {
+		x64: "@rometools/cli-linux-x64/rome",
+		arm64: "@rometools/cli-linux-arm64/rome",
+	},
+};
+
+const binName = PLATFORMS?.[platform]?.[arch];
+if (binName) {
+	let binPath;
+	try {
+		binPath = require.resolve(binName);
+	} catch (err) {
+		console.warn(
+			`The Rome CLI postinstall script failed to resolve the binary file "${binName}". ` + "Running Rome from the npm package will probably not work correctly.",
+		);
+	}
+
+	if (binPath) {
+		try {
+			require("node:fs").chmodSync(binPath, 0o755);
+		} catch (err) {
+			console.warn(
+				"The Rome CLI postinstall script failed to set execution permissions to the native binary. " + "Running Rome from the npm package will probably not work correctly.",
+			);
+		}
+	}
+} else {
+	console.warn(
+		"The Rome CLI package doesn't ship with prebuilt binaries for your platform yet. " +
+			"You can still use the CLI by cloning the rome/tools repo from GitHub, " +
+			"and follow the instructions there to build the CLI for your platform.",
+	);
+}
