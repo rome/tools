dd582a627c7eec6537a22f7b803a9640124975f3
l3ops <leo@rome.tools>
2022-03-31 11:18:08 +0200

    implement the rendering of unified diffs

--- crates/rome_console/src/diff.rs
+++ crates/rome_console/src/diff.rs
@@ -8,8 +8,69 @@ use crate::{
     markup,
 };
 
+/// Determines how a diff should be printed in the console
+pub enum DiffMode {
+    /// Render the diff in a single column by stacking the changes
+    ///
+    /// # Example
+    /// ```text
+    ///       | @@ -3,10 +3,8 @@
+    ///  2  2 |   dolor
+    ///  3  3 |   sit
+    ///  4  4 |   amet,
+    ///  5    | - function
+    ///  6    | - name(
+    ///  7    | -     args
+    ///  8    | - ) {}
+    ///     5 | + function name(args) {
+    ///     6 | + }
+    ///  9  7 |   consectetur
+    /// 10  8 |   adipiscing
+    /// 11  9 |   elit,
+    ///       | @@ -15,7 +13,5 @@
+    /// 14 12 |   eiusmod
+    /// 15 13 |   
+    /// 16 14 |   incididunt
+    /// 17    | - function
+    /// 18    | - name(
+    /// 19    | -     args
+    /// 20    | - ) {}
+    ///    15 | + function name(args) {
+    ///    16 | + }
+    /// ```
+    Unified,
+    /// Render the diff over two columns, with the old text on the
+    /// left and the new text on the right
+    ///
+    /// # Example
+    /// ```text
+    /// @@ -3,10 +3,8 @@
+    ///  3   dolor                 3   dolor
+    ///  4   sit                   4   sit
+    ///  5   amet,                 5   amet,
+    ///  6 - function              6 + function name(args) {
+    ///  7 - name(                 7 + }
+    ///  8 -     args
+    ///  9 - ) {}
+    /// 10   consectetur           8   consectetur
+    /// 11   adipiscing            9   adipiscing
+    /// 12   elit,                10   elit,
+    /// @@ -15,7 +13,5 @@
+    /// 15   eiusmod              13   eiusmod
+    /// 16                        14
+    /// 17   incididunt           15   incididunt
+    /// 18 - function             16 + function name(args) {
+    /// 19 - name(                17 + }
+    /// 20 -     args
+    /// 21 - ) {}
+    /// ```
+    Split,
+}
+
 /// Utility struct to print a diff between two texts in the console
 pub struct Diff<'a> {
+    /// Rendering mode for this diff
+    pub mode: DiffMode,
     /// The previous version of the text
     pub left: &'a str,
     /// The next version of the text
@@ -57,110 +118,160 @@ impl<'a> Display for Diff<'a> {
                 None => continue,
             };
 
-            // Print out the hunk header
-            fmt.write_markup(markup! {
-                <Info>{hunk.header()}</Info>"\n"
-            })?;
-
-            // Buffer the left and right sides to keep them aligned
-            let mut left = Vec::new();
-            let mut right = Vec::new();
-
-            for change in hunk.iter_changes() {
-                match change.tag() {
-                    ChangeTag::Delete => {
-                        left.push(Some(change.value().trim_end()));
-                    }
-                    ChangeTag::Insert => {
-                        right.push(Some(change.value().trim_end()));
-                    }
-                    ChangeTag::Equal => {
-                        let position = left.len().max(right.len());
-
-                        // Ensure the two buffers are at the same position
-                        // by padding them with empty lines
-                        if left.len() < position {
-                            left.resize(position, None);
-                        }
-                        if right.len() < position {
-                            right.resize(position, None);
-                        }
-
-                        left.push(Some(change.value().trim_end()));
-                        right.push(Some(change.value().trim_end()));
-                    }
-                }
-            }
-
-            // Print the two buffers in lockstep, specifically does *not* use
-            // Iterator::zip since that would short-circuit once any of the two
-            // iterators returned None while we want to fully consume both
-            let mut left = left.into_iter();
-            let mut right = right.into_iter();
-
-            loop {
-                let (left, right) = match (left.next(), right.next()) {
-                    // Stop once both iterators return None
-                    (None, None) => break,
-                    (left, right) => (left.flatten(), right.flatten()),
-                };
-
-                // In the future this could be a per-character diff to help
-                // make small changes stand out
-                let is_same = left == right;
-
-                if let Some(left) = left {
-                    // Print the left line number and increment it
+            match &self.mode {
+                DiffMode::Unified => {
+                    // Print out the hunk header
                     fmt.write_markup(markup! {
-                        <Warn>
-                            {format_args!("{: >1$} ", left_line, max_digits)}
-                        </Warn>
+                        {" ".repeat(max_digits * 2)}
+                        "  | "<Info>{hunk.header()}</Info>"\n"
                     })?;
 
-                    left_line += 1;
+                    for change in hunk.iter_changes() {
+                        if let Some(old_index) = change.old_index() {
+                            fmt.write_fmt(format_args!("{: >1$}", old_index, max_digits))?;
+                        } else {
+                            fmt.write_str(&" ".repeat(max_digits))?;
+                        }
 
-                    // Don't print any padding space if the right side is empty anyway
-                    let padding = if right.is_some() { max_line_length } else { 0 };
+                        fmt.write_str(" ")?;
 
-                    // Print the left side in red if the sides are different,
-                    // use the standard text color otherwise
-                    if !is_same {
-                        fmt.write_markup(markup! {
-                            <Error>{format_args!("- {left: <0$}", padding)}</Error>
-                        })?;
-                    } else {
-                        fmt.write_fmt(format_args!("  {left: <0$}", padding))?;
+                        if let Some(new_index) = change.new_index() {
+                            fmt.write_fmt(format_args!("{: >1$}", new_index, max_digits))?;
+                        } else {
+                            fmt.write_str(&" ".repeat(max_digits))?;
+                        }
+
+                        fmt.write_str(" | ")?;
+
+                        match change.tag() {
+                            ChangeTag::Delete => {
+                                fmt.write_markup(markup! {
+                                    <Error>"- "{change.value()}</Error>
+                                })?;
+                            }
+                            ChangeTag::Insert => {
+                                fmt.write_markup(markup! {
+                                    <Success>"+ "{change.value()}</Success>
+                                })?;
+                            }
+                            ChangeTag::Equal => {
+                                fmt.write_str("  ")?;
+                                fmt.write_str(change.value())?;
+                            }
+                        }
+
+                        if change.missing_newline() {
+                            writeln!(fmt)?;
+                        }
                     }
-                } else if right.is_some() {
-                    // If the left side is empty but the right side isn't,
-                    // print some padding spaces to align the columns
-                    fmt.write_str(&" ".repeat(max_digits + 3 + max_line_length))?;
                 }
-
-                if let Some(right) = right {
-                    // Print the right line number and increment it
+                DiffMode::Split => {
+                    // Print out the hunk header
                     fmt.write_markup(markup! {
-                        <Warn>
-                            {format_args!("{: >1$} ", right_line, max_digits)}
-                        </Warn>
+                        <Info>{hunk.header()}</Info>"\n"
                     })?;
 
-                    right_line += 1;
+                    // Buffer the left and right sides to keep them aligned
+                    let mut left = Vec::new();
+                    let mut right = Vec::new();
 
-                    // Print the right side in green if the sides are different,
-                    // use the standard text color otherwise
-                    if !is_same {
-                        fmt.write_markup(markup! {
-                            <Success>"+ "{right}</Success>
-                        })?;
-                    } else if !right.is_empty() {
-                        fmt.write_str("  ")?;
-                        fmt.write_str(right)?;
+                    for change in hunk.iter_changes() {
+                        match change.tag() {
+                            ChangeTag::Delete => {
+                                left.push(Some(change.value().trim_end()));
+                            }
+                            ChangeTag::Insert => {
+                                right.push(Some(change.value().trim_end()));
+                            }
+                            ChangeTag::Equal => {
+                                let position = left.len().max(right.len());
+
+                                // Ensure the two buffers are at the same position
+                                // by padding them with empty lines
+                                if left.len() < position {
+                                    left.resize(position, None);
+                                }
+                                if right.len() < position {
+                                    right.resize(position, None);
+                                }
+
+                                left.push(Some(change.value().trim_end()));
+                                right.push(Some(change.value().trim_end()));
+                            }
+                        }
+                    }
+
+                    // Print the two buffers in lockstep, specifically does *not* use
+                    // Iterator::zip since that would short-circuit once any of the two
+                    // iterators returned None while we want to fully consume both
+                    let mut left = left.into_iter();
+                    let mut right = right.into_iter();
+
+                    loop {
+                        let (left, right) = match (left.next(), right.next()) {
+                            // Stop once both iterators return None
+                            (None, None) => break,
+                            (left, right) => (left.flatten(), right.flatten()),
+                        };
+
+                        // In the future this could be a per-character diff to help
+                        // make small changes stand out
+                        let is_same = left == right;
+
+                        if let Some(left) = left {
+                            // Print the left line number and increment it
+                            fmt.write_markup(markup! {
+                                <Warn>
+                                    {format_args!("{: >1$} ", left_line, max_digits)}
+                                </Warn>
+                            })?;
+
+                            left_line += 1;
+
+                            // Don't print any padding space if the right side is empty anyway
+                            let padding = if right.is_some() { max_line_length } else { 0 };
+
+                            // Print the left side in red if the sides are different,
+                            // use the standard text color otherwise
+                            if !is_same {
+                                fmt.write_markup(markup! {
+                                    <Error>{format_args!("- {left: <0$}", padding)}</Error>
+                                })?;
+                            } else {
+                                fmt.write_fmt(format_args!("  {left: <0$}", padding))?;
+                            }
+                        } else if right.is_some() {
+                            // If the left side is empty but the right side isn't,
+                            // print some padding spaces to align the columns
+                            fmt.write_str(&" ".repeat(max_digits + 3 + max_line_length))?;
+                        }
+
+                        if let Some(right) = right {
+                            // Print the right line number and increment it
+                            fmt.write_markup(markup! {
+                                <Warn>
+                                    {format_args!("{: >1$} ", right_line, max_digits)}
+                                </Warn>
+                            })?;
+
+                            right_line += 1;
+
+                            // Print the right side in green if the sides are different,
+                            // use the standard text color otherwise
+                            if !is_same {
+                                fmt.write_markup(markup! {
+                                    <Success>"+ "{right}</Success>
+                                })?;
+                            } else if !right.is_empty() {
+                                fmt.write_str("  ")?;
+                                fmt.write_str(right)?;
+                            }
+                        }
+
+                        // Print a line break
+                        writeln!(fmt)?;
                     }
                 }
-
-                // Print a line break
-                writeln!(fmt)?;
             }
         }
 
@@ -186,11 +297,13 @@ fn count_digits(mut value: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::{self as rome_console, diff::Diff, markup, BufferConsole, Console, Message};
+    use crate::{
+        self as rome_console,
+        diff::{Diff, DiffMode},
+        markup, BufferConsole, Console, Message,
+    };
 
-    #[test]
-    fn test_diff() {
-        const SOURCE_LEFT: &str = "Lorem
+    const SOURCE_LEFT: &str = "Lorem
 ipsum
 dolor
 sit
@@ -212,7 +325,7 @@ name(
     args
 ) {}";
 
-        const SOURCE_RIGHT: &str = "Lorem
+    const SOURCE_RIGHT: &str = "Lorem
 ipsum
 dolor
 sit
@@ -230,7 +343,32 @@ incididunt
 function name(args) {
 }";
 
-        const DIFF_RESULT: &str = "@@ -3,10 +3,8 @@
+    const UNIFIED_RESULT: &str = "      | @@ -3,10 +3,8 @@
+ 2  2 |   dolor
+ 3  3 |   sit
+ 4  4 |   amet,
+ 5    | - function
+ 6    | - name(
+ 7    | -     args
+ 8    | - ) {}
+    5 | + function name(args) {
+    6 | + }
+ 9  7 |   consectetur
+10  8 |   adipiscing
+11  9 |   elit,
+      | @@ -15,7 +13,5 @@
+14 12 |   eiusmod
+15 13 |   
+16 14 |   incididunt
+17    | - function
+18    | - name(
+19    | -     args
+20    | - ) {}
+   15 | + function name(args) {
+   16 | + }
+";
+
+    const SPLIT_RESULT: &str = "@@ -3,10 +3,8 @@
  3   dolor                 3   dolor
  4   sit                   4   sit
  5   amet,                 5   amet,
@@ -251,7 +389,10 @@ function name(args) {
 21 - ) {}
 ";
 
+    #[test]
+    fn test_diff_unified() {
         let diff = Diff {
+            mode: DiffMode::Unified,
             left: SOURCE_LEFT,
             right: SOURCE_RIGHT,
         };
@@ -267,7 +408,31 @@ function name(args) {
             other => panic!("unexpected message {other:?}"),
         };
 
-        assert_eq!(message, DIFF_RESULT);
+        assert_eq!(message, UNIFIED_RESULT);
+
+        assert!(messages.next().is_none());
+    }
+
+    #[test]
+    fn test_diff_split() {
+        let diff = Diff {
+            mode: DiffMode::Split,
+            left: SOURCE_LEFT,
+            right: SOURCE_RIGHT,
+        };
+
+        let mut console = BufferConsole::default();
+        console.message(markup! {
+            {diff}
+        });
+
+        let mut messages = console.buffer.into_iter();
+        let message = match messages.next() {
+            Some(Message::Message(msg)) => msg,
+            other => panic!("unexpected message {other:?}"),
+        };
+
+        assert_eq!(message, SPLIT_RESULT);
 
         assert!(messages.next().is_none());
     }
