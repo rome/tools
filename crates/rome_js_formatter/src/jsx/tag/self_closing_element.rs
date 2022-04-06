659bbd373448f3503d9df838b4d1b37ad7e73e2a
Nicholas Yang <nicholas@rome.tools>
2022-04-06 14:51:11 -0400

    Switched to using `format_or_empty`

--- crates/rome_js_formatter/src/jsx/tag/self_closing_element.rs
+++ crates/rome_js_formatter/src/jsx/tag/self_closing_element.rs
@@ -1,10 +1,9 @@
-use crate::formatter_traits::FormatTokenAndNode;
+use crate::formatter_traits::{FormatOptionalTokenAndNode, FormatTokenAndNode};
 use crate::group_elements;
 use crate::{
     format_elements, soft_block_indent, soft_line_break_or_space, space_token, FormatElement,
     FormatResult, Formatter, ToFormatElement,
 };
-use rome_formatter::empty_element;
 use rome_js_syntax::JsxSelfClosingElement;
 
 impl ToFormatElement for JsxSelfClosingElement {
@@ -15,11 +14,7 @@ impl ToFormatElement for JsxSelfClosingElement {
             attributes.push(soft_line_break_or_space());
         }
 
-        let type_arguments = self
-            .type_arguments()
-            .map(|arg| arg.format(formatter))
-            .transpose()?
-            .unwrap_or_else(empty_element);
+        let type_arguments = self.type_arguments().format_or_empty(formatter)?;
 
         Ok(format_elements![
             self.l_angle_token().format(formatter)?,
