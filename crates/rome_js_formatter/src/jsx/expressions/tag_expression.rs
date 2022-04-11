d288a56e004457ec1143ed5df369d38ea1b4169b
Nicholas Yang <nicholas@rome.tools>
2022-04-11 16:11:09 -0400

    feature: Added formatting for self-closing JSX element

--- crates/rome_js_formatter/src/jsx/expressions/tag_expression.rs
+++ crates/rome_js_formatter/src/jsx/expressions/tag_expression.rs
@@ -1,9 +1,9 @@
+use crate::formatter_traits::FormatTokenAndNode;
 use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
 use rome_js_syntax::JsxTagExpression;
-use rome_rowan::AstNode;
 
 impl ToFormatElement for JsxTagExpression {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        Ok(formatter.format_verbatim(self.syntax()))
+        self.tag().format(formatter)
     }
 }
--- crates/rome_js_formatter/src/jsx/tag/self_closing_element.rs
+++ crates/rome_js_formatter/src/jsx/tag/self_closing_element.rs
@@ -1,9 +1,25 @@
-use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
+use crate::formatter_traits::FormatTokenAndNode;
+use crate::group_elements;
+use crate::{
+    format_elements, soft_block_indent, soft_line_break_or_space, FormatElement, FormatResult,
+    Formatter, ToFormatElement,
+};
 use rome_js_syntax::JsxSelfClosingElement;
-use rome_rowan::AstNode;
 
 impl ToFormatElement for JsxSelfClosingElement {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        Ok(formatter.format_verbatim(self.syntax()))
+        let mut attributes = Vec::new();
+        for attribute in self.attributes() {
+            attributes.push(attribute.format(formatter)?);
+            attributes.push(soft_line_break_or_space());
+        }
+
+        Ok(format_elements![
+            self.l_angle_token().format(formatter)?,
+            self.name().format(formatter)?,
+            group_elements(soft_block_indent(concat_elements(attributes))),
+            self.slash_token().format(formatter)?,
+            self.r_angle_token().format(formatter)?
+        ])
     }
 }
