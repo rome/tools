e4197d756fc82434ee1168ac7179b5dc270e484a
Nicholas Yang <nicholas@rome.tools>
2022-04-11 15:33:14 -0400

    Added jsx name formatting

--- crates/rome_js_formatter/src/jsx/auxiliary/name.rs
+++ crates/rome_js_formatter/src/jsx/auxiliary/name.rs
@@ -1,9 +1,9 @@
+use crate::formatter_traits::FormatTokenAndNode;
 use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
 use rome_js_syntax::JsxName;
-use rome_rowan::AstNode;
 
 impl ToFormatElement for JsxName {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        Ok(formatter.format_verbatim(self.syntax()))
+        self.value_token().format(formatter)
     }
 }
--- crates/rome_js_formatter/src/jsx/objects/member_name.rs
+++ crates/rome_js_formatter/src/jsx/objects/member_name.rs
@@ -1,9 +1,20 @@
+use crate::formatter_traits::FormatTokenAndNode;
 use crate::{FormatElement, FormatResult, Formatter, ToFormatElement};
-use rome_js_syntax::JsxMemberName;
-use rome_rowan::AstNode;
+use rome_formatter::format_elements;
+use rome_js_syntax::{JsxMemberName, JsxMemberNameFields};
 
 impl ToFormatElement for JsxMemberName {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        Ok(formatter.format_verbatim(self.syntax()))
+        let JsxMemberNameFields {
+            object,
+            dot_token,
+            member,
+        } = self.as_fields();
+
+        Ok(format_elements![
+            object.format(formatter)?,
+            dot_token.format(formatter)?,
+            member.format(formatter)?,
+        ])
     }
 }
