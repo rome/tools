7eb3e9fdba1d533a400e51eee3ac55990008d823
Nicholas Yang <nicholas@rome.tools>
2022-04-12 10:44:16 -0400

    fix: switched to join_elements

--- crates/rome_js_formatter/src/jsx/tag/self_closing_element.rs
+++ crates/rome_js_formatter/src/jsx/tag/self_closing_element.rs
@@ -4,15 +4,15 @@ use crate::{
     format_elements, soft_block_indent, soft_line_break_or_space, space_token, FormatElement,
     FormatResult, Formatter, ToFormatElement,
 };
+use rome_formatter::join_elements;
 use rome_js_syntax::JsxSelfClosingElement;
 
 impl ToFormatElement for JsxSelfClosingElement {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        let mut attributes = Vec::new();
-        for attribute in self.attributes() {
-            attributes.push(attribute.format(formatter)?);
-            attributes.push(soft_line_break_or_space());
-        }
+        let attributes = join_elements(
+            soft_line_break_or_space(),
+            formatter.format_nodes(self.attributes())?,
+        );
 
         let type_arguments = self.type_arguments().format_or_empty(formatter)?;
 
@@ -21,7 +21,8 @@ impl ToFormatElement for JsxSelfClosingElement {
             self.name().format(formatter)?,
             type_arguments,
             space_token(),
-            group_elements(soft_block_indent(concat_elements(attributes))),
+            group_elements(soft_block_indent(attributes)),
+            space_token(),
             self.slash_token().format(formatter)?,
             self.r_angle_token().format(formatter)?
         ])
