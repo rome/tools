403036ae7929a30e8fce97405c52b52abe4dbe24
Nicholas Yang <nicholas@rome.tools>
2022-04-13 15:36:42 -0400

    Update crates/rome_js_formatter/src/jsx/attribute/attribute.rs
    Co-authored-by: Micha Reiser <micha@rome.tools>

--- crates/rome_js_formatter/src/jsx/attribute/attribute.rs
+++ crates/rome_js_formatter/src/jsx/attribute/attribute.rs
@@ -6,13 +6,9 @@ impl ToFormatElement for JsxAttribute {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
         let JsxAttributeFields { name, initializer } = self.as_fields();
 
-        if let Some(initializer) = initializer {
-            Ok(format_elements![
-                name.format(formatter)?,
-                initializer.format(formatter)?
-            ])
-        } else {
-            Ok(format_elements![name.format(formatter)?])
-        }
+        Ok(format_elements![
+          name.format(formatter)?,
+          initializer.format_or_empty(formatter)?
+        ])
     }
 }
