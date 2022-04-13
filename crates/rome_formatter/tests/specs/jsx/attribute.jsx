42d5d691df5ddb98ec8d1302f663e52de30febf8
Nicholas Yang <nicholas@rome.tools>
2022-04-13 13:41:04 -0400

    chore: removed old test file

--- crates/rome_formatter/tests/specs/jsx/attribute.jsx
+++
@@ -1,5 +0,0 @@
-<Foo bar={10} />
-
-<GoogleSignInFormPage handleSubmit={(event, callback, callbackHandler, callbackHandlerHandler) => {
-	callbackHandlerHandler(callbackHandler(callback(event)))
-}} />
