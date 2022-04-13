d88d6b3ca19fc3057f56ac14f55b26f6f0a3de63
Emanuele Stoppa <my.burning@gmail.com>
2022-04-13 17:29:54 +0100

    fix: restore assignment chain

--- crates/rome_js_formatter/src/js/expressions/assignment_expression.rs
+++ crates/rome_js_formatter/src/js/expressions/assignment_expression.rs
@@ -3,9 +3,8 @@ use crate::{
     format_elements, group_elements, space_token, FormatElement, FormatResult, Formatter,
     ToFormatElement,
 };
-
-use rome_js_syntax::JsAssignmentExpression;
-use rome_js_syntax::JsAssignmentExpressionFields;
+use rome_formatter::soft_line_indent_or_space;
+use rome_js_syntax::{JsAnyExpression, JsAssignmentExpression, JsAssignmentExpressionFields};
 
 impl ToFormatElement for JsAssignmentExpression {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
@@ -15,11 +14,19 @@ impl ToFormatElement for JsAssignmentExpression {
             right,
         } = self.as_fields();
 
+        let right = right?;
+
+        let right = if matches!(right, JsAnyExpression::JsAssignmentExpression(_)) {
+            soft_line_indent_or_space(right.format(formatter)?)
+        } else {
+            format_elements![space_token(), right.format(formatter)?]
+        };
+
         Ok(group_elements(format_elements![
             left.format(formatter)?,
             space_token(),
             operator_token.format(formatter)?,
-            group_elements(format_elements![space_token(), right.format(formatter)?]),
+            group_elements(right),
         ]))
     }
 }
