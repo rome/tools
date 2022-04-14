5c5c70c068f6af93c922be76af5879e012f458bf
Nicholas Yang <nicholas@rome.tools>
2022-04-14 15:09:45 -0400

    Modified styling for settings. Changed to two row layout with a one column layout on mobile

--- website/playground/src/QuoteStyleSelect.tsx
+++ website/playground/src/QuoteStyleSelect.tsx
@@ -1,21 +1,21 @@
 import { QuoteStyle } from "./types";
 
 interface Props {
-	setQuoteStyle: (v: QuoteStyle) => void,
-	quoteStyle: QuoteStyle,
+	setQuoteStyle: (v: QuoteStyle) => void;
+	quoteStyle: QuoteStyle;
 }
 
 export default function QuoteStyleSelect({ setQuoteStyle, quoteStyle }: Props) {
 	return (
 		<div className="pl-5">
-			<fieldset className="space-y-5">
+			<fieldset>
 				<legend className="sr-only">File Type</legend>
-
 				<div className="relative flex items-start">
 					<div className="">
 						<label
 							htmlFor="quoteStyle"
-							className="block text-sm font-medium text-gray-700">
+							className="block text-sm font-medium text-gray-700"
+						>
 							Quote type:
 						</label>
 						<span id="quote-type-description" className="text-gray-500">
@@ -25,9 +25,10 @@ export default function QuoteStyleSelect({ setQuoteStyle, quoteStyle }: Props) {
 							id="quoteStyle"
 							aria-describedby="quote-type-description"
 							name="quoteStyle"
-							value={quoteStyle ?? ''}
+							value={quoteStyle ?? ""}
 							onChange={(e) => setQuoteStyle(e.target.value as QuoteStyle)}
-							className="w-[100px] mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+							className="w-[100px] mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+						>
 							<option value={QuoteStyle.Double}>Double</option>
 							<option value={QuoteStyle.Single}>Single</option>
 						</select>
