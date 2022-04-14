b540af8a36b7b58976ddeae76f9e6d798d6e7ef0
Nicholas Yang <nicholas@rome.tools>
2022-04-14 17:08:18 -0400

    feature: Starting spread attribute

--- crates/rome_js_formatter/src/jsx/attribute/spread_attribute.rs
+++ crates/rome_js_formatter/src/jsx/attribute/spread_attribute.rs
@@ -1,10 +1,21 @@
-use crate::{FormatElement, FormatNode, Formatter};
-use rome_formatter::FormatResult;
-use rome_js_syntax::JsxSpreadAttribute;
-use rome_rowan::AstNode;
+use crate::{Format, FormatElement, FormatNode, Formatter};
+use rome_formatter::{format_elements, FormatResult};
+use rome_js_syntax::{JsxSpreadAttribute, JsxSpreadAttributeFields};
 
 impl FormatNode for JsxSpreadAttribute {
     fn format_fields(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        Ok(formatter.format_verbatim(self.syntax()))
+        let JsxSpreadAttributeFields {
+            l_curly_token,
+            dotdotdot_token,
+            argument,
+            r_curly_token,
+        } = self.as_fields();
+
+        Ok(format_elements![
+            l_curly_token.format(formatter)?,
+            dotdotdot_token.format(formatter)?,
+            argument.format(formatter)?,
+            r_curly_token.format(formatter)?,
+        ])
     }
 }
