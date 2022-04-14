e5daa1b40e9d9b111a6905a9add093caa1abd2da
Nicholas Yang <nicholas@rome.tools>
2022-04-14 15:42:53 -0400

    Added ability to propagate base url via environment variable, so switched to that

--- website/playground/vite.config.ts
+++ website/playground/vite.config.ts
@@ -2,4 +2,4 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({ base: process.env.BASE_URL, plugins: [react()] });
