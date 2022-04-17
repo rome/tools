4bab462e77bd500ff6643210c1811136f2b2e232
Emanuele Stoppa <my.burning@gmail.com>
2022-04-17 16:25:42 +0100

    wip: broken test

--- crates/rome_js_formatter/tests/specs/js/module/expression/sequence_expression.js
+++ crates/rome_js_formatter/tests/specs/js/module/expression/sequence_expression.js
@@ -11,3 +11,9 @@ abcdefghijklkmnopqrstufwkdsf,
     abcdefghijklkmnopqrstufwkdsf,
     abcdefghijklkmnopqrstufwkdsf,
     abcdefghijklkmnopqrstufwkdsf;
+
+const f = (argument1, argument2, argument3) => (
+    doSomethingWithArgument(argument1),
+    doSomethingWithArgument(argument2),
+    argument1
+);
