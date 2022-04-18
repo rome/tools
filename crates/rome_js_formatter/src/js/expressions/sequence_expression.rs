2a06bc625b036fcb19cb986af3581ec70964215f
Emanuele Stoppa <my.burning@gmail.com>
2022-04-18 17:42:24 +0100

    chore: code review

--- crates/rome_js_formatter/src/js/expressions/sequence_expression.rs
+++ crates/rome_js_formatter/src/js/expressions/sequence_expression.rs
@@ -3,12 +3,14 @@ use crate::{
     format_elements, space_token, FormatElement, FormatResult, Formatter, ToFormatElement,
 };
 use rome_formatter::{concat_elements, group_elements, soft_line_indent_or_space};
-use rome_js_syntax::{JsSequenceExpression, JsSyntaxKind, JsSyntaxNode, JsSyntaxToken};
+use rome_js_syntax::{
+    JsAnyExpression, JsSequenceExpression, JsSyntaxKind, JsSyntaxNode, JsSyntaxToken,
+};
 use rome_rowan::AstNode;
 
 impl ToFormatElement for JsSequenceExpression {
     fn to_format_element(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        format_sequence_expression(self, formatter)
+        format_sequence_expression(self.clone(), formatter)
     }
 }
 
@@ -36,44 +38,40 @@ impl FlattenItemList {
 }
 
 fn format_sequence_expression(
-    node: &JsSequenceExpression,
+    node: JsSequenceExpression,
     formatter: &Formatter,
 ) -> FormatResult<FormatElement> {
     let mut flatten_item_list = FlattenItemList::default();
 
-    flat_sequence_expression(
-        &mut flatten_item_list,
-        node.syntax().clone(),
-        formatter,
-        None,
-    )?;
+    flat_sequence_expression(&mut flatten_item_list, node, formatter, None)?;
 
     Ok(group_elements(flatten_item_list.into_format_element()))
 }
 
 fn flat_sequence_expression(
     list: &mut FlattenItemList,
-    node: JsSyntaxNode,
+    sequence_expression: JsSequenceExpression,
     formatter: &Formatter,
     previous_comma_token: Option<JsSyntaxToken>,
 ) -> FormatResult<()> {
     let formatted_comma = previous_comma_token.format_or_empty(formatter)?;
-    match node.kind() {
-        JsSyntaxKind::JS_SEQUENCE_EXPRESSION => {
-            // SAFETY: protected from the previous match
-            let sequence_expression = JsSequenceExpression::cast(node).unwrap();
-            let left = sequence_expression.left()?;
-            let right = sequence_expression.right()?;
-            let comma = sequence_expression.comma_token()?;
-            flat_sequence_expression(list, left.syntax().clone(), formatter, Some(comma))?;
-            let formatted =
-                format_elements![space_token(), right.format(formatter)?, formatted_comma];
-            list.push(formatted)
-        }
-        _ => {
-            let formatted = format_elements![node.to_format_element(formatter)?, formatted_comma];
-            list.push(formatted)
-        }
+    let left = sequence_expression.left()?;
+    let right = sequence_expression.right()?;
+    let comma = sequence_expression.comma_token()?;
+    if let JsAnyExpression::JsSequenceExpression(sequence_expression) = left {
+        flat_sequence_expression(list, sequence_expression, formatter, Some(comma))?;
+        let formatted = format_elements![space_token(), right.format(formatter)?, formatted_comma];
+        list.push(formatted)
+    } else {
+        list.push(format_elements![
+            left.format(formatter)?,
+            comma.format(formatter)?,
+        ]);
+        list.push(format_elements![
+            space_token(),
+            right.format(formatter)?,
+            formatted_comma
+        ])
     }
 
     Ok(())
