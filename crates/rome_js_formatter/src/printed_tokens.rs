63a273b5b210656821d302ae9a81920ef52f601f
Micha Reiser <micha@rome.tools>
2022-04-22 13:37:26 +0200

    refactor(rome_js_formatter): Track token offsets instead of tokens (#2481)
    The `Formatter` tracks all formatted token in debug builds to catch if the same token gets formatted twice or not formatted at all.
    
    The current implementation tracks the `JsSyntaxToken`s. The downside of doing this is that a language agnostic implementation needs to track `SyntaxToken<L>` which:
    
    * has the consequence that `Formatter` must be generic over `L`
    * which in turn has the consequence that `Format` must be generic over `L`
    
    This PR changes the node tracking to instead only track the absolute offsets of the formatted nodes rather than holding on to the node itself. This removes the need for a generic `Language` parameter and provides the same functionality.
    
    ## Test
    
    * Commented out the `track_replaced` call in `format_replaced` and verified that some tests now panic with a message that token X wasn't formatted.
    * Added one an additional `track_replaced` call to `format_replaced` and verified that tests are now failing with a message saying that the token X was formatted twice.

---
+++ crates/rome_js_formatter/src/printed_tokens.rs
@@ -0,0 +1,56 @@
+use rome_rowan::{Language, SyntaxNode, SyntaxToken, TextSize};
+use std::collections::BTreeSet;
+
+/// Tracks the ranges of the formatted (including replaced or tokens formatted as verbatim) tokens.
+///
+/// This implementation uses the fact that no two tokens can have an overlapping range to avoid the need for an interval tree.
+/// Thus, testing if a token has already been formatted only requires testing if a token starting at the same offset has been formatted.
+#[derive(Debug, Clone, Default)]
+pub(crate) struct PrintedTokens {
+    /// Key: Start of a token's range
+    offsets: BTreeSet<TextSize>,
+}
+
+impl PrintedTokens {
+    /// Tracks a formatted token
+    ///
+    /// ## Panics
+    /// If this token has been formatted before.
+    pub(crate) fn track_token<L: Language>(&mut self, token: &SyntaxToken<L>) {
+        let range = token.text_trimmed_range();
+
+        if !self.offsets.insert(range.start()) {
+            panic!("You tried to print the token '{token:?}' twice, and this is not valid.");
+        }
+    }
+
+    /// Asserts that all tokens of the passed in node have been tracked
+    ///
+    /// ## Panics
+    /// If any descendant token of `root` hasn't been tracked
+    pub(crate) fn assert_all_tracked<L: Language>(&self, root: &SyntaxNode<L>) {
+        let mut descendants = root.descendants_tokens();
+        let mut offsets = self.offsets.iter();
+
+        loop {
+            match (descendants.next(), offsets.next()) {
+                (Some(descendant), Some(offset)) => match descendant.text_trimmed_range().start() {
+                    descendant_offset if descendant_offset < *offset => {
+                        panic!("token has not been seen by the formatter: {descendant:#?}.\nUse `formatter.format_replaced` if you intentionally remove or replace a token from the formatted output.")
+                    }
+                    descendant_offset if descendant_offset > *offset => {
+                        panic!("tracked offset {offset:?} doesn't match any token of {root:#?}. Have you passed a token from another tree?");
+                    }
+                    _ => {}
+                },
+                (Some(descendant), None) => {
+                    panic!("token has not been seen by the formatter: {descendant:#?}.\n Use `formatter.format_replaced` if you intentionally remove or replace a token from the formatted output.")
+                }
+                (None, Some(offset)) => {
+                    panic!("tracked offset {offset:?} doesn't match any token of {root:#?}. Have you passed a token from another tree?");
+                }
+                (None, None) => break,
+            };
+        }
+    }
+}
