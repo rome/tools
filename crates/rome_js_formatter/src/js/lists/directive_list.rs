df27bbf495e156a164cf98e9369a5cb69614314e
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-27 14:27:37 +0800

    feat: 🎸 (formater_js): add some explainning comments

--- crates/rome_js_formatter/src/js/lists/directive_list.rs
+++ crates/rome_js_formatter/src/js/lists/directive_list.rs
@@ -7,26 +7,36 @@ impl Format for JsDirectiveList {
     fn format(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
         if !self.is_empty() {
             let syntax_node = self.syntax();
-            // let child = match syntax_node.last_child() {
-            //     Some(node) => {
-            //     }
-            //     None => false,
-            // };
-            let token = syntax_node.next_sibling();
-            let child = match token {
+            let next_sibling = syntax_node.next_sibling();
+            let need_extra_empty_line = match next_sibling {
                 Some(node_or_token) => {
                     match node_or_token.first_leading_trivia() {
-                        Some(trivia) => trivia.pieces().filter(|piece| piece.is_newline()).count() > 1,
+                        // if next_sibling's first leading_trivia has more than one new_line, we should add an extra empty line at the end of
+                        // JsDirectiveList, for example:
+                        //```js
+                        // "use strict"; <- first leading new_line
+                        //  			 <- second leading new_line
+                        // function foo() {
+
+                        // }
+                        //```
+                        // so we should keep an extra empty line after JsDirectiveList
+                        Some(trivia) => {
+                            trivia.pieces().filter(|piece| piece.is_newline()).count() > 1
+                        }
                         None => false,
                     }
                 }
                 None => false,
             };
-            // child.
             Ok(format_elements![
                 formatter.format_list(self.clone()),
                 hard_line_break(),
-                if child { empty_line() } else { empty_element() }
+                if need_extra_empty_line {
+                    empty_line()
+                } else {
+                    empty_element()
+                }
             ])
         } else {
             Ok(empty_element())
