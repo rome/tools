e11033cabf2772755d3771a9794b5a577bd17f1e
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-27 17:09:35 +0800

    fix: 🐛 fix CR issue

--- crates/rome_js_formatter/src/js/lists/directive_list.rs
+++ crates/rome_js_formatter/src/js/lists/directive_list.rs
@@ -1,5 +1,5 @@
 use crate::{empty_element, format_elements, hard_line_break, Format, FormatElement, Formatter};
-use rome_formatter::{empty_line, FormatResult};
+use rome_formatter::{empty_line, format_element::get_lines_before, FormatResult};
 use rome_js_syntax::JsDirectiveList;
 use rome_rowan::{AstNode, AstNodeList};
 
@@ -8,26 +8,20 @@ impl Format for JsDirectiveList {
         if !self.is_empty() {
             let syntax_node = self.syntax();
             let next_sibling = syntax_node.next_sibling();
-            let need_extra_empty_line = match next_sibling {
-                Some(node_or_token) => {
-                    match node_or_token.first_leading_trivia() {
-                        // if next_sibling's first leading_trivia has more than one new_line, we should add an extra empty line at the end of
-                        // JsDirectiveList, for example:
-                        //```js
-                        // "use strict"; <- first leading new_line
-                        //  			 <- second leading new_line
-                        // function foo() {
+            // if next_sibling's first leading_trivia has more than one new_line, we should add an extra empty line at the end of
+            // JsDirectiveList, for example:
+            //```js
+            // "use strict"; <- first leading new_line
+            //  			 <- second leading new_line
+            // function foo() {
 
-                        // }
-                        //```
-                        // so we should keep an extra empty line after JsDirectiveList
-                        Some(trivia) => {
-                            trivia.pieces().filter(|piece| piece.is_newline()).count() > 1
-                        }
-                        None => false,
-                    }
-                }
-                None => false,
+            // }
+            //```
+            // so we should keep an extra empty line after JsDirectiveList
+            let need_extra_empty_line = if let Some(next_sibling) = next_sibling {
+                get_lines_before(&next_sibling) > 1
+            } else {
+                false
             };
             Ok(format_elements![
                 formatter.format_list(self.clone()),
