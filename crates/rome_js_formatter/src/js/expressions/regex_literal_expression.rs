ebaae9a9e6993a702d8485ec1c3942647b697f11
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-27 22:32:44 +0800

    style: 💄 fmt and clippy

--- crates/rome_js_formatter/src/js/expressions/regex_literal_expression.rs
+++ crates/rome_js_formatter/src/js/expressions/regex_literal_expression.rs
@@ -2,37 +2,38 @@ use crate::{Format, FormatElement, FormatNode, Formatter};
 use rome_formatter::FormatError;
 use rome_formatter::FormatResult;
 
-use rome_formatter::concat_elements;
-use rome_formatter::format_elements;
-use rome_formatter::token;
 use rome_formatter::Token;
 use rome_js_syntax::JsRegexLiteralExpression;
 use rome_js_syntax::JsRegexLiteralExpressionFields;
-use rome_js_syntax::TextRange;
-use rome_js_syntax::TextSize;
 
 impl FormatNode for JsRegexLiteralExpression {
     fn format_fields(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
         let JsRegexLiteralExpressionFields { value_token } = self.as_fields();
         let value_token = value_token?;
         let raw_string = value_token.text();
-        let range = value_token.text_range();
+        // find end slash, so we could split our regex literal to two part `body`: raw_string[0..end_slash_pos + 1] and `flags`: raw_string[end_slash_pos + 1..]
+        // reference https://tc39.es/ecma262/#prod-RegularExpressionLiteral
         let end_slash_pos = raw_string
             .rfind('/')
-            .ok_or_else(|| FormatError::MissingRequiredChild)?;
+            .ok_or(FormatError::MissingRequiredChild)?;
+        // this means that we have a regex literal with no flags
         if end_slash_pos == raw_string.len() - 1 {
             return value_token.format(formatter);
         }
-        let mut flag = raw_string[end_slash_pos + 1..].chars().collect::<Vec<_>>();
-        flag.sort();
-        let flag = flag.iter().collect::<String>();
-        // formatter.format_replaced(current_token, content_to_replace_with);
-        let flag_token = Token::from_syntax_token_cow_slice(
-            std::borrow::Cow::Owned(format!("{}{}", &raw_string[0..end_slash_pos + 1], flag)),
+        let regex_literal_range = value_token.text_range();
+        let mut flag_char_vec = raw_string[end_slash_pos + 1..].chars().collect::<Vec<_>>();
+        flag_char_vec.sort_unstable();
+        let sorted_flag_string = flag_char_vec.iter().collect::<String>();
+
+        let sorted_regex_literal = Token::from_syntax_token_cow_slice(
+            std::borrow::Cow::Owned(format!(
+                "{}{}",
+                &raw_string[0..end_slash_pos + 1],
+                sorted_flag_string
+            )),
             &value_token,
-            range.start(),
+            regex_literal_range.start(),
         );
-        Ok(formatter.format_replaced(&value_token, FormatElement::Token(flag_token)))
-        // value_token.format(formatter)
+        Ok(formatter.format_replaced(&value_token, FormatElement::Token(sorted_regex_literal)))
     }
 }
