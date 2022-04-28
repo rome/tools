66e95572f6fbe86423aca73ff3841bdc86dfebc7
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-29 01:32:20 +0800

    test: 💍 update snapshot testing

--- crates/rome_js_formatter/src/js/auxiliary/default_clause.rs
+++ crates/rome_js_formatter/src/js/auxiliary/default_clause.rs
@@ -2,10 +2,11 @@ use crate::Format;
 use crate::{
     format_elements, hard_line_break, indent, space_token, FormatElement, FormatNode, Formatter,
 };
-use rome_formatter::FormatResult;
+use rome_formatter::{empty_element, FormatResult};
 
-use rome_js_syntax::JsDefaultClause;
 use rome_js_syntax::JsDefaultClauseFields;
+use rome_js_syntax::{JsDefaultClause, JsSyntaxKind};
+use rome_rowan::AstNode;
 
 impl FormatNode for JsDefaultClause {
     fn format_fields(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
@@ -15,16 +16,32 @@ impl FormatNode for JsDefaultClause {
             consequent,
         } = self.as_fields();
 
+        let syntax_node = consequent.syntax();
+        let is_first_child_block_stmt = match syntax_node.first_child() {
+            Some(stmt) => stmt.kind() == JsSyntaxKind::JS_BLOCK_STATEMENT,
+            None => false,
+        };
         let default = default_token.format(formatter)?;
         let colon = colon_token.format(formatter)?;
         let statements = formatter.format_list(consequent);
 
+        let cons = if statements.is_empty() {
+            // Skip inserting an indent block is the consequent is empty to print
+            // the trailing comments for the case clause inline if there is no
+            // block to push them into
+            hard_line_break()
+        } else if is_first_child_block_stmt {
+            format_elements![space_token(), statements]
+        } else {
+            // no line break needed after because it is added by the indent in the switch statement
+            indent(format_elements![hard_line_break(), statements])
+        };
         Ok(format_elements![
             default,
             colon,
             space_token(),
             // no line break needed after because it is added by the indent in the switch statement
-            indent(format_elements![hard_line_break(), statements])
+            cons
         ])
     }
 }
