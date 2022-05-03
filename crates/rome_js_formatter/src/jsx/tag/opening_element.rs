42a84ae0c9b1f71481e5dbd6dc587b00c212f526
Nicholas Yang <nicholas@rome.tools>
2022-05-03 12:47:21 -0400

    Patched  up tests

--- crates/rome_js_formatter/src/jsx/tag/opening_element.rs
+++ crates/rome_js_formatter/src/jsx/tag/opening_element.rs
@@ -1,6 +1,7 @@
-use crate::{Format, FormatElement, FormatNode, Formatter, JsFormatter};
-use rome_formatter::{format_elements, FormatResult};
+use crate::{space_token, Format, FormatElement, FormatNode, Formatter, JsFormatter};
+use rome_formatter::{empty_element, format_elements, FormatResult};
 use rome_js_syntax::{JsxOpeningElement, JsxOpeningElementFields};
+use rome_rowan::AstNodeList;
 
 impl FormatNode for JsxOpeningElement {
     fn format_fields(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
@@ -12,9 +13,16 @@ impl FormatNode for JsxOpeningElement {
             r_angle_token,
         } = self.as_fields();
 
+        let optional_space = if attributes.is_empty() {
+            empty_element()
+        } else {
+            space_token()
+        };
+
         Ok(format_elements![
             l_angle_token.format(formatter)?,
             name.format(formatter)?,
+            optional_space,
             formatter.format_list(attributes),
             r_angle_token.format(formatter)?
         ])
