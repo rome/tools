6fee04c1911b0d7f713fa28a19fae41a2a1b009c
Emanuele Stoppa <my.burning@gmail.com>
2022-05-04 11:13:58 +0100

    fix(rome_js_formatter): keep quotes for numeric members

---
+++ crates/rome_js_formatter/tests/specs/ts/string/parameter_quotes.ts
@@ -0,0 +1,28 @@
+type X = {
+    "member": string;
+    // you stay like this
+    "member-member": number,
+    "4": number,
+    "with_underscore": number
+    "0197": number,
+    "3n": number,
+    "3p": number,
+    "p9": number
+    "_$_ff$_morning_not_quotes": number,
+    "_$_ff$_morning_yes_quotes_@": number
+};
+
+
+interface Y {
+    "member": string;
+    // you stay like this
+    "member-member": number,
+    "4": number,
+    "with_underscore": number
+    "0197": number,
+    "3n": number,
+    "3p": number,
+    "p9": number
+    "_$_ff$_morning_not_quotes": number,
+    "_$_ff$_morning_yes_quotes_@": number
+}
