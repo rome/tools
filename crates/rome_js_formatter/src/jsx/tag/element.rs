82407a0286af074c601563cbe8d70ec88ec65a76
Nicholas Yang <nicholas@rome.tools>
2022-05-04 11:25:14 -0400

    Updated tests and reverted element formatting changes

--- crates/rome_js_formatter/src/jsx/tag/element.rs
+++ crates/rome_js_formatter/src/jsx/tag/element.rs
@@ -1,19 +1,10 @@
-use crate::{Format, FormatElement, FormatNode, FormatResult, Formatter, JsFormatter};
-use rome_formatter::format_elements;
-use rome_js_syntax::{JsxElement, JsxElementFields};
+use crate::formatter::verbatim_node;
+use crate::{Format, FormatElement, FormatNode, FormatResult, Formatter};
+use rome_js_syntax::JsxElement;
+use rome_rowan::AstNode;
 
 impl FormatNode for JsxElement {
     fn format_fields(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        let JsxElementFields {
-            opening_element,
-            children,
-            closing_element,
-        } = self.as_fields();
-
-        Ok(format_elements![
-            opening_element.format(formatter)?,
-            formatter.format_list(children),
-            closing_element.format(formatter)?
-        ])
+        verbatim_node(self.syntax()).format(formatter)
     }
 }
--- crates/rome_js_formatter/src/jsx/tag/opening_element.rs
+++ crates/rome_js_formatter/src/jsx/tag/opening_element.rs
@@ -1,30 +1,11 @@
-use crate::{space_token, Format, FormatElement, FormatNode, Formatter, JsFormatter};
-use rome_formatter::{empty_element, format_elements, FormatResult};
-use rome_js_syntax::{JsxOpeningElement, JsxOpeningElementFields};
-use rome_rowan::AstNodeList;
+use crate::formatter::verbatim_node;
+use crate::{Format, FormatElement, FormatNode, Formatter};
+use rome_formatter::FormatResult;
+use rome_js_syntax::JsxOpeningElement;
+use rome_rowan::AstNode;
 
 impl FormatNode for JsxOpeningElement {
     fn format_fields(&self, formatter: &Formatter) -> FormatResult<FormatElement> {
-        let JsxOpeningElementFields {
-            type_arguments: _,
-            l_angle_token,
-            name,
-            attributes,
-            r_angle_token,
-        } = self.as_fields();
-
-        let optional_space = if attributes.is_empty() {
-            empty_element()
-        } else {
-            space_token()
-        };
-
-        Ok(format_elements![
-            l_angle_token.format(formatter)?,
-            name.format(formatter)?,
-            optional_space,
-            formatter.format_list(attributes),
-            r_angle_token.format(formatter)?
-        ])
+        verbatim_node(self.syntax()).format(formatter)
     }
 }
