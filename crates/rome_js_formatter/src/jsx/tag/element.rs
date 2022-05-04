4cfd10da20b610eb0b7479fe41aa7b7129ac1b23
Nicholas Yang <nicholas@rome.tools>
2022-05-04 15:00:40 -0400

    Starting jsx element formatting

--- crates/rome_js_formatter/src/jsx/tag/element.rs
+++ crates/rome_js_formatter/src/jsx/tag/element.rs
@@ -1,14 +1,25 @@
-use crate::formatter::verbatim_node;
 use crate::prelude::*;
 use crate::FormatNodeFields;
-use rome_js_syntax::JsxElement;
-use rome_rowan::AstNode;
+use rome_js_syntax::{JsxElement, JsxElementFields};
 
 impl FormatNodeFields<JsxElement> for FormatNodeRule<JsxElement> {
     fn format_fields(
         node: &JsxElement,
         formatter: &Formatter<JsFormatOptions>,
     ) -> FormatResult<FormatElement> {
-        verbatim_node(node.syntax()).format(formatter)
+        let JsxElementFields {
+            opening_element,
+            children,
+            closing_element,
+        } = node.as_fields();
+
+        formatted![
+            formatter,
+            [
+                opening_element.format(),
+                soft_block_indent(formatted![formatter, [children.format()]]?),
+                closing_element.format()?
+            ]
+        ]
     }
 }
