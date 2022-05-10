d3aa3a50ea9fe79bd5d6f5ecba750fdc58a679be
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-05-10 14:23:12 +0800

    fix(rome_js_formatter): wrap string literal expression statements in parentheses
    Wraps string literal expressions that are direct children of an expression statement in parentheses.

--- crates/rome_js_formatter/src/js/expressions/string_literal_expression.rs
+++ crates/rome_js_formatter/src/js/expressions/string_literal_expression.rs
@@ -1,5 +1,7 @@
 use crate::{FormatElement, FormatNode, Formatter};
-use rome_formatter::FormatResult;
+use rome_formatter::{format_elements, token, FormatResult};
+use rome_js_syntax::JsExpressionStatement;
+use rome_rowan::AstNode;
 
 use crate::utils::format_string_literal_token;
 use rome_js_syntax::JsStringLiteralExpression;
@@ -10,7 +12,21 @@ impl FormatNode for JsStringLiteralExpression {
         let JsStringLiteralExpressionFields { value_token } = self.as_fields();
 
         let value_token = value_token?;
+        let syntax_node = self.syntax();
+        let parent = syntax_node.parent();
 
-        Ok(format_string_literal_token(value_token, formatter))
+        let needs_parenthesis = parent.and_then(JsExpressionStatement::cast).is_some();
+
+        let formatted_element = format_string_literal_token(value_token, formatter);
+        if needs_parenthesis {
+            let (leading_trivia, content, trailing_trivia) = formatted_element.split_trivia();
+            Ok(format_elements![
+                leading_trivia,
+                format_elements![token("("), content, token(")"),],
+                trailing_trivia,
+            ])
+        } else {
+            Ok(formatted_element)
+        }
     }
 }
