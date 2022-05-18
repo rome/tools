79960ec47a624f5c143a5f6c6dae28b6388b4f2d
Nicholas Yang <nicholas@rome.tools>
2022-05-18 13:13:37 -0400

    Cleaning up code

--- crates/rome_js_formatter/src/jsx/lists/child_list.rs
+++ crates/rome_js_formatter/src/jsx/lists/child_list.rs
@@ -4,7 +4,6 @@ use crate::utils::jsx_utils::contains_text;
 use crate::{FormatElement, Formatter, JsFormatter};
 use rome_formatter::{empty_element, fill_elements, FormatResult};
 use rome_js_syntax::JsxChildList;
-use rome_rowan::AstNode;
 
 impl FormatRule<JsxChildList> for FormatJsxChildList {
     type Options = JsFormatOptions;
@@ -17,12 +16,6 @@ impl FormatRule<JsxChildList> for FormatJsxChildList {
             .format_list(node)
             .into_iter()
             .map(|(_, element)| element);
-
-        println!();
-        println!("\"{}\"", node.syntax().text());
-        println!("{:#?}", node);
-        println!("{:#?}", children);
-        println!();
 
         if contains_text(node) {
             Ok(fill_elements(empty_element(), children))
