e96186f153be38c2c038620917f3c8adbe1bcc1a
Nicholas Yang <nicholas@rome.tools>
2022-05-24 12:22:12 -0500

    Added documentation

--- crates/rome_js_formatter/src/utils/jsx_utils.rs
+++ crates/rome_js_formatter/src/utils/jsx_utils.rs
@@ -12,6 +12,18 @@ use rome_rowan::SyntaxNode;
 
 /// Creates either a space using an expression child and a string literal,
 /// or a regular space, depending on whether the group breaks or not.
+///
+/// ```jsx
+///  <div> Winter Light </div>;
+///
+///  <div>
+///    {" "}Winter Light
+///    Through A Glass Darkly
+///    The Silence
+///    Seventh Seal
+///    Wild Strawberries
+///  </div>
+/// ```
 pub fn jsx_space(formatter: &Formatter<JsFormatOptions>) -> FormatElement {
     let jsx_space = match formatter.options().quote_style {
         QuoteStyle::Double => "{{\" \"}}",
@@ -24,6 +36,7 @@ pub fn jsx_space(formatter: &Formatter<JsFormatOptions>) -> FormatElement {
     ]
 }
 
+/// Detects if the child is a JSX whitespace expression, i.e. `{" "}`
 pub fn is_jsx_whitespace_expression(child: JsxAnyChild) -> bool {
     if let JsxAnyChild::JsxExpressionChild(expr_child) = child {
         if let Some(JsAnyExpression::JsAnyLiteralExpression(
@@ -55,6 +68,18 @@ pub fn contains_text(children: &JsxChildList) -> bool {
 
 pub static WHITESPACE: [char; 4] = [' ', '\n', '\t', '\r'];
 
+/// Meaningful JSX text is defined to be text that has either non-whitespace
+/// characters, or does not contain a newline. Whitespace is defined as ASCII
+/// whitespace.
+///
+/// ```
+/// use rome_js_formatter::utils::jsx_utils::is_meaningful_jsx_text;
+///
+/// assert_eq!(is_meaningful_jsx_text("     \t\r   "), true);
+/// assert_eq!(is_meaningful_jsx_text("     \n\r   "), false);
+/// assert_eq!(is_meaningful_jsx_text("  Alien   "), true);
+/// assert_eq!(is_meaningful_jsx_text(""), true);
+/// ```
 pub fn is_meaningful_jsx_text(text: &str) -> bool {
     let mut has_newline = false;
     for c in text.chars() {
@@ -105,11 +130,34 @@ pub enum WrapState {
     AlwaysWrap,
 }
 
-/// Takes in a syntax node because we need to handle both JsxElement and JsxSelfClosingElement
+/// Checks if a JSX Element should be wrapped in parentheses. Returns a WrapState which
+/// indicates that an element should always be wrapped in parentheses, should be wrapped
+/// only when it's line broken, or should not be wrapped at all.
+///
+/// For instance, an element nested in a let binding should be wrapped in parentheses
+/// when it is line broken:
+/// ```jsx
+///  let component = <div> La Haine dir. Mathieu Kassovitz </div>;
+///
+///  let component = (
+///   <div> Uncle Boonmee Who Can Recall His Past Lives dir. Apichatpong Weerasethakul </div>
+///  );
+/// ```
+///
+/// An element inside a static member expression should always be wrapped:
+/// ```jsx
+/// (<div>Badlands</div>).property
+/// ```
+///
+/// An element that is another element's attribute should never be wrapped:
+/// ```jsx
+///  <Route path="/" component={<HomePage />} />
+/// ```
+///
 pub fn should_wrap_element_in_parens(node: &SyntaxNode<JsLanguage>) -> WrapState {
-    let mut ancestors = node.ancestors();
-    // We skip 1 for the current node
-    ancestors.next();
+    // We skip the first item because the first item in ancestors is the node itself, i.e.
+    // the JSX Element in this case.
+    let mut ancestors = node.ancestors().skip(1);
 
     ancestors
         .next()
@@ -144,23 +192,23 @@ pub fn should_wrap_element_in_parens(node: &SyntaxNode<JsLanguage>) -> WrapState
 ///
 /// If you're wondering why this is the only other case, it's because
 /// Prettier defines it to be that way.
+///
+/// ```jsx
+///  let bar = <div>
+///    {foo(() => <div> the quick brown fox jumps over the lazy dog </div>)}
+///  </div>;
+/// ```
 pub fn is_jsx_inside_arrow_function_inside_call_inside_expression_child(
     node: &SyntaxNode<JsLanguage>,
 ) -> bool {
-    // ```jsx
-    //  let bar = <div>
-    //    {foo(() => <div> the quick brown fox jumps over the lazy dog </div>)}
-    //  </div>;
-    // ```
-    let mut ancestors = node.ancestors();
+    // We skip the first item because the first item in ancestors is the node itself, i.e.
+    // the JSX Element in this case.
+    let mut ancestors = node.ancestors().skip(1);
     let required_ancestors = [
         JsSyntaxKind::JS_ARROW_FUNCTION_EXPRESSION,
         JsSyntaxKind::JS_CALL_EXPRESSION,
         JsSyntaxKind::JSX_EXPRESSION_CHILD,
     ];
-
-    // Skip current node
-    ancestors.next();
 
     for required_ancestor in required_ancestors {
         let is_required_ancestor = ancestors
