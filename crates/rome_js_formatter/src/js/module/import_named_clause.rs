baaf4cd35c99638ad7ad2de4e5133967411db5e8
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-05-26 20:01:49 +0800

    perf: ⚡️ simplify code structure

--- crates/rome_js_formatter/src/js/module/import_named_clause.rs
+++ crates/rome_js_formatter/src/js/module/import_named_clause.rs
@@ -24,6 +24,7 @@ impl FormatNodeFields<JsImportNamedClause> for FormatNodeRule<JsImportNamedClaus
         let named_import = named_import?;
 
         // can_break implementation, return `format_element` instead of boolean to reduce enum conversion overhead.
+        // if `can_break` is true we just use the previous format strategy, otherwise we use the new format strategy.
         // reference https://github.com/prettier/prettier/blob/5b113e71b1808d6916f446c3aa49c3c53e3bdb98/src/language-js/print/module.js#L173
         let formatted_named_import = if default_specifier.is_some() {
             // `can_break` is true.
@@ -31,50 +32,51 @@ impl FormatNodeFields<JsImportNamedClause> for FormatNodeRule<JsImportNamedClaus
         } else {
             match named_import {
                 JsAnyNamedImport::JsNamedImportSpecifiers(ref specifiers)
-                    if specifiers.specifiers().len() > 1 =>
-                {
-                    named_import.format().format(formatter)
-                }
-                JsAnyNamedImport::JsNamespaceImportSpecifier(_) => {
-                    // this means `standaloneSpecifiers.length > 0`, then we can break;
-
-                    named_import.format().format(formatter)
-                }
-                JsAnyNamedImport::JsNamedImportSpecifiers(ref specifiers)
                     if specifiers.specifiers().len() == 1 =>
                 {
-                    let syntax_node = specifiers.clone().into_syntax();
-                    if syntax_node.has_leading_comments() || syntax_node.has_trailing_comments() {
-                        named_import.format().format(formatter)
-                    } else {
-                        let JsNamedImportSpecifiersFields {
-                            l_curly_token,
-                            specifiers,
-                            r_curly_token,
-                        } = specifiers.as_fields();
-                        let first_specifier = specifiers.iter().next().unwrap();
-                        let can_break = match first_specifier {
-                            Ok(JsAnyNamedImportSpecifier::JsShorthandNamedImportSpecifier(_)) => {
-                                false
-                            }
-                            _ => true,
-                        };
-                        if can_break {
-                            named_import.format().format(formatter)
-                        } else {
-                            formatted![
-                                formatter,
-                                [
-                                    l_curly_token.format(),
-                                    space_token(),
-                                    first_specifier.format(),
-                                    space_token(),
-                                    r_curly_token.format()
+                    let first_specifier = specifiers.specifiers().iter().next().unwrap();
+                    match first_specifier {
+                        Ok(JsAnyNamedImportSpecifier::JsShorthandNamedImportSpecifier(_)) => {
+                            let syntax_node = specifiers.clone().into_syntax();
+                            if syntax_node.has_leading_comments()
+                                || syntax_node.has_trailing_comments()
+                            {
+                                named_import.format().format(formatter)
+                            } else {
+                                let JsNamedImportSpecifiersFields {
+                                    l_curly_token,
+                                    specifiers: _,
+                                    r_curly_token,
+                                } = specifiers.as_fields();
+                                formatted![
+                                    formatter,
+                                    [
+                                        l_curly_token.format(),
+                                        space_token(),
+                                        first_specifier.format(),
+                                        space_token(),
+                                        r_curly_token.format()
+                                    ]
                                 ]
-                            ]
+                            }
                         }
+                        _ => named_import.format().format(formatter),
                     }
+
+                    // if syntax_node.has_leading_comments() || syntax_node.has_trailing_comments() {
+                    // } else {
+                    // }
                 }
+                // JsAnyNamedImport::JsNamespaceImportSpecifier(_) => {
+                //     // this means `standaloneSpecifiers.length > 0`, then we can break;
+
+                //     named_import.format().format(formatter)
+                // }
+                // JsAnyNamedImport::JsNamedImportSpecifiers(ref specifiers)
+                //     if specifiers.specifiers().len() > 1 =>
+                // {
+                //     named_import.format().format(formatter)
+                // }
                 _ => named_import.format().format(formatter),
             }
         };
