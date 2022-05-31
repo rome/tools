79744a1e59e7680a23e189971c07eea9195f668a
Daniel Frederico Lins Leite <1985.daniel@gmail.com>
2022-05-31 13:17:48 +0100

    more comemnts on public structs/fns

--- crates/rome_js_semantic/src/events.rs
+++ crates/rome_js_semantic/src/events.rs
@@ -1,10 +1,19 @@
+//! Events emitted by the [SemanticEventExtractor] which are then constructed into the Semantic Model
+
 use std::collections::VecDeque;
 
 use rome_js_syntax::{JsLanguage, JsSyntaxNode, TextRange};
 use rome_rowan::syntax::Preorder;
 
+/// Events emitted by the [SemanticEventExtractor]. These events are later
+/// made into the Semantic Model.
 #[derive(Debug)]
 pub enum SemanticEvent {
+    /// Signifies that a new symbol declaration was found.
+    /// Currently is generated for:
+    /// - Variable Declarations
+    /// - Import bindings
+    /// - Functions parameters
     DeclarationFound { range: TextRange },
 }
 
@@ -23,12 +32,52 @@ impl SemanticEvent {
     }
 }
 
+/// Extracts [SemanticEvent] from [SyntaxNode].
+///
+/// The extraction is not enterily pull based, nor entirely push based.
+/// This happens because some nodes can generate multiple events.
+/// A hoisted variable declaration like ```var a```, being the more obvious
+/// example. As soon ```a``` is hoisted, all references of ```a``` are solved
+/// on this node.
+///
+/// For a simpler way to extract [SemanticEvent] see [semantic_events] or [SemanticEventIterator].
+///
+/// To use the [SemanticEventExtractor] one must push the current node, following
+/// the [PreOrder] of the tree, and must pull events unti [Pop] returuns [None].
+///
+/// ```rust
+/// use rome_js_parser::*;
+/// use rome_js_syntax::*;
+/// use rome_js_semantic::*;
+/// let tree = parse("let a = 1", 0, SourceType::js_script());
+/// let mut extractor = SemanticEventExtractor::new();
+/// for e in tree.syntax().preorder() {
+///     match e {
+///         WalkEvent::Enter(node) => {
+///             extractor.extract_from(&node);
+///         }
+///         _ => {}
+///     }
+///     
+///     while let Some(e) = extractor.pop() {
+///         dbg!(e);
+///     }
+/// }
+/// ```
 #[derive(Default)]
 pub struct SemanticEventExtractor {
     stash: VecDeque<SemanticEvent>,
 }
 
 impl SemanticEventExtractor {
+    pub fn new() -> Self {
+        Self {
+            stash: VecDeque::new(),
+        }
+    }
+
+    /// See [SemanticEvent] for a more detailed description
+    /// of which ```SyntaxNode``` generated which events.
     pub fn extract_from(&mut self, node: &JsSyntaxNode) {
         use rome_js_syntax::JsSyntaxKind::*;
         use SemanticEvent::*;
@@ -38,7 +87,15 @@ impl SemanticEventExtractor {
             })
         }
     }
+
+    /// Return any previous extracted [SemanticEvent].
+    pub fn pop(&mut self) -> Option<SemanticEvent> {
+        self.stash.pop_front()
+    }
 }
+
+/// Extracts [SemanticEvent] from [SyntaxNode].
+/// See [semantic_events] how to create this iterator.
 struct SemanticEventIterator {
     iter: Preorder<JsLanguage>,
     extractor: SemanticEventExtractor,
@@ -49,7 +106,7 @@ impl Iterator for SemanticEventIterator {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(e) = self.extractor.stash.pop_front() {
+            if let Some(e) = self.extractor.pop() {
                 break Some(e);
             } else {
                 use rome_js_syntax::WalkEvent::*;
@@ -65,6 +122,19 @@ impl Iterator for SemanticEventIterator {
     }
 }
 
+/// Extracts [SemanticEvent] from [SyntaxNode].
+///
+/// For a way to extract [SemanticEvent] which gives more control see [SemanticEventExtractor].
+///
+/// ```rust
+/// use rome_js_parser::*;
+/// use rome_js_syntax::*;
+/// use rome_js_semantic::*;
+/// let tree = parse("let a = 1", 0, SourceType::js_script());
+/// for e in semantic_events(tree.syntax()) {
+///     dbg!(e);
+/// }
+/// ```
 pub fn semantic_events(root: JsSyntaxNode) -> impl IntoIterator<Item = SemanticEvent> {
     SemanticEventIterator {
         iter: root.preorder(),
