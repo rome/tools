b3a6cd274daeb050ad36cc2b7f58180d9a8e5827
Denis Bezrukov <dbezrukov@ctrader.local>
2022-06-01 18:20:06 +0300

    extract MIN_OVERLAP_FOR_BREAK. delete JsAnyExpression::TsAsExpression replace is_binaryish_expression with JsAnyBinaryLikeExpression::can_cast

--- crates/rome_js_formatter/src/js/objects/property_object_member.rs
+++ crates/rome_js_formatter/src/js/objects/property_object_member.rs
@@ -1,14 +1,12 @@
 use crate::prelude::*;
 
+use crate::utils::JsAnyBinaryLikeExpression;
 use crate::FormatNodeFields;
 use rome_js_syntax::JsAnyExpression;
 use rome_js_syntax::JsAnyLiteralExpression;
-use rome_js_syntax::JsCallExpressionFields;
-use rome_js_syntax::JsConditionalExpressionFields;
 use rome_js_syntax::JsPropertyObjectMember;
 use rome_js_syntax::JsPropertyObjectMemberFields;
 use rome_js_syntax::JsSyntaxKind;
-use rome_js_syntax::JsSyntaxNode;
 use rome_rowan::SyntaxResult;
 use rome_rowan::TextSize;
 
@@ -79,8 +77,10 @@ enum PropertyObjectMemberLayoutMode {
     NeverBreakAfterOperator,
 }
 
+const MIN_OVERLAP_FOR_BREAK: u8 = 3;
+
 fn get_object_member_layout(
-    formatter: &Formatter<JsFormatOptions>,
+    formatter: &JsFormatter,
     node: &JsPropertyObjectMember,
 ) -> SyntaxResult<PropertyObjectMemberLayoutMode> {
     let JsPropertyObjectMemberFields {
@@ -92,14 +92,7 @@ fn get_object_member_layout(
     let name = name?;
     let value = value?;
 
-    let value = match value {
-        JsAnyExpression::TsAsExpression(expression) => expression.expression()?,
-        _ => value,
-    };
-
-    const MIN_OVERLAP_FOR_BREAK: u8 = 3;
-
-    let text_width_for_break = (formatter.options().tab_width() + MIN_OVERLAP_FOR_BREAK) as u32;
+    let text_width_for_break = (formatter.context().tab_width() + MIN_OVERLAP_FOR_BREAK) as u32;
     let has_short_key = name.range().len() < TextSize::from(text_width_for_break);
 
     if is_break_after_operator(&value, has_short_key)? {
@@ -114,7 +107,7 @@ fn get_object_member_layout(
 }
 
 fn is_break_after_operator(value: &JsAnyExpression, has_short_key: bool) -> SyntaxResult<bool> {
-    if is_binaryish_expression(value.syntax()) {
+    if JsAnyBinaryLikeExpression::can_cast(value.syntax().kind()) {
         return Ok(true);
     }
 
@@ -123,15 +116,7 @@ fn is_break_after_operator(value: &JsAnyExpression, has_short_key: bool) -> Synt
     }
 
     if let JsAnyExpression::JsConditionalExpression(conditional) = &value {
-        let JsConditionalExpressionFields {
-            test,
-            question_mark_token: _,
-            consequent: _,
-            colon_token: _,
-            alternate: _,
-        } = conditional.as_fields();
-
-        if is_binaryish_expression(test?.syntax()) {
+        if JsAnyBinaryLikeExpression::can_cast(conditional.test()?.syntax().kind()) {
             return Ok(true);
         }
     }
@@ -155,14 +140,7 @@ fn is_never_break_after_operator(
     has_short_key: bool,
 ) -> SyntaxResult<bool> {
     if let JsAnyExpression::JsCallExpression(call_expression) = &value {
-        let JsCallExpressionFields {
-            callee,
-            optional_chain_token: _,
-            type_arguments: _,
-            arguments: _,
-        } = call_expression.as_fields();
-
-        if callee?.syntax().kind() == JsSyntaxKind::REQUIRE_KW {
+        if call_expression.callee()?.syntax().kind() == JsSyntaxKind::REQUIRE_KW {
             return Ok(true);
         }
     }
@@ -186,11 +164,4 @@ fn is_never_break_after_operator(
     }
 
     Ok(false)
-}
-
-fn is_binaryish_expression(node: &JsSyntaxNode) -> bool {
-    matches!(
-        node.kind(),
-        JsSyntaxKind::JS_BINARY_EXPRESSION | JsSyntaxKind::JS_LOGICAL_EXPRESSION
-    )
 }
