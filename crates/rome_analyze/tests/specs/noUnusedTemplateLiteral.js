5f054cba9c5eef71c784218ebb10fd044c6532bf
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-06-04 15:14:28 +0800

    test: 💍 add more testcase

--- crates/rome_analyze/tests/specs/noUnusedTemplateLiteral.js
+++ crates/rome_analyze/tests/specs/noUnusedTemplateLiteral.js
@@ -1,9 +1,24 @@
 // valid
-const foo2 = `bar
+var foo2 = `bar
 has newline`;
-const foo3 = `\"bar\"`
-const foo4 = `'bar'`
+var foo3 = `\"bar\"`
+var foo4 = `'bar'`
+var foo = `bar 'baz'`;
 
+var foo = `back${x}tick`;
+var foo = tag`backtick`;
+var foo = `something 
+else`;
 //invalid
-const foo = `bar`;
-const foo1 = `bar `;
+var foo = `bar`;
+var foo1 = `bar `;
+var foo = `back\rtick`;
+var foo = `back\ntick`;
+var foo = `back\u2028tick`
+var foo = `back\u2029tick`;
+var foo = `back\\\\\ntick`;
+var foo = `\n`;
+function foo() { `use strict`; foo(); }
+var foo = `foo\\nbar`;
+var foo = `foo\\\nbar`;
+var foo = `foo\\\\\\\nbar`;
