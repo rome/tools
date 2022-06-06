fbc80b874b2c60810cf9ea77b631e4fb247f19bb
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-06-06 23:13:39 +0800

    Fix(rome_js_formatter): named import (#2498)

--- crates/rome_js_formatter/src/js/module/import_named_clause.rs
+++ crates/rome_js_formatter/src/js/module/import_named_clause.rs
@@ -1,8 +1,11 @@
 use crate::prelude::*;
 use crate::FormatNodeFields;
 use rome_formatter::write;
+use rome_js_syntax::JsAnyNamedImport;
+use rome_js_syntax::JsAnyNamedImportSpecifier;
 use rome_js_syntax::JsImportNamedClause;
 use rome_js_syntax::JsImportNamedClauseFields;
+use rome_js_syntax::JsNamedImportSpecifiersFields;
 
 impl FormatNodeFields<JsImportNamedClause> for FormatNodeRule<JsImportNamedClause> {
     fn fmt_fields(node: &JsImportNamedClause, f: &mut JsFormatter) -> FormatResult<()> {
@@ -19,14 +22,70 @@ impl FormatNodeFields<JsImportNamedClause> for FormatNodeRule<JsImportNamedClaus
             write!(f, [type_token.format(), space_token()])?;
         }
 
+        let is_default_specifier_empty = default_specifier.is_none();
+
         if let Some(default_specifier) = default_specifier {
             write!(f, [default_specifier.format(), space_token()])?;
         }
 
+        let named_import = named_import?;
+
+        // can_break implementation, return `format_element` instead of boolean to reduce enum conversion overhead.
+        // if `can_break` is true we just use the previous format strategy, otherwise we use the new format strategy.
+        // reference https://github.com/prettier/prettier/blob/5b113e71b1808d6916f446c3aa49c3c53e3bdb98/src/language-js/print/module.js#L173
+
+        // https://github.com/prettier/prettier/blob/5b113e71b1808d6916f446c3aa49c3c53e3bdb98/src/language-js/print/module.js#L184-L209v,
+        // `standaloneSpecifiers` corresponding our `JsDefaultImportSpecifier` + part of `JsNamespaceImportSpecifier`,
+        // `groupedSpecifiers` corresponding our `JsNamedImportSpecifiers`
+
+        //  Here we use an opposite way of thinking, we only thinking about the way that can not break
+        // That's to say
+        // 1. `default_specifier` need to be none.
+        // 2. length of `JsNamedImportSpecifiers` at least is one
+        // 3. Surrounding of the only `JsNamedImportSpecifiers` should not have any comments
+        if !is_default_specifier_empty {
+            // `can_break` is true.
+            write![f, [named_import.format()]]
+        } else {
+            match named_import {
+                JsAnyNamedImport::JsNamedImportSpecifiers(ref specifiers)
+                    if specifiers.specifiers().len() == 1 =>
+                {
+                    // SAFETY: we know that the `specifiers.specifiers().len() == 1`, so unwrap `iter().next()` is safe.
+                    let first_specifier = specifiers.specifiers().iter().next().unwrap();
+                    match first_specifier {
+                        Ok(JsAnyNamedImportSpecifier::JsShorthandNamedImportSpecifier(_)) => {
+                            let syntax_node = specifiers.syntax();
+                            if syntax_node.has_comments_direct() {
+                                write![f, [named_import.format()]]
+                            } else {
+                                let JsNamedImportSpecifiersFields {
+                                    l_curly_token,
+                                    specifiers: _,
+                                    r_curly_token,
+                                } = specifiers.as_fields();
+                                write![
+                                    f,
+                                    [
+                                        l_curly_token.format(),
+                                        space_token(),
+                                        first_specifier.format(),
+                                        space_token(),
+                                        r_curly_token.format()
+                                    ]
+                                ]
+                            }
+                        }
+                        _ => write![f, [named_import.format()]],
+                    }
+                }
+                _ => write![f, [named_import.format()]],
+            }
+        }?;
+
         write![
             f,
             [
-                named_import.format(),
                 space_token(),
                 from_token.format(),
                 space_token(),
