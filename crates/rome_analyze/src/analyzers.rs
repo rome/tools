f023aa451ba3c16fc9a006da5b8fb55f8eca7d1a
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-06-08 20:40:14 +0800

    fix: 🐛 clippy

--- crates/rome_analyze/src/analyzers.rs
+++ crates/rome_analyze/src/analyzers.rs
@@ -1,11 +1,11 @@
 mod no_delete;
 mod no_double_equals;
+mod no_negation_else;
 mod use_single_var_declarator;
 mod use_while;
-mod no_negation_else;
 
 pub(crate) use no_delete::NoDelete;
 pub(crate) use no_double_equals::NoDoubleEquals;
+pub(crate) use no_negation_else::NoNegationElse;
 pub(crate) use use_single_var_declarator::UseSingleVarDeclarator;
 pub(crate) use use_while::UseWhile;
-pub (crate) use no_negation_else::NoNegationElse;
