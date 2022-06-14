746dba1746b1e6f2ff9ccfe451b7797062d5e3a2
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2022-06-14 17:55:14 +0100

    feature(rome_js_semantic): matching declarations and scopes (#2690)
    * variable declaration pointing to its parent scope

--- crates/rome_js_semantic/src/tests/mod.rs
+++ crates/rome_js_semantic/src/tests/mod.rs
@@ -1,2 +1,15 @@
-pub mod events;
+mod assertions;
+pub mod declarations;
 pub mod scopes;
+
+#[macro_export]
+macro_rules! assert_semantics {
+    ($(#[$attr:meta])* $($name:ident, $code:expr,)*) => {
+        $(
+            #[test]
+            pub fn $name() {
+                crate::tests::assertions::assert($code, stringify!($name));
+            }
+        )*
+    };
+}
