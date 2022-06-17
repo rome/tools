58f73b4f99d4224c95df6b3e930858bcfeef1b8a
Emanuele Stoppa <my.burning@gmail.com>
2022-06-17 17:48:57 -0300

    feat(rome_js_formatter): object pattern formatting

--- crates/rome_js_formatter/src/js/assignments/object_assignment_pattern_property.rs
+++ crates/rome_js_formatter/src/js/assignments/object_assignment_pattern_property.rs
@@ -1,10 +1,8 @@
 use crate::prelude::*;
-use rome_formatter::write;
-
-use crate::utils::FormatMemberName;
+use crate::utils::JsAnyAssignmentLike;
 use crate::FormatNodeFields;
+use rome_formatter::write;
 use rome_js_syntax::JsObjectAssignmentPatternProperty;
-use rome_js_syntax::JsObjectAssignmentPatternPropertyFields;
 
 impl FormatNodeFields<JsObjectAssignmentPatternProperty>
     for FormatNodeRule<JsObjectAssignmentPatternProperty>
@@ -13,27 +11,6 @@ impl FormatNodeFields<JsObjectAssignmentPatternProperty>
         node: &JsObjectAssignmentPatternProperty,
         f: &mut JsFormatter,
     ) -> FormatResult<()> {
-        let JsObjectAssignmentPatternPropertyFields {
-            member,
-            colon_token,
-            pattern,
-            init,
-        } = node.as_fields();
-
-        write!(
-            f,
-            [
-                FormatMemberName::from(member?),
-                colon_token.format(),
-                space_token(),
-                pattern.format(),
-            ]
-        )?;
-
-        if let Some(init) = init {
-            write!(f, [space_token(), init.format()])?;
-        }
-
-        Ok(())
+        write!(f, [JsAnyAssignmentLike::from(node.clone())])
     }
 }
