6bb786661d3919ef2d8c7648b6f4f40f7589577f
xunilrj <xunilrj@hotmail.com>
2022-06-17 13:19:25 +0100

    fix event filtering in coverage

--- xtask/coverage/src/symbols/msts.rs
+++ xtask/coverage/src/symbols/msts.rs
@@ -78,12 +78,13 @@ impl TestCase for SymbolsMicrosoftTestCase {
                 // We filter any event pointing to string literals.
                 // We do the same below because TS classifies some string literals as symbols and we also
                 // filter them below.
-                let name = x.str(&code);
-                (name.contains('\"') || name.contains('\''))
-                    && matches!(
-                        x,
-                        SemanticEvent::DeclarationFound { .. } | SemanticEvent::Read { .. }
-                    )
+                match x {
+                    SemanticEvent::DeclarationFound { .. } | SemanticEvent::Read { .. } => {
+                        let name = x.str(&code);
+                        !name.contains('\"') && !name.contains('\'')
+                    }
+                    _ => false,
+                }
             })
             .collect();
         actual.sort_by_key(|x| x.range().start());
