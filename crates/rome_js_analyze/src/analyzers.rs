1d03ae50486f1e97e4ab78f219690d59a7b9b163
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-06-18 14:12:56 +0800

    feat: 🎸 no-empty-pattern

--- crates/rome_js_analyze/src/analyzers.rs
+++ crates/rome_js_analyze/src/analyzers.rs
@@ -8,6 +8,8 @@ mod no_delete;
 pub(crate) use no_delete::NoDelete;
 mod no_double_equals;
 pub(crate) use no_double_equals::NoDoubleEquals;
+mod no_empty_pattern;
+pub(crate) use no_empty_pattern::NoEmptyPattern;
 mod no_implicit_boolean;
 pub(crate) use no_implicit_boolean::NoImplicitBoolean;
 mod no_multiple_spaces_in_regular_expression_literals;
---
+++ crates/rome_js_analyze/src/analyzers/no_empty_pattern.rs
@@ -0,0 +1,83 @@
+use rome_analyze::{context::RuleContext, declare_rule, Rule, RuleCategory, RuleDiagnostic};
+use rome_console::markup;
+use rome_js_syntax::{JsArrayBindingPattern, JsObjectBindingPattern};
+use rome_rowan::{declare_node_union, AstNode, AstSeparatedList};
+
+declare_rule! {
+    /// Disallows empty destructuring patterns.
+    ///
+    /// ## Examples
+    ///
+    /// 
+    /// ### Invalid
+    ///
+    /// ```js,expect_diagnostic
+    ///    var {a = {}} = foo;
+    /// ```
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    pub(crate) NoEmptyPattern = "noEmptyPattern"
+}
+
+impl Rule for NoEmptyPattern {
+    const CATEGORY: RuleCategory = RuleCategory::Lint;
+
+    type Query = JsAnyBindPatternLike;
+    type State = ();
+
+    fn run(ctx: &RuleContext<Self>) -> Option<Self::State> {
+        match ctx.query() {
+            JsAnyBindPatternLike::JsArrayBindingPattern(array) => {
+                if array.elements().len() == 0 {
+                    Some(())
+                } else {
+                    None
+                }
+            }
+            JsAnyBindPatternLike::JsObjectBindingPattern(object) => {
+                if object.properties().len() == 0 {
+                    Some(())
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    fn diagnostic(ctx: &RuleContext<Self>, _: &Self::State) -> Option<RuleDiagnostic> {
+        let node = ctx.query();
+        let node_type = match node {
+            JsAnyBindPatternLike::JsArrayBindingPattern(_) => "array",
+            JsAnyBindPatternLike::JsObjectBindingPattern(_) => "object",
+        };
+
+        Some(RuleDiagnostic::warning(
+            node.range(),
+            markup! {
+                "Unexpected empty "{node_type}" pattern."
+            },
+        ))
+    }
+}
+
+declare_node_union! {
+    /// enum of `JsObjectBindingPattern` and `JsArrayBindingPattern`
+    pub(crate) JsAnyBindPatternLike = JsArrayBindingPattern | JsObjectBindingPattern
+}
