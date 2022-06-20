e63c9781d2f7cfeadc53efadbe925599d787a13b
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2022-06-20 09:52:29 +0300

    add spec for left-hand side layout for variable declaration

---
+++ crates/rome_js_formatter/tests/specs/js/module/declarations/variable_declaration.js
@@ -0,0 +1,19 @@
+//break left-hand side layout
+{
+    const {
+        id,
+        static: isStatic,
+        method: isMethod,
+        methodId,
+        getId,
+        setId,
+    } = privateNamesMap.get(name);
+
+    const { id1, method: isMethod1, methodId1 } = privateNamesMap.get(name);
+
+    const {
+        id3,
+        method: isMethod3,
+        methodId3,
+    } = anodyneCondosMalateOverateRetinol.get(bifornCringerMoshedPerplexSawder);
+}
