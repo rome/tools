ebe37353f4def931d76c647a1679a8e0a9ccd7ba
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-06-21 23:46:58 +0800

    style: 💄 fmt

--- crates/rome_js_analyze/src/analyzers/use_block_statements.rs
+++ crates/rome_js_analyze/src/analyzers/use_block_statements.rs
@@ -12,7 +12,7 @@ use crate::{use_block_statements_diagnostic, use_block_statements_replace_body};
 
 declare_rule! {
     /// Requires following curly brace conventions.
-    /// JavaScript allows the omission of curly braces when a block contains only one statement. However, it is considered by many to be best practice to never omit curly braces around blocks, even when they are optional, because it can lead to bugs and reduces code clarity. 
+    /// JavaScript allows the omission of curly braces when a block contains only one statement. However, it is considered by many to be best practice to never omit curly braces around blocks, even when they are optional, because it can lead to bugs and reduces code clarity.
     ///
     /// ## Examples
     ///
