5525f51146fed62edc77e6a90ecc08d1d15d4d1b
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-06-21 13:51:24 +0800

    test: 💍 update snapshot

--- crates/rome_js_analyze/tests/specs/useShorthandArrayType.ts
+++ crates/rome_js_analyze/tests/specs/useShorthandArrayType.ts
@@ -1,5 +1,9 @@
-// let valid: Array<foo>;
-// let invalid1: bar[];
-// let invalid2: Array<foo, Array<string>>;
-// let invalid3: Promise<Array<string>>;
-let invalid4: Array<Foo<Bar>>;
+// valid
+let valid: Array<Foo | Bar>;
+let valid: Array<keyof Bar>;
+let valid: Array<foo | bar>;
+// invalid
+let valid: Array<foo>;
+let invalid1: Array<foo, Array<string>>;
+let invalid2: Promise<Array<string>>;
+let invalid3: Array<Foo<Bar>>;
