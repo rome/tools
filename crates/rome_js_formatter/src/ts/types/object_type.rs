f378e33e891a6365c9be94bf222c914be745d48a
Emanuele Stoppa <my.burning@gmail.com>
2022-06-28 09:31:15 +0100

    feat(rome_js_formatter): type alias as assignment like (#2787)

--- crates/rome_js_formatter/src/ts/types/object_type.rs
+++ crates/rome_js_formatter/src/ts/types/object_type.rs
@@ -1,36 +1,13 @@
 use crate::prelude::*;
-use crate::utils::node_has_leading_newline;
-
+use crate::utils::JsObjectLike;
 use rome_formatter::write;
-use rome_js_syntax::{TsObjectType, TsObjectTypeFields};
+use rome_js_syntax::TsObjectType;
 
 #[derive(Debug, Clone, Default)]
 pub struct FormatTsObjectType;
 
 impl FormatNodeRule<TsObjectType> for FormatTsObjectType {
     fn fmt_fields(&self, node: &TsObjectType, f: &mut JsFormatter) -> FormatResult<()> {
-        let TsObjectTypeFields {
-            l_curly_token,
-            members,
-            r_curly_token,
-        } = node.as_fields();
-
-        if node_has_leading_newline(members.syntax()) {
-            write!(
-                f,
-                [
-                    format_delimited(&l_curly_token?, &members.format(), &r_curly_token?)
-                        .block_indent()
-                ]
-            )
-        } else {
-            write!(
-                f,
-                [
-                    format_delimited(&l_curly_token?, &members.format(), &r_curly_token?,)
-                        .soft_block_spaces()
-                ]
-            )
-        }
+        write!(f, [JsObjectLike::from(node.clone())])
     }
 }
---
+++ crates/rome_js_formatter/src/utils/object_like.rs
@@ -0,0 +1,81 @@
+use crate::prelude::*;
+use crate::utils::node_has_leading_newline;
+use crate::JsFormatContext;
+use rome_formatter::write;
+use rome_formatter::{Format, FormatResult};
+use rome_js_syntax::{JsObjectExpression, JsSyntaxToken, TsObjectType};
+use rome_rowan::{declare_node_union, AstNode, AstNodeList, AstSeparatedList, SyntaxResult};
+
+declare_node_union! {
+    pub (crate) JsObjectLike = JsObjectExpression | TsObjectType
+}
+impl JsObjectLike {
+    fn l_curly_token(&self) -> SyntaxResult<JsSyntaxToken> {
+        match self {
+            JsObjectLike::JsObjectExpression(oe) => oe.l_curly_token(),
+            JsObjectLike::TsObjectType(ot) => ot.l_curly_token(),
+        }
+    }
+    fn r_curly_token(&self) -> SyntaxResult<JsSyntaxToken> {
+        match self {
+            JsObjectLike::JsObjectExpression(oe) => oe.r_curly_token(),
+            JsObjectLike::TsObjectType(ot) => ot.r_curly_token(),
+        }
+    }
+
+    fn members_have_leading_newline(&self) -> bool {
+        match self {
+            JsObjectLike::JsObjectExpression(oe) => node_has_leading_newline(oe.members().syntax()),
+            JsObjectLike::TsObjectType(ot) => node_has_leading_newline(ot.members().syntax()),
+        }
+    }
+
+    fn members_are_empty(&self) -> bool {
+        match self {
+            JsObjectLike::JsObjectExpression(oe) => oe.members().is_empty(),
+            JsObjectLike::TsObjectType(ot) => ot.members().is_empty(),
+        }
+    }
+
+    fn write_members(&self, f: &mut JsFormatter) -> FormatResult<()> {
+        match self {
+            JsObjectLike::JsObjectExpression(oe) => {
+                write!(f, [oe.members().format()])
+            }
+            JsObjectLike::TsObjectType(ot) => {
+                write!(f, [ot.members().format()])
+            }
+        }
+    }
+}
+
+impl Format<JsFormatContext> for JsObjectLike {
+    fn fmt(&self, f: &mut JsFormatter) -> FormatResult<()> {
+        let members = format_with(|f| self.write_members(f));
+        if self.members_are_empty() {
+            write!(
+                f,
+                [
+                    format_delimited(&self.l_curly_token()?, &members, &self.r_curly_token()?)
+                        .soft_block_indent()
+                ]
+            )
+        } else if self.members_have_leading_newline() {
+            write!(
+                f,
+                [
+                    format_delimited(&self.l_curly_token()?, &members, &self.r_curly_token()?)
+                        .block_indent()
+                ]
+            )
+        } else {
+            write!(
+                f,
+                [
+                    format_delimited(&self.l_curly_token()?, &members, &self.r_curly_token()?)
+                        .soft_block_spaces()
+                ]
+            )
+        }
+    }
+}
