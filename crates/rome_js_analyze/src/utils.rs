c71e718f36474dcbabfa6fe67173004c2132ba6e
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-01 00:10:55 +0800

    feat: 🎸 init

--- crates/rome_js_analyze/src/utils.rs
+++ crates/rome_js_analyze/src/utils.rs
@@ -28,3 +28,34 @@ impl<'a> Iterator for InterpretEscapedString<'a> {
 pub fn interpret_escaped_string(s: &str) -> Result<String, MyError> {
     (InterpretEscapedString { s: s.chars() }).collect()
 }
+
+#[derive(Debug, PartialEq)]
+pub enum MyError {
+    EscapeAtEndOfString,
+    InvalidEscapedChar(char),
+}
+
+struct InterpretEscapedString<'a> {
+    s: std::str::Chars<'a>,
+}
+
+impl<'a> Iterator for InterpretEscapedString<'a> {
+    type Item = Result<char, MyError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.s.next().map(|c| match c {
+            '\\' => match self.s.next() {
+                None => Err(MyError::EscapeAtEndOfString),
+                Some('n') => Ok('\n'),
+                Some('\\') => Ok('\\'),
+                // etc.
+                Some(c) => Err(MyError::InvalidEscapedChar(c)),
+            },
+            c => Ok(c),
+        })
+    }
+}
+
+pub fn interpret_escaped_string(s: &str) -> Result<String, MyError> {
+    (InterpretEscapedString { s: s.chars() }).collect()
+}
