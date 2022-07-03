60358797b7c66a52f78538368178009d86f09a1b
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-03 15:23:02 +0800

    feat: 🎸 finish useTemplate

--- crates/rome_js_analyze/src/analyzers/use_template.rs
+++ crates/rome_js_analyze/src/analyzers/use_template.rs
@@ -26,13 +26,23 @@ declare_rule! {
     /// ```js,expect_diagnostic
     /// console.log(1 * 2 + "foo");
     /// ```
-    ///
+    /// ```js,expect_diagnostic
+    /// console.log(1 + "foo" + 2 + "bar" + "baz" + 3);
+    /// ```
     /// ```js,expect_diagnostic
     /// console.log((1 + "foo") * 2);
     /// ```
-    ///
+    /// ```js,expect_diagnostic
+    /// console.log(1 * (2 + "foo") + "bar");
+    /// ```
     /// ```js,expect_diagnostic
     /// console.log("foo" + 1);
+    /// ```
+    /// ```js,expect_diagnostic
+    /// console.log("foo" + `bar${`baz${"bat" + "bam"}`}` + "boo");
+    /// ```
+    /// ```js,expect_diagnostic
+    /// console.log("foo" + 1 + 2);
     /// ```
     ///
     /// ### Valid
@@ -64,6 +74,7 @@ impl Rule for UseTemplate {
                         )
                     )
                 }) {
+                    // println!("{:#?}", collections);
                     return Some(collections);
                 }
             }
@@ -95,7 +106,7 @@ impl Rule for UseTemplate {
         Some(JsRuleAction {
             category: ActionCategory::QuickFix,
             applicability: Applicability::MaybeIncorrect,
-            message: markup! { "Use a "<Emphasis>"TemplateLiteral"</Emphasis>"." }.to_owned(),
+            message: markup! { "Use a while loop" }.to_owned(),
             root: next_root,
         })
     }
@@ -126,7 +137,7 @@ fn convert_expressions_to_js_template(exprs: &Vec<JsAnyExpression>) -> Option<Js
             }
             _ => {
                 // drop the trivia of original expression make the generated `JsTemplate` a little nicer, if we don't do this
-                // the `1 * (2 + "foo") + "bar"` will become
+                // the `1 * (2 + "foo") + "bar"` will be convert to
                 // ```js
                 // `${1 * (2 + "foo") }bar`
                 // ```
@@ -161,14 +172,6 @@ fn convert_expressions_to_js_template(exprs: &Vec<JsAnyExpression>) -> Option<Js
     )
 }
 
-/// Flatten a [JsTemplateElementList] of [JsTemplate] which could possibly be recursive, into a `Vec<JsAnyTemplateElement>`
-/// ## Example
-/// flatten
-/// ```js
-/// `${1 + 2 + `${a}test` }bar`
-/// ```
-/// into
-/// `[1, 2, a, "test", "bar"]`
 fn flatten_template_element_list(list: JsTemplateElementList) -> Option<Vec<JsAnyTemplateElement>> {
     let mut ret = Vec::with_capacity(list.len());
     for element in list {
@@ -238,10 +241,9 @@ fn is_un_necessary_string_concat_expression(node: &JsBinaryExpression) -> Option
     None
 }
 
-/// Convert [JsBinaryExpression] recursively only if the `operator` is `+` into Vec<[JsAnyExpression]>
+/// Convert [JsBinaryExpression] to Vec<[JsAnyExpression]>
 /// ## Example
-/// - from: `1 + 2 + 3 + (1 * 2)`
-/// - to: `[1, 2, 3, (1 * 2)]`
+/// 1 + 2 + 3 + (1 * 2) -> `[1, 2, 3, (1 * 2)]`
 fn collect_binary_add_expression(node: &JsBinaryExpression) -> Option<Vec<JsAnyExpression>> {
     let mut result = vec![];
     match node.left().ok()? {
@@ -266,3 +268,169 @@ fn collect_binary_add_expression(node: &JsBinaryExpression) -> Option<Vec<JsAnyE
     };
     Some(result)
 }
+
+// fn zip_template_literal_parts(template: &JsTemplate) -> Option<Vec<TemplatePart>> {
+//     let elements_length = template.elements().len();
+//     let mut res = Vec::with_capacity(elements_length);
+//     for ele in template.elements().into_iter() {
+//         match ele {
+//             JsAnyTemplateElement::JsTemplateChunkElement(e) => {
+//                 res.push(TemplatePart::JsTemplateChunkElement(e));
+//             }
+//             JsAnyTemplateElement::JsTemplateElement(e) => {
+//                 res.push(TemplatePart::JsAnyExpression(e.expression().ok()?))
+//             }
+//         }
+//     }
+//     Some(res)
+// }
+
+// function flattenExpressionsToTemplateParts(
+// 	expressions: AnyJSExpression[],
+// ): TemplatePart[] {
+// 	let parts: TemplatePart[] = [];
+// 	let queue: TemplatePart[] = [...expressions];
+
+// 	while (true) {
+// 		let node = queue.shift();
+// 		if (!node) {
+// 			break;
+// 		}
+
+// 		if (node.type === "JSTemplateLiteral") {
+// 			queue = [...zipTemplateLiteralParts(node), ...queue];
+// 		} else {
+// 			parts.push(node);
+// 		}
+// 	}
+
+// 	return parts;
+// }
+
+// fn flatten_expressions_to_template_parts(
+//     exprs: &Vec<JsAnyExpression>,
+// ) -> Option<Vec<TemplatePart>> {
+//     let mut res = Vec::with_capacity(exprs.len());
+//     for expr in exprs.iter() {
+//         match expr {
+//             // flatten templates, for edge case like
+//             // ```js
+//             // `${`${a}`}`
+//             // ```
+//             JsAnyExpression::JsTemplate(template) => {
+//                 res.extend(flatten_template_parts(zip_template_literal_parts(
+//                     template,
+//                 )?)?);
+//             }
+//             // TemplatePart::JsTemplateChunkElement(_) => {
+//             //     res.push(expr);
+//             // }
+//             _ => res.push(TemplatePart::JsAnyExpression(expr.clone())),
+//         }
+//     }
+//     Some(res)
+// }
+
+// fn flatten_template_parts(parts: Vec<TemplatePart>) -> Option<Vec<TemplatePart>> {
+//     let mut res = Vec::with_capacity(parts.len());
+//     for expr in parts.into_iter() {
+//         match expr {
+//             TemplatePart::JsAnyExpression(JsAnyExpression::JsTemplate(template)) => {
+//                 res.extend(zip_template_literal_parts(&template)?);
+//             }
+//             _ => {
+//                 res.push(expr);
+//             }
+//         }
+//     }
+//     Some(res)
+// }
+
+// /// 'str' + expr + 'str' -> `str${expr}str`
+// /// Convert `Vec<TemplateParts>` into a `JsTemplate`
+// fn combine_template_parts(expressions: Vec<TemplatePart>) -> Option<JsTemplate> {
+//     let mut reduced_expressions = Vec::with_capacity(expressions.len());
+//     let mut index = 0;
+//     println!("{:?}", expressions);
+//     while index < expressions.len() {
+//         let cur: &TemplatePart = &expressions[index];
+//         match cur {
+//             TemplatePart::JsAnyExpression(JsAnyExpression::JsAnyLiteralExpression(
+//                 JsAnyLiteralExpression::JsStringLiteralExpression(_),
+//             ))
+//             | TemplatePart::JsTemplateChunkElement(_) => {
+//                 let mut strings = vec![cur.text_trimmed()?.to_string()];
+//                 while index + 1 < expressions.len() {
+//                     let next: &TemplatePart = &expressions[index + 1];
+
+//                     if matches!(
+//                         next,
+//                         TemplatePart::JsAnyExpression(JsAnyExpression::JsAnyLiteralExpression(
+//                             JsAnyLiteralExpression::JsStringLiteralExpression(_),
+//                         )) | TemplatePart::JsTemplateChunkElement(_),
+//                     ) {
+//                         strings.push(next.text_trimmed()?.to_string());
+//                         index += 1;
+//                     } else {
+//                         break;
+//                     }
+//                 }
+//                 reduced_expressions.push(TemplatePart::JsTemplateChunkElement(
+//                     make::js_template_chunk_element(SyntaxToken::new_detached(
+//                         JsSyntaxKind::JS_TEMPLATE_CHUNK_ELEMENT,
+//                         &strings.join(""),
+//                         [],
+//                         [],
+//                     )),
+//                 ))
+//                 // reduced_expressions.push(JsAnyExpression::JsAnyLiteralExpression(
+//                 //     JsAnyLiteralExpression::JsStringLiteralExpression(
+//                 //         make::js_string_literal_expression(make::js_string_literal(
+//                 //         )),
+//                 //     ),
+//                 // ));
+//             }
+//             TemplatePart::JsAnyExpression(_) => {
+//                 reduced_expressions.push(cur.clone());
+//             }
+//         }
+//         index += 1;
+//     }
+//     let template_element_list_iter = reduced_expressions.into_iter().map(|expr| match expr {
+//         TemplatePart::JsAnyExpression(expr) => {
+//             JsAnyTemplateElement::JsTemplateElement(make::js_template_element(
+//                 SyntaxToken::new_detached(JsSyntaxKind::DOLLAR_CURLY, "${", [], []),
+//                 expr,
+//                 SyntaxToken::new_detached(JsSyntaxKind::DOLLAR_CURLY, "}", [], []),
+//             ))
+//         }
+//         TemplatePart::JsTemplateChunkElement(expr) => {
+//             JsAnyTemplateElement::JsTemplateChunkElement(expr)
+//         }
+//     });
+//     Some(
+//         make::js_template(
+//             make::token(T!['`']),
+//             make::js_template_element_list(template_element_list_iter),
+//             make::token(T!['`']),
+//         )
+//         .build(),
+//     )
+// }
+
+// declare_node_union! {
+//     /// Matches an if statement or a conditional expression
+//     pub(crate) TemplatePart = JsAnyExpression | JsTemplateChunkElement
+// }
+
+// impl TemplatePart {
+//     fn text_trimmed(&self) -> Option<SyntaxNodeText> {
+//         match self {
+//             TemplatePart::JsAnyExpression(JsAnyExpression::JsAnyLiteralExpression(
+//                 rome_js_syntax::JsAnyLiteralExpression::JsStringLiteralExpression(string),
+//             )) => Some(string.syntax().text_trimmed()),
+//             TemplatePart::JsTemplateChunkElement(element) => Some(element.syntax().text_trimmed()),
+//             _ => None,
+//         }
+//     }
+// }
