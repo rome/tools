61fe02ac54be17cb470979cc76296b6beeb02253
l3ops <leo@rome.tools>
2022-07-06 11:13:11 +0200

    fix(rome_js_analyze): add missing control flow root nodes

---
+++ crates/rome_js_analyze/tests/specs/noDeadCode/NestedFunctions.js
@@ -0,0 +1,50 @@
+const JsFunctionExpression = function () {
+    return;
+}
+
+function JsFunctionDeclaration() {
+    return;
+}
+
+const JsArrowFunctionExpression = function () {
+    return;
+}
+
+export default function JsFunctionExportDefaultDeclaration() {
+    return;
+}
+
+const object = {
+    get JsGetterObjectMember() {
+        return;
+    },
+
+    set JsSetterObjectMember(value) {
+        return;
+    },
+
+    JsMethodObjectMember() {
+        return;
+    },
+};
+
+class Class {
+    // JsConstructorClassMember
+    constructor() {
+        return;
+    }
+
+    JsMethodClassMember() {
+        return;
+    }
+
+    get JsGetterClassMember() {
+        return;
+    }
+
+    set JsSetterClassMember(value) {
+        return;
+    }
+}
+
+statement();
