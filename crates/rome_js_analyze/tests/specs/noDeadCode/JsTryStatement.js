d95e02c8eb2e9f22aeb8547985a318e4a05e9630
l3ops <leops@users.noreply.github.com>
2022-07-07 08:11:17 +0200

    feat(rome_control_flow): introduce explicit exception control flow (#2821)

--- crates/rome_js_analyze/tests/specs/noDeadCode/JsTryStatement.js
+++ crates/rome_js_analyze/tests/specs/noDeadCode/JsTryStatement.js
@@ -1,11 +1,39 @@
 function JsTryStatement1() {
     try {
-        test();
+        tryBlock();
         return;
     } catch (err) {
-        test();
+        catchClause();
         return;
     }
 
     afterTryCatchReturn();
+}
+
+function JsTryStatement2() {
+    try {
+        tryBlock();
+        return;
+    } catch (err) {
+        catchClause();
+    }
+
+    afterTryCatchReturn();
+}
+
+function JsTryStatement3() {
+    return;
+
+    try {
+        tryBlock();
+    } catch (err) {
+        catchClause();
+    }
+}
+
+function JsTryStatement4() {
+    try {
+    } catch (err) {
+        catchClause();
+    }
 }
