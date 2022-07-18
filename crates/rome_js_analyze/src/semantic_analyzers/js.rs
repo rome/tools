0f4a6cbfb89383547cdfe1d37f54760c3a2decb5
xunilrj <xunilrj@hotmail.com>
2022-07-18 07:59:48 +0100

    lint rule no shouty constants

--- crates/rome_js_analyze/src/semantic_analyzers/js.rs
+++ crates/rome_js_analyze/src/semantic_analyzers/js.rs
@@ -4,4 +4,5 @@ use rome_analyze::declare_group;
 mod no_arguments;
 mod no_catch_assign;
 mod no_label_var;
-declare_group! { pub (crate) Js { name : "js" , rules : [no_arguments :: NoArguments , no_catch_assign :: NoCatchAssign , no_label_var :: NoLabelVar ,] } }
+mod no_shouty_constants;
+declare_group! { pub (crate) Js { name : "js" , rules : [no_arguments :: NoArguments , no_catch_assign :: NoCatchAssign , no_label_var :: NoLabelVar , no_shouty_constants :: NoShoutyConstants ,] } }
---
+++ crates/rome_js_analyze/tests/specs/js/noShoutyConstants.js
@@ -0,0 +1,7 @@
+const FOO = "FOO";
+console.log(FOO, FOO2);
+
+// rome-ignore lint(js/useSingleVarDeclarator): ignore useSingleVarDeclarator
+const FOO2 = "FOO2", a = "FOO3", FOO4 = "FOO4";
+
+console.log(FOO, FOO4);
