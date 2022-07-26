0e7aaa505470b32c6d7766e8352accd1d5af228b
Emanuele Stoppa <my.burning@gmail.com>
2022-07-26 16:26:17 +0100

    chore: init message using newlines and tabs (#2931)

--- crates/rome_cli/src/commands/init.rs
+++ crates/rome_cli/src/commands/init.rs
@@ -7,35 +7,19 @@ pub(crate) fn init(mut session: CliSession) -> Result<(), Termination> {
     let fs = &mut session.app.fs;
     create_config(fs, Configuration::default())?;
     let message = markup! {
-                <Info><Emphasis>"Files created:
-        
-"</Emphasis></Info>
-                <Emphasis>"- rome.json: "</Emphasis>"Your project configuration. Documentation: "<Underline>"https://rome.tools/#project-configuration"</Underline>
+                    <Info><Emphasis>"Files created:\n\n"</Emphasis></Info>
+                    <Emphasis>"\t- rome.json: "</Emphasis>"Your project configuration. Documentation: "<Underline>"https://rome.tools/#project-configuration \n\n"</Underline>
+    <Info><Emphasis>"What's next?\n\n"</Emphasis></Info>
+    "\t1. "<Emphasis>"Setup an editor extension\n"</Emphasis>
+            "\t\tGet live errors as you type and format when you save. Learn more:"<Underline>"https://rome.tools/#install-official-rome-vs-code-extension \n"</Underline>
+    "\t2. "<Emphasis>"Try a command\n"</Emphasis>
+            <Italic>"\t\trome check"</Italic>" is used to validate your code, verify formatting, and check for lint errors. Run " <Italic>"rome --help"</Italic>" for a full list of commands and flags\n"
+    "\t3. "<Emphasis>"Read documentation \n"</Emphasis>
+            "\t\tOur website serves as a comprehensive source of guides and documentation: "<Underline>"https://rome.tools/ \n"</Underline>
+    "\t4. "<Emphasis>"Get involved in the community\n"</Emphasis>
+            "\t\tAsk questions, get support, or contribute by participating on GitHub ("<Underline>"https://github.com/rome/tools"</Underline>"), or our community Discord ("<Underline>"https://discord.gg/rome"</Underline>")"
 
-
-    <Info><Emphasis>"
-
-What's next?
-    
-"</Emphasis></Info>
-"   
-    1. "<Emphasis>"Setup an editor extension      
-"</Emphasis>
-    "       Get live errors as you type and format when you save. Learn more:"<Underline>"https://rome.tools/#install-official-rome-vs-code-extension"</Underline>
-"   
-    2. "<Emphasis>"Try a command
-"</Emphasis>
-        <Italic>"      rome check"</Italic>" is used to validate your code, verify formatting, and check for lint errors. Run " <Italic>"rome --help"</Italic>" for a full list of commands and flags"
-"   
-    3. "<Emphasis>"Read documentation 
-"</Emphasis>
-        "       Our website serves as a comprehensive source of guides and documentation: "<Underline>"https://rome.tools/"</Underline>
-"
-    4. "<Emphasis>"Get involved in the community
-"</Emphasis>
-        "       Ask questions, get support, or contribute by participating on GitHub ("<Underline>"https://github.com/rome/tools"</Underline>"), or our community Discord ("<Underline>"https://discord.gg/rome"</Underline>")"
-
-                };
+                    };
 
     session.app.console.log(message);
 
