07032eb8d6e614347a7823ec41116176bc79d110
Micha Reiser <micha@rome.tools>
2022-07-29 17:09:37 +0200

    test(rome_js_formatter): Only snapshot not matching prettier tests (#2968)

--- crates/rome_js_formatter/tests/prettier_tests.rs
+++ crates/rome_js_formatter/tests/prettier_tests.rs
@@ -1,6 +1,7 @@
 use parking_lot::{const_mutex, Mutex};
 use rome_rowan::{TextRange, TextSize};
-use similar::{utils::diff_lines, Algorithm, ChangeTag};
+use similar::{utils::diff_lines, Algorithm, ChangeTag, TextDiff};
+use std::fs::remove_file;
 use std::{
     env,
     ffi::OsStr,
@@ -21,6 +22,11 @@ use rome_js_syntax::SourceType;
 use serde::Serialize;
 
 use crate::check_reformat::CheckReformatParams;
+
+#[derive(serde::Serialize)]
+struct TestInfo {
+    test_file: String,
+}
 
 mod check_reformat;
 
@@ -114,18 +120,95 @@ fn test_snapshot(input: &'static str, _: &str, _: &str, _: &str) {
 
     let formatted = formatted.replace(ROME_IGNORE, PRETTIER_IGNORE);
 
-    let mut snapshot = String::new();
+    let root_path = Path::new(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/specs/prettier/"
+    ));
 
+    let relative_file_name = input_file
+        .strip_prefix(root_path)
+        .unwrap_or_else(|_| {
+            panic!(
+                "failed to strip prefix {:?} from {:?}",
+                root_path, input_file
+            )
+        })
+        .to_str()
+        .unwrap();
+
+    let input_extension = input_file.extension().and_then(OsStr::to_str);
+
+    let prettier_snapshot_path = input_extension
+        .map(|ext| input_file.with_extension(format!("{}.prettier-snap", ext)))
+        .filter(|path| path.exists());
+
+    let prettier_diff = match prettier_snapshot_path {
+        Some(prettier_snapshot_path) => {
+            let mut prettier_snapshot = read_to_string(prettier_snapshot_path).unwrap();
+
+            strip_placeholders(&mut prettier_snapshot);
+
+            DiffReport::get().report(relative_file_name, &formatted, &prettier_snapshot);
+
+            if formatted == prettier_snapshot {
+                // The output matches prettier's output. There's no need for a snapshot that duplicates the output.
+                // Delete the snapshot file if it already exists, otherwise return early to not create a new snapshot.
+                if let Some(input_extension) = input_extension {
+                    let snapshot_file_name =
+                        input_file.with_extension(format!("{}.snap", input_extension));
+
+                    if snapshot_file_name.exists() && snapshot_file_name.is_file() {
+                        remove_file(snapshot_file_name).ok(); // not the end of the world if it fails
+                    }
+
+                    let new_snapshot_file_name =
+                        input_file.with_extension(format!("{}.snap.new", input_extension));
+                    if new_snapshot_file_name.exists() && new_snapshot_file_name.is_file() {
+                        remove_file(new_snapshot_file_name).ok(); // not the end of the world if it fails
+                    }
+                }
+
+                return;
+            } else {
+                let mut prettier_differences = Vec::new();
+
+                TextDiff::from_lines(&prettier_snapshot, &formatted)
+                    .unified_diff()
+                    .header("Prettier", "Rome")
+                    .to_writer(&mut prettier_differences)
+                    .unwrap();
+
+                Some(String::from_utf8(prettier_differences).expect("Input file to be in UTF8"))
+            }
+        }
+        None => None,
+    };
+
+    let mut snapshot = String::new();
+    writeln!(snapshot).unwrap();
     writeln!(snapshot, "# Input").unwrap();
+    writeln!(snapshot).unwrap();
     writeln!(snapshot, "```js").unwrap();
-    writeln!(snapshot, "{}", input_code).unwrap();
+    snapshot.push_str(&input_code);
     writeln!(snapshot, "```").unwrap();
     writeln!(snapshot).unwrap();
+    writeln!(snapshot).unwrap();
+
+    if let Some(prettier_diff) = prettier_diff {
+        writeln!(snapshot, "# Prettier differences").unwrap();
+        writeln!(snapshot).unwrap();
+        writeln!(snapshot, "```diff").unwrap();
+        snapshot.push_str(&prettier_diff);
+        writeln!(snapshot, "```").unwrap();
+        writeln!(snapshot).unwrap();
+    }
 
     writeln!(snapshot, "# Output").unwrap();
+    writeln!(snapshot).unwrap();
     writeln!(snapshot, "```js").unwrap();
-    writeln!(snapshot, "{}", formatted).unwrap();
+    snapshot.push_str(&formatted);
     writeln!(snapshot, "```").unwrap();
+    writeln!(snapshot).unwrap();
     writeln!(snapshot).unwrap();
 
     if has_errors {
@@ -175,43 +258,18 @@ fn test_snapshot(input: &'static str, _: &str, _: &str, _: &str) {
         writeln!(snapshot, "```").unwrap();
     }
 
+    let info = TestInfo {
+        test_file: relative_file_name.to_owned(),
+    };
+
     insta::with_settings!({
         prepend_module_to_snapshot => false,
         snapshot_path => input_file.parent().unwrap(),
+        omit_expression => true,
+        info => &info
     }, {
-        insta::assert_snapshot!(file_name, snapshot, file_name);
+        insta::assert_snapshot!(file_name, snapshot);
     });
-
-    let snapshot_file = input_file
-        .extension()
-        .and_then(OsStr::to_str)
-        .map(|ext| input_file.with_extension(format!("{}.prettier-snap", ext)))
-        .filter(|path| path.exists());
-
-    if let Some(snapshot_file) = snapshot_file {
-        let mut content = read_to_string(snapshot_file).unwrap();
-
-        strip_placeholders(&mut content);
-
-        let root_path = Path::new(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/tests/specs/prettier/"
-        ));
-
-        let input_file = input_file.strip_prefix(root_path).unwrap_or_else(|_| {
-            panic!(
-                "failed to strip prefix {:?} from {:?}",
-                root_path, input_file
-            )
-        });
-        if formatted != content {
-            let input_file = input_file.to_str().unwrap();
-            DiffReport::get().report_diff(input_file, formatted, content);
-        } else {
-            let input_file = input_file.to_str().unwrap();
-            DiffReport::get().report_match(input_file, formatted, content);
-        }
-    }
 }
 
 /// Find and replace the cursor, range start and range end placeholders in a
@@ -261,15 +319,6 @@ fn strip_placeholders(input_code: &mut String) -> (Option<usize>, Option<usize>,
     (cursor_index, range_start_index, range_end_index)
 }
 
-/// This enum type is used to represent if our formatting result is expected
-/// [MatchCategory::Diff] means that our formatting result have some thing different from the expected
-/// [MatchCategory::Match] means that our formatting result is the same as the expected
-#[derive(Debug, PartialEq, Eq)]
-enum MatchCategory {
-    Diff,
-    Match,
-}
-
 #[derive(Debug, PartialEq, Eq)]
 enum ReportType {
     Json,
@@ -281,7 +330,7 @@ struct SingleFileMetricData {
     filename: String,
     single_file_compatibility: f64,
     #[serde(skip)]
-    diff: String,
+    diff: Option<String>,
 }
 
 #[derive(Clone, Debug, Default, Serialize)]
@@ -307,7 +356,6 @@ struct DiffReportItem {
     file_name: &'static str,
     rome_formatted_result: String,
     prettier_formatted_result: String,
-    match_category: MatchCategory,
 }
 struct DiffReport {
     state: Mutex<Vec<DiffReportItem>>,
@@ -343,31 +391,16 @@ impl DiffReport {
         &REPORTER
     }
 
-    fn report_diff(
+    fn report(
         &self,
         file_name: &'static str,
-        rome_formatted_result: String,
-        prettier_formatted_result: String,
+        rome_formatted_result: &str,
+        prettier_formatted_result: &str,
     ) {
         self.state.lock().push(DiffReportItem {
             file_name,
-            rome_formatted_result,
-            prettier_formatted_result,
-            match_category: MatchCategory::Diff,
-        });
-    }
-
-    fn report_match(
-        &self,
-        file_name: &'static str,
-        rome_formatted_result: String,
-        prettier_formatted_result: String,
-    ) {
-        self.state.lock().push(DiffReportItem {
-            file_name,
-            rome_formatted_result,
-            prettier_formatted_result,
-            match_category: MatchCategory::Match,
+            rome_formatted_result: rome_formatted_result.to_owned(),
+            prettier_formatted_result: prettier_formatted_result.to_owned(),
         });
     }
     fn print(&self) {
@@ -398,27 +431,30 @@ impl DiffReport {
     fn report_prettier(&self, report_type: ReportType, report_filename: String) {
         let mut state = self.state.lock();
         state.sort_by_key(|DiffReportItem { file_name, .. }| *file_name);
+
         let mut report_metric_data = PrettierCompatibilityMetricData::default();
-        let mut sum_of_single_compatibility_file = 0_f64;
-        let mut total_line = 0;
-        let mut total_matched_line = 0;
+        let mut file_ratio_sum = 0_f64;
+        let mut total_lines = 0;
+        let mut total_matched_lines = 0;
         let mut file_count = 0;
+
         for DiffReportItem {
             file_name,
             rome_formatted_result,
             prettier_formatted_result,
-            match_category,
         } in state.iter()
         {
-            let mut single_file_metric_data = SingleFileMetricData::default();
             file_count += 1;
+
             let rome_lines = rome_formatted_result.lines().count();
             let prettier_lines = prettier_formatted_result.lines().count();
-            let mut matched_lines = 0;
-            let mut single_file_compatibility = 1f64;
-            let mut diff = String::new();
-            if *match_category == MatchCategory::Diff {
-                writeln!(diff, "```diff").unwrap();
+
+            let (matched_lines, ratio, diff) = if rome_formatted_result == prettier_formatted_result
+            {
+                (rome_lines, 1f64, None)
+            } else {
+                let mut matched_lines = 0;
+                let mut diff = String::new();
 
                 for (tag, line) in diff_lines(
                     Algorithm::default(),
@@ -426,33 +462,37 @@ impl DiffReport {
                     rome_formatted_result,
                 ) {
                     if matches!(tag, ChangeTag::Equal) {
-                        matched_lines += line.lines().count();
+                        matched_lines += 1;
                     }
+
                     let line = line.strip_suffix('\n').unwrap_or(line);
                     writeln!(diff, "{}{}", tag, line).unwrap();
                 }
 
-                single_file_compatibility =
+                let single_file_compatibility =
                     matched_lines as f64 / rome_lines.max(prettier_lines) as f64;
-                sum_of_single_compatibility_file += single_file_compatibility;
-                writeln!(diff, "```").unwrap();
-            } else {
-                // in this branch `rome_lines` == `prettier_lines` == `matched_lines`
-                assert!(rome_lines == prettier_lines);
-                matched_lines = rome_lines;
-                sum_of_single_compatibility_file += single_file_compatibility;
-            }
-            total_line += rome_lines.max(prettier_lines);
-            total_matched_line += matched_lines;
-            single_file_metric_data.diff = diff;
-            single_file_metric_data.filename = file_name.to_string();
-            single_file_metric_data.single_file_compatibility = single_file_compatibility;
+
+                (matched_lines, single_file_compatibility, Some(diff))
+            };
+
+            total_lines += rome_lines.max(prettier_lines);
+            total_matched_lines += matched_lines;
+            file_ratio_sum += ratio;
+
+            let single_file_metric_data = SingleFileMetricData {
+                diff,
+                filename: file_name.to_string(),
+                single_file_compatibility: ratio,
+            };
+
             report_metric_data.files.push(single_file_metric_data);
         }
+
         report_metric_data.file_based_average_prettier_similarity =
-            sum_of_single_compatibility_file / file_count as f64;
+            file_ratio_sum / file_count as f64;
         report_metric_data.line_based_average_prettier_similarity =
-            total_matched_line as f64 / total_line as f64;
+            total_matched_lines as f64 / total_lines as f64;
+
         match report_type {
             ReportType::Json => self.report_json(report_filename, report_metric_data),
             ReportType::Markdown => self.report_markdown(report_filename, report_metric_data),
@@ -472,7 +512,12 @@ impl DiffReport {
         } in report_metric_data.files.iter()
         {
             writeln!(report, "# {}", filename).unwrap();
-            write!(report, "{}", diff).unwrap();
+
+            if let Some(diff) = diff {
+                writeln!(report, "```diff").unwrap();
+                writeln!(report, "{diff}").unwrap();
+                writeln!(report, "```").unwrap()
+            }
             writeln!(
                 report,
                 "**Prettier Similarity**: {:.2}%",
