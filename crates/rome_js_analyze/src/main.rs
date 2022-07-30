b8685d533943bdc60e5344f5d47b8687407c3e73
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-07-30 17:08:12 +0800

    fix: 🐛 function expression should push new scope

---
+++ crates/rome_js_analyze/src/main.rs
@@ -0,0 +1,44 @@
+use rome_js_syntax::JsIdentifierBinding;
+
+fn main() {
+    use rome_js_semantic::{semantic_model, SemanticScopeExtensions};
+    use rome_js_syntax::{JsReferenceIdentifier, SourceType};
+    use rome_rowan::{AstNode, SyntaxNodeCast};
+
+    let source = r#"
+        // function a({a, b}, {c, d}){}
+        let result = function(a, b, c, d){
+        }
+        // let result = 3;
+    "#;
+    let r = rome_js_parser::parse(source, 0, SourceType::js_module());
+    use rome_js_parser::*;
+    use rome_js_semantic::*;
+    use rome_js_syntax::*;
+    let tree = parse(source, 0, SourceType::js_module());
+    for e in semantic_events(tree.syntax()) {
+        dbg!(e);
+    }
+    // let model = semantic_model(&r.tree());
+
+    // let arguments_reference = r
+    //     .syntax()
+    //     .descendants()
+    //     .filter_map(|x| x.cast::<JsIdentifierBinding>())
+    //     .find(|x| x.text() == "a")
+    //     .unwrap();
+
+    // // println!("{}", arguments_reference);
+    // let block_scope = model.scope(&arguments_reference.syntax());
+    // // let hoisted_scope = model.scope_hoisted_to(arguments_reference.syntax()).unwrap();
+    // // or
+    // for binding in block_scope.bindings() {
+    //     dbg!(binding.syntax());
+    // }
+    // println!("----------------");
+    // for binding in hoisted_scope.bindings() {
+    //     dbg!(binding.syntax());
+    // }
+    // dbg!(&block_scope.);
+    // let block_scope = arguments_reference.scope(&model);
+}
