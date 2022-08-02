9fb6a7cf459c90dc4518087d8bc425d14d9f23df
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-08-02 19:19:35 +0800

    fix: broken ci (#2989)

--- crates/rome_js_analyze/tests/specs/jsx/noImplicitBoolean.jsx
+++ crates/rome_js_analyze/tests/specs/jsx/noImplicitBoolean.jsx
@@ -5,7 +5,7 @@
 <input disabled={undefined} />;
 <input disabled="false" />;
 // https://github.com/rome/tools/issues/2944
-<div className={asdf asdf} />;
+// <div className={asdf asdf} />;
 
 //invalid
 <input disabled />;
