12dc6292143f5ca78bffaf74b7d55fb5b9c679d4
xunilrj <xunilrj@hotmail.com>
2022-08-03 14:47:50 +0100

    moving all benchmarks to another crate

--- xtask/libs_bench/bins/contains_iai.rs
+++ xtask/libs_bench/bins/contains_iai.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, process::Command};
 
 fn main() {
     let result = Command::new("cargo")
-        .args(["bench", "-p", "rome_js_analyze"])
+        .args(["bench", "-p", "xtask_libs_bench"])
         .output();
     let re = Regex::new(r#"(?P<NAME>\w*?)\s*Instructions:\s*(?P<INST>\d*).*\n\s*L1 Accesses:\s*(?P<L1>\d*).*\n\s*L2 Accesses:\s*(?P<L2>\d*).*\n\s*RAM Accesses:\s*(?P<RAM>\d*).*\n\s*Estimated Cycles:\s*(?P<CYCLES>\d*).*"#).unwrap();
     let stdout = String::from_utf8(result.unwrap().stdout).unwrap();
