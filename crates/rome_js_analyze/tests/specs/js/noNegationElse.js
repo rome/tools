fca0e21a7f78142db71cc706ac2a77c2fb6c6324
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-08-04 16:11:15 +0800

    fix: 🐛 cr issue

--- crates/rome_js_analyze/tests/specs/js/noNegationElse.js
+++ crates/rome_js_analyze/tests/specs/js/noNegationElse.js
@@ -1,18 +1,21 @@
 // valid
-if (!true) {consequent;};
-// https://github.com/rome/tools/issues/2999
+if (!true) {
+	consequent;
+}
+true ? consequent : alternate;
+// invalid
+if (!true) {
+	consequent;
+} else {
+	alternate;
+}
+!condition ? consequent : alternate;
+
+let a = !test ? c : d;
+
+//valid https://github.com/rome/tools/issues/2999 
 if (!/^NCT/.test(input)) {
 	messages.push("NCT Number must start with NCT");
 } else if (!/^NCT\d{8}$/.test(input)) {
 	messages.push("NCT Number must have exactly 8 digits after NCT");
 }
-true ? consequent : alternate;
-// invalid
-if (!true) {
-  consequent;
-} else {
-  alternate;
-}
-!condition ? consequent : alternate;
-
-let a = !test ? c : d;
