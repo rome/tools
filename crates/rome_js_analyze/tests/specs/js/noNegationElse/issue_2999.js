33a26bae8c7eec6bfa609d1539ee03a85a825e7e
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-08-04 16:25:51 +0800

    fix: 🐛 cr issue

--- crates/rome_js_analyze/tests/specs/js/noNegationElse/issue_2999.js
+++ crates/rome_js_analyze/tests/specs/js/noNegationElse/issue_2999.js
@@ -1,15 +1,3 @@
-// valid
-if (!true) {consequent;};
-true ? consequent : alternate;
-// invalid
-if (!true) {
-  consequent;
-} else {
-  alternate;
-}
-!condition ? consequent : alternate;
-
-let a = !test ? c : d;
 if (!true) {
 	consequent;
 } else something();
