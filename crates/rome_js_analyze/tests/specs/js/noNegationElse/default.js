1bccb64a41b7bd7137c77875980f3fa84a211435
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-08-08 17:59:48 +0800

    fix(rome_js_analyze): Only emit diagnostic when alternate of `JsIfStatement` is `JsBlockStatement` (#3001)
    * fix: 🐛 (rome_js_analyze): fix 2999
    
    * fix: 🐛 cargo lnit
    
    * fix: 🐛 cr issue
    
    * fix: 🐛 cr issue
    
    * fix: 🐛 cr issue
    
    * fix: 🐛 cr issue

--- crates/rome_js_analyze/tests/specs/js/noNegationElse/default.js
+++ crates/rome_js_analyze/tests/specs/js/noNegationElse/default.js
@@ -1,11 +1,13 @@
 // valid
-if (!true) {consequent;};
+if (!true) {
+	consequent;
+}
 true ? consequent : alternate;
 // invalid
 if (!true) {
-  consequent;
+	consequent;
 } else {
-  alternate;
+	alternate;
 }
 !condition ? consequent : alternate;
 
