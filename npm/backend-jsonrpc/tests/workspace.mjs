d7f15adbfccb8481c41ec2f35e0c28dbe7eb9abd
l3ops <leo@rome.tools>
2022-08-16 18:26:54 +0200

    add tests

---
+++ npm/backend-jsonrpc/tests/workspace.mjs
@@ -0,0 +1,48 @@
+import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
+import { equal } from "node:assert";
+
+import { createWorkspaceWithBinary } from "../dist/index.js";
+
+async function testWorkspace() {
+	const extension = process.platform === "win32" ? ".exe" : "";
+	const command = resolve(
+		fileURLToPath(import.meta.url),
+		"../../../..",
+		`target/release/rome${extension}`,
+	);
+
+	const workspace = await createWorkspaceWithBinary(command);
+
+	await workspace.open_file({
+		path: {
+			path: "test.js",
+			id: 0,
+		},
+		content: "statement()",
+		version: 0,
+	});
+
+	const printed = await workspace.format_file({
+		path: {
+			path: "test.js",
+			id: 0,
+		},
+	});
+
+	equal(printed.code, "statement();\n");
+
+	await workspace.close_file({
+		path: {
+			path: "test.js",
+			id: 0,
+		},
+	});
+
+	workspace.destroy();
+}
+
+testWorkspace().catch((err) => {
+	console.error(err);
+	process.exit(1);
+});
