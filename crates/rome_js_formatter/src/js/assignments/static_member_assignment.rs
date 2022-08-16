85ddb4b2c622cac9638d5230dcefb6cf571677f8
Micha Reiser <micha@rome.tools>
2022-08-16 16:53:16 +0100

    feat(rome_js_formatter): Member assignment formatting (#3061)

--- crates/rome_js_formatter/src/js/assignments/static_member_assignment.rs
+++ crates/rome_js_formatter/src/js/assignments/static_member_assignment.rs
@@ -1,19 +1,12 @@
+use crate::js::expressions::static_member_expression::JsAnyStaticMemberLike;
 use crate::prelude::*;
-use rome_formatter::write;
 use rome_js_syntax::JsStaticMemberAssignment;
-use rome_js_syntax::JsStaticMemberAssignmentFields;
 
 #[derive(Debug, Clone, Default)]
 pub struct FormatJsStaticMemberAssignment;
 
 impl FormatNodeRule<JsStaticMemberAssignment> for FormatJsStaticMemberAssignment {
     fn fmt_fields(&self, node: &JsStaticMemberAssignment, f: &mut JsFormatter) -> FormatResult<()> {
-        let JsStaticMemberAssignmentFields {
-            object,
-            dot_token,
-            member,
-        } = node.as_fields();
-
-        write![f, [object.format(), dot_token.format(), member.format(),]]
+        JsAnyStaticMemberLike::from(node.clone()).fmt(f)
     }
 }
