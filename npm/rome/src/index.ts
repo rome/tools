f1729b2e4f26321b6fd7f121bc1a48751d1ea73d
Emanuele Stoppa <my.burning@gmail.com>
2022-08-17 08:10:23 +0100

    feat(npm): API facade and first compile pipeline

--- npm/rome/src/index.ts
+++ npm/rome/src/index.ts
@@ -1,50 +1,36 @@
-interface FormatFilesDebugOptions extends FormatFilesOptions {
+interface FormatDebugOptions {
 	/**
 	 * If `true`, you'll be able to inspect the IR of the formatter
 	 */
 	debug: boolean;
 }
 
-interface FormatContentDebugOptions extends FormatContentOptions {
-	/**
-	 * If `true`, you'll be able to inspect the IR of the formatter
-	 */
-	debug: boolean;
-}
-
-interface FormatFilesOptions {
+interface FormatFilesOptions extends FormatDebugOptions {
 	/**
 	 * Writes the new content to disk
 	 */
-	write?: boolean;
+	write: boolean;
 }
 
-interface FormatContentOptions {
+interface FormatContentOptions extends FormatDebugOptions {
 	/**
 	 * A virtual path of the file. You should add the extension,
 	 * so Rome knows how to parse the content
 	 */
-	filePath: string;
+	filePath: String;
 	/**
 	 * The range where to format the content
 	 */
-	range?: [number, number];
+	range: [number, number];
 }
 
 interface FormatResult {
 	// Not final
-	content: string;
+	content: String;
 	// Not final
-	errors: string[];
-}
-
-interface FormatDebugResult {
-	// Not final
-	content: string;
-	// Not final
-	errors: string[];
+	errors: String[];
 	// Available when in debug mode
-	ir: string | null;
+	ir: String | null;
 }
 
 interface ParseOptions {
@@ -52,92 +38,50 @@ interface ParseOptions {
 	 * A virtual path of the file. You should add the extension,
 	 * so Rome knows how to parse the content
 	 */
-	filePath: string;
+	filePath: String;
 }
 
 interface ParseResult {
 	/**
 	 * The CST of the code
 	 */
-	cst: string;
+	cst: String;
 	/**
 	 * The AST of the code
 	 */
-	ast: string;
+	ast: String;
 	// Not final
-	errors: string[];
-}
-
-function isFormatFilesDebug(
-	options: FormatFilesOptions | FormatFilesDebugOptions,
-): options is FormatFilesDebugOptions {
-	return "debug" in options && options.debug !== undefined;
-}
-
-function isFormatContentDebug(
-	options: any,
-): options is FormatContentDebugOptions {
-	return options?.debug !== undefined;
+	errors: String[];
 }
 
 export class Rome {
-	async formatFiles(paths: string[]): Promise<FormatResult>;
 	async formatFiles(
-		paths: string[],
-		options?: FormatFilesOptions,
-	): Promise<FormatResult>;
-	async formatFiles(
-		paths: string[],
-		options?: FormatFilesDebugOptions,
-	): Promise<FormatDebugResult>;
-	async formatFiles(
-		paths: string[],
-		options?: FormatFilesOptions | FormatFilesDebugOptions,
-	): Promise<FormatResult | FormatDebugResult> {
+		paths: String[],
+		options: Partial<FormatFilesOptions> | undefined = undefined,
+	): Promise<FormatResult> {
 		paths;
 
-		if (options && isFormatFilesDebug(options)) {
-			return {
-				content: "",
-				errors: [],
-				ir: "",
-			};
-		}
 		return {
 			content: "",
 			errors: [],
+			ir: options?.debug ? "" : null,
 		};
 	}
 
 	async formatContent(
-		content: string,
-		options: FormatContentOptions,
-	): Promise<FormatResult>;
-	async formatContent(
-		content: string,
-		options: FormatContentDebugOptions,
-	): Promise<FormatDebugResult>;
-	async formatContent(
-		content: string,
-		options: FormatContentOptions | FormatContentDebugOptions,
-	): Promise<FormatResult | FormatDebugResult> {
+		content: String,
+		options: Partial<FormatContentOptions> | undefined = undefined,
+	): Promise<FormatResult> {
 		content;
-		options.filePath;
-		if (isFormatContentDebug(options)) {
-			return {
-				content: "",
-				errors: [],
-				ir: "",
-			};
-		}
 		return {
 			content: "",
 			errors: [],
+			ir: options?.debug ? "" : null,
 		};
 	}
 
 	async parseContent(
-		content: string,
+		content: String,
 		options: ParseOptions,
 	): Promise<ParseResult> {
 		content;
--- npm/rome/tests/index.test.ts
+++ npm/rome/tests/index.test.ts
@@ -11,66 +11,24 @@ describe("Rome formatter", () => {
 		expect(result.errors).toEqual([]);
 	});
 
-	it("should not format files in debug mode", async () => {
-		const rome = new Rome();
-
-		let result = await rome.formatFiles(["./path/to/file.js"], {
-			debug: true,
-		});
-
-		expect(result.content).toEqual("");
-		expect(result.errors).toEqual([]);
-		expect(result.ir).toEqual("");
-	});
-
 	it("should not format content", async () => {
 		const rome = new Rome();
 
-		let result = await rome.formatContent("function f() {}", {
-			filePath: "example.js",
-		});
+		let result = await rome.formatContent("function f() {}");
 
 		expect(result.content).toEqual("");
 		expect(result.errors).toEqual([]);
-	});
-
-	it("should not format content in debug mode", async () => {
-		const rome = new Rome();
-
-		let result = await rome.formatContent("function f() {}", {
-			filePath: "example.js",
-			debug: true,
-		});
-
-		expect(result.content).toEqual("");
-		expect(result.errors).toEqual([]);
-		expect(result.ir).toEqual("");
 	});
 
 	it("should not format content with range", async () => {
 		const rome = new Rome();
 
 		let result = await rome.formatContent("function f() {}", {
-			filePath: "file.js",
 			range: [5, 10],
 		});
 
 		expect(result.content).toEqual("");
 		expect(result.errors).toEqual([]);
-	});
-
-	it("should not format content with range in debug mode", async () => {
-		const rome = new Rome();
-
-		let result = await rome.formatContent("function f() {}", {
-			filePath: "file.js",
-			range: [5, 10],
-			debug: true,
-		});
-
-		expect(result.content).toEqual("");
-		expect(result.errors).toEqual([]);
-		expect(result.ir).toEqual("");
 	});
 });
 
