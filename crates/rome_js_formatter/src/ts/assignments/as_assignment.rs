6357e13087e11789d6302dc67ac87ecd148b2d54
Micha Reiser <micha@rome.tools>
2022-08-18 14:14:08 +0200

    feat(rome_js_formatter): Parenthesize assignments
    This PR implements the addition and removal of parentheses around assignments.
    
    There are only very few cases where parentheses are necessary around assignments. Mainly:
    
    * `TsTypeAssertionAssignment`
    * `TsAsAssignment`
    * identifiers named `async` in for loops
    
    ## Tests
    
    I added unit tests to the cases where parentheses are necessary

--- crates/rome_js_formatter/src/ts/assignments/as_assignment.rs
+++ crates/rome_js_formatter/src/ts/assignments/as_assignment.rs
@@ -1,8 +1,9 @@
 use crate::prelude::*;
 
+use crate::parentheses::{AssignmentNode, NeedsParentheses};
 use rome_formatter::write;
-use rome_js_syntax::TsAsAssignment;
-use rome_js_syntax::TsAsAssignmentFields;
+use rome_js_syntax::{JsAnyAssignment, JsAnyAssignmentPattern, TsAsAssignment};
+use rome_js_syntax::{JsSyntaxKind, JsSyntaxNode, TsAsAssignmentFields};
 
 #[derive(Debug, Clone, Default)]
 pub struct FormatTsAsAssignment;
@@ -25,5 +26,52 @@ impl FormatNodeRule<TsAsAssignment> for FormatTsAsAssignment {
                 ty.format(),
             ]
         ]
+    }
+
+    fn needs_parentheses(&self, item: &TsAsAssignment) -> bool {
+        item.needs_parentheses()
+    }
+}
+
+impl AssignmentNode for TsAsAssignment {
+    #[inline]
+    fn resolve(&self) -> JsAnyAssignmentPattern {
+        JsAnyAssignmentPattern::JsAnyAssignment(JsAnyAssignment::from(self.clone()))
+    }
+
+    #[inline]
+    fn into_resolved(self) -> JsAnyAssignmentPattern {
+        JsAnyAssignmentPattern::JsAnyAssignment(JsAnyAssignment::from(self))
+    }
+}
+
+impl NeedsParentheses for TsAsAssignment {
+    fn needs_parentheses_with_parent(&self, parent: &JsSyntaxNode) -> bool {
+        matches!(
+            parent.kind(),
+            JsSyntaxKind::JS_ASSIGNMENT_EXPRESSION
+                | JsSyntaxKind::TS_NON_NULL_ASSERTION_ASSIGNMENT
+                | JsSyntaxKind::TS_TYPE_ASSERTION_ASSIGNMENT
+                | JsSyntaxKind::JS_PRE_UPDATE_EXPRESSION
+                | JsSyntaxKind::JS_POST_UPDATE_EXPRESSION
+                | JsSyntaxKind::JS_OBJECT_ASSIGNMENT_PATTERN_PROPERTY
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::assert_needs_parentheses;
+    use rome_js_syntax::TsAsAssignment;
+
+    #[test]
+    fn needs_parentheses() {
+        assert_needs_parentheses!("a as number = 'test'", TsAsAssignment);
+        assert_needs_parentheses!("(a as number)! = 'test'", TsAsAssignment);
+        assert_needs_parentheses!("(<number>(a as number)) = 'test'", TsAsAssignment);
+        assert_needs_parentheses!("++(a as number)", TsAsAssignment);
+        assert_needs_parentheses!("(a as number)--", TsAsAssignment);
+        assert_needs_parentheses!("({ a: a as number } = { a: 5 })", TsAsAssignment);
     }
 }
--- crates/rome_js_formatter/src/ts/assignments/type_assertion_assignment.rs
+++ crates/rome_js_formatter/src/ts/assignments/type_assertion_assignment.rs
@@ -1,7 +1,11 @@
 use crate::prelude::*;
 use rome_formatter::write;
-use rome_js_syntax::TsTypeAssertionAssignmentFields;
+use rome_js_syntax::{
+    JsAnyAssignment, JsAnyAssignmentPattern, JsSyntaxKind, JsSyntaxNode,
+    TsTypeAssertionAssignmentFields,
+};
 
+use crate::parentheses::{AssignmentNode, NeedsParentheses};
 use rome_js_syntax::TsTypeAssertionAssignment;
 
 #[derive(Debug, Clone, Default)]
@@ -28,5 +32,52 @@ impl FormatNodeRule<TsTypeAssertionAssignment> for FormatTsTypeAssertionAssignme
                 assignment.format()
             ]
         ]
+    }
+
+    fn needs_parentheses(&self, item: &TsTypeAssertionAssignment) -> bool {
+        item.needs_parentheses()
+    }
+}
+
+impl AssignmentNode for TsTypeAssertionAssignment {
+    #[inline]
+    fn resolve(&self) -> JsAnyAssignmentPattern {
+        JsAnyAssignmentPattern::JsAnyAssignment(JsAnyAssignment::from(self.clone()))
+    }
+
+    #[inline]
+    fn into_resolved(self) -> JsAnyAssignmentPattern {
+        JsAnyAssignmentPattern::JsAnyAssignment(JsAnyAssignment::from(self))
+    }
+}
+
+impl NeedsParentheses for TsTypeAssertionAssignment {
+    fn needs_parentheses_with_parent(&self, parent: &JsSyntaxNode) -> bool {
+        matches!(
+            parent.kind(),
+            JsSyntaxKind::JS_ASSIGNMENT_EXPRESSION
+                | JsSyntaxKind::TS_TYPE_ASSERTION_ASSIGNMENT
+                | JsSyntaxKind::TS_NON_NULL_ASSERTION_ASSIGNMENT
+                | JsSyntaxKind::JS_PRE_UPDATE_EXPRESSION
+                | JsSyntaxKind::JS_POST_UPDATE_EXPRESSION
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{assert_needs_parentheses, assert_not_needs_parentheses};
+    use rome_js_syntax::TsTypeAssertionAssignment;
+
+    #[test]
+    fn needs_parentheses() {
+        assert_needs_parentheses!("(<number>a) = 'test'", TsTypeAssertionAssignment);
+        assert_needs_parentheses!("(<number>a)! = 'test'", TsTypeAssertionAssignment);
+        assert_needs_parentheses!("(<number>(<any>a)) = 'test'", TsTypeAssertionAssignment[0]);
+        assert_needs_parentheses!("(<number>(<any>a)) = 'test'", TsTypeAssertionAssignment[1]);
+        assert_needs_parentheses!("++(<number>a)", TsTypeAssertionAssignment);
+        assert_needs_parentheses!("(<number>a)--", TsTypeAssertionAssignment);
+        assert_not_needs_parentheses!("({ a: <number>a } = { a: 5 })", TsTypeAssertionAssignment);
     }
 }
