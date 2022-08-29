703f696987b385a993964fe38bb1930b00f1575b
Micha Reiser <micha@rome.tools>
2022-08-29 17:16:43 +0200

    Update crates/rome_js_formatter/src/js/expressions/array_expression.rs
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- crates/rome_js_formatter/src/js/expressions/array_expression.rs
+++ crates/rome_js_formatter/src/js/expressions/array_expression.rs
@@ -47,9 +47,9 @@ impl FormatNodeRule<JsArrayExpression> for FormatJsArrayExpression {
     }
 }
 
-/// Returns `true` for arrays containing at least two elements if
-/// * all elements are either object xor array expressions
-/// * each child array has at least two elements or each child object has at least two members.
+/// Returns `true` for arrays containing at least two elements if:
+/// * all elements are either object or array expressions
+/// * each child array expression has at least two elements, or each child object expression has at least two members.
 fn should_break(elements: &JsArrayElementList) -> SyntaxResult<bool> {
     if elements.len() < 2 {
         Ok(false)
