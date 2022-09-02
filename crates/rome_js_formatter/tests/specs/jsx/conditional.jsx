220904f57ac6938e3c966970a6580dc686646fc9
Micha Reiser <micha@rome.tools>
2022-09-02 15:53:13 +0200

    feat(rome_js_formatter): Conditional JSX Chain layout (#3150)
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

---
+++ crates/rome_js_formatter/tests/specs/jsx/conditional.jsx
@@ -0,0 +1,49 @@
+(bifornCringerMoshedPerplexSawder ? (
+	askTrovenaBeenaDependsRowans
+) : (
+	glimseGlyphsHazardNoopsTieTie
+)) ? null : (
+	<Element2>
+		<Sub />
+		<Sub />
+		<Sub />
+	</Element2>
+);
+
+
+(bifornCringerMoshedPerplexSawder ? (
+	askTrovenaBeenaDependsRowans
+) : (
+	glimseGlyphsHazardNoopsTieTie
+)) ? undefined : (
+	<Element2>
+		<Sub />
+		<Sub />
+		<Sub />
+	</Element2>
+);
+
+(bifornCringerMoshedPerplexSawder ? (
+	askTrovenaBeenaDependsRowans
+) : (
+	glimseGlyphsHazardNoopsTieTie
+)) ? x : (
+	<Element2>
+		<Sub />
+		<Sub />
+		<Sub />
+	</Element2>
+);
+
+
+(bifornCringerMoshedPerplexSawder ? (
+	askTrovenaBeenaDependsRowans
+) : (
+	glimseGlyphsHazardNoopsTieTie
+)) ? a ? b : (
+	<Element2>
+		<Sub />
+		<Sub />
+		<Sub />
+	</Element2>
+) : null;
