fe72d2512afc042dc4e5b2ae2828958fb4e2ff39
Denis Bezrukov <dbezrukov@ctrader.local>
2022-09-07 16:50:35 +0300

    feat(rome_js_formatter): TS Intersection & Union types #3162

--- crates/rome_js_formatter/src/ts/types/intersection_type.rs
+++ crates/rome_js_formatter/src/ts/types/intersection_type.rs
@@ -4,6 +4,7 @@ use crate::parentheses::{
     is_in_many_type_union_or_intersection_list, operator_type_or_higher_needs_parens,
     NeedsParentheses,
 };
+use crate::utils::FormatTypeMemberSeparator;
 use rome_formatter::{format_args, write};
 use rome_js_syntax::{
     JsSyntaxKind, JsSyntaxNode, TsIntersectionTypeElementList, TsIntersectionTypeFields,
@@ -20,17 +21,12 @@ impl FormatNodeRule<TsIntersectionType> for FormatTsIntersectionType {
             leading_separator_token,
             types,
         } = node.as_fields();
-
         write!(
             f,
-            [group(&indent(&format_args!(
-                soft_line_break(),
-                FormatTypeSetLeadingSeparator {
-                    separator: JsSyntaxKind::AMP,
-                    leading_separator: leading_separator_token.as_ref()
-                },
+            [group(&format_args!(
+                FormatTypeMemberSeparator::new(leading_separator_token.as_ref()),
                 types.format()
-            )))]
+            ))]
         )
     }
 
