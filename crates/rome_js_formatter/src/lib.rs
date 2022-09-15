d426ce4b373af12af25b96c55304090b54a83fd8
Micha Reiser <micha@rome.tools>
2022-09-15 08:59:20 +0200

    Constructor and call signature formatting

--- crates/rome_js_formatter/src/lib.rs
+++ crates/rome_js_formatter/src/lib.rs
@@ -814,11 +814,10 @@ function() {
     // use this test check if your snippet prints as you wish, without using a snapshot
     fn quick_test() {
         let src = r#"
-some.member.with.
-	// rome-ignore format: Verify that formatting calls into right.format()
-	rather.hard.to.test.because.name.doesnt.format
-
-	. being.ignored;"#;
+interface ConstructorInline {
+  // https://github.com/prettier/prettier/issues/2163
+  (i): any;
+}"#;
 
         let syntax = SourceType::tsx();
         let tree = parse(src, 0, syntax);
