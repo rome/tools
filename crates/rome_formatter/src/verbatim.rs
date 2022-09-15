a124d3654f297830e3c4aa8ce1e594ad078b2a49
Micha Reiser <micha@rome.tools>
2022-09-15 13:44:28 +0200

    Rename `cst` back to `token`
    Comments Formatter integration

--- crates/rome_formatter/src/verbatim.rs
+++ crates/rome_formatter/src/verbatim.rs
@@ -1,5 +1,5 @@
-use crate::cst::{FormatLeadingComments, FormatTrailingComments};
 use crate::prelude::*;
+use crate::token::{FormatLeadingComments, FormatTrailingComments};
 use crate::VecBuffer;
 use crate::{write, CstFormatContext};
 use rome_rowan::{Direction, Language, SyntaxElement, SyntaxNode, TextRange};
