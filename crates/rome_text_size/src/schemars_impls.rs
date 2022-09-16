662571762c05f077a58145ea0a8d369979a75f61
l3ops <leo@rome.tools>
2022-09-16 09:33:45 +0200

    add documentation

--- crates/rome_text_size/src/schemars_impls.rs
+++ crates/rome_text_size/src/schemars_impls.rs
@@ -1,3 +1,10 @@
+//! This module implements the [JsonSchema] trait from the [schemars] crate for
+//! [TextSize] and [TextRange] if the `schemars` feature is enabled. This trait
+//! exposes meta-information on how a given type is serialized and deserialized
+//! using `serde`, and is currently used to generate autocomplete information
+//! for the `rome.json` configuration file and TypeScript types for the node.js
+//! bindings to the Workspace API
+
 use crate::{TextRange, TextSize};
 use schemars::{gen::SchemaGenerator, schema::Schema, JsonSchema};
 
@@ -7,6 +14,8 @@ impl JsonSchema for TextSize {
     }
 
     fn json_schema(gen: &mut SchemaGenerator) -> Schema {
+        // TextSize is represented as a raw u32, see serde_impls.rs for the
+        // actual implementation
         <u32>::json_schema(gen)
     }
 }
@@ -17,6 +26,8 @@ impl JsonSchema for TextRange {
     }
 
     fn json_schema(gen: &mut SchemaGenerator) -> Schema {
+        // TextSize is represented as (TextSize, TextSize), see serde_impls.rs
+        // for the actual implementation
         <(TextSize, TextSize)>::json_schema(gen)
     }
 }
