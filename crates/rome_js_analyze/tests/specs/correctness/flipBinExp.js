0542fdc83e92efb3f0117e49a8db64b7b7ae31f0
xunilrj <xunilrj@hotmail.com>
2022-09-16 22:52:12 +0100

    poc better batch mutation

--- crates/rome_js_analyze/tests/specs/correctness/flipBinExp.js
+++ crates/rome_js_analyze/tests/specs/correctness/flipBinExp.js
@@ -1,30 +1,30 @@
 if (a < b) {
 }
-if (a > b) {
-}
-if (a <= b) {
-}
-if (a >= b) {
-}
-if (a == b) {
-}
-if (a === b) {
-}
-if (a != b) {
-}
-if (a !== b) {
-}
+// if (a > b) {
+// }
+// if (a <= b) {
+// }
+// if (a >= b) {
+// }
+// if (a == b) {
+// }
+// if (a === b) {
+// }
+// if (a != b) {
+// }
+// if (a !== b) {
+// }
 
-const a = b + c;
-const a = b - c;
-const a = b * c;
-const a = b / c;
-const a = b % c;
-const a = b ** c;
+// const a = b + c;
+// const a = b - c;
+// const a = b * c;
+// const a = b / c;
+// const a = b % c;
+// const a = b ** c;
 
-const a = b << c;
-const a = b >> c;
-const a = b >>> c;
-const a = b & c;
-const a = b | c;
-const a = b ^ c;
+// const a = b << c;
+// const a = b >> c;
+// const a = b >>> c;
+// const a = b & c;
+// const a = b | c;
+// const a = b ^ c;
