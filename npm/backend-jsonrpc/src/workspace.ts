3792124a0cffbcba03dd8eaf39cc01d4ebf5377b
kaioduarte <duartedossantos@gmail.com>
2022-10-05 23:08:55 +0100

    chore: add missing config for the rule

--- npm/backend-jsonrpc/src/workspace.ts
+++ npm/backend-jsonrpc/src/workspace.ts
@@ -68,7 +68,7 @@ export interface JavascriptConfiguration {
 	/**
 	* A list of global bindings that should be ignored by the analyzers
 
-If defined here, they should not emit diagnostics. 
+If defined here, they should not emit diagnostics.
 	 */
 	globals?: string[];
 }
@@ -90,7 +90,7 @@ export type PlainIndentStyle = "tab" | "space";
 /**
 	* Validated value for the `line_width` formatter options
 
-The allowed range of values is 1..=320 
+The allowed range of values is 1..=320
 	 */
 export type LineWidth = number;
 export interface JavascriptFormatter {
@@ -161,6 +161,7 @@ export interface Nursery {
 	noDangerouslySetInnerHtml?: RuleConfiguration;
 	noDangerouslySetInnerHtmlWithChildren?: RuleConfiguration;
 	noNewSymbol?: RuleConfiguration;
+	noPositiveTabindex?: RuleConfiguration;
 	noRenderReturnValue?: RuleConfiguration;
 	noRestrictedGlobals?: RuleConfiguration;
 	noUndeclaredVariables?: RuleConfiguration;
@@ -314,6 +315,7 @@ export type Category =
 	| "lint/nursery/useBlankTarget"
 	| "lint/nursery/useValidAnchor"
 	| "lint/nursery/noRestrictedGlobals"
+	| "lint/nursery/noPositiveTabindex"
 	| "lint/style/noNegationElse"
 	| "lint/style/noShoutyConstants"
 	| "lint/style/useSelfClosingElements"
@@ -344,7 +346,7 @@ export type DiagnosticTags = DiagnosticTag[];
 /**
 	* Serializable representation of a [Diagnostic](super::Diagnostic) advice
 
-See the [Visitor] trait for additional documentation on all the supported advice types. 
+See the [Visitor] trait for additional documentation on all the supported advice types.
 	 */
 export type Advice =
 	| { Log: [LogCategory, MarkupBuf] }
