a7fcdfb4bbb32c1f20eaa06ec9c6310226c2c62f
kaioduarte <duartedossantos@gmail.com>
2022-10-05 17:38:50 +0100

    chore: update snapshots

--- crates/rome_js_analyze/tests/specs/nursery/noPositiveTabindex/invalidJsx.jsx
+++ crates/rome_js_analyze/tests/specs/nursery/noPositiveTabindex/invalidJsx.jsx
@@ -6,7 +6,6 @@ test = <div tabIndex={"1"} />;
 
 test = <div tabIndex={'5'} />;
 
-// not working - inner_string_text can't capture the number
 test = <div tabIndex="1" />;
 
 test = <div tabIndex={1}>foo</div>;
--- crates/rome_js_analyze/tests/specs/nursery/noPositiveTabindex/reactCreateElementInvalid.js
+++ crates/rome_js_analyze/tests/specs/nursery/noPositiveTabindex/reactCreateElementInvalid.js
@@ -1,3 +1,2 @@
-// not working - token_text is adding a space at the end
 React.createElement("div", { tabIndex: '1' })
 React.createElement("div", { tabIndex: 1 })
