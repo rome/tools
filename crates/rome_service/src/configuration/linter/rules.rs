1dd6cfc7be663c5d1d6876d93b0ac15041d78858
ematipico <my.burning@gmail.com>
2022-10-10 10:41:03 +0100

    chore: rebase

--- crates/rome_service/src/configuration/linter/rules.rs
+++ crates/rome_service/src/configuration/linter/rules.rs
@@ -407,7 +407,7 @@ struct NurserySchema {
 }
 impl Nursery {
     const CATEGORY_NAME: &'static str = "nursery";
-    pub(crate) const CATEGORY_RULES: [&'static str; 17] = [
+    pub(crate) const CATEGORY_RULES: [&'static str; 18] = [
         "noArrayIndexKey",
         "noAutofocus",
         "noChildrenProp",
