186c5a764edfbf318c15867a2f4e1cccebaebf17
Micha Reiser <micha@rome.tools>
2022-10-12 11:26:31 +0200

    Restore lib.rs

--- crates/rome_js_formatter/src/lib.rs
+++ crates/rome_js_formatter/src/lib.rs
@@ -861,16 +861,14 @@ function() {
     // use this test check if your snippet prints as you wish, without using a snapshot
     fn quick_test() {
         let src = r#"
-  function a({
-  animationConfig,
-  style,
-  unit,
-}: { animationConfig: AnimationConfig;
-  style: string | undefined | null;
-  unit: Unit;
-}) {
-}
-
+const b4 = (
+	<div>
+		Text <a data-very-long-prop-breakline-rome-playground data-other>
+			some link
+		</a>{" "}
+		| some other text,{" "}
+	</div>
+);
 
 "#;
         let syntax = SourceType::jsx();
