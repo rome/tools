a7639427e92e555f742edf6cb4972b937aaeabd4
ematipico <my.burning@gmail.com>
2022-10-12 15:15:51 +0100

    chore: suggestions and more test cases

--- crates/rome_js_analyze/tests/specs/nursery/useFlatMap.js
+++ crates/rome_js_analyze/tests/specs/nursery/useFlatMap.js
@@ -1,5 +1,12 @@
 // invalid
 [0, [12]].map(Number).flat();
 [0, [12], [[16]]].map(Number).flat(1);
+[0, [12], [[16]]].map((element) => {}).flat(1);
+[0, [12], [[16]]].map((element, index) => {}).flat(1);
+[0, [12], [[16]]].map((element, index, array) => {}).flat(1);
 // valid
 [0, [12], [[16]]].map(Number).flat(2);
+array.flat().map(Number);
+array.map(Number).flat(customFn);
+MyObject.map(one, two, tree).flat();
+MyObject.map().flat(1, "test")
