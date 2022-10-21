28c86f4333562000eebd7124b888554a07fff746
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2022-10-21 10:32:32 -0300

    explanation for limit of 16 attributes

--- crates/rome_js_syntax/src/jsx_ext.rs
+++ crates/rome_js_syntax/src/jsx_ext.rs
@@ -143,8 +143,12 @@ impl JsxAttributeList {
     /// Find and return the `JsxAttribute` that matches the given name like [find_attribute_by_name].  
     /// Only attributes with name as [JsxName] can be returned.   
     ///
-    /// Each name of "names_to_lookup" should be unique.
-    /// Support maximum of 16 names.
+    /// Each name of "names_to_lookup" should be unique.  
+    /// 
+    /// Supports maximum of 16 names to avoid stack overflow. Eeach attribute will consume:
+    /// 
+    /// - 8 bytes for the [Option<JsxAttribute>] result;
+    /// - plus 16 bytes for the [&str] argument.
     pub fn find_attributes_by_name<const N: usize>(
         &self,
         names_to_lookup: [&str; N],
