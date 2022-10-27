c20f6da9da22152b6a1938affa0705934d0143f2
Micha Reiser <micha@rome.tools>
2022-10-27 13:49:07 +0200

    fix(rome_js_formatter): Arrow with JSX Body (#3505)

--- website/playground/src/TrailingCommaSelect.tsx
+++ website/playground/src/TrailingCommaSelect.tsx
@@ -30,7 +30,8 @@ export default function TrailingCommaSelect({
 							name="trailingComma"
 							value={trailingComma ?? "all"}
 							onChange={(e) =>
-								setTrailingComma(e.target.value as TrailingComma)}
+								setTrailingComma(e.target.value as TrailingComma)
+							}
 							className="w-[100px] mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 						>
 							<option value={TrailingComma.All}>All</option>
