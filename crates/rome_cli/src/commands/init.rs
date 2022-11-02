4a0b36b2489e156bcba81ca47528e2a7a3d3a000
Micha Reiser <micha@rome.tools>
2022-11-02 12:12:43 +0100

    Use `Hyperlink`

--- crates/rome_cli/src/commands/init.rs
+++ crates/rome_cli/src/commands/init.rs
@@ -12,21 +12,21 @@ pub(crate) fn init(mut session: CliSession) -> Result<(), Termination> {
 
 "<Info><Emphasis>"Files created "</Emphasis></Info>{HorizontalLine::new(136)}"
 
-  "<Dim>"- "</Dim><Emphasis>"rome.json: "</Emphasis>"Your project configuration. Documentation: https://rome.tools/configuration
+  "<Dim>"- "</Dim><Emphasis>"rome.json: "</Emphasis>"Your project configuration ("<Hyperlink href="https://rome.tools/configuration">"Documentation"</Hyperlink>").
 
 "<Info><Emphasis>"Next Steps "</Emphasis></Info>{HorizontalLine::new(139)}"
 
   "<Dim>"1."</Dim>" "<Emphasis>"Setup an editor extension"</Emphasis>"
-     Get live errors as you type and format when you save. Learn more: https://rome.tools/editors
+     Get live errors as you type and format when you save. "<Hyperlink href="https://rome.tools/editors">"Learn more"</Hyperlink>".
 
   "<Dim>"2."</Dim>" "<Emphasis>"Try a command"</Emphasis>"
      "<Italic>"rome ci"</Italic>" checks for lint errors and verifies formatting. Run " <Italic>"rome --help"</Italic>" for a full list of commands and options.
 
   "<Dim>"3."</Dim>" "<Emphasis>"Read the documentation"</Emphasis>"
-     Our website serves as a comprehensive source of guides and documentation: https://rome.tools/docs
+     Our "<Hyperlink href="https://rome.tools/docs">"website"</Hyperlink>" serves as a comprehensive source of guides and documentation.
 
   "<Dim>"4."</Dim>" "<Emphasis>"Get involved in the community"</Emphasis>"
-     Ask questions, get support, or contribute by participating on GitHub (https://github.com/rome/tools), or join our community Discord (https://discord.gg/rome)"
+     Ask questions, get support, or contribute by participating on "<Hyperlink href="https://github.com/rome/tools">"GitHub"</Hyperlink>", or join our community "<Hyperlink href="https://discord.gg/rome">"Discord"</Hyperlink>"."
         });
 
     Ok(())
