90ec8a48b28431d78a57fa7a61e6f3fdeb84131a
l3ops <leops@users.noreply.github.com>
2022-11-10 15:29:52 +0100

    fix(rome_cli): ensures the service only connects to compatible versions (#3642)
    * fix(rome_cli): ensures the service only connects to compatible versions
    
    * enumerate running rome servers in `rome rage`
    
    * fix the build on unix

--- crates/rome_cli/src/service/unix.rs
+++ crates/rome_cli/src/service/unix.rs
@@ -21,7 +21,24 @@ use tracing::Instrument;
 /// Returns the filesystem path of the global socket used to communicate with
 /// the server daemon
 fn get_socket_name() -> PathBuf {
-    env::temp_dir().join("rome-socket")
+    env::temp_dir().join(format!("rome-socket-{}", rome_service::VERSION))
+}
+
+pub(crate) fn enumerate_pipes() -> io::Result<impl Iterator<Item = String>> {
+    fs::read_dir(env::temp_dir()).map(|iter| {
+        iter.filter_map(|entry| {
+            let entry = entry.ok()?.path();
+            let file_name = entry.file_name()?;
+            let file_name = file_name.to_str()?;
+
+            let rome_version = file_name.strip_prefix("rome-socket")?;
+            if rome_version.is_empty() {
+                Some(String::new())
+            } else {
+                Some(rome_version.strip_prefix('-')?.to_string())
+            }
+        })
+    })
 }
 
 /// Try to connect to the global socket and wait for the connection to become ready
--- crates/rome_cli/src/service/windows.rs
+++ crates/rome_cli/src/service/windows.rs
@@ -1,6 +1,7 @@
 use std::{
     convert::Infallible,
     env,
+    fs::read_dir,
     io::{self, ErrorKind},
     mem::swap,
     os::windows::process::CommandExt,
@@ -19,8 +20,28 @@ use tokio::{
 };
 use tracing::Instrument;
 
-/// Name of the global named pipe used to communicate with the server daemon
-const PIPE_NAME: &str = r"\\.\pipe\rome-service";
+/// Returns the name of the global named pipe used to communicate with the
+/// server daemon
+fn get_pipe_name() -> String {
+    format!(r"\\.\pipe\rome-service-{}", rome_service::VERSION)
+}
+
+pub(crate) fn enumerate_pipes() -> io::Result<impl Iterator<Item = String>> {
+    read_dir(r"\\.\pipe").map(|iter| {
+        iter.filter_map(|entry| {
+            let entry = entry.ok()?.path();
+            let file_name = entry.file_name()?;
+            let file_name = file_name.to_str()?;
+
+            let rome_version = file_name.strip_prefix("rome-service")?;
+            if rome_version.is_empty() {
+                Some(String::new())
+            } else {
+                Some(rome_version.strip_prefix('-')?.to_string())
+            }
+        })
+    })
+}
 
 /// Error code from the Win32 API
 const ERROR_PIPE_BUSY: i32 = 231;
@@ -28,7 +49,7 @@ const ERROR_PIPE_BUSY: i32 = 231;
 /// Try to connect to the global pipe and wait for the connection to become ready
 async fn try_connect() -> io::Result<NamedPipeClient> {
     loop {
-        match ClientOptions::new().open(PIPE_NAME) {
+        match ClientOptions::new().open(get_pipe_name()) {
             Ok(client) => return Ok(client),
             // If the connection failed with ERROR_PIPE_BUSY, wait a few
             // milliseconds then retry the connection (we should be using
@@ -165,7 +186,7 @@ pub(crate) async fn ensure_daemon() -> io::Result<bool> {
 /// print the global pipe name in the standard output
 pub(crate) async fn print_socket() -> io::Result<()> {
     ensure_daemon().await?;
-    println!("{PIPE_NAME}");
+    println!("{}", get_pipe_name());
     Ok(())
 }
 
@@ -174,11 +195,11 @@ pub(crate) async fn print_socket() -> io::Result<()> {
 pub(crate) async fn run_daemon(factory: ServerFactory) -> io::Result<Infallible> {
     let mut prev_server = ServerOptions::new()
         .first_pipe_instance(true)
-        .create(PIPE_NAME)?;
+        .create(get_pipe_name())?;
 
     loop {
         prev_server.connect().await?;
-        let mut next_server = ServerOptions::new().create(PIPE_NAME)?;
+        let mut next_server = ServerOptions::new().create(get_pipe_name())?;
         swap(&mut prev_server, &mut next_server);
 
         let connection = factory.create();
