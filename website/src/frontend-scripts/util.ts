1bf7cdbe8d58ec8452772ad85c9d8cd1c32c5bb9
Sebastian <sebastian@rome.tools>
2022-11-12 16:26:56 -0600

    website(docs): Fix some regressions (#3694)

--- website/src/frontend-scripts/util.ts
+++ website/src/frontend-scripts/util.ts
@@ -1,14 +1,15 @@
 export type ThemeName = "dark" | "light";
 
-export function matchesDark() {
-	return window.matchMedia("(prefers-color-scheme: dark)");
-}
+export const matchesDark = window.matchMedia("(prefers-color-scheme: dark)");
 
 export function getCurrentTheme(): ThemeName {
 	let currentScheme = window.localStorage.getItem("data-theme");
 	if (currentScheme == null) {
-		const prefersDarkMode = matchesDark().matches;
-		currentScheme = prefersDarkMode ? "dark" : "light";
+		currentScheme = matchesDark.matches ? "dark" : "light";
 	}
 	return currentScheme === "dark" ? "dark" : "light";
+}
+
+export function setCurrentTheme(theme: ThemeName) {
+	document.documentElement.setAttribute("data-theme", theme);
 }
