fda5284036b0a4ecd4f6519606266718e83ec59a
Sebastian McKenzie <sebastian@rome.tools>
2022-11-12 16:33:51 -0600

    Update netlify.toml

--- website/build-netlify.sh
+++ website/build-netlify.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh 
 pnpm build:wasm
