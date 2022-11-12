62550ba60cc0532b308bf37d8ecf42667861a66c
Sebastian McKenzie <sebastian@rome.tools>
2022-11-12 16:47:28 -0600

    Use wasm dev builds for deploy previews

--- website/build-netlify.sh
+++ website/build-netlify.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh 
-pnpm build:wasm
+if ! command -v wasm-pack &> /dev/null; then
+  curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
+if [ "$1" == "preview" ]; then
+  pnpm build:wasm-dev
+else
+  pnpm build:wasm
+fi
+
+# Run pnpm install again to ensure wasm gets linked
 pnpm install
+
+# Finally build website
 pnpm build:js
