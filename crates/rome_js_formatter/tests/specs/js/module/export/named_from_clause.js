ac8f2e87e21b4b976b8f9c17a19d053fc64962e9
unvalley <kirohi.code@gmail.com>
2022-11-15 02:11:20 +0900

    test: add long string single specifier to export and update snapshot

--- crates/rome_js_formatter/tests/specs/js/module/export/named_from_clause.js
+++ crates/rome_js_formatter/tests/specs/js/module/export/named_from_clause.js
@@ -8,4 +8,5 @@ export {
     lorem_lorem_lorem_lorem_lorem_ as ipsum_ipsum_ipsum_ipsum_ipsum_ipsum_
 } from "fancy" assert { type: "json", "type2": "json", type23: "json", "type24": "json"}
 
-export { loooooooooooooooooooooooooooooooooooooooooooooooooong } from "loooooooooooooooooooooooooooooooooooooooooooooong";
+
+export { loooooooooooooooooooooooooooooooooooooooooooooooooong } from "loooooooooooooooooooooooooooooooooooooooooooooong"
