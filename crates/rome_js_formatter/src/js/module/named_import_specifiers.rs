18d0ee1810d8ac85b71358ac276a98517194f7e9
unvalley <kirohi.code@gmail.com>
2022-11-17 01:52:43 +0900

    feat(rome_js_formatter): restrict import line break for single specifier

--- crates/rome_js_formatter/src/js/module/named_import_specifiers.rs
+++ crates/rome_js_formatter/src/js/module/named_import_specifiers.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 use rome_formatter::write;
 use rome_js_syntax::JsNamedImportSpecifiers;
 use rome_js_syntax::JsNamedImportSpecifiersFields;
+use rome_rowan::AstSeparatedElement;
 
 #[derive(Debug, Clone, Default)]
 pub struct FormatJsNamedImportSpecifiers;
@@ -17,16 +18,31 @@ impl FormatNodeRule<JsNamedImportSpecifiers> for FormatJsNamedImportSpecifiers {
 
         write!(f, [l_curly_token.format()])?;
 
-        if specifiers.is_empty() {
-            write!(
-                f,
-                [format_dangling_comments(node.syntax()).with_soft_block_indent()]
-            )?;
-        } else {
-            write!(
-                f,
-                [group(&soft_space_or_block_indent(&specifiers.format()))]
-            )?;
+        match specifiers.elements().next() {
+            Some(AstSeparatedElement {
+                node: Ok(node),
+                trailing_separator: Ok(separator),
+            }) if specifiers.len() == 1 && !f.comments().has_comments(node.syntax()) => {
+                write!(f, [space(), node.format()])?;
+
+                if let Some(separator) = separator {
+                    write!(f, [format_removed(&separator)])?;
+                }
+
+                write!(f, [space()])?;
+            }
+            Some(_) => {
+                write!(
+                    f,
+                    [group(&soft_space_or_block_indent(&specifiers.format()))]
+                )?;
+            }
+            None => {
+                write!(
+                    f,
+                    [format_dangling_comments(node.syntax()).with_soft_block_indent()]
+                )?;
+            }
         }
 
         write!(f, [r_curly_token.format()])
