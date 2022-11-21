56cd32c1876ffa22231b258d03567ce0852e55bd
unvalley <kirohi.code@gmail.com>
2022-11-22 01:45:16 +0900

    fix: handle JsNamedImportSpecifier

--- crates/rome_js_formatter/src/js/module/named_import_specifiers.rs
+++ crates/rome_js_formatter/src/js/module/named_import_specifiers.rs
@@ -18,31 +18,16 @@ impl FormatNodeRule<JsNamedImportSpecifiers> for FormatJsNamedImportSpecifiers {
 
         write!(f, [l_curly_token.format()])?;
 
-        match specifiers.elements().next() {
-            Some(AstSeparatedElement {
-                node: Ok(node),
-                trailing_separator: Ok(separator),
-            }) if specifiers.len() == 1 && !f.comments().has_comments(node.syntax()) => {
-                write!(f, [space(), node.format()])?;
-
-                if let Some(separator) = separator {
-                    write!(f, [format_removed(&separator)])?;
-                }
-
-                write!(f, [space()])?;
-            }
-            Some(_) => {
-                write!(
-                    f,
-                    [group(&soft_space_or_block_indent(&specifiers.format()))]
-                )?;
-            }
-            None => {
-                write!(
-                    f,
-                    [format_dangling_comments(node.syntax()).with_soft_block_indent()]
-                )?;
-            }
+        if specifiers.is_empty() {
+            write!(
+                f,
+                [format_dangling_comments(node.syntax()).with_soft_block_indent()]
+            )?;
+        } else {
+            write!(
+                f,
+                [group(&soft_space_or_block_indent(&specifiers.format()))]
+            )?;
         }
 
         write!(f, [r_curly_token.format()])
