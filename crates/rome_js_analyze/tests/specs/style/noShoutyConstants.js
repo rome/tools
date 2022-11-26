f3fe131a909882c49e65881c50305db9cdf37faf
Ross Hill <ross@rosshill.ca>
2022-11-26 13:27:04 -0500

    Implement fix

--- crates/rome_js_analyze/tests/specs/style/noShoutyConstants.js
+++ crates/rome_js_analyze/tests/specs/style/noShoutyConstants.js
@@ -36,3 +36,9 @@ export const F = NotMatching;
 
 const matchingLowercase = "matchingLowercase";
 export const G = matchingLowercase;
+
+const AL = "ALPHA"
+export const H = AL;
+
+const ALPHA = "AL"
+export const I = ALPHA;
