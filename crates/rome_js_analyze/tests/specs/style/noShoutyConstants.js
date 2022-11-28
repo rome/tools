20e2ed0539cb06433bc731f198c7b9d44db1421a
Ross Hill <ross@rosshill.ca>
2022-11-28 03:40:55 -0500

    fix(rome_js_analyze): fix noShoutyConstants with empty string (#3868)
    Closes https://github.com/rome/tools/issues/3867

--- crates/rome_js_analyze/tests/specs/style/noShoutyConstants.js
+++ crates/rome_js_analyze/tests/specs/style/noShoutyConstants.js
@@ -27,3 +27,18 @@ const D ="D";
 export const e = {
 	D
 };
+
+const Empty = "";
+export const E = Empty;
+
+const NotMatching = "DoesNotMatch";
+export const F = NotMatching;
+
+const matchingLowercase = "matchingLowercase";
+export const G = matchingLowercase;
+
+const AL = "ALPHA";
+export const H = AL;
+
+const ALPHA = "AL";
+export const I = ALPHA;
