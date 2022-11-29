02e85bd759ffafd65d7ab4a08143e1998e68a49c
Anchen Li <anchen.li@pslm-qxy9gygyvj.lan>
2022-11-29 19:45:08 +1100

    update tests

--- crates/rome_js_analyze/tests/specs/nursery/NoRedundantUseStrict.js
+++ crates/rome_js_analyze/tests/specs/nursery/NoRedundantUseStrict.js
@@ -2,4 +2,12 @@
 
 function test() {
 	"use strict";
+	function inner_a() {
+		"use strict"; // redundant directive
+	}
+	function inner_b() {
+		function inner_inner() {
+			"use strict"; // additional redundant directive
+		}
+	}
 }
