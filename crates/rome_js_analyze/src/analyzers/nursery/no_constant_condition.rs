e4fe0fd4448595c2981219c62cd28d9e3dac90ae
notmd <tinhmeo10@gmail.com>
2022-12-07 20:04:25 +0700

    wip

--- crates/rome_js_analyze/src/analyzers/nursery/no_constant_condition.rs
+++ crates/rome_js_analyze/src/analyzers/nursery/no_constant_condition.rs
@@ -1,12 +1,15 @@
+use std::f32::consts::E;
+
 use rome_analyze::{context::RuleContext, declare_rule, Ast, Rule, RuleDiagnostic};
 use rome_console::markup;
 
 use rome_js_syntax::{
-    AnyJsExpression, AnyJsLiteralExpression, JsConditionalExpression, JsDoWhileStatement,
-    JsForStatement, JsIfStatement, JsLogicalOperator, JsSyntaxKind, JsSyntaxToken,
+    AnyJsArrayElement, AnyJsExpression, AnyJsLiteralExpression, AnyJsTemplateElement,
+    JsAssignmentOperator, JsBinaryOperator, JsConditionalExpression, JsDoWhileStatement,
+    JsForStatement, JsIfStatement, JsLogicalOperator, JsSyntaxKind, JsSyntaxToken, JsUnaryOperator,
     JsWhileStatement,
 };
-use rome_rowan::{declare_node_union, AstNode, SyntaxToken};
+use rome_rowan::{declare_node_union, AstNode, AstSeparatedList, SyntaxToken};
 
 declare_rule! {
     /// Disallow constant expressions in conditions
@@ -52,7 +55,7 @@ impl Rule for NoConstantCondition {
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
         let test = ctx.query().test()?.omit_parentheses();
 
-        is_constant_condition(&test).map(|_| test)
+        is_constant_condition(&test, true).map(|_| test)
     }
 
     fn diagnostic(_ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
@@ -78,33 +81,59 @@ impl ConditionalStatement {
     }
 }
 
-fn is_constant_condition(test: &AnyJsExpression) -> Option<()> {
+fn is_constant_condition(test: &AnyJsExpression, in_boolean_position: bool) -> Option<()> {
     use AnyJsExpression::*;
+    // dbg!(
+    //     test.clone().text(),
+    //     test.clone().omit_parentheses().syntax().kind()
+    // );
     match test.clone().omit_parentheses() {
         AnyJsLiteralExpression(_)
-        | JsTemplateExpression(_)
         | JsObjectExpression(_)
-        | JsAssignmentExpression(_) => Some(()),
-        JsUnaryExpression(node) => is_constant_condition(&node.argument().ok()?),
-        JsBinaryExpression(node) => is_constant_condition(&node.left().ok()?)
-            .and_then(|_| is_constant_condition(&node.right().ok()?)),
-        JsLogicalExpression(node) => {
-            let left = node.left().ok()?;
-            let right = node.left().ok()?;
+        | JsFunctionExpression(_)
+        | JsArrowFunctionExpression(_)
+        | JsClassExpression(_) => Some(()),
+        JsUnaryExpression(node) => {
             let op = node.operator().ok()?;
-            let is_left_constant = is_constant_condition(&left).is_some();
-            let is_right_constant = is_constant_condition(&right).is_some();
+            // dbg!(op);
+            if op == JsUnaryOperator::Void || op == JsUnaryOperator::Typeof {
+                // dbg!("void or typeof");
+                return Some(());
+            }
+            if op == JsUnaryOperator::LogicalNot {
+                return is_constant_condition(&node.argument().ok()?, true);
+            }
+            is_constant_condition(&node.argument().ok()?, false)
+        }
+        JsBinaryExpression(node) => {
+            // let operator = node.operator().ok()?;
+            // if operator == JsBinaryOperator:: {
 
+            // }
+            is_constant_condition(&node.left().ok()?, false)
+                .and_then(|_| is_constant_condition(&node.right().ok()?, false))
+        }
+        JsLogicalExpression(node) => {
+            // dbg!("JsLogicalExpression", node.text());
+            let left = node.left().ok()?;
+            let right = node.right().ok()?;
+            let op = node.operator().ok()?;
+            let is_left_constant = is_constant_condition(&left, in_boolean_position).is_some();
+            let is_right_constant = is_constant_condition(&right, in_boolean_position).is_some();
+            // dbg!(is_left_constant, left.clone().text());
+            // dbg!(is_right_constant, right.clone().text());
             let is_left_short_circuit = if is_left_constant {
                 is_logical_identity(left, op)
             } else {
                 false
             };
-            let is_right_short_circuit = if is_right_constant {
+            let is_right_short_circuit = if in_boolean_position && is_right_constant {
                 is_logical_identity(right, op)
             } else {
                 false
             };
+
+            // dbg!(is_left_short_circuit, is_right_short_circuit);
 
             if (is_left_constant && is_right_constant)
                 || is_left_short_circuit
@@ -115,23 +144,193 @@ fn is_constant_condition(test: &AnyJsExpression) -> Option<()> {
                 None
             }
         }
-        _ => {
-            dbg!(test);
-            todo!("is_constant_condition not cover this case");
+        JsSequenceExpression(node) => {
+            is_constant_condition(&node.right().ok()?, in_boolean_position)
         }
+        JsIdentifierExpression(node) => {
+            let is_named_undefined = node.is_undefined().ok()?;
+            // TODO check if node is reference to global
+            is_named_undefined.then_some(())
+        }
+        JsArrayExpression(node) => {
+            if !in_boolean_position {
+                node.elements()
+                    .into_iter()
+                    .all(|x| {
+                        if x.is_ok() {
+                            let element = x.unwrap();
+                            // dbg!(element.clone());
+                            match element {
+                                AnyJsArrayElement::JsArrayHole(_) => true,
+                                AnyJsArrayElement::JsSpread(node) => {
+                                    if let Some(argument) = node.argument().ok() {
+                                        is_constant_condition(&argument, in_boolean_position)
+                                            .is_some()
+                                    } else {
+                                        false
+                                    }
+                                }
+                                _ => element
+                                    .as_any_js_expression()
+                                    .and_then(|node| is_constant_condition(node, false))
+                                    .is_some(),
+                            }
+                        } else {
+                            false
+                        }
+                    })
+                    .then_some(())
+            } else {
+                Some(())
+            }
+        }
+        JsNewExpression(_) => in_boolean_position.then_some(()),
+        JsCallExpression(node) => {
+            if node.has_callee("Boolean") {
+                let args = node.arguments().ok()?.args();
+                if args.is_empty() {
+                    return Some(());
+                }
+                // TODO check Boolean is referrenced to global.
+                return is_constant_condition(args.first()?.ok()?.as_any_js_expression()?, true);
+            }
+
+            None
+        }
+        JsAssignmentExpression(node) => {
+            use JsAssignmentOperator::*;
+
+            let operator = node.operator().ok()?;
+            if operator == Assign {
+                return is_constant_condition(&node.right().ok()?, in_boolean_position);
+            }
+
+            if matches!(operator, LogicalOrAssign | LogicalAndAssign) && in_boolean_position {
+                let new_op = match operator {
+                    LogicalAndAssign => JsLogicalOperator::LogicalAnd,
+                    LogicalOrAssign => JsLogicalOperator::LogicalOr,
+                    _ => unreachable!(),
+                };
+
+                return is_logical_identity(node.right().ok()?, new_op).then_some(());
+            }
+            None
+        }
+        JsTemplateExpression(node) => {
+            dbg!(node.text());
+            
+            let elements = node.elements();
+            let has_truthy_quasi = elements.clone().into_iter().any(|element| {
+                match element {
+                    AnyJsTemplateElement::JsTemplateChunkElement(element) => {
+                        if let Some(quasi) = element.template_chunk_token().ok() {
+                            quasi.text_trimmed().len() > 0
+                        } else {
+                            false
+                        }
+                    },
+                    AnyJsTemplateElement::JsTemplateElement(_) => false,
+                }
+            });
+
+            if has_truthy_quasi && in_boolean_position {
+                return Some(())
+            }
+
+            let are_expressions_constant = elements.into_iter().all(|element| {
+                match element {
+                    AnyJsTemplateElement::JsTemplateChunkElement(element) => {
+                        true
+                    }
+                    AnyJsTemplateElement::JsTemplateElement(element) => {
+                        if let Some(expr) = element.expression().ok() {
+                            is_constant_condition(&expr, false).is_some()
+                        } else {
+                            false
+                        }
+                    }
+                }
+            });
+           
+            dbg!(are_expressions_constant, node.text());
+            (are_expressions_constant).then_some(())
+        }
+        _ => None,
     }
 }
 
 fn is_logical_identity(node: AnyJsExpression, operator: JsLogicalOperator) -> bool {
     use AnyJsExpression::*;
-
-    match node {
-        AnyJsLiteralExpression(expr) => {
-            let boolean_value = get_boolean_value(expr).unwrap_or(false);
+    // dbg!(
+    //     "is_logical_identity",
+    //     node.clone().omit_parentheses().syntax().kind()
+    // );
+    match node.omit_parentheses() {
+        AnyJsLiteralExpression(node) => {
+            let boolean_value = get_boolean_value(node).unwrap_or(false);
             operator == JsLogicalOperator::LogicalOr && boolean_value
                 || (operator == JsLogicalOperator::LogicalAnd && !boolean_value)
         }
-        _ => todo!(),
+        JsUnaryExpression(node) => {
+            if operator != JsLogicalOperator::LogicalAnd {
+                return false;
+            }
+
+            if let Some(node_operator) = node.operator().ok() {
+                node_operator == JsUnaryOperator::Void
+            } else {
+                false
+            }
+        }
+        JsLogicalExpression(node) => {
+            if let Some(node_operator) = node.operator().ok() {
+                // handles `a && false || b`
+                // `false` is an identity element of `&&` but not `||`
+                // so the logical identity of the whole expression can not be defined.
+                if operator != node_operator {
+                    return false;
+                }
+
+                let is_left_logical_identify = node
+                    .left()
+                    .ok()
+                    .map_or(false, |left| is_logical_identity(left, operator));
+                let is_right_logical_identify = node
+                    .right()
+                    .ok()
+                    .map_or(false, |right| is_logical_identity(right, operator));
+
+                is_left_logical_identify || is_right_logical_identify
+            } else {
+                false
+            }
+        }
+        JsAssignmentExpression(node) => {
+            if let Some(node_operator) = node.operator().ok() {
+                if let Some(right) = node.right().ok() {
+                    let is_valid_logical_assignment = match node_operator {
+                        JsAssignmentOperator::LogicalAndAssign
+                            if operator == JsLogicalOperator::LogicalAnd =>
+                        {
+                            true
+                        }
+                        JsAssignmentOperator::LogicalOrAssign
+                            if operator == JsLogicalOperator::LogicalOr =>
+                        {
+                            true
+                        }
+                        _ => false,
+                    };
+
+                    is_valid_logical_assignment && is_logical_identity(right, operator)
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        }
+        _ => false,
     }
 }
 
@@ -150,7 +349,7 @@ fn get_boolean_value(node: AnyJsLiteralExpression) -> Option<bool> {
             .map(|value| value.text_trimmed() != "0"),
         AnyJsLiteralExpression::JsRegexLiteralExpression(_) => Some(true),
         AnyJsLiteralExpression::JsStringLiteralExpression(node) => {
-            dbg!(node.value_token().unwrap().text_trimmed());
+            // dbg!(node.value_token().unwrap().text_trimmed());
             node.value_token().ok().map(|value| {
                 let text_trimmed = value.text_trimmed();
                 text_trimmed != "''" && text_trimmed != "\"\""
--- crates/rome_js_analyze/tests/specs/nursery/noConstantCondition/invalid.js
+++ crates/rome_js_analyze/tests/specs/nursery/noConstantCondition/invalid.js
@@ -23,121 +23,121 @@ if ({});
 if (0 < 1);
 if (0 || 1);
 if (a, 1);
-// if (`foo`);
-// if (``);
-// if (`\\\n`);
-// if (`${'bar'}`);
-// if (`${'bar' + `foo`}`);
-// if (`foo${false || true}`);
-// if (`foo${0 || 1}`);
-// if (`foo${bar}`);
-// if (`${bar}foo`);
-// if (!(true || a));
-// if (!(a && void b && c));
-// if (0 || !(a && null));
-// if (1 + !(a || true));
-// if (!(null && a) > 1);
-// if (+(!(a && 0)));
-// if (!typeof a === 'string');
-// if (-('foo' || a));
-// if (+(void a && b) === ~(1 || c));
-// if (a ||= true);
-// if (a ||= 5);
-// if (a ||= 'foo' || b);
-// if (a ||= b || /regex/);
-// if (a ||= b ||= true);
-// if (a ||= b ||= c || 1);
-// if (!(a ||= true));
-// if (!(a ||= 'foo') === true);
-// if (!(a ||= 'foo') === false);
-// if (a || (b ||= true));
-// if ((a ||= 1) || b);
-// if ((a ||= true) && true);
-// if (true && (a ||= true));
-// if (a &&= false);
-// if (a &&= null);
-// if (a &&= void b);
-// if (a &&= 0 && b);
-// if (a &&= b && '');
-// if (a &&= b &&= false);
-// if (a &&= b &&= c && false);
-// if (!(a &&= false));
-// if (!(a &&= 0) + 1);
-// if (a && (b &&= false));
-// if ((a &&= null) && b);
-// if (false || (a &&= false));
-// if ((a &&= false) || false);
-// while ([]);
-// while (~!0);
-// while (x = 1);
-// while (function () { });
-// while (true);
-// while (1);
-// while (() => { });
-// while (`foo`);
-// while (``);
-// while (`${'foo'}`);
-// while (`${'foo' + 'bar'}`);
-// if (typeof x) { }
-// if (typeof 'abc' === 'string') { }
-// if (a = typeof b) { }
-// if (a, typeof b) { }
-// if (typeof 'a' == 'string' || typeof 'b' == 'string') { }
-// while (typeof x) { }
-// if (1 || void x);
-// if (void x);
-// if (y = void x);
-// if (x, void x);
-// if (void x === void y);
-// if (void x && a);
-// if (a && void x);
-// if (false && abc === 'str') { }
-// if (true || abc === 'str') { }
-// if (1 || abc === 'str') { }
-// if (abc === 'str' || true) { }
-// if (abc === 'str' || true || def === 'str') { }
-// if (false || true) { }
-// if (typeof abc === 'str' || true) { }
-// if ('str' || a) { }
-// if ('str' || abc === 'str') { }
-// if ('str1' || 'str2') { }
-// if ('str1' && 'str2') { }
-// if (abc === 'str' || 'str') { }
-// if (a || 'str') { }
-// function* foo() { while (true) { } yield 'foo'; }
-// function* foo() { while (true) { if (true) { yield 'foo'; } } }
-// function* foo() { while (true) { yield 'foo'; } while (true) { } }
-// var a = function* foo() { while (true) { } yield 'foo'; }
-// while (true) { function* foo() { yield; } }
-// function* foo() { if (true) { yield 'foo'; } }
-// function* foo() { for (let foo = yield; true;) { } }
-// function* foo() { for (foo = yield; true;) { } }
-// function foo() { while (true) { function* bar() { while (true) { yield; } } } }
-// function foo() { while (true) { const bar = function* { while (true) { yield; } } } } ",
-// function* foo() { for (let foo = 1 + 2 + 3 + (yield); true; baz) { } }
-// if ([a]) { }
-// if ([]) { }
-// if ('' + ['a']) { }
-// if ('' + []) { }
-// if (+1) { }
-// if ([,] + '') { }
-// if (/foo/ui);
-// if (0n);
-// if (0b0n);
-// if (0o0n);
-// if (0x0n);
-// if (0b1n);
-// if (0o1n);
-// if (0x1n);
-// if (0x1n || foo);
-// if (class { }) { }
-// if (new Foo()) { }
-// if (new Boolean(foo)) { }
-// if (new String(foo)) { }
-// if (new Number(foo)) { }
-// if (`${[...['a']]}`) { }
-// if (undefined) { }
-// if (Boolean(1)) { }
-// if (Boolean()) { }
-// if (Boolean([a])) { }
-// if (Boolean(1)) { function Boolean() { } }
+if (`foo`);
+if (``);
+if (`\\\n`);
+if (`${'bar'}`);
+if (`${'bar' + `foo`}`);
+if (`foo${false || true}`);
+if (`foo${0 || 1}`);
+if (`foo${bar}`);
+if (`${bar}foo`);
+if (!(true || a));
+if (!(a && void b && c));
+if (0 || !(a && null));
+if (1 + !(a || true));
+if (!(null && a) > 1);
+if (+(!(a && 0)));
+if (!typeof a === 'string');
+if (-('foo' || a));
+if (+(void a && b) === ~(1 || c));
+if (a ||= true);
+if (a ||= 5);
+if (a ||= 'foo' || b);
+if (a ||= b || /regex/);
+if (a ||= b ||= true);
+if (a ||= b ||= c || 1);
+if (!(a ||= true));
+if (!(a ||= 'foo') === true);
+if (!(a ||= 'foo') === false);
+if (a || (b ||= true));
+if ((a ||= 1) || b);
+if ((a ||= true) && true);
+if (true && (a ||= true));
+if (a &&= false);
+if (a &&= null);
+if (a &&= void b);
+if (a &&= 0 && b);
+if (a &&= b && '');
+if (a &&= b &&= false);
+if (a &&= b &&= c && false);
+if (!(a &&= false));
+if (!(a &&= 0) + 1);
+if (a && (b &&= false));
+if ((a &&= null) && b);
+if (false || (a &&= false));
+if ((a &&= false) || false);
+while ([]);
+while (~!0);
+while (x = 1);
+while (function () { });
+while (true);
+while (1);
+while (() => { });
+while (`foo`);
+while (``);
+while (`${'foo'}`);
+while (`${'foo' + 'bar'}`);
+if (typeof x) { }
+if (typeof 'abc' === 'string') { }
+if (a = typeof b) { }
+if (a, typeof b) { }
+if (typeof 'a' == 'string' || typeof 'b' == 'string') { }
+while (typeof x) { }
+if (1 || void x);
+if (void x);
+if (y = void x);
+if (x, void x);
+if (void x === void y);
+if (void x && a);
+if (a && void x);
+if (false && abc === 'str') { }
+if (true || abc === 'str') { }
+if (1 || abc === 'str') { }
+if (abc === 'str' || true) { }
+if (abc === 'str' || true || def === 'str') { }
+if (false || true) { }
+if (typeof abc === 'str' || true) { }
+if ('str' || a) { }
+if ('str' || abc === 'str') { }
+if ('str1' || 'str2') { }
+if ('str1' && 'str2') { }
+if (abc === 'str' || 'str') { }
+if (a || 'str') { }
+function* foo() { while (true) { } yield 'foo'; }
+function* foo() { while (true) { if (true) { yield 'foo'; } } }
+function* foo() { while (true) { yield 'foo'; } while (true) { } }
+var a = function* foo() { while (true) { } yield 'foo'; }
+while (true) { function* foo() { yield; } }
+function* foo() { if (true) { yield 'foo'; } }
+function* foo() { for (let foo = yield; true;) { } }
+function* foo() { for (foo = yield; true;) { } }
+function foo() { while (true) { function* bar() { while (true) { yield; } } } }
+function foo() { while (true) { const bar = function* () { while (true) { yield; } } } }
+function* foo() { for (let foo = 1 + 2 + 3 + (yield); true; baz) { } }
+if ([a]) { }
+if ([]) { }
+if ('' + ['a']) { }
+if ('' + []) { }
+if (+1) { }
+if ([,] + '') { }
+if (/foo/ui);
+if (0n);
+if (0b0n);
+if (0o0n);
+if (0x0n);
+if (0b1n);
+if (0o1n);
+if (0x1n);
+if (0x1n || foo);
+if (class { }) { }
+if (new Foo()) { }
+if (new Boolean(foo)) { }
+if (new String(foo)) { }
+if (new Number(foo)) { }
+if (`${[...['a']]}`) { }
+if (undefined) { }
+if (Boolean(1)) { }
+if (Boolean()) { }
+if (Boolean([a])) { }
+if (Boolean(1)) { function Boolean() { } }
