aff8e521394be79b65f90dc97b74007f19db81c4
Anchen <li.anchen.au@gmail.com>
2022-12-13 23:51:53 +1100

    wip: add extract code point for escape characters

--- crates/rome_js_analyze/src/analyzers/nursery/no_control_characters_in_regex.rs
+++ crates/rome_js_analyze/src/analyzers/nursery/no_control_characters_in_regex.rs
@@ -4,6 +4,8 @@ use rome_analyze::{context::RuleContext, declare_rule, Ast, Rule};
 use rome_js_syntax::{JsNewExpression, JsRegexLiteralExpression};
 use rome_rowan::{declare_node_union, AstNode};
 
+use crate::utils::escape;
+
 declare_rule! {
  /// Promotes the use of awesome tricks
  ///
@@ -21,14 +23,43 @@ declare_rule! {
 declare_node_union! {
   pub(crate) PossibleRegexExpression = JsNewExpression | JsRegexLiteralExpression
 }
-fn get_number_from_hex_character(it: &mut Peekable<Chars>) -> Option<i64> {
-    it.next();
+fn get_code_point_from_hex_character(it: &mut Peekable<Chars>) -> Option<i64> {
     let mut digits = Vec::new();
     digits.push(it.next()?);
     digits.push(it.next()?);
     let s: String = digits.into_iter().collect();
     i64::from_str_radix(s.as_str(), 16).ok()
 }
+
+fn get_code_point_from_escape_character(it: &mut Peekable<Chars>) -> Option<i64> {
+    let mut digits = Vec::new();
+
+    if let Some(&c) = it.peek() {
+        if c == '{' {
+            it.next();
+            while let Some(&c) = it.peek() {
+                match c {
+                    '}' => {
+                        it.next();
+                        let s: String = digits.into_iter().collect();
+                        return i64::from_str_radix(s.as_str(), 16).ok();
+                    }
+                    _ => digits.push(it.next()?),
+                }
+            }
+        } else {
+            for _ in 1..=4 {
+                digits.push(it.next()?);
+            }
+
+            let s: String = digits.into_iter().collect();
+            return i64::from_str_radix(s.as_str(), 16).ok();
+        }
+    }
+
+    None
+}
+
 impl Rule for NoControlCharactersInRegex {
     type Query = Ast<PossibleRegexExpression>;
     type State = String;
@@ -39,6 +70,7 @@ impl Rule for NoControlCharactersInRegex {
         let node = ctx.query();
         let ele = JsRegexLiteralExpression::cast(node.syntax().clone()).unwrap();
         let pattern = ele.pattern().unwrap();
+        println!("patten : {}", pattern);
         let mut it = pattern.chars().peekable();
 
         while let Some(&c) = it.peek() {
@@ -52,38 +84,30 @@ impl Rule for NoControlCharactersInRegex {
                                 if let Some(&c) = it.peek() {
                                     match c {
                                         'x' => {
-                                            let cp = get_number_from_hex_character(&mut it)?;
+                                            it.next();
+                                            let cp = get_code_point_from_hex_character(&mut it)?;
                                             println!("{:?}", cp)
                                         }
                                         'u' => {
                                             it.next();
                                             // get number with {}
-                                            let mut digits = Vec::new();
-                                            digits.push(it.next()?);
-                                            digits.push(it.next()?);
-                                            digits.push(it.next()?);
-                                            digits.push(it.next()?);
-                                            let s: String = digits.iter().collect();
-                                            println!("{}", s);
+                                            let cp = get_code_point_from_escape_character(&mut it)?;
+                                            println!("{}", cp);
                                         }
                                         _ => {}
                                     }
                                 }
                             }
                             'x' => {
-                                let cp = get_number_from_hex_character(&mut it)?;
+                                it.next();
+                                let cp = get_code_point_from_hex_character(&mut it)?;
                                 println!("{:?}", cp)
                             }
                             'u' => {
                                 it.next();
                                 // get number with {}
-                                let mut digits = Vec::new();
-                                digits.push(it.next()?);
-                                digits.push(it.next()?);
-                                digits.push(it.next()?);
-                                digits.push(it.next()?);
-                                let s: String = digits.iter().collect();
-                                println!("{}", s);
+                                let cp = get_code_point_from_escape_character(&mut it)?;
+                                println!("{}", cp);
                             }
                             _ => {}
                         }
--- crates/rome_js_analyze/tests/specs/nursery/noControlCharactersInRegex/invalid.js
+++ crates/rome_js_analyze/tests/specs/nursery/noControlCharactersInRegex/invalid.js
@@ -1,3 +1,5 @@
 var pattern1 = /\x00/;
 var pattern2 = /\x0C/;
 var pattern3 = /\x1F/;
+var pattern4 = /\u000C/;
+var pattern5 = /\u{C}/u;
