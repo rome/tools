236ee53ab028429f00295074aac2a9f0ecfef533
Anchen <li.anchen.au@gmail.com>
2022-12-14 00:20:48 +1100

    wip: add check control characters

--- crates/rome_js_analyze/src/analyzers/nursery/no_control_characters_in_regex.rs
+++ crates/rome_js_analyze/src/analyzers/nursery/no_control_characters_in_regex.rs
@@ -23,15 +23,17 @@ declare_rule! {
 declare_node_union! {
   pub(crate) PossibleRegexExpression = JsNewExpression | JsRegexLiteralExpression
 }
-fn get_code_point_from_hex_character(it: &mut Peekable<Chars>) -> Option<i64> {
+fn get_code_point_from_hex_character(it: &mut Peekable<Chars>) -> Option<(String, i64)> {
     let mut digits = Vec::new();
-    digits.push(it.next()?);
-    digits.push(it.next()?);
+    for _ in 1..=2 {
+        digits.push(it.next()?);
+    }
     let s: String = digits.into_iter().collect();
-    i64::from_str_radix(s.as_str(), 16).ok()
+    let cp = i64::from_str_radix(s.as_str(), 16).ok()?;
+    Some((s, cp))
 }
 
-fn get_code_point_from_escape_character(it: &mut Peekable<Chars>) -> Option<i64> {
+fn get_code_point_from_escape_character(it: &mut Peekable<Chars>) -> Option<(String, i64)> {
     let mut digits = Vec::new();
 
     if let Some(&c) = it.peek() {
@@ -42,7 +44,8 @@ fn get_code_point_from_escape_character(it: &mut Peekable<Chars>) -> Option<i64>
                     '}' => {
                         it.next();
                         let s: String = digits.into_iter().collect();
-                        return i64::from_str_radix(s.as_str(), 16).ok();
+                        let cp = i64::from_str_radix(s.as_str(), 16).ok()?;
+                        return Some((format!("{{{}}}", s), cp));
                     }
                     _ => digits.push(it.next()?),
                 }
@@ -53,11 +56,72 @@ fn get_code_point_from_escape_character(it: &mut Peekable<Chars>) -> Option<i64>
             }
 
             let s: String = digits.into_iter().collect();
-            return i64::from_str_radix(s.as_str(), 16).ok();
+            let cp = i64::from_str_radix(s.as_str(), 16).ok()?;
+            return Some((s, cp));
         }
     }
 
     None
+}
+
+fn collect_control_characters(pattern: String) -> Option<Vec<String>> {
+    let mut control_characters: Vec<String> = Vec::new();
+
+    let mut it = pattern.chars().peekable();
+
+    while let Some(&c) = it.peek() {
+        match c {
+            '\\' => {
+                it.next();
+                if let Some(&c) = it.peek() {
+                    match c {
+                        '\\' => {
+                            it.next();
+                            if let Some(&c) = it.peek() {
+                                match c {
+                                    'x' => {
+                                        it.next();
+                                        let (s, cp) = get_code_point_from_hex_character(&mut it)?;
+                                        if cp >= 0 && cp < 32 {
+                                            control_characters.push(format!("\\\\x{}", s));
+                                        }
+                                    }
+                                    'u' => {
+                                        it.next();
+                                        let (s, cp) =
+                                            get_code_point_from_escape_character(&mut it)?;
+                                        if cp >= 0 && cp < 32 {
+                                            control_characters.push(format!("\\\\u{}", s));
+                                        }
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        'x' => {
+                            it.next();
+                            let (s, cp) = get_code_point_from_hex_character(&mut it)?;
+                            if cp >= 0 && cp < 32 {
+                                control_characters.push(format!("\\x{}", s));
+                            }
+                        }
+                        'u' => {
+                            it.next();
+                            let (s, cp) = get_code_point_from_escape_character(&mut it)?;
+                            if cp >= 0 && cp < 32 {
+                                control_characters.push(format!("\\u{}", s));
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {
+                it.next();
+            }
+        }
+    }
+    Some(control_characters)
 }
 
 impl Rule for NoControlCharactersInRegex {
@@ -71,53 +135,13 @@ impl Rule for NoControlCharactersInRegex {
         let ele = JsRegexLiteralExpression::cast(node.syntax().clone()).unwrap();
         let pattern = ele.pattern().unwrap();
         println!("patten : {}", pattern);
-        let mut it = pattern.chars().peekable();
-
-        while let Some(&c) = it.peek() {
-            match c {
-                '\\' => {
-                    it.next();
-                    if let Some(&c) = it.peek() {
-                        match c {
-                            '\\' => {
-                                it.next();
-                                if let Some(&c) = it.peek() {
-                                    match c {
-                                        'x' => {
-                                            it.next();
-                                            let cp = get_code_point_from_hex_character(&mut it)?;
-                                            println!("{:?}", cp)
-                                        }
-                                        'u' => {
-                                            it.next();
-                                            // get number with {}
-                                            let cp = get_code_point_from_escape_character(&mut it)?;
-                                            println!("{}", cp);
-                                        }
-                                        _ => {}
-                                    }
-                                }
-                            }
-                            'x' => {
-                                it.next();
-                                let cp = get_code_point_from_hex_character(&mut it)?;
-                                println!("{:?}", cp)
-                            }
-                            'u' => {
-                                it.next();
-                                // get number with {}
-                                let cp = get_code_point_from_escape_character(&mut it)?;
-                                println!("{}", cp);
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-                _ => {
-                    it.next();
-                }
+        let controls = collect_control_characters(pattern);
+        if let Some(ctrls) = controls {
+            for c in ctrls {
+                println!("{}", c);
             }
         }
+
         None
     }
 }
