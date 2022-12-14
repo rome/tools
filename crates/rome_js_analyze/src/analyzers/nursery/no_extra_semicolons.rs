aaa93dad485368402ac711d97b977e6564bea7f0
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-12-14 12:33:41 -0300

    feat(rome_js_analyzer): added check for short statements and class empty members
    added check for short statements
    added class empty members check

--- crates/rome_js_analyze/src/analyzers/nursery/no_extra_semicolons.rs
+++ crates/rome_js_analyze/src/analyzers/nursery/no_extra_semicolons.rs
@@ -1,9 +1,9 @@
 use rome_analyze::context::RuleContext;
 use rome_analyze::{declare_rule, Ast, Rule, RuleDiagnostic};
 use rome_console::markup;
-use rome_js_syntax::{JsEmptyStatement, JsSyntaxKind, T};
+use rome_js_syntax::{JsEmptyStatement, JsEmptyClassMember, JsSyntaxKind, T};
 
-use rome_rowan::{AstNode};
+use rome_rowan::{declare_node_union, AstNode};
 
 declare_rule! {
     /// Typing mistakes and misunderstandings about where semicolons are required can lead to semicolons that are unnecessary.
@@ -43,10 +43,12 @@ declare_rule! {
     }
 }
 
-pub type JsExtraSemicolon = JsEmptyStatement;
+declare_node_union! {
+    pub(crate) AnyJsExtraSemicolon = JsEmptyStatement | JsEmptyClassMember
+}
 
 impl Rule for NoExtraSemicolons {
-    type Query = Ast<JsExtraSemicolon>;
+    type Query = Ast<AnyJsExtraSemicolon>;
     type State = ();
     type Signals = Option<Self::State>;
     type Options = ();
@@ -54,27 +56,43 @@ impl Rule for NoExtraSemicolons {
     fn run(ctx: &RuleContext<Self>) -> Option<Self::State> {
         let node = ctx.query();
 
-        let parent = node.syntax().parent()?;
-        let has_empty_statements_in_list = parent
-            .children_with_tokens()
-            .into_iter()
-            .filter(|child| child.kind() == JsSyntaxKind::JS_EMPTY_STATEMENT)
-            .map(|child| {
-                let child = child.into_node().unwrap();
-                let child = JsEmptyStatement::cast(child).unwrap();
-                child.syntax().first_token().unwrap().kind() == T![;]
-            })
-            .filter(|value| *value)
-            .count() > 0;
-        let has_last_entity_in_parent =
-            parent.prev_sibling_or_token()?.kind() == JsSyntaxKind::JS_MODULE_ITEM_LIST;
-        let has_first_semicolon_in_node = node.syntax().first_token()?.kind() == T![;];
-        let has_empty_statements = !has_last_entity_in_parent && has_empty_statements_in_list && has_first_semicolon_in_node;
+        match node {
+            AnyJsExtraSemicolon::JsEmptyStatement(stmt) => {
+                let parent = stmt.syntax().parent()?;
+                let has_last_entity_in_parent =
+                    parent.kind() == JsSyntaxKind::JS_MODULE_ITEM_LIST;
+                let has_empty_statements_in_list = parent
+                    .children_with_tokens()
+                    .into_iter()
+                    .filter(|child| child.kind() == JsSyntaxKind::JS_EMPTY_STATEMENT)
+                    .map(|child| {
+                        let child = child.into_node().unwrap();
+                        let child = JsEmptyStatement::cast(child).unwrap();
+                        child.syntax().first_token().unwrap().kind() == T![;]
+                    })
+                    .filter(|value| *value)
+                    .count()
+                    > 0;
+                let has_first_semicolon_in_node = stmt.syntax().first_token()?.kind() == T![;];
+                let has_empty_statements = has_last_entity_in_parent
+                    && has_empty_statements_in_list
+                    && has_first_semicolon_in_node;
 
-        if has_empty_statements {
-            Some(())
-        } else {
-            None
+                if has_empty_statements {
+                    Some(())
+                } else {
+                    None
+                }
+            },
+            AnyJsExtraSemicolon::JsEmptyClassMember(stmt) => {
+                let has_first_semicolon_in_node = stmt.syntax().first_token()?.kind() == T![;];
+
+                if has_first_semicolon_in_node {
+                    Some(())
+                } else {
+                    None
+                }
+            }
         }
     }
 
