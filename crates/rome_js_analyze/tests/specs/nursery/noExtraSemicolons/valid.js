0b8aa72c7da2bc0b93ad064880e9bd034789f2c2
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-12-15 15:17:32 -0300

    feat(rome_js_analyzer): added dir structure

---
+++ crates/rome_js_analyze/tests/specs/nursery/noExtraSemicolons/valid.js
@@ -0,0 +1,33 @@
+// valid
+
+{
+  function baz() {}
+}
+
+{
+  for(;true;);for(;true;);for(;true;);for(;true;);for(;true;);
+}
+
+{
+  const x = 5;
+}
+
+{
+  const foo = function() {
+      // code
+  };
+}
+
+{
+  class C {
+      field;
+  
+      method() {
+          // code
+      }
+  
+      static {
+          // code
+      }
+  }
+}
