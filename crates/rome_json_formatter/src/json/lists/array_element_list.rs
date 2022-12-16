dcf3dfad1b06a2dd26e39bcababa8be3fd31a2b2
Dhiraj Arun <dhrjarun@gmail.com>
2022-12-16 22:24:16 +0530

    fix(rome_json_formatter): fix formatting

--- crates/rome_json_formatter/src/json/lists/array_element_list.rs
+++ crates/rome_json_formatter/src/json/lists/array_element_list.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
-use rome_json_syntax::JsonArrayElementList;
 use crate::separated::FormatAstSeparatedListExtension;
+use rome_json_syntax::JsonArrayElementList;
 use rome_rowan::{AstNode, AstSeparatedList};
 
 #[derive(Debug, Clone, Default)]
@@ -9,7 +9,7 @@ pub(crate) struct FormatJsonArrayElementList;
 impl FormatRule<JsonArrayElementList> for FormatJsonArrayElementList {
     type Context = JsonFormatContext;
     fn fmt(&self, node: &JsonArrayElementList, f: &mut JsonFormatter) -> FormatResult<()> {
-		let mut join = f.join_nodes_with_soft_line();
+        let mut join = f.join_nodes_with_soft_line();
 
         for (element, formatted) in node.elements().zip(node.format_separated(",")) {
             join.entry(element.node()?.syntax(), &formatted);
--- crates/rome_json_formatter/src/json/value/array_value.rs
+++ crates/rome_json_formatter/src/json/value/array_value.rs
@@ -1,20 +1,15 @@
 use crate::prelude::*;
-use rome_json_syntax::JsonArrayValue;
 use rome_formatter::write;
+use rome_json_syntax::JsonArrayValue;
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatJsonArrayValue;
 impl FormatNodeRule<JsonArrayValue> for FormatJsonArrayValue {
     fn fmt_fields(&self, node: &JsonArrayValue, f: &mut JsonFormatter) -> FormatResult<()> {
-		write!(f, [node.l_brack_token().format()])?;
+        write!(f, [node.l_brack_token().format()])?;
 
-		write!(
-            f,
-            [group(&soft_block_indent(
-                &node.elements().format()
-            ))]
-        )?;
+        write!(f, [group(&soft_block_indent(&node.elements().format()))])?;
 
-		write!(f, [node.r_brack_token().format()])
+        write!(f, [node.r_brack_token().format()])
     }
 }
