7adcbb985a1ab35d5830c85f9e40be7ee185f3cc
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-12-16 03:55:28 -0300

    feat(rome_js_analayzer): changed to one file

--- crates/rome_js_analyze/tests/specs/nursery/noExtraSemicolons.js
+++ crates/rome_js_analyze/tests/specs/nursery/noExtraSemicolons.js
@@ -85,3 +85,55 @@ class C {
         // code
     }
 };
+
+// valid
+
+const x = 10;
+if (x > 5) {
+  x = 5;
+};
+
+while(true);
+
+while(true) {}
+
+loop1:
+for (let i = 0; i < 5; i++) {
+  str = str + i;
+}
+
+loop1:
+for (let i = 0; i < 5; i++) {
+  if (i === 1) {
+    continue loop1;
+  }
+  str = str + i;
+}
+
+function baz() {}
+
+function buzz() {
+  const x = 10;
+}
+
+for(;true;);
+
+for(;true;);for(;true;);for(;true;);for(;true;);for(;true;);
+
+const x = 5;
+
+const foo = function() {
+    // code
+};
+
+class C {
+    field;
+
+    method() {
+        // code
+    }
+
+    static {
+        // code
+    }
+}
