7d47d6d8d5b7c8ae336c1df87257978260f41cec
Dhiraj Arun <69076988+dhrjarun@users.noreply.github.com>
2022-12-17 17:35:34 +0530

    refactor crates/rome_json_formatter/src/json/value/array_value.rs
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/rome_json_formatter/src/json/value/array_value.rs
+++ crates/rome_json_formatter/src/json/value/array_value.rs
@@ -6,10 +6,16 @@ use rome_json_syntax::JsonArrayValue;
 pub(crate) struct FormatJsonArrayValue;
 impl FormatNodeRule<JsonArrayValue> for FormatJsonArrayValue {
     fn fmt_fields(&self, node: &JsonArrayValue, f: &mut JsonFormatter) -> FormatResult<()> {
-        write!(f, [node.l_brack_token().format()])?;
-
-        write!(f, [group(&soft_block_indent(&node.elements().format()))])?;
-
-        write!(f, [node.r_brack_token().format()])
+		let JsonArrayValueFields {
+			l_brack_token,
+			elements,
+			r_brack_token
+		} = node.as_fields();
+		
+        write!(f, [
+        	l_brack_token.format(),
+        	group(&soft_block_indent(&elements.format())),
+        	r_brack_token.format()
+      	])
     }
 }
