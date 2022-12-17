4b19191fdeecfdeb2c5ebd7de97f59ec25b0dba5
Micha Reiser <micha@rome.tools>
2022-12-17 12:49:28 +0100

    feat(rome_json_formatter): Fallback to Verbatim for nodes with syntax errors
    Some prettier tests for JSON5 are failing after implementing array formatting in #4064
    
    The tests fail because the formatting returns a `FormatError::SyntaxError` if a mandatory child node is missing but the error is never handled by the JSON formatting.
    
    This PR fixes this by
    
    * Gracefully handle missing values on the root value by calling into verbatim formatting if that's the case
    * Use `format_or_verbatim` when formatting values
    
    I had to pull out the `format_or_verbatim` from the `rome_js_formatter` and move it to `rome_formatter` so that it can be re-used between js and json formatting.

--- crates/rome_json_formatter/src/json/auxiliary/member.rs
+++ crates/rome_json_formatter/src/json/auxiliary/member.rs
@@ -1,19 +1,25 @@
 use crate::prelude::*;
 use rome_formatter::{format_args, write};
-use rome_json_syntax::JsonMember;
+use rome_json_syntax::{JsonMember, JsonMemberFields};
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatJsonMember;
 
 impl FormatNodeRule<JsonMember> for FormatJsonMember {
     fn fmt_fields(&self, node: &JsonMember, f: &mut JsonFormatter) -> FormatResult<()> {
+        let JsonMemberFields {
+            name,
+            colon_token,
+            value,
+        } = node.as_fields();
+
         write!(
             f,
             [group(&format_args![
-                &node.name().format(),
-                node.colon_token().format(),
+                &name.format(),
+                colon_token.format(),
                 space(),
-                node.value().format()
+                format_or_verbatim(value?.format())
             ])]
         )
     }
