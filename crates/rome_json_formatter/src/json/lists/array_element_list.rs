0e42ee8382753d23046bbad5a3aabf259145a4f1
Dhiraj Arun <dhrjarun@gmail.com>
2022-12-20 22:02:54 +0530

    add fill Layout

--- crates/rome_json_formatter/src/json/lists/array_element_list.rs
+++ crates/rome_json_formatter/src/json/lists/array_element_list.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 use crate::separated::FormatAstSeparatedListExtension;
 use rome_json_syntax::JsonArrayElementList;
 use rome_rowan::{AstNode, AstSeparatedList};
+use rome_formatter::write;
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatJsonArrayElementList;
@@ -9,12 +10,103 @@ pub(crate) struct FormatJsonArrayElementList;
 impl FormatRule<JsonArrayElementList> for FormatJsonArrayElementList {
     type Context = JsonFormatContext;
     fn fmt(&self, node: &JsonArrayElementList, f: &mut JsonFormatter) -> FormatResult<()> {
-        let mut join = f.join_nodes_with_soft_line();
 
-        for (element, formatted) in node.elements().zip(node.format_separated(",")) {
-            join.entry(element.node()?.syntax(), &formatted);
-        }
+		let layout = if can_concisely_print_array_list(node) {
+            ArrayLayout::Fill
+        } else {
+            ArrayLayout::OnePerLine
+        };
 
-        join.finish()
+
+		match layout {
+			ArrayLayout::Fill => {
+
+				let mut filler = f.fill();
+
+				// Using format_separated is valid in this case as can_print_fill does not allow holes
+				for (element, formatted) in node.iter().zip(node.format_separated(",")) {
+					filler.entry(
+						&format_once(|f| {
+							if get_lines_before(element?.syntax()) > 1 {
+								write!(f, [empty_line()])
+							} else {
+								write!(f, [soft_line_break_or_space()])
+							}
+						}),
+						&formatted,
+					);
+				}
+
+				filler.finish()
+			}
+
+			ArrayLayout::OnePerLine => {
+				let mut join = f.join_nodes_with_soft_line();
+
+        		for (element, formatted) in node.elements().zip(node.format_separated(",")) {
+            		join.entry(element.node()?.syntax(), &formatted);
+        		}
+
+        		join.finish()
+			}
+		}
+
+
+
     }
+}
+
+
+#[derive(Copy, Clone, Debug)]
+enum ArrayLayout {
+    /// Tries to fit as many array elements on a single line as possible.
+    ///
+    /// ```json
+    /// { list: [
+    ///     1, 2, 3,
+    ///     5, 6,
+    ///   ]
+	/// }
+    /// ```
+    Fill,
+
+    /// Prints every element on a single line if the whole array expression exceeds the line width, or any
+    /// of its elements gets printed in *expanded* mode.
+    /// ```json
+    /// { list: [
+    ///     a.b(),
+    ///     4,
+    ///     3,
+    ///  ]
+	/// }
+    /// ```
+    OnePerLine,
+}
+
+
+/// Returns true if the provided JsArrayElementList could
+/// be "fill-printed" instead of breaking each element on
+/// a different line.
+///
+/// The underlying logic only allows lists of literal expressions
+/// with 10 or less characters, potentially wrapped in a "short"
+/// unary expression (+, -, ~ or !)
+pub(crate) fn can_concisely_print_array_list(
+    list: &JsonArrayElementList,
+) -> bool {
+
+    if list.is_empty() {
+        return false;
+    }
+
+    list.elements().all(|item| {
+        match item.into_node() {
+			Ok(rome_json_syntax::AnyJsonValue::JsonArrayValue(_)) => {false}
+			Ok(rome_json_syntax::AnyJsonValue::JsonObjectValue(_)) => {false}
+			Ok(rome_json_syntax::AnyJsonValue::JsonBogusValue(_)) => {false}
+            _ => {
+                return true;
+            }
+        }
+    })
 }
