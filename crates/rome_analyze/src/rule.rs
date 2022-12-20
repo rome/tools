18ff0c28e2a057520489f1e6c58804bf286cf4f8
ematipico <my.burning@gmail.com>
2022-12-20 15:54:35 +0000

    chore(rome_analyze): add groups

--- crates/rome_analyze/src/rule.rs
+++ crates/rome_analyze/src/rule.rs
@@ -229,7 +229,9 @@ macro_rules! impl_group_language {
 
 impl_group_language!(
     T00, T01, T02, T03, T04, T05, T06, T07, T08, T09, T10, T11, T12, T13, T14, T15, T16, T17, T18,
-    T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29
+    T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37,
+    T38, T39, T40, T41, T42, T43, T44, T45, T46, T47, T48, T49, T50, T51, T52, T53, T54, T55, T56,
+    T57, T58, T59
 );
 
 pub trait DeserializableRuleOptions: Default + DeserializeOwned + Sized {
