8d5b2ea85c0f64f10b35b7af2f3ef6063daad7af
Dhiraj Arun <69076988+dhrjarun@users.noreply.github.com>
2022-12-21 20:26:37 +0530

    feat(rome_json_formatter): implement JSON array formatting (#4064)
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/rome_json_formatter/src/json/lists/array_element_list.rs
+++ crates/rome_json_formatter/src/json/lists/array_element_list.rs
@@ -1,5 +1,8 @@
 use crate::prelude::*;
-use rome_json_syntax::JsonArrayElementList;
+use crate::separated::FormatAstSeparatedListExtension;
+use rome_formatter::write;
+use rome_json_syntax::{AnyJsonValue, JsonArrayElementList};
+use rome_rowan::{AstNode, AstSeparatedList};
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatJsonArrayElementList;
@@ -7,6 +10,85 @@ pub(crate) struct FormatJsonArrayElementList;
 impl FormatRule<JsonArrayElementList> for FormatJsonArrayElementList {
     type Context = JsonFormatContext;
     fn fmt(&self, node: &JsonArrayElementList, f: &mut JsonFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let layout = if can_concisely_print_array_list(node) {
+            ArrayLayout::Fill
+        } else {
+            ArrayLayout::OnePerLine
+        };
+
+        match layout {
+            ArrayLayout::Fill => {
+                let mut filler = f.fill();
+
+                for (element, formatted) in node.iter().zip(node.format_separated(",")) {
+                    filler.entry(
+                        &format_once(|f| {
+                            if get_lines_before(element?.syntax()) > 1 {
+                                write!(f, [empty_line()])
+                            } else {
+                                write!(f, [soft_line_break_or_space()])
+                            }
+                        }),
+                        &formatted,
+                    );
+                }
+
+                filler.finish()
+            }
+
+            ArrayLayout::OnePerLine => {
+                let mut join = f.join_nodes_with_soft_line();
+
+                for (element, formatted) in node.elements().zip(node.format_separated(",")) {
+                    join.entry(element.node()?.syntax(), &formatted);
+                }
+
+                join.finish()
+            }
+        }
     }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum ArrayLayout {
+    /// Tries to fit as many array elements on a single line as possible.
+    ///
+    /// ```json
+    /// [
+    ///     1, 2, 3,
+    ///     5, 6,
+    /// ]
+    /// ```
+    Fill,
+
+    /// Prints every element on a single line if the array exceeds the line width, or any
+    /// of its elements gets printed in *expanded* mode.
+    /// ```json
+    /// [
+    ///    { "a": 3 },
+    ///    4,
+    ///    3,
+    /// ]
+    /// ```
+    OnePerLine,
+}
+
+/// Returns `true` if the array can be "fill-printed" instead of breaking each element on
+/// a different line.
+///
+/// An array can be "fill printed" if it only contains literal elements.
+pub(crate) fn can_concisely_print_array_list(list: &JsonArrayElementList) -> bool {
+    if list.is_empty() {
+        return false;
+    }
+
+    list.iter().all(|node| {
+        matches!(
+            node,
+            Ok(AnyJsonValue::JsonStringValue(_)
+                | AnyJsonValue::JsonBooleanValue(_)
+                | AnyJsonValue::JsonNullValue(_)
+                | AnyJsonValue::JsonNumberValue(_))
+        )
+    })
 }
--- crates/rome_json_formatter/src/json/value/array_value.rs
+++ crates/rome_json_formatter/src/json/value/array_value.rs
@@ -1,10 +1,25 @@
 use crate::prelude::*;
+use rome_formatter::write;
 use rome_json_syntax::JsonArrayValue;
-use rome_rowan::AstNode;
+use rome_json_syntax::JsonArrayValueFields;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatJsonArrayValue;
 impl FormatNodeRule<JsonArrayValue> for FormatJsonArrayValue {
     fn fmt_fields(&self, node: &JsonArrayValue, f: &mut JsonFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let JsonArrayValueFields {
+            l_brack_token,
+            elements,
+            r_brack_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                l_brack_token.format(),
+                group(&soft_block_indent(&elements.format())),
+                r_brack_token.format()
+            ]
+        )
     }
 }
