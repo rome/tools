37acac1d9714a0fe33e275ab38b0d65af03da291
unvalley <kirohi.code@gmail.com>
2023-02-12 11:38:24 +0900

    feat(rome_js_analyze): noSvgWithoutTitle
    feat: check title tag
    
    test: add test cases
    
    wip
    
    feat: update impl to show diagnostics correctly
    
    refactor: logic for detecting a valid title
    
    test: update snapshot
    
    fix: check attribute usage
    
    fix: doc test, comment, diagnostics
    
    chore: update description and codegen
    
    test: simplify texts
    
    chore: restore waste change
    
    refactor
    
    refactor: naming

--- crates/rome_service/src/configuration/parse/json/rules.rs
+++ crates/rome_service/src/configuration/parse/json/rules.rs
@@ -737,6 +737,7 @@ impl VisitNode<JsonLanguage> for Nursery {
                 "noSelfCompare",
                 "noSetterReturn",
                 "noStringCaseMismatch",
+                "noSvgWithoutTitle",
                 "noSwitchDeclarations",
                 "noUnreachableSuper",
                 "noUnsafeFinally",
@@ -1384,6 +1385,24 @@ impl VisitNode<JsonLanguage> for Nursery {
                     let mut configuration = RuleConfiguration::default();
                     self.map_to_object(&value, name_text, &mut configuration, diagnostics)?;
                     self.no_string_case_mismatch = Some(configuration);
+                }
+                _ => {
+                    diagnostics.push(DeserializationDiagnostic::new_incorrect_type(
+                        "object or string",
+                        value.range(),
+                    ));
+                }
+            },
+            "noSvgWithoutTitle" => match value {
+                AnyJsonValue::JsonStringValue(_) => {
+                    let mut configuration = RuleConfiguration::default();
+                    self.map_to_known_string(&value, name_text, &mut configuration, diagnostics)?;
+                    self.no_svg_without_title = Some(configuration);
+                }
+                AnyJsonValue::JsonObjectValue(_) => {
+                    let mut configuration = RuleConfiguration::default();
+                    self.map_to_object(&value, name_text, &mut configuration, diagnostics)?;
+                    self.no_svg_without_title = Some(configuration);
                 }
                 _ => {
                     diagnostics.push(DeserializationDiagnostic::new_incorrect_type(
