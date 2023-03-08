b472367e68db1ca839fe7e574d29da5a75de387f
Victorien ELVINGER <victorien@elvinger.fr>
2023-03-08 15:44:16 +0100

    docs(rome_js_analyze): improve CONTRIBUTING and code gen (#4278)

--- xtask/codegen/src/generate_new_lintrule.rs
+++ xtask/codegen/src/generate_new_lintrule.rs
@@ -26,7 +26,16 @@ use rome_js_semantic::{{Reference, ReferencesExtensions}};
 use rome_js_syntax::JsIdentifierBinding;
 
 declare_rule! {{
-    /// Put your description here
+    /// Succinct description of the rule.
+    ///
+    /// Put context and details about the rule.
+    /// As a starting point, you can take the description of the corresponding _ESLint_ rule (if any).
+    ///
+    /// Try to stay consistent with the descriptions of implemented rules.
+    ///
+    /// Add a link to the corresponding ESLint rule (if any):
+    ///
+    /// Source: https://eslint.org/docs/latest/rules/<rule-name>
     ///
     /// ## Examples
     ///
@@ -56,7 +65,7 @@ impl Rule for {rule_name_upper_camel} {{
     type Signals = Vec<Self::State>;
     type Options = ();
 
-    fn run(ctx: &RuleContext<Self>) -> Vec<Self::State> {{
+    fn run(ctx: &RuleContext<Self>) -> Self::Signals {{
         let binding = ctx.query();
         let model = ctx.model();
 
@@ -69,11 +78,11 @@ impl Rule for {rule_name_upper_camel} {{
                 rule_category!(),
                 reference.syntax().text_trimmed_range(),
                 markup! {{
-                    "Variable is read here"
+                    "Variable is read here."
                 }},
             )
             .note(markup! {{
-                "This note will give you more information"
+                "This note will give you more information."
             }}),
         )
     }}
