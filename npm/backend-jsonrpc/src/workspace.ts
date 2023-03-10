cf04739a498933614ed00c4eee6252fa91ef15dd
Emanuele Stoppa <my.burning@gmail.com>
2023-03-10 14:27:05 +0000

    merge

--- npm/backend-jsonrpc/src/workspace.ts
+++ npm/backend-jsonrpc/src/workspace.ts
@@ -88,7 +88,7 @@ export interface JavascriptConfiguration {
 	/**
 	* A list of global bindings that should be ignored by the analyzers
 
-If defined here, they should not emit diagnostics.
+If defined here, they should not emit diagnostics. 
 	 */
 	globals?: string[];
 	organize_imports?: JavascriptOrganizeImports;
@@ -117,7 +117,7 @@ export type PlainIndentStyle = "tab" | "space";
 /**
 	* Validated value for the `line_width` formatter options
 
-The allowed range of values is 1..=320
+The allowed range of values is 1..=320 
 	 */
 export type LineWidth = number;
 export interface JavascriptFormatter {
@@ -1017,7 +1017,7 @@ export type DiagnosticTags = DiagnosticTag[];
 /**
 	* Serializable representation of a [Diagnostic](super::Diagnostic) advice
 
-See the [Visitor] trait for additional documentation on all the supported advice types.
+See the [Visitor] trait for additional documentation on all the supported advice types. 
 	 */
 export type Advice =
 	| { Log: [LogCategory, MarkupBuf] }
@@ -1105,7 +1105,7 @@ export interface CodeAction {
 /**
 	* The category of a code action, this type maps directly to the [CodeActionKind] type in the Language Server Protocol specification
 
-[CodeActionKind]: https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#codeActionKind
+[CodeActionKind]: https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#codeActionKind 
 	 */
 export type ActionCategory =
 	| "QuickFix"
