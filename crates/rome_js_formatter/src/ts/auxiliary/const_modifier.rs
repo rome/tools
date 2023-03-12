275c500203853676edf4142534e7330e0399d976
nissy-dev <nd.12021218@gmail.com>
2023-03-12 21:03:49 +0900

    fix: add new line

--- crates/rome_js_formatter/src/ts/auxiliary/const_modifier.rs
+++ crates/rome_js_formatter/src/ts/auxiliary/const_modifier.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 use rome_js_syntax::TsConstModifier;
 use rome_rowan::AstNode;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatTsConstModifier;
+
 impl FormatNodeRule<TsConstModifier> for FormatTsConstModifier {
     fn fmt_fields(&self, node: &TsConstModifier, f: &mut JsFormatter) -> FormatResult<()> {
         format_verbatim_node(node.syntax()).fmt(f)
--- crates/rome_js_formatter/src/ts/auxiliary/in_modifier.rs
+++ crates/rome_js_formatter/src/ts/auxiliary/in_modifier.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 use rome_js_syntax::TsInModifier;
 use rome_rowan::AstNode;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatTsInModifier;
+
 impl FormatNodeRule<TsInModifier> for FormatTsInModifier {
     fn fmt_fields(&self, node: &TsInModifier, f: &mut JsFormatter) -> FormatResult<()> {
         format_verbatim_node(node.syntax()).fmt(f)
