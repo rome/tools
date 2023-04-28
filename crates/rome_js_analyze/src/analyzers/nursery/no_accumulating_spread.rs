d511e02dd82383276705301a4cdd58fd9f498f2a
Tyler Cosgrove <Vivalldi@users.noreply.github.com>
2023-04-28 17:46:01 -0400

    all my cases pass as expected

--- crates/rome_js_analyze/src/analyzers/nursery/no_accumulating_spread.rs
+++ crates/rome_js_analyze/src/analyzers/nursery/no_accumulating_spread.rs
@@ -1,7 +1,9 @@
 use rome_analyze::{context::RuleContext, declare_rule, Ast, Rule, RuleDiagnostic};
 use rome_console::markup;
-use rome_js_syntax::{JsCallExpression, JsSpread};
-use rome_rowan::AstNode;
+use rome_js_syntax::{
+    AnyJsBindingPattern, AnyJsFormalParameter, AnyJsParameter, JsCallExpression, JsSpread,
+};
+use rome_rowan::{AstNode, AstSeparatedList};
 
 declare_rule! {
     /// Disallow the use of spread (`...`) syntax on accumulators.
@@ -77,11 +79,11 @@ impl Rule for NoAccumulatingSpread {
 }
 
 fn is_known_accumulator(node: &JsSpread) -> Option<bool> {
-    println!("----- is_known_accumulator -----");
-    let call_expression = node.syntax().ancestors().find_map(JsCallExpression::cast)?;
-    println!("call_expression: {:#?}", call_expression);
+    let node_name = node.argument().ok()?.text();
 
-    let name = call_expression
+    let call_expression = node.syntax().ancestors().find_map(JsCallExpression::cast)?;
+
+    let expression_name = call_expression
         .callee()
         .ok()?
         .as_js_static_member_expression()?
@@ -90,15 +92,53 @@ fn is_known_accumulator(node: &JsSpread) -> Option<bool> {
         .as_js_name()?
         .value_token()
         .ok()?;
-    let name = name.text_trimmed();
-    println!("name: {:#?}", name);
-    if matches!(name, "reduce" | "reduceRight") {
-        // Check if node is the same variable as the first argument to the
-        // function declaration.
-        println!("node: {:#?}", node);
-        println!("node.text: {:#?}", node.text());
-        Some(true)
+    let expression_name = expression_name.text_trimmed();
+    if matches!(expression_name, "reduce" | "reduceRight") {
+        // Get the callback function of the reduce call
+        let first_arg = call_expression
+            .arguments()
+            .ok()?
+            .args()
+            .iter()
+            .next()?
+            .ok()?;
+
+        // check if first_arg is a JsFunction
+        let callback = first_arg
+            .as_any_js_expression()?
+            .as_js_arrow_function_expression()?;
+
+        let param = callback
+            .parameters()
+            .ok()?
+            .as_js_parameters()?
+            .clone()
+            .items()
+            .iter()
+            .next()?
+            .ok()?;
+
+        if let Some(binding) = binding_of(&param) {
+            if let Some(binding) = binding.as_any_js_binding() {
+                // TODO: figure out how to check if the spread operator (node) is part of the param binding
+                if binding.text() == node_name {
+                    return Some(true);
+                }
+            }
+        }
+        None
     } else {
         None
+    }
+}
+
+fn binding_of(param: &AnyJsParameter) -> Option<AnyJsBindingPattern> {
+    match param {
+        AnyJsParameter::AnyJsFormalParameter(formal_param) => match &formal_param {
+            AnyJsFormalParameter::JsBogusParameter(_) => None,
+            AnyJsFormalParameter::JsFormalParameter(param) => param.binding().ok(),
+        },
+        AnyJsParameter::JsRestParameter(param) => param.binding().ok(),
+        AnyJsParameter::TsThisParameter(_) => None,
     }
 }
