399450cd5145e1ba4dc5ad250655df0550fce1b1
nissy-dev <nd.12021218@gmail.com>
2023-05-04 12:56:12 +0900

    refactor: update tests

--- crates/rome_js_analyze/tests/specs/a11y/useAltText/img.jsx
+++ crates/rome_js_analyze/tests/specs/a11y/useAltText/img.jsx
@@ -1,24 +1,57 @@
 // invalid
-
 <>
-	<img src="foo" />
-	<img src="foo" role="presentation" />
-	<img src="foo" role="none" />
-	<img alt />
-	<img alt={null} />
-	<img alt={undefined} />
-</>
+  <img />
+  <img alt />
+  <img alt={undefined} />
+  <img src="xyz" />
+  <img role />
+  <img {...this.props} />
+  <img alt={false || false} />
+  <img alt={undefined} role="presentation" />
+  <img alt role="presentation" />
+  <img role="presentation" />
+  <img role="none" />
+  <img aria-label={undefined} />
+  <img aria-labelledby={undefined} />
+  <img aria-label="" />
+  <img aria-labelledby="" />
+</>;
 
 // valid
 
 <>
-	<img alt={undefined} {...spread} />
-	<img src="foo" {...spread} />
-	<img src="foo" alt="Foo eating a sandwich." />
-	<img src="foo" alt={"Foo eating a sandwich."} />
-	<img src="foo" alt={altText} />
-	<img src="foo" aria-label {...spread} />
-    <img src="foo" alt={<><span class="token punctuation">${</span><span class="token variable">person</span><span class="token punctuation">}</span><span class="token string"> smiling</span><span class="token string"></>} />
-    <img src="foo" alt="" />
-
+  <img alt="foo" />
+  <img alt={"foo"} />
+  <img alt={alt} />
+  <img ALT="foo" />
+  <img ALT={`This is the ${alt} text`} />
+  <img ALt="foo" />
+  <img alt="foo" salt={undefined} />
+  <img {...this.props} alt="foo" />
+  <a />
+  <div />
+  <img alt={function(e) {} } />
+  <div alt={function(e) {} } />
+  <img alt={() => void 0} />
+  <Img />
+  <Component>test</Compoennt>
+  <img alt={alt || "Alt text" } />
+  <img alt={photo.caption} />;
+  <img alt={bar()} />;
+  <img alt={foo.bar || ""} />
+  <img alt={bar() || ""} />
+  <img alt={foo.bar() || ""} />
+  <img alt="" />
+  <img alt={`${undefined}`} />
+  <img alt=" " />
+  <img alt="" role="presentation" />
+  <img alt="" role="none" />
+  <img alt="" role={`presentation`} />
+  <img alt="" role={"presentation"} />
+  <img alt="this is lit..." role="presentation" />
+  <img alt={error ? "not working": "working"} />
+  <img alt={undefined ? "working": "not working"} />
+  <img alt={plugin.name + " Logo"} />
+  <img aria-label="foo" />
+  <img aria-labelledby="id1" />
 </>;
--- crates/rome_js_analyze/tests/specs/a11y/useAltText/input.jsx
+++ crates/rome_js_analyze/tests/specs/a11y/useAltText/input.jsx
@@ -1,15 +1,25 @@
 // invalid
 <>
-	<input type="image" />
-	<input type="image" alt />
-	<input type="image" alt={undefined} />
-	<input type="image" alt={null} />
+  <input type="image" />
+  <input type="image" alt />
+  <input type="image" alt={undefined} />
+  <input type="image">Foo</input>
+  <input type="image" {...this.props} />
+  <input type="image" aria-label="" />
+  <input type="image" aria-label={undefined} />
+  <input type="image" aria-labelledby="" />
+  <input type="image" aria-labelledby={undefined} />
 </>;
 
 // valid
 
 <>
-	<input type="image" alt="This is descriptive!" />
-	<input type="image" aria-label="foo" />
-	<input type="image" aria-labelledby="id1" />
+  <input />
+  <input type="foo" />
+  <input type="image" aria-label="foo" />
+  <input type="image" aria-labelledby="id1" />
+  <input type="image" alt="" />
+  <input type="image" alt="This is descriptive!" />
+  <input type="image" alt={altText} />
+  <Input type="image" />
 </>;
