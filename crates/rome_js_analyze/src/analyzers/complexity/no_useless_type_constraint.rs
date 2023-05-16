52e08c6f321bfc9d2620865d3c90dab5248dd187
Niklas Mollenhauer <nikeee@outlook.com>
2023-05-16 12:29:13 +0200

    chore: Apply suggestions from code review

--- crates/rome_js_analyze/src/analyzers/complexity/no_useless_type_constraint.rs
+++ crates/rome_js_analyze/src/analyzers/complexity/no_useless_type_constraint.rs
@@ -3,8 +3,8 @@ use rome_analyze::{declare_rule, ActionCategory, Ast, Rule, RuleDiagnostic};
 use rome_console::markup;
 
 use rome_diagnostics::Applicability;
-use rome_js_syntax::TsTypeConstraintClause;
-use rome_rowan::{AstNode, BatchMutationExt};
+use rome_js_syntax::{TsAnyType, TsTypeConstraintClause, TsUnknownType};
+use rome_rowan::{declare_node_union, AstNode, BatchMutationExt, TextRange};
 
 use crate::JsRuleAction;
 
@@ -72,9 +72,22 @@ declare_rule! {
     }
 }
 
+declare_node_union! {
+    pub(crate) AnyInvalidType = TsAnyType | TsUnknownType
+}
+
+impl AnyInvalidType {
+    fn range(&self) -> TextRange {
+        match self {
+            AnyInvalidType::TsAnyType(node) => node.range(),
+            AnyInvalidType::TsUnknownType(node) => node.range(),
+        }
+    }
+}
+
 impl Rule for NoUselessTypeConstraint {
     type Query = Ast<TsTypeConstraintClause>;
-    type State = TsTypeConstraintClause;
+    type State = AnyInvalidType;
     type Signals = Option<Self::State>;
     type Options = ();
 
@@ -82,20 +95,20 @@ impl Rule for NoUselessTypeConstraint {
         let node = ctx.query();
         let ty = node.ty().ok()?;
 
-        if ty.as_ts_any_type().is_some() || ty.as_ts_unknown_type().is_some() {
-            Some(node.clone())
+        if let Some(any_type_node) = ty.as_ts_any_type() {
+            Some(any_type_node.clone().into())
+        } else if let Some(unknown_type) = ty.as_ts_unknown_type() {
+            Some(unknown_type.clone().into())
         } else {
             None
         }
     }
 
     fn diagnostic(_ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
-        state.ty().ok()?;
-
         Some(
             RuleDiagnostic::new(
                 rule_category!(),
-                state.syntax().text_trimmed_range(),
+                state.range(),
                 markup! {
                     "Useless type constraint."
                 },
