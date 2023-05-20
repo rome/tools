9574ee059af3a83811dd17984e789a645e6b4cce
Niklas Mollenhauer <nikeee@outlook.com>
2023-05-20 21:28:13 +0200

    chore: Move rule to analysis nursery

--- crates/rome_js_analyze/src/analyzers/nursery.rs
+++ crates/rome_js_analyze/src/analyzers/nursery.rs
@@ -6,10 +6,11 @@ mod no_confusing_arrow;
 mod no_duplicate_jsx_props;
 mod no_for_each;
 mod no_self_assign;
+mod no_static_only_class;
 mod use_grouped_type_import;
 mod use_heading_content;
 mod use_is_nan;
 mod use_literal_enum_members;
 mod use_literal_keys;
 mod use_simple_number_keys;
-declare_group! { pub (crate) Nursery { name : "nursery" , rules : [self :: no_banned_types :: NoBannedTypes , self :: no_confusing_arrow :: NoConfusingArrow , self :: no_duplicate_jsx_props :: NoDuplicateJsxProps , self :: no_for_each :: NoForEach , self :: no_self_assign :: NoSelfAssign , self :: use_grouped_type_import :: UseGroupedTypeImport , self :: use_heading_content :: UseHeadingContent , self :: use_is_nan :: UseIsNan , self :: use_literal_enum_members :: UseLiteralEnumMembers , self :: use_literal_keys :: UseLiteralKeys , self :: use_simple_number_keys :: UseSimpleNumberKeys ,] } }
+declare_group! { pub (crate) Nursery { name : "nursery" , rules : [self :: no_banned_types :: NoBannedTypes , self :: no_confusing_arrow :: NoConfusingArrow , self :: no_duplicate_jsx_props :: NoDuplicateJsxProps , self :: no_for_each :: NoForEach , self :: no_self_assign :: NoSelfAssign , self :: no_static_only_class :: NoStaticOnlyClass , self :: use_grouped_type_import :: UseGroupedTypeImport , self :: use_heading_content :: UseHeadingContent , self :: use_is_nan :: UseIsNan , self :: use_literal_enum_members :: UseLiteralEnumMembers , self :: use_literal_keys :: UseLiteralKeys , self :: use_simple_number_keys :: UseSimpleNumberKeys ,] } }
