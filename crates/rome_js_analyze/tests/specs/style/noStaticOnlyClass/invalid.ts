bc038e0b7ba289c11f283a2104a4f1f87062ed30
Niklas Mollenhauer <nikeee@outlook.com>
2023-05-20 04:40:02 +0200

    chore: Add basic tests

---
+++ crates/rome_js_analyze/tests/specs/style/noStaticOnlyClass/invalid.ts
@@ -0,0 +1,29 @@
+class X0 {
+	static foo = false;
+	static bar() { }
+}
+
+class X1 {
+	static #foo = false;
+	static bar() {}
+}
+
+const X2 = class {
+	static foo = false;
+};
+
+const X3 = class A {
+	static foo = false;
+};
+
+export default class {
+	static foo = false;
+}
+
+class StaticConstants0 {
+  static readonly version = 42;
+
+  static isProduction() {
+    return Math.random() > 0.5;
+  }
+}
--- crates/rome_js_analyze/tests/specs/style/noStaticOnlyClass/valid.js
+++
@@ -1,3 +0,0 @@
-/* should not generate diagnostics */
-
- var a = 1;
