f5a4c1f17c58957817efeddd7636417b9e5484f6
Emanuele Stoppa <my.burning@gmail.com>
2023-05-30 13:48:33 +0100

    chore: update changelog

--- website/src/pages/schemas/12.1.3/schema.json.js
+++ website/src/pages/schemas/12.1.3/schema.json.js
@@ -1,16 +1,18 @@
 // Run `ROME_VERSION=<version number> cargo codegen-website
 // to generate a new schema
-import {readFileSync} from "fs";
-import {join, resolve} from "path"
+import { readFileSync } from "fs";
+import { join, resolve } from "path";
 
 export function get() {
-	const schemaPath = resolve(join("..", "npm", "rome", "configuration_schema.json"));
-	const schema = readFileSync(schemaPath, "utf8")
+	const schemaPath = resolve(
+		join("..", "npm", "rome", "configuration_schema.json"),
+	);
+	const schema = readFileSync(schemaPath, "utf8");
 
 	return new Response(schema, {
 		status: 200,
 		headers: {
-			"content-type": "application/json"
-		}
-	})
+			"content-type": "application/json",
+		},
+	});
 }
