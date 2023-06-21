5c8e197c4d877d4c1d60e1cf5fd8bc4ea85194e9
Emanuele Stoppa <my.burning@gmail.com>
2023-06-21 10:19:31 +0100

    code suggestions

--- crates/rome_json_analyze/src/analyzers/nursery/no_duplicate_keys.rs
+++ crates/rome_json_analyze/src/analyzers/nursery/no_duplicate_keys.rs
@@ -1,11 +1,11 @@
 use rome_analyze::{context::RuleContext, declare_rule, Ast, Rule, RuleDiagnostic};
 use rome_console::markup;
-use rome_json_syntax::{JsonObjectValue, TextRange};
+use rome_json_syntax::{JsonMemberName, JsonObjectValue, TextRange};
 use rome_rowan::{AstNode, AstSeparatedList};
 use std::collections::HashMap;
 
 declare_rule! {
-    /// Disallow two keys with the same name inside an object.
+    /// Disallow two keys with the same name inside a JSON object.
     ///
     /// ## Examples
     ///
@@ -16,6 +16,14 @@ declare_rule! {
     ///   "title": "New title",
     ///   "title": "Second title"
     /// }
+    ///
+    /// ### Valid
+    ///
+    /// ```json
+    /// {
+    ///   "title": "New title",
+    ///   "secondTitle": "Second title"
+    /// }
     /// ```
     pub(crate) NoDuplicateKeys {
         version: "next",
@@ -24,53 +32,77 @@ declare_rule! {
     }
 }
 
+pub(crate) struct DuplicatedKeys {
+    /// The fist key, which should be the correct one
+    original_key: JsonMemberName,
+    /// The ranges where the duplicated keys are found
+    duplicated_keys: Vec<TextRange>,
+}
+
 impl Rule for NoDuplicateKeys {
     type Query = Ast<JsonObjectValue>;
-    type State = (String, TextRange);
-    type Signals = Vec<Self::State>;
+    type State = DuplicatedKeys;
+    type Signals = Option<Self::State>;
     type Options = ();
 
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
         let query = ctx.query();
         let mut names = HashMap::<String, Vec<TextRange>>::new();
-        for member in query.json_member_list().iter().flatten() {
-            if let Ok(name) = member.name() {
-                if let Ok(text) = name.inner_string_text() {
-                    if let Some(ranges) = names.get_mut(text.text()) {
-                        ranges.push(name.range());
-                    } else {
-                        names.insert(text.text().to_string(), vec![]);
-                    }
-                }
+        let mut original_key = None;
+        for (index, member) in query.json_member_list().iter().flatten().enumerate() {
+            let name = member.name().ok()?;
+            if index == 0 {
+                original_key = Some(name.clone());
+            }
+            let text = name.inner_string_text().ok()?;
+            if let Some(ranges) = names.get_mut(text.text()) {
+                ranges.push(name.range());
+            } else {
+                names.insert(text.text().to_string(), vec![]);
             }
         }
 
-        let names: Vec<_> = names
-            .into_iter()
-            .filter(|(_, ranges)| !ranges.is_empty())
-            .flat_map(|(string, ranges)| {
-                ranges.into_iter().map(move |range| (string.clone(), range))
-            })
+        let duplicated_keys: Vec<_> = names
+            .into_values()
+            .filter(|ranges| !ranges.is_empty())
+            .flat_map(|ranges| ranges.into_iter().map(move |range| range))
             .collect();
 
-        names
+        if !duplicated_keys.is_empty() {
+            let Some(original_key) = original_key else { return None};
+
+            return Some(DuplicatedKeys {
+                original_key,
+                duplicated_keys,
+            });
+        }
+        None
     }
 
-    fn diagnostic(_ctx: &RuleContext<Self>, (name, range): &Self::State) -> Option<RuleDiagnostic> {
-        Some(
-            RuleDiagnostic::new(
-                rule_category!(),
+    fn diagnostic(_ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
+        let DuplicatedKeys {
+            duplicated_keys,
+            original_key,
+        } = state;
+        let mut diagnostic = RuleDiagnostic::new(
+            rule_category!(),
+            original_key.range(),
+            markup! {
+                "The key "<Emphasis>{{original_key.inner_string_text().ok()?.text()}}</Emphasis>" was already declared."
+            },
+        );
+        for range in duplicated_keys {
+            diagnostic = diagnostic.detail(
                 range,
                 markup! {
-                    "The key "<Emphasis>{{name}}</Emphasis>" was already declared"
+                    "This where a duplicated key was declared again."
                 },
-            )
-            .detail(
-                range,
-                markup! {
-                    "Remove or rename the key"
-                },
-            ),
-        )
+            );
+        }
+        Some(diagnostic.note(
+            markup! {
+                "If a key is defined multiple times, only the last definition takes effect. Previous definitions are ignored."
+            },
+        ))
     }
 }
