a5243d446c264202dda6a85bcbd2e0e847153c9a
Micha Reiser <micha@reiser.io>
2023-06-21 23:58:24 +0200

    refactor(rome_formatter): Use custom trait for unique `LabelId`s

--- crates/rome_js_formatter/src/lib.rs
+++ crates/rome_js_formatter/src/lib.rs
@@ -179,6 +179,7 @@ mod parentheses;
 pub(crate) mod separated;
 mod syntax_rewriter;
 
+use rome_formatter::format_element::tag::LabelDefinition;
 use rome_formatter::prelude::*;
 use rome_formatter::{
     comments::Comments, write, CstFormatContext, Format, FormatLanguage, FormatToken,
@@ -196,143 +197,7 @@ use crate::context::{JsFormatContext, JsFormatOptions};
 use crate::cst::FormatJsSyntaxNode;
 use crate::syntax_rewriter::transform;
 
-/// Used to get an object that knows how to format this object.
-pub(crate) trait AsFormat<Context> {
-    type Format<'a>: rome_formatter::Format<Context>
-    where
-        Self: 'a;
-
-    /// Returns an object that is able to format this object.
-    fn format(&self) -> Self::Format<'_>;
-}
-
-/// Implement [AsFormat] for references to types that implement [AsFormat].
-impl<T, C> AsFormat<C> for &T
-where
-    T: AsFormat<C>,
-{
-    type Format<'a> = T::Format<'a> where Self: 'a;
-
-    fn format(&self) -> Self::Format<'_> {
-        AsFormat::format(&**self)
-    }
-}
-
-/// Implement [AsFormat] for [SyntaxResult] where `T` implements [AsFormat].
-///
-/// Useful to format mandatory AST fields without having to unwrap the value first.
-impl<T, C> AsFormat<C> for rome_rowan::SyntaxResult<T>
-where
-    T: AsFormat<C>,
-{
-    type Format<'a> = rome_rowan::SyntaxResult<T::Format<'a>> where Self: 'a;
-
-    fn format(&self) -> Self::Format<'_> {
-        match self {
-            Ok(value) => Ok(value.format()),
-            Err(err) => Err(*err),
-        }
-    }
-}
-
-/// Implement [AsFormat] for [Option] when `T` implements [AsFormat]
-///
-/// Allows to call format on optional AST fields without having to unwrap the field first.
-impl<T, C> AsFormat<C> for Option<T>
-where
-    T: AsFormat<C>,
-{
-    type Format<'a> = Option<T::Format<'a>> where Self: 'a;
-
-    fn format(&self) -> Self::Format<'_> {
-        self.as_ref().map(|value| value.format())
-    }
-}
-
-/// Used to convert this object into an object that can be formatted.
-///
-/// The difference to [AsFormat] is that this trait takes ownership of `self`.
-pub(crate) trait IntoFormat<Context> {
-    type Format: rome_formatter::Format<Context>;
-
-    fn into_format(self) -> Self::Format;
-}
-
-impl<T, Context> IntoFormat<Context> for rome_rowan::SyntaxResult<T>
-where
-    T: IntoFormat<Context>,
-{
-    type Format = rome_rowan::SyntaxResult<T::Format>;
-
-    fn into_format(self) -> Self::Format {
-        self.map(IntoFormat::into_format)
-    }
-}
-
-/// Implement [IntoFormat] for [Option] when `T` implements [IntoFormat]
-///
-/// Allows to call format on optional AST fields without having to unwrap the field first.
-impl<T, Context> IntoFormat<Context> for Option<T>
-where
-    T: IntoFormat<Context>,
-{
-    type Format = Option<T::Format>;
-
-    fn into_format(self) -> Self::Format {
-        self.map(IntoFormat::into_format)
-    }
-}
-
-/// Formatting specific [Iterator] extensions
-pub(crate) trait FormattedIterExt {
-    /// Converts every item to an object that knows how to format it.
-    fn formatted<Context>(self) -> FormattedIter<Self, Self::Item, Context>
-    where
-        Self: Iterator + Sized,
-        Self::Item: IntoFormat<Context>,
-    {
-        FormattedIter {
-            inner: self,
-            options: std::marker::PhantomData,
-        }
-    }
-}
-
-impl<I> FormattedIterExt for I where I: std::iter::Iterator {}
-
-pub(crate) struct FormattedIter<Iter, Item, Context>
-where
-    Iter: Iterator<Item = Item>,
-{
-    inner: Iter,
-    options: std::marker::PhantomData<Context>,
-}
-
-impl<Iter, Item, Context> std::iter::Iterator for FormattedIter<Iter, Item, Context>
-where
-    Iter: Iterator<Item = Item>,
-    Item: IntoFormat<Context>,
-{
-    type Item = Item::Format;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        Some(self.inner.next()?.into_format())
-    }
-}
-
-impl<Iter, Item, Context> std::iter::FusedIterator for FormattedIter<Iter, Item, Context>
-where
-    Iter: std::iter::FusedIterator<Item = Item>,
-    Item: IntoFormat<Context>,
-{
-}
-
-impl<Iter, Item, Context> std::iter::ExactSizeIterator for FormattedIter<Iter, Item, Context>
-where
-    Iter: Iterator<Item = Item> + std::iter::ExactSizeIterator,
-    Item: IntoFormat<Context>,
-{
-}
+include!("../../rome_formatter/shared_traits.rs");
 
 pub(crate) type JsFormatter<'buf> = Formatter<'buf, JsFormatContext>;
 
@@ -535,6 +400,23 @@ pub fn format_node(
 /// It returns a [Formatted] result
 pub fn format_sub_tree(options: JsFormatOptions, root: &JsSyntaxNode) -> FormatResult<Printed> {
     rome_formatter::format_sub_tree(root, JsFormatLanguage::new(options))
+}
+
+#[derive(Copy, Clone, Debug)]
+pub(crate) enum JsLabels {
+    MemberChain,
+}
+
+impl LabelDefinition for JsLabels {
+    fn value(&self) -> u64 {
+        *self as u64
+    }
+
+    fn debug_name(&self) -> &'static str {
+        match self {
+            JsLabels::MemberChain => "MemberChain",
+        }
+    }
 }
 
 #[cfg(test)]
