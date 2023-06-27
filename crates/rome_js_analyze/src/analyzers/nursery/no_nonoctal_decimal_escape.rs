f135d76944d53ecea7b436ac3e0feafa274fc0aa
unvalley <kirohi.code@gmail.com>
2023-06-28 00:56:55 +0900

    docs: update lint rule description

--- crates/rome_js_analyze/src/analyzers/nursery/no_nonoctal_decimal_escape.rs
+++ crates/rome_js_analyze/src/analyzers/nursery/no_nonoctal_decimal_escape.rs
@@ -17,6 +17,18 @@ lazy_static! {
 declare_rule! {
     /// Disallow `\8` and `\9` escape sequences in string literals.
     ///
+    /// Since ECMAScript 2021, the escape sequences \8 and \9 have been defined as non-octal decimal escape sequences.
+    /// However, most JavaScript engines consider them to be "useless" escapes. For example:
+    ///
+    /// ```js
+    /// "\8" === "8"; // true
+    /// "\9" === "9"; // true
+    /// ```
+    ///
+    /// Although this syntax is deprecated, it is still supported for compatibility reasons.
+    /// If the ECMAScript host is not a web browser, this syntax is optional.
+    /// However, web browsers are still required to support it, but only in non-strict mode.
+    /// Regardless of your targeted environment, it is recommended to avoid using these escape sequences in new code.
     ///
     /// Source: https://eslint.org/docs/latest/rules/no-nonoctal-decimal-escape
     ///
