08645a80c6139ab969aa1917f7e01dae3845d236
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2023-06-30 12:30:21 +0300

    fix(rome_js_analyze): Add constructor support (#4605)

---
+++ crates/rome_js_analyze/tests/specs/suspicious/noDuplicateParameters/invalid.ts
@@ -0,0 +1,28 @@
+function b(a, b, b) {}
+function c(a, a, a) {}
+const d = (a, b, a) => {};
+function e(a, b, a, b) {}
+var f = function (a, b, b) {};
+class G {
+	ggg(a, a, a) {}
+}
+let objectMethods = { method(a, b, c, c) {} };
+var h = function (a, b, a) {};
+export default function (a, b, a, a) {}
+function f({ test: res = 3 }, res) {}
+export function f2(a, b, c = (a, b, b) => {}) {}
+class A {
+	constructor(a, a) {}
+}
+class A {
+	constructor(private a, a) {}
+}
+class A {
+	constructor(a, readonly a) {}
+}
+class A {
+	constructor(private a, private a) {}
+}
+class A {
+	constructor(readonly a, private a) {}
+}
