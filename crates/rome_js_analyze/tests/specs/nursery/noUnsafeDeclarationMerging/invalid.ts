8d511f7f79ac69e2e728a3eccd2a57788fe73678
Victorien Elvinger <victorien@elvinger.fr>
2023-07-28 23:05:26 +0200

    feat(rome_js_analyze): noUnsafeDeclarationMerging

---
+++ crates/rome_js_analyze/tests/specs/nursery/noUnsafeDeclarationMerging/invalid.ts
@@ -0,0 +1,17 @@
+interface Foo {}
+class Foo {}
+
+class Foo2 {}
+interface Foo2 {}
+
+function f(){
+    interface Foo3 {}
+    class Foo3 {}
+}
+
+{
+    interface Foo4 {}
+    class Foo4 {}
+}
+
+export {}
